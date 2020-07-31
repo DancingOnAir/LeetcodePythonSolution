f9a218eda8e0b73017a4c5bb5bce338c21e0e362
DancingOnAir <taotong1984@hotmail.com>
2020-07-31 08:48:02 +0800

    feat(Array): optimized enumerate in hash map solution for q0001

--- Array/0001_two_sum.py
+++ Array/0001_two_sum.py
@@ -1,10 +1,19 @@
 class Solution:
     def twoSum(self, nums: list, target: int) -> list:
         count = {}
+        for i, v in enumerate(nums):
+            if v in count:
+                return [count[v], i]
+            count[target - v] = i
+        return []
+
+    def twoSum1(self, nums: list, target: int) -> list:
+        count = {}
         for i in range(len(nums)):
             if nums[i] in count:
                 return [count[nums[i]], i]
             count[target - nums[i]] = i
+        return []
 
 
 def test_two_sum():
