2fe822d9fcc63d24b3632da1bb3d97a7f8539123
DancingOnAir <taotong1984@hotmail.com>
2020-08-02 10:38:33 +0800

    feat(Array): init q0004 & accepted solution for q0004

---
+++ Array/0004_median_of_two_sorted_arrays.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: list, nums2: list) -> float:
+        nums = nums1 + nums2
+        nums = sorted(nums)
+
+        n = len(nums)
+        if n & 1:
+            return nums[n // 2]
+        return (nums[n // 2 - 1] + nums[n // 2]) * 0.5
+
+
+def test_find_median_sorted_arrays():
+    solution = Solution()
+
+    nums11 = [1, 3]
+    nums21 = [2]
+    print(solution.findMedianSortedArrays(nums11, nums21))
+
+    nums12 = [1, 2]
+    nums22 = [3, 4]
+    print(solution.findMedianSortedArrays(nums12, nums22))
+
+
+if __name__ == '__main__':
+    test_find_median_sorted_arrays()
