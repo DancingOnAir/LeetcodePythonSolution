94c8180addb74b07e4b93e71cdf84c83712778c4
DancingOnAir <taotong1984@hotmail.com>
2020-08-06 21:59:18 +0800

    refactor(Array): optimized two pointers solution for q0018

--- Array/0018_four_sum.py
+++ Array/0018_four_sum.py
@@ -3,6 +3,42 @@ from typing import List
 
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        results = []
+        self.find_n_sum(nums, target, 4, [], results)
+
+        return results
+
+    def find_n_sum(self, nums, target, N, result, results):
+        if len(nums) < N or N < 2:
+            return
+
+        if N == 2:
+            left, right = 0, len(nums) - 1
+            while left < right:
+                if nums[left] + nums[right] == target:
+                    results.append(result + [nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+                elif nums[left] + nums[right] < target:
+                    left += 1
+                else:
+                    right -= 1
+        else:
+            for i in range(len(nums) - N + 1):
+                if target < nums[i] * N or target > nums[-1] * N:
+                    break
+                if i == 0 or (i > 0 and nums[i - 1] != nums[i]):
+                    self.find_n_sum(nums[i + 1:], target - nums[i], N - 1, result + [nums[i]], results)
+        return
+
+    def fourSum1(self, nums: List[int], target: int) -> List[List[int]]:
         nums = sorted(nums)
 
         n = len(nums)
