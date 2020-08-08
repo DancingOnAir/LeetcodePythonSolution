b8822cae6c864b456aba63dd00f0e2aff951033e
DancingOnAir <taotong1984@hotmail.com>
2020-08-08 22:40:52 +0800

    feat(Array): accepted more pythonic solution for q0033

--- Array/0033_search_in_rotated_sorted_array.py
+++ Array/0033_search_in_rotated_sorted_array.py
@@ -1,8 +1,26 @@
 from typing import List
+import bisect
 
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+
+        if n == 0:
+            return -1
+
+        b = [0, n]
+        while b[0] != b[1]:
+            mid, left, right = (b[0] + b[1]) // 2, b[0], b[1]
+            b = [mid, right] if nums[mid] > nums[left] else [left, mid]
+        if nums[(left + 1) % n] < nums[left]:
+            left = (left + 1) % n
+        res = (bisect.bisect_left(nums[left: n] + nums[0: left], target) + left) % n
+
+        return [-1, res][nums[res] == target]
+
+
+    def search1(self, nums: List[int], target: int) -> int:
         if not nums:
             return -1
         left, right = 0, len(nums) - 1
@@ -34,7 +52,7 @@ class Solution:
 
         return left
 
-    def search1(self, nums: List[int], target: int) -> int:
+    def search2(self, nums: List[int], target: int) -> int:
         pivot = self.find_pivot(nums)
         n = len(nums)
         left, right = 0, n - 1
