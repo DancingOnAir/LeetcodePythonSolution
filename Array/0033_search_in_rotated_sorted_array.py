ed3abfcf5eecb6d9d6e42c9c1fdc88594df73b09
DancingOnAir <taotong1984@hotmail.com>
2020-08-08 19:04:55 +0800

    feat(Array): accepted binary search solution for q0033

--- Array/0033_search_in_rotated_sorted_array.py
+++ Array/0033_search_in_rotated_sorted_array.py
@@ -2,6 +2,26 @@ from typing import List
 
 
 class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[0] <= nums[mid]:
+                if nums[0] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] < target <= nums[-1]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
+
     def find_pivot(self, nums: List[int]) -> int:
         left, right = 0, len(nums) - 1
 
@@ -14,7 +34,7 @@ class Solution:
 
         return left
 
-    def search(self, nums: List[int], target: int) -> int:
+    def search1(self, nums: List[int], target: int) -> int:
         pivot = self.find_pivot(nums)
         n = len(nums)
         left, right = 0, n - 1
