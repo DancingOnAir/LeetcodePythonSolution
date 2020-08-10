994a047d351149332cf4fc6f950ed4ead323cdc5
DancingOnAir <taotong1984@hotmail.com>
2020-08-10 08:37:39 +0800

    feat(Array): accepted binary search solution for q0035

--- Array/0035_search_insert_position.py
+++ Array/0035_search_insert_position.py
@@ -3,23 +3,23 @@ import bisect
 
 
 class Solution:
-    # def searchInsert(self, nums: List[int], target: int) -> int:
-    #     if not nums:
-    #         return 0
-    #
-    #     left, right = 0, len(nums) - 1
-    #     while left <= right:
-    #         mid = left + (right - left) // 2
-    #         if nums[mid] < target:
-    #             left = mid + 1
-    #         elif nums[mid] > target:
-    #             right = mid - 1
-    #         else:
-    #             return mid
-    #
-    #     return left
-
     def searchInsert(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return 0
+
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                return mid
+
+        return left
+
+    def searchInsert1(self, nums: List[int], target: int) -> int:
         return bisect.bisect_left(nums, target)
 
 
