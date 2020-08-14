a98f7dde17ee8d88fd562bc31ed3620e2a7b4914
DancingOnAir <taotong1984@hotmail.com>
2020-08-14 23:54:45 +0800

    feat(Array): accepted two pointers solution for q0042

--- Array/0042_trapping_rain_water.py
+++ Array/0042_trapping_rain_water.py
@@ -2,8 +2,28 @@ from typing import List
 
 
 class Solution:
-    # brute force but TLE
+    # two pointer
     def trap(self, height: List[int]) -> int:
+        if len(height) < 3:
+            return 0
+
+        left, right = 0, len(height) - 1
+        left_max, right_max = 0, 0
+        res = 0
+        while left < right:
+            if height[left] < height[right]:
+                left_max = max(left_max, height[left])
+                res += left_max - height[left]
+                left += 1
+            else:
+                right_max = max(right_max, height[right])
+                res += right_max - height[right]
+                right -= 1
+
+        return res
+
+    # brute force but TLE
+    def trap1(self, height: List[int]) -> int:
         if len(height) < 3:
             return 0
 
