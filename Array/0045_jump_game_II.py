363f77486c9ca598dd6dc48b9a645e68c7525eb1
DancingOnAir <taotong1984@hotmail.com>
2020-08-16 23:12:40 +0800

    feat(Array): accepted solution for q0045

--- Array/0045_jump_game_II.py
+++ Array/0045_jump_game_II.py
@@ -3,9 +3,19 @@ from typing import List
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return 0
+
+        left, right, res = 0, 0, 0
+        while right < len(nums) - 1:
+            res += 1
+            max_right = max(i + nums[i] for i in range(left, right + 1))
+            left, right = right + 1, max_right
+        return res
+
+    def jump1(self, nums: List[int]) -> int:
         max_index, res, end = nums[0], 0, 0
         for i in range(len(nums) - 1):
-            # if max_index >= i:
             max_index = max(max_index, i + nums[i])
             if i == end:
                 end = max_index
