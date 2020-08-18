a5c0211951e30cf78e181caa7c7def9d4e83f1a9
DancingOnAir <taotong1984@hotmail.com>
2020-08-18 08:29:53 +0800

    feat(Array): init q0053 & accepted solution for q0053

---
+++ Array/0053_maximum_subarray.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        res, sub_array_sum = float('-inf'), 0
+        for i, val in enumerate(nums):
+            sub_array_sum += val
+            res = max(res, sub_array_sum)
+            sub_array_sum = max(sub_array_sum, 0)
+
+        return res
+
+
+def test_max_sub_array():
+    solution = Solution()
+
+    nums1 = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    print(solution.maxSubArray(nums1))
+
+
+if __name__ == '__main__':
+    test_max_sub_array()
