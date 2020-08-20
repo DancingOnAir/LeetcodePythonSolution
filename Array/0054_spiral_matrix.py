b04b222351c601eb72210bcd4fe1f9bdbc80e38c
DancingOnAir <taotong1984@hotmail.com>
2020-08-20 08:10:41 +0800

    feat(Array): accepted solution for q0054

--- Array/0054_spiral_matrix.py
+++ Array/0054_spiral_matrix.py
@@ -3,6 +3,34 @@ from typing import List
 
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        res = []
+        if not matrix:
+            return res
+
+        top, bottom, left, right = 0, len(matrix) - 1, 0, len(matrix[0]) - 1
+        while top <= bottom and left <= right:
+            for i in range(left, right + 1):
+                res.append(matrix[top][i])
+            top += 1
+
+            for i in range(top, bottom + 1):
+                res.append((matrix[i][right]))
+            right -= 1
+
+            if top <= bottom:
+                for i in range(right, left - 1, -1):
+                    res.append(matrix[bottom][i])
+                bottom -= 1
+
+            if left <= right:
+                for i in range(bottom, top - 1, -1):
+                    res.append(matrix[i][left])
+                left += 1
+        return res
+
+
+    # list pop method
+    def spiralOrder1(self, matrix: List[List[int]]) -> List[int]:
         return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
 
 
