d386217b7a22901a54697e41fd1635dcfe3838ea
DancingOnAir <taotong1984@hotmail.com>
2020-08-21 17:30:59 +0800

    feat(Array): accepted solution for q0056

--- Array/0056_merge_intervals.py
+++ Array/0056_merge_intervals.py
@@ -3,18 +3,12 @@ from typing import List
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        n = len(intervals)
-        if n < 2:
-            return intervals
-
-        intervals = sorted(intervals, key=lambda x: (x[0], x[1]))
-
-        res = [intervals[0]]
-        for i in range(1, n):
-            if intervals[i][0] > res[-1][1]:
-                res.append(intervals[i])
+        res = []
+        for i in sorted(intervals, key=lambda x: (x[0], x[1])):
+            if res and i[0] <= res[-1][-1]:
+                res[-1][-1] = max(res[-1][-1], i[-1])
             else:
-                res[-1][1] = max(res[-1][1], intervals[i][1])
+                res += i,
         return res
 
 
