d86b3b4c12552ec0ce3774ecc03d970bb4d3c0e4
DancingOnAir <taotong1984@hotmail.com>
2020-08-23 21:07:13 +0800

    feat(Array): accepted walk the spiral solution for q0059

--- Array/0059_spiral_matrix_II.py
+++ Array/0059_spiral_matrix_II.py
@@ -2,7 +2,20 @@ from typing import List
 
 
 class Solution:
+    # walk the spiral
     def generateMatrix(self, n: int) -> List[List[int]]:
+        res = [[0] * n for _ in range(n)]
+        i, j, di, dj = 0, 0, 0, 1
+        for k in range(n * n):
+            res[i][j] = k + 1
+            if res[(i + di) % n][(j + dj) % n]:
+                di, dj = dj, -di
+            i += di
+            j += dj
+
+        return res
+
+    def generateMatrix1(self, n: int) -> List[List[int]]:
         res = [[0 for i in range(n)] for j in range(n)]
         top, bottom, left, right = 0, n - 1, 0, n - 1
         k = 1
