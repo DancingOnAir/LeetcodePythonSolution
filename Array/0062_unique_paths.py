2feb5f7198d2bbec3ea4638baba9399ab2c3547c
DancingOnAir <taotong1984@hotmail.com>
2020-08-24 08:36:55 +0800

    feat(Array): accepted 1d vector dp solution for q0062

--- Array/0062_unique_paths.py
+++ Array/0062_unique_paths.py
@@ -3,6 +3,14 @@ from typing import List
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1] * m
+        for _ in range(1, n):
+            for j in range(1, m):
+                dp[j] += dp[j - 1]
+
+        return dp[m - 1]
+
+    def uniquePaths1(self, m: int, n: int) -> int:
         dp = [[0] * (m + 1) for _ in range(n + 1)]
         for i in range(1, n + 1):
             for j in range(1, m + 1):
