0d9036baead7533011685235d95c83c918a668d2
DancingOnAir <taotong1984@hotmail.com>
2020-08-24 08:10:23 +0800

    feat(Array): init q0062 & accepted 2d matrix dp solution for q0062

---
+++ Array/0062_unique_paths.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if 1 == i or 1 == j:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+        return dp[n][m]
+
+
+def test_unique_paths():
+    solution = Solution()
+
+    m1, n1 = 3, 2
+    print(solution.uniquePaths(m1, n1))
+
+    m2, n2 = 7, 3
+    print(solution.uniquePaths(m2, n2))
+
+
+if __name__ == '__main__':
+    test_unique_paths()
