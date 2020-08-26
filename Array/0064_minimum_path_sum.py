542c02f78aea76809ca481fc5a4fe2d6dc6d3c9f
DancingOnAir <taotong1984@hotmail.com>
2020-08-26 08:53:31 +0800

    feat(Array): accepted 1d dp solution for q0084

--- Array/0064_minimum_path_sum.py
+++ Array/0064_minimum_path_sum.py
@@ -3,6 +3,16 @@ from typing import List
 
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        dp = [0] * n
+        for i in range(m):
+            dp[0] += grid[i][0]
+            for j in range(1, n):
+                dp[j] = (min(dp[j - 1], dp[j]) or dp[j - 1]) + grid[i][j]
+        return dp[-1]
+
+    # 2d dp
+    def minPathSum1(self, grid: List[List[int]]) -> int:
         if not len(grid) or not len(grid[0]):
             return 0
 
