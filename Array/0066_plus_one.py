fe5d17861d4a3562256870e680947f4e21f42473
DancingOnAir <taotong1984@hotmail.com>
2020-08-27 10:36:24 +0800

    feat(Array): accepted more pythonic solution for q0066

--- Array/0066_plus_one.py
+++ Array/0066_plus_one.py
@@ -3,6 +3,12 @@ from typing import List
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+        num = 0
+        for i, val in enumerate(digits):
+            num += val * pow(10, len(digits) - i - 1)
+        return [int(i) for i in str(num + 1)]
+
+    def plusOne2(self, digits: List[int]) -> List[int]:
         n = len(digits)
         extra = 0
         for i, val in enumerate(digits[::-1]):
