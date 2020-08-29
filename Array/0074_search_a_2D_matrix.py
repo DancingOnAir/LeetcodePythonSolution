40e22d5229c3b4b760f7dae4c7f1bd1bd0f8210e
DancingOnAir <taotong1984@hotmail.com>
2020-08-29 10:16:38 +0800

    feat(Array): init q0074 & accepted solution for q0074

---
+++ Array/0074_search_a_2D_matrix.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        if not m:
+            return 0
+
+        n = len(matrix[0])
+        if not n:
+            return 0
+
+        for i in range(m):
+            if matrix[i][0] <= target <= matrix[i][-1] and target in matrix[i]:
+                return True
+        return False
+
+
+def test_search_matrix():
+    solution = Solution()
+
+    matrix1 = [[1, 3, 5, 7],
+               [10, 11, 16, 20],
+               [23, 30, 34, 50]]
+    target1 = 3
+    print(solution.searchMatrix(matrix1, target1))
+
+    matrix2 = [[1, 3, 5, 7],
+               [10, 11, 16, 20],
+               [23, 30, 34, 50]]
+    target2 = 13
+    print(solution.searchMatrix(matrix2, target2))
+
+
+if __name__ == '__main__':
+    test_search_matrix()
