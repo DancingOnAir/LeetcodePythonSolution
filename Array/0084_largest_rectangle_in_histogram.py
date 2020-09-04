9a0f11f9616392e9f171c3bbe6fe26f4a44fab24
DancingOnAir <taotong1984@hotmail.com>
2020-09-04 11:07:03 +0800

    feat(Array): accepted stack solution for q0084

---
+++ Array/0084_largest_rectangle_in_histogram.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        if not heights:
+            return 0
+
+        res = 0
+        stk = [-1]
+        heights.append(0)
+        for i in range(len(heights)):
+            while heights[stk[-1]] > heights[i]:
+                h = heights[stk.pop()]
+                w = i - stk[-1] - 1
+                res = max(res, w * h)
+            stk.append(i)
+
+        return res
+
+
+def test_largest_rectangle_area():
+    solution = Solution()
+
+    heights1 = [2, 1, 5, 6, 2, 3]
+    print(solution.largestRectangleArea(heights1))
+
+    heights2 = [1, 1]
+    print(solution.largestRectangleArea(heights2))
+
+
+if __name__ == '__main__':
+    test_largest_rectangle_area()
