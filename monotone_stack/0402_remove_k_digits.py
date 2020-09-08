af324c7fae536d44b1f3373c33fc7e32a7195e0d
DancingOnAir <taotong1984@hotmail.com>
2020-09-08 22:09:51 +0800

    feat(monotone_stack): init q0402 & accepted monotone stack solution for q0402

---
+++ monotone_stack/0402_remove_k_digits.py
@@ -0,0 +1,39 @@
+from typing import List
+
+
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        nums = [int(i) for i in num]
+        stk = []
+        for num in nums:
+            while stk and stk[-1] > num and k:
+                stk.pop()
+                k -= 1
+            stk.append(num)
+
+        while k and stk:
+            stk.pop()
+            k -= 1
+
+        res = [str(i) for i in stk]
+        return str(int(''.join(res))) if res else "0"
+
+
+def test_remove_k_digits():
+    solution = Solution()
+
+    num1 = "1432219"
+    k1 = 3
+    print(solution.removeKdigits(num1, k1))
+
+    num2 = "10200"
+    k2 = 1
+    print(solution.removeKdigits(num2, k2))
+
+    num3 = "10"
+    k3 = 2
+    print(solution.removeKdigits(num3, k3))
+
+
+if __name__ == '__main__':
+    test_remove_k_digits()
