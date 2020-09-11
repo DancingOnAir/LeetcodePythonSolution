c7980eecc71e4487f763994b2d86baa1fd986101
DancingOnAir <taotong1984@hotmail.com>
2020-09-12 00:10:15 +0800

    feat(monotone_stack): accepted solution for q0907

--- monotone_stack/0907_sum_of_subarray_minimums.py
+++ monotone_stack/0907_sum_of_subarray_minimums.py
@@ -3,6 +3,26 @@ from typing import List
 
 class Solution:
     def sumSubarrayMins(self, A: List[int]) -> int:
+        res, n, mod = 0, len(A), 10 ** 9 + 7
+        left, right, s1, s2 = [0] * n, [0] * n, [], []
+
+        for i in range(n):
+            count = 1
+            while s1 and s1[-1][0] > A[i]:
+                count += s1.pop()[1]
+            left[i] = count
+            s1.append([A[i], count])
+
+        for i in range(n - 1, -1, -1):
+            count = 1
+            while s2 and s2[-1][0] >= A[i]:
+                count += s2.pop()[1]
+            right[i] = count
+            s2.append([A[i], count])
+
+        return sum(l * r * a for l, r, a in zip(left, right, A)) % mod
+
+    def sumSubarrayMins1(self, A: List[int]) -> int:
         stk = [-1]
         dp = [0] * (len(A) + 1)
         res = 0
