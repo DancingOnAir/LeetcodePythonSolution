3c45c94e137d3fc04bb326fa0645e4892796fc2e
DancingOnAir <taotong1984@hotmail.com>
2020-09-16 14:05:46 +0800

    feat(Array): init q1365 & accepted solution for q1365

---
+++ Array/1365_how_many_numbers_are_smaller_than_the_current_number.py
@@ -0,0 +1,31 @@
+from typing import List
+import bisect
+
+
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        record, res = [], [0] * len(nums)
+        for i, val in enumerate(nums):
+            record.append([val, i])
+        record.sort()
+        for i in range(len(record)):
+            pos = bisect.bisect_left(record, [record[i][0], 0])
+            res[record[i][1]] = pos
+        return res
+
+
+def test_smaller_number_than_current():
+    solution = Solution()
+
+    nums1 = [8, 1, 2, 2, 3]
+    print(solution.smallerNumbersThanCurrent(nums1))
+
+    nums2 = [6, 5, 4, 8]
+    print(solution.smallerNumbersThanCurrent(nums2))
+
+    nums3 = [7, 7, 7, 7]
+    print(solution.smallerNumbersThanCurrent(nums3))
+
+
+if __name__ == '__main__':
+    test_smaller_number_than_current()
