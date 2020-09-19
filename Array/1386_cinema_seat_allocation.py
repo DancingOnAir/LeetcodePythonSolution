e04ee26c71e4f0155bd19b88d9e85a2af0c8a016
DancingOnAir <taotong1984@hotmail.com>
2020-09-19 22:45:07 +0800

    feat(Array): accepted bitmask solution for q1386

--- Array/1386_cinema_seat_allocation.py
+++ Array/1386_cinema_seat_allocation.py
@@ -4,6 +4,20 @@ import collections
 
 class Solution:
     def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
+        seats = collections.defaultdict(int)
+        for row, col in reservedSeats:
+            seats[row] = seats[row] | (1 << (col - 1))
+
+        res = 2 * n
+        for reserved in seats.values():
+            cnt = 0
+            cnt += (reserved & int('0111100000', 2)) == 0
+            cnt += (reserved & int('0000011110', 2)) == 0
+            cnt += (reserved & int('0001111000', 2)) == 0 and cnt == 0
+            res += (cnt - 2)
+        return res
+
+    def maxNumberOfFamilies1(self, n: int, reservedSeats: List[List[int]]) -> int:
         matrix = collections.defaultdict(list)
         for i in range(len(reservedSeats)):
             matrix[reservedSeats[i][0]].append(reservedSeats[i][1])
