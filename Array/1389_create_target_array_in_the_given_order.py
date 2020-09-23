d51f5349b083a87fee9e8b7aad125728b4a2738a
DancingOnAir <taotong1984@hotmail.com>
2020-09-23 10:32:11 +0800

    feat(Array): init q1389 & accepted solution for q1389

---
+++ Array/1389_create_target_array_in_the_given_order.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        for i, val in enumerate(index):
+            for j in range(len(index[:i])):
+                if val <= index[j]:
+                    index[j] += 1
+        res = [0] * len(nums)
+        for i in range(len(res)):
+            res[index[i]] = nums[i]
+
+        return res
+
+
+def test_create_target_array():
+    solution = Solution()
+
+    nums1 = [0, 1, 2, 3, 4]
+    index1 = [0, 1, 2, 2, 1]
+    print(solution.createTargetArray(nums1, index1))
+
+    nums2 = [1, 2, 3, 4, 0]
+    index2 = [0, 1, 2, 3, 0]
+    print(solution.createTargetArray(nums2, index2))
+
+    nums3 = [1]
+    index3 = [0]
+    print(solution.createTargetArray(nums3, index3))
+
+
+if __name__ == '__main__':
+    test_create_target_array()
