1a40785f7f9677cc9c00112f342df54e476bb2ef
DancingOnAir <taotong1984@hotmail.com>
2020-09-24 08:39:52 +0800

    feat(Array): init q1394 & accepted defaultdict solution for q1394

---
+++ Array/1394_find_lucky_integer_in_an_array.py
@@ -0,0 +1,38 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def findLucky(self, arr: List[int]) -> int:
+        counts = defaultdict(int)
+        res = -1
+        for a in arr:
+            counts[a] += 1
+
+        for k, v in counts.items():
+            if k == v:
+                res = max(res, k)
+        return res
+
+
+def test_find_lucky():
+    solution = Solution()
+
+    arr1 = [2, 2, 3, 4]
+    print(solution.findLucky(arr1))
+
+    arr2 = [1, 2, 2, 3, 3, 3]
+    print(solution.findLucky(arr2))
+
+    arr3 = [2, 2, 2, 3, 3]
+    print(solution.findLucky(arr3))
+
+    arr4 = [5]
+    print(solution.findLucky(arr4))
+
+    arr5 = [7, 7, 7, 7, 7, 7, 7]
+    print(solution.findLucky(arr5))
+
+
+if __name__ == '__main__':
+    test_find_lucky()
