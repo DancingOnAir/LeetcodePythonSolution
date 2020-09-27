452d986d1779fbc99779dca0688926d3308138be
DancingOnAir <taotong1984@hotmail.com>
2020-09-27 14:43:55 +0800

    feat(Array): accepted pythonic solution for q1414

--- Array/1414_find_the_minimum_number_of_fibonacci_numbers_whose_sum_is_K.py
+++ Array/1414_find_the_minimum_number_of_fibonacci_numbers_whose_sum_is_K.py
@@ -3,6 +3,14 @@ from typing import List
 
 class Solution:
     def findMinFibonacciNumbers(self, k: int) -> int:
+        if k < 2:
+            return k
+        a, b = 1, 1
+        while b <= k:
+            a, b = b, a + b
+        return 1 + self.findMinFibonacciNumbers(k - a)
+
+    def findMinFibonacciNumbers1(self, k: int) -> int:
         def get_next_fibonacci_number(num):
             fibonacci_num = 0
             count = 0
