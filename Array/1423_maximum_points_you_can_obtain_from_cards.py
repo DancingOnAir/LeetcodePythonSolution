327c96a2b33d6645373b6c4c0ed05aa0bc23b09c
DancingOnAir <taotong1984@hotmail.com>
2020-10-01 22:08:45 +0800

    feat(Array): init q1423 & accepted sliding window solution for q1423

---
+++ Array/1423_maximum_points_you_can_obtain_from_cards.py
@@ -0,0 +1,57 @@
+from typing import List
+
+
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        n = len(cardPoints)
+        if k >= n:
+            return sum(cardPoints)
+
+        new_card_points = cardPoints[-k:] + cardPoints[:k]
+        res = 0
+        left, count, total = 0, 0, 0
+        for right in range(2*k):
+            total += new_card_points[right]
+            count += 1
+
+            if count > k:
+                total -= new_card_points[left]
+                count -= 1
+                left += 1
+
+            if count == k:
+                res = max(res, total)
+
+        return res
+
+
+def test_max_score():
+    solution = Solution()
+
+    cardPoints1 = [1, 2, 3, 4, 5, 6, 1]
+    k1 = 3
+    print(solution.maxScore(cardPoints1, k1))
+
+    cardPoints2 = [2, 2, 2]
+    k2 = 2
+    print(solution.maxScore(cardPoints2, k2))
+
+    cardPoints3 = [9, 7, 7, 9, 7, 7, 9]
+    k3 = 7
+    print(solution.maxScore(cardPoints3, k3))
+
+    cardPoints4 = [1, 1000, 1]
+    k4 = 1
+    print(solution.maxScore(cardPoints4, k4))
+
+    cardPoints5 = [1, 79, 80, 1, 1, 1, 200, 1]
+    k5 = 3
+    print(solution.maxScore(cardPoints5, k5))
+
+    cardPoints6 = [100, 40, 17, 9, 73, 75]
+    k6 = 3
+    print(solution.maxScore(cardPoints6, k6))
+
+
+if __name__ == '__main__':
+    test_max_score()
