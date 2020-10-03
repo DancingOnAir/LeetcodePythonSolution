e8f622626209d2894d5401cd9b1af8831e136356
DancingOnAir <taotong1984@hotmail.com>
2020-10-03 19:02:20 +0800

    feat(Array): init q1424 & tried a brute force method but TLE

---
+++ Array/1424_diagonal_traverse_II.py
@@ -0,0 +1,46 @@
+from typing import List
+
+
+class Solution:
+    def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
+        row = len(nums)
+        if row == 1:
+            return nums[0]
+        col = max([len(i) for i in nums])
+        if col == 1:
+            return list(*zip(*nums))
+
+        res = []
+        for i in range(row + col):
+            r, c = i if i < row else row - 1, 0 if i < row else i - row + 1
+
+            while r >= 0 and c < col:
+                try:
+                    res.append(nums[r][c])
+
+                except IndexError:
+                    continue
+                finally:
+                    r -= 1
+                    c += 1
+        return res
+
+
+def test_find_diagonal_order():
+    solution = Solution()
+
+    nums1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    print(solution.findDiagonalOrder(nums1))
+
+    nums2 = [[1, 2, 3, 4, 5], [6, 7], [8], [9, 10, 11], [12, 13, 14, 15, 16]]
+    print(solution.findDiagonalOrder(nums2))
+
+    nums3 = [[1, 2, 3], [4], [5, 6, 7], [8], [9, 10, 11]]
+    print(solution.findDiagonalOrder(nums3))
+
+    nums4 = [[1, 2, 3, 4, 5, 6]]
+    print(solution.findDiagonalOrder(nums4))
+
+
+if __name__ == '__main__':
+    test_find_diagonal_order()
