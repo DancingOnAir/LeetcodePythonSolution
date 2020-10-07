36e8e1bcab779066f2d62ce41179fd1fb3a3a172
DancingOnAir <taotong1984@hotmail.com>
2020-10-08 01:01:53 +0800

    feat(Array): init q1450 accepted brute force solution for q1450

---
+++ Array/1450_number_of_students_doing_homework_at_a_given_time.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        res = 0
+        for i in range(len(startTime)):
+            if startTime[i] <= queryTime <= endTime[i]:
+                res += 1
+        return res
+
+
+def test_busy_student():
+    solution = Solution()
+
+    startTime1 = [1, 2, 3]
+    endTime1 = [3, 2, 7]
+    queryTime1 = 4
+    print(solution.busyStudent(startTime1, endTime1, queryTime1))
+
+    startTime2 = [4]
+    endTime2 = [4]
+    queryTime2 = 4
+    print(solution.busyStudent(startTime2, endTime2, queryTime2))
+
+    startTime3 = [4]
+    endTime3 = [4]
+    queryTime3 = 5
+    print(solution.busyStudent(startTime3, endTime3, queryTime3))
+
+
+if __name__ == '__main__':
+    test_busy_student()
