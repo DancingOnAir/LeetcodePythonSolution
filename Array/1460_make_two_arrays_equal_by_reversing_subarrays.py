a82692ffcaf3deccf42f509f68de8227cbebbffd
DancingOnAir <taotong1984@hotmail.com>
2020-10-07 09:59:49 +0800

    feat(Array): init q1460 & accepted solution for q1460

---
+++ Array/1460_make_two_arrays_equal_by_reversing_subarrays.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+class Solution:
+    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        target.sort()
+        arr.sort()
+        return target == arr
+
+
+def test_can_be_equal():
+    solution = Solution()
+
+    target1 = [1, 2, 3, 4]
+    arr1 = [2, 4, 1, 3]
+    print(solution.canBeEqual(target1, arr1))
+
+    target2 = [7]
+    arr2 = [7]
+    print(solution.canBeEqual(target2, arr2))
+
+    target3 = [1, 12]
+    arr3 = [12, 1]
+    print(solution.canBeEqual(target3, arr3))
+
+    target4 = [3, 7, 9]
+    arr4 = [3, 7, 11]
+    print(solution.canBeEqual(target4, arr4))
+
+    target5 = [1, 1, 1, 1, 1]
+    arr5 = [1, 1, 1, 1, 1]
+    print(solution.canBeEqual(target5, arr5))
+
+
+if __name__ == '__main__':
+    test_can_be_equal()
+
+
+
+
+
