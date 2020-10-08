5d398b2914840fee719190c41a2aca5ed630f77b
DancingOnAir <taotong1984@hotmail.com>
2020-10-08 10:02:41 +0800

    feat(Array): accepted zip solution for q1450

--- Array/1450_number_of_students_doing_homework_at_a_given_time.py
+++ Array/1450_number_of_students_doing_homework_at_a_given_time.py
@@ -3,6 +3,9 @@ from typing import List
 
 class Solution:
     def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        return sum(i <= queryTime <= j for i, j in zip(startTime, endTime))
+
+    def busyStudent1(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
         res = 0
         for i in range(len(startTime)):
             if startTime[i] <= queryTime <= endTime[i]:
