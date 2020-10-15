04dbf674f16ad4cfaf2c045a4a8e5275dc8dc914
DancingOnAir <taotong1984@hotmail.com>
2020-10-15 10:41:13 +0800

    feat(Array): accepted prefix & suffix sum solution for q1493

--- Array/1493_longest_subarray_of_1's_after_deleting_one_element.py
+++ Array/1493_longest_subarray_of_1's_after_deleting_one_element.py
@@ -3,6 +3,26 @@ from typing import List
 
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
+        n = len(nums)
+        pre, suf = [nums[0]] + [0] * (n - 1), [0] * (n - 1) + [nums[-1]]
+
+        for i in range(1, n):
+            if nums[i]:
+                pre[i] = pre[i - 1] + 1
+
+        for i in range(n - 1)[::-1]:
+            if nums[i]:
+                suf[i] = suf[i + 1] + 1
+
+        res = 0
+        for i in range(n):
+            pre_sum = pre[i - 1] if i else 0
+            suf_sum = suf[i + 1] if i != n - 1 else 0
+            res = max(res, pre_sum + suf_sum)
+
+        return res
+
+    def longestSubarray1(self, nums: List[int]) -> int:
         # a is the len of subarray which contains a one, b is the length of subarray which only contains ones
         a, b = 0, 0
         res = 0
@@ -37,6 +57,9 @@ def test_longest_subarray():
 
     nums5 = [0, 0, 0]
     print(solution.longestSubarray(nums5))
+
+    nums6 = [0, 1, 1, 1, 0, 1, 1]
+    print(solution.longestSubarray(nums6))
 
 
 if __name__ == '__main__':
