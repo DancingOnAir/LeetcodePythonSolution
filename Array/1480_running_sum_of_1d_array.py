24ce62daaba8067660929e7ae3c0863fe09cf6f9
DancingOnAir <taotong1984@hotmail.com>
2020-10-16 00:03:38 +0800

    feat(Array): accepted accumulate solution for q1480

--- Array/1480_running_sum_of_1d_array.py
+++ Array/1480_running_sum_of_1d_array.py
@@ -1,8 +1,12 @@
 from typing import List
+from itertools import accumulate
 
 
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
+        return accumulate(nums)
+
+    def runningSum1(self, nums: List[int]) -> List[int]:
         if len(nums) == 1:
             return nums
 
