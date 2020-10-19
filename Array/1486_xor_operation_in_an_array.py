4ebc4d4eec23c8e0dd1ae41b33dbf2e89ce2453f
DancingOnAir <taotong1984@hotmail.com>
2020-10-19 15:53:40 +0800

    feat(Array): accpetd biwise solution for q1486

--- Array/1486_xor_operation_in_an_array.py
+++ Array/1486_xor_operation_in_an_array.py
@@ -2,7 +2,19 @@ from typing import List
 
 
 class Solution:
+    # https://leetcode.com/problems/xor-operation-in-an-array/discuss/703026/One-liner-O(1)-time-with-no-recursion-and-detailed-explanation
+    # bitwise solution
+    # tips1: 2a ^ (2a + 1) = 1
+    # tips2: ( 0^1^2^...^(a-1) ) ^ (0^1^2^...^b) if we want to get a^(a+1)^...^b
     def xorOperation(self, n: int, start: int) -> int:
+        # here is not 2 * (n - 1)
+        end = start + 2 * n
+        a, b = start // 2, end // 2
+        return ((start & 1) * (n & 1)) + \
+               ((((a - 1) * (a & 1)) ^ ((a // 2) & 1)) ^ (((b - 1) * (b & 1)) ^ ((b // 2) & 1))) * 2
+
+    # brute force
+    def xorOperation1(self, n: int, start: int) -> int:
         res = 0
         for i in range(n):
             res ^= (start + 2 * i)
