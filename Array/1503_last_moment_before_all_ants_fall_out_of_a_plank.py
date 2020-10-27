a979e3260f76dfb9a029d572414d93ab60fce50b
DancingOnAir <taotong1984@hotmail.com>
2020-10-27 08:30:18 +0800

    feat(Array): init q1503 & accepted solution for q1503

---
+++ Array/1503_last_moment_before_all_ants_fall_out_of_a_plank.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+class Solution:
+    def getLastMoment(self, n: int, left: List[int], right: List[int]) -> int:
+        left.sort()
+        right.sort()
+
+        if not left:
+            return n - right[0]
+        if not right:
+            return left[-1]
+
+        return max(n - right[0], left[-1])
+
+
+def test_get_last_moment():
+    solution = Solution()
+
+    n1 = 4
+    left1 = [4, 3]
+    right1 = [0, 1]
+    print(solution.getLastMoment(n1, left1, right1))
+
+    n2 = 7
+    left2 = []
+    right2 = [0, 1, 2, 3, 4, 5, 6, 7]
+    print(solution.getLastMoment(n2, left2, right2))
+
+    n3 = 6
+    left3 = [6]
+    right3 = [0]
+    print(solution.getLastMoment(n3, left3, right3))
+
+
+if __name__ == '__main__':
+    test_get_last_moment()
