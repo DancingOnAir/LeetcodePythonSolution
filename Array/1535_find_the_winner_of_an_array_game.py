3697f8150f6aa1a4a55512c4326cfec25987e5b1
DancingOnAir <taotong1984@hotmail.com>
2020-11-01 14:45:24 +0800

    feat(Array): init q1535 &  accepted solution for q1535

---
+++ Array/1535_find_the_winner_of_an_array_game.py
@@ -0,0 +1,47 @@
+from typing import List
+
+
+class Solution:
+    def getWinner(self, arr: List[int], k: int) -> int:
+        n = len(arr)
+        if k >= n:
+            return max(arr)
+
+        res = arr[0]
+        count = 0
+        for i in range(1, n):
+
+            if res >= arr[i]:
+                count += 1
+            else:
+                count = 1
+                res = arr[i]
+
+            if count == k:
+                return res
+
+        return res
+
+
+def test_get_winner():
+    solution = Solution()
+
+    arr1 = [2, 1, 3, 5, 4, 6, 7]
+    k1 = 2
+    print(solution.getWinner(arr1, k1))
+
+    arr2 = [3, 2, 1]
+    k2 = 10
+    print(solution.getWinner(arr2, k2))
+
+    arr3 = [1, 9, 8, 2, 3, 7, 6, 4, 5]
+    k3 = 7
+    print(solution.getWinner(arr3, k3))
+
+    arr4 = [1, 11, 22, 33, 44, 55, 66, 77, 88, 99]
+    k4 = 1000000000
+    print(solution.getWinner(arr4, k4))
+
+
+if __name__ == '__main__':
+    test_get_winner()
