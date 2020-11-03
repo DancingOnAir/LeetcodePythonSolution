bf4edc6090da798bbd1bbca043c2e859fd9def6d
DancingOnAir <taotong1984@hotmail.com>
2020-11-03 08:57:50 +0800

    feat(Array): init q1572 & accepted solution for q1572

---
+++ Array/1572_matrix_diagonal_sum.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        n = len(mat)
+        res = 0
+        for i in range(n):
+            res += mat[i][i] + mat[n - 1 - i][i]
+        return res - mat[n // 2][n // 2] if n & 1 else res
+
+
+def test_diagonal_sum():
+    solution = Solution()
+
+    mat1 = [[1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]]
+    assert solution.diagonalSum(mat1) == 25, "error input"
+
+    mat2 = [[1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1]]
+    assert solution.diagonalSum(mat2) == 8, "error input"
+
+
+if __name__ == '__main__':
+    test_diagonal_sum()
