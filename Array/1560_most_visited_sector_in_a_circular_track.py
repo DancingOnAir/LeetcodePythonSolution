a9cd69928d3889b42fb48700d65b631637a692bc
DancingOnAir <taotong1984@hotmail.com>
2020-11-05 10:08:06 +0800

    feat(Array): accepted pythonic solution for q1560

--- Array/1560_most_visited_sector_in_a_circular_track.py
+++ Array/1560_most_visited_sector_in_a_circular_track.py
@@ -3,6 +3,9 @@ from typing import List
 
 class Solution:
     def mostVisited(self, n: int, rounds: List[int]) -> List[int]:
+        return list(range(rounds[0], rounds[-1] + 1)) or list(range(1, rounds[-1] + 1)) + list(range(rounds[0], n + 1))
+
+    def mostVisited1(self, n: int, rounds: List[int]) -> List[int]:
         return [x for x in range(rounds[0], rounds[-1] + 1)] if rounds[0] <= rounds[-1] \
             else [x for x in range(1, rounds[-1] + 1)] + [y for y in range(rounds[0], n + 1)]
 
@@ -12,6 +15,7 @@ def test_most_visited():
 
     n1 = 4
     rounds1 = [1, 3, 1, 2]
+    print(solution.mostVisited(n1, rounds1))
     assert solution.mostVisited(n1, rounds1) == [1, 2], "wrong result"
 
     n2 = 2
