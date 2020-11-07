0bf6f0e137e1a30211aa7ef67bc8d6d68000683c
DancingOnAir <taotong1984@hotmail.com>
2020-11-07 22:45:46 +0800

    feat(Array): init q1588 & accepted brute force solution for q1588

---
+++ Array/1588_sum_of_all_odd_length_subarrays.py
@@ -0,0 +1,39 @@
+from typing import List
+
+
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        n = len(arr)
+
+        if n < 3:
+            return sum(arr)
+
+        res, i, count = 0, 1, 0
+        while i <= n:
+            for j in range(n - i + 1):
+                res += sum(arr[j:j+i])
+
+            count += 1
+            i = 2 * count + 1
+
+        return res
+
+
+def test_sum_odd_length_subarrays():
+    solution = Solution()
+
+    arr1 = [1, 4, 2, 5, 3]
+    print(solution.sumOddLengthSubarrays(arr1))
+    assert solution.sumOddLengthSubarrays(arr1) == 58, "wrong result"
+
+    arr2 = [1, 2]
+    print(solution.sumOddLengthSubarrays(arr2))
+    assert solution.sumOddLengthSubarrays(arr2) == 3, "wrong result"
+
+    arr3 = [10, 11, 12]
+    print(solution.sumOddLengthSubarrays(arr3))
+    assert solution.sumOddLengthSubarrays(arr3) == 66, "wrong result"
+
+
+if __name__ == '__main__':
+    test_sum_odd_length_subarrays()
