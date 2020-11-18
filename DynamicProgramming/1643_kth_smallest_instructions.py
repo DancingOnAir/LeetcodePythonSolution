1a8a5fec07ec3a3b6486e97c98e8e1fa2c5426b1
DancingOnAir <taotong1984@hotmail.com>
2020-11-18 17:23:48 +0800

    feat(DynamicProgramming): accepted math solution for q1643

--- DynamicProgramming/1643_kth_smallest_instructions.py
+++ DynamicProgramming/1643_kth_smallest_instructions.py
@@ -2,7 +2,35 @@ from typing import List
 
 
 class Solution:
+    def ncr(self, n, r):
+        res = 1
+        diff = n - r + 1
+        for i in range(1, r + 1):
+            res = res * diff // i
+            diff += 1
+        return res
+
     def kthSmallestPath(self, destination: List[int], k: int) -> str:
+        r, c = destination
+        res = ''
+
+        for i in range(r + c):
+            if k == 1:
+                res += 'H' * c + 'V' * r
+                break
+
+            if k <= self.ncr(r+c-1, r):
+                c -= 1
+                res += 'H'
+            else:
+                k -= self.ncr(r+c-1, r)
+                r -= 1
+                res += 'V'
+
+        return res
+
+    # TLE
+    def kthSmallestPath1(self, destination: List[int], k: int) -> str:
         dp = [[[''] for _ in range(destination[1] + 1)] for _ in range(destination[0] + 1)]
         for i in range(1, destination[0] + 1):
             dp[i][0] = [x + 'V' for x in dp[i - 1][0]]
@@ -16,6 +44,7 @@ class Solution:
                 dp[i][j] += [x + 'H' for x in dp[i][j - 1]]
 
         dp[destination[0]][destination[1]].sort()
+        print(dp[destination[0]][destination[1]])
         return dp[destination[0]][destination[1]][k - 1]
 
 
