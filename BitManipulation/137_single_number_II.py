baa86715014d9d8c3c8433fd7ce0e83679984b6b
DancingOnAir <taotong1984@hotmail.com>
2020-11-25 00:25:47 +0800

    feat(BitManipulation): accepted solution for q137

--- BitManipulation/137_single_number_II.py
+++ BitManipulation/137_single_number_II.py
@@ -1,8 +1,15 @@
 from typing import List
+from collections import Counter
 
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        count = Counter(nums)
+        for k in count:
+            if count[k] == 1:
+                return k
+
+    def singleNumber1(self, nums: List[int]) -> int:
         x1, x2, mask = 0, 0, 0
         for num in nums:
             x2 ^= (x1 & num)
