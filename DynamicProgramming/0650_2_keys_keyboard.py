94afb806de5897fde13472503778e3a623e31ea5
DancingOnAir <taotong1984@hotmail.com>
2020-12-01 23:03:40 +0800

    feat(DynamicProgramming): accepted dp solution for q0650

--- DynamicProgramming/0650_2_keys_keyboard.py
+++ DynamicProgramming/0650_2_keys_keyboard.py
@@ -2,6 +2,18 @@ from typing import List
 
 
 class Solution:
+    # dp
+    def minSteps(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        for i in range(2, n+1):
+            dp[i] = i
+        for i in range(2, n+1):
+            for j in range(2, i):
+                if not i % j:
+                    dp[i] = min(dp[i], dp[i // j] + j)
+        return dp[n]
+
+    # prime factorization solution
     def minSteps(self, n: int) -> int:
         for i in range(2, int(n ** 1/2) + 1):
             if not n % i:
