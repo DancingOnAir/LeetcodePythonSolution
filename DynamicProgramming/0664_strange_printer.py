f70d866f2270ec6259ef656f41ee97310a9ee58b
DancingOnAir <taotong1984@hotmail.com>
2020-12-02 23:23:57 +0800

    feat(DynamicProgramming): init q0664 & gave a failure dp solution

--- DynamicProgramming/0664_strange_printer.py
+++ DynamicProgramming/0664_strange_printer.py
@@ -2,11 +2,21 @@ from typing import List
 
 
 class Solution:
-    # dp[i] = dp[i-1] if s[i-1] == s[i-2]
     def strangePrinter(self, s: str) -> int:
         n = len(s)
+        if n < 2:
+            return n
 
-        pass
+        dp = [[1] * n for _ in range(n)]
+        for l in range(1, n):
+            for i in range(n):
+                if i + l < n:
+                    temp = min(dp[i][i+l-1], dp[i+1][i+l])
+                    if s[i] != s[i+l]:
+                        temp += 1
+                    dp[i][i+l] = min(dp[i][i+l-1] + (s[i+l-1] != s[i+l]), dp[i+1][i+l] + (s[i] != s[i+1]), temp)
+
+        return dp[0][n-1]
 
 
 def test_strange_printer():
@@ -14,6 +24,7 @@ def test_strange_printer():
 
     assert solution.strangePrinter('aaabbb') == 2, 'wrong result'
     assert solution.strangePrinter('aba') == 2, 'wrong result'
+    assert solution.strangePrinter("ccdcadbddbaddcbccdcdabcbcddbccdcbad") == 17, 'wrong result'
 
 
 if __name__ == '__main__':
