a4edb4c09bf69560ab69ee0fcc535e8f84557b77
DancingOnAir <taotong1984@hotmail.com>
2020-12-03 11:23:40 +0800

    refactor(DynamicProgramming): optimized dp solution by lru_cache for q0664

--- DynamicProgramming/0664_strange_printer.py
+++ DynamicProgramming/0664_strange_printer.py
@@ -1,13 +1,35 @@
 from typing import List
+from functools import lru_cache
+import re
 
 
 class Solution:
     def strangePrinter(self, s: str) -> int:
+        s = re.sub(r'(.)\1*', r'\1', s)
+        memo = dict()
+
+        @lru_cache(None)
+        def dp(i, j):
+            if i > j:
+                return 0
+            if (i, j) not in memo:
+                ans = dp(i+1, j) + 1
+                for k in range(i+1, j+1):
+                    if s[k] == s[i]:
+                        ans = min(ans, dp(i, k-1) + dp(k+1, j))
+                memo[i, j] = ans
+            return memo[i, j]
+
+        return dp(0, len(s) - 1)
+
+    def strangePrinter1(self, s: str) -> int:
         if not s:
             return 0
 
-        s = ''.join(a for a, b in zip(s, '#'+s) if a != b)
+        # s = ''.join(a for a, b in zip(s, '#'+s) if a != b)
+        s = re.sub(r'(.)\1*', r'\1', s)
         n = len(s)
+
         dp = [[1] * n for _ in range(n)]
         for l in range(1, n):
             for i in range(n):
