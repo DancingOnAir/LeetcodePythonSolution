b2534ce48dcd087f5dfe9b69a8a9bda0f7f0ff52
DancingOnAir <taotong1984@hotmail.com>
2020-12-05 00:27:34 +0800

    feat(DynamicProgramming): init q0300 & accepted dp solution for q0300

---
+++ DynamicProgramming/0300_longest_increasing_subsequence.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 2:
+            return n
+
+        dp = [1] * n
+        for i in range(1, n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+
+
+def test_length_of_lis():
+    solution = Solution()
+
+    nums1 = [10, 9, 2, 5, 3, 7, 101, 18]
+    assert solution.lengthOfLIS(nums1) == 4, 'wrong result'
+    nums2 = [0, 1, 0, 3, 2, 3]
+    assert solution.lengthOfLIS(nums2) == 4, 'wrong result'
+    nums3 = [7, 7, 7, 7, 7, 7, 7]
+    assert solution.lengthOfLIS(nums3) == 1, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_length_of_lis()
