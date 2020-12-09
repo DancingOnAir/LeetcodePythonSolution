4af21b68b9a02675c543a949669bd0eb6c822941
DancingOnAir <taotong1984@hotmail.com>
2020-12-09 23:22:37 +0800

    feat(DynamicProgramming): accepted a memoization dp solution

--- DynamicProgramming/0688_knight_probability_in_chessboard.py
+++ DynamicProgramming/0688_knight_probability_in_chessboard.py
@@ -5,8 +5,36 @@ class Solution:
     def __init__(self):
         self.dp = [[[0.0] * 101 for _ in range(25)] for _ in range(25)]
 
-    # memoization but TLE
     def knightProbability(self, N: int, K: int, r: int, c: int) -> float:
+        # Generates all 8 moves
+        total_moves = {(i, j) for i in {-2, -1, 1, 2} for j in {-2, -1, 1, 2} - {i} - {-i}}
+        # A dictionary to store (K, r, c) for DP
+        mmap = {}
+
+        def dp(K, r, c, cur_prob=1):
+            if not K:
+                # If there are no more moves left, then store the probability landing on that
+                # square, then return the value
+                mmap[(K, r, c)] = cur_prob
+                return cur_prob
+            else:
+                # Generates all possible moves originating from the current square
+                pos_moves = {(r + dr, c + dc) for dr, dc in total_moves if 0 <= r + dr < N and 0 <= c + dc < N}
+                res = 0
+                for dr, dc in pos_moves:
+                    if (K - 1, dr, dc) in mmap:
+                        res += mmap[(K - 1, dr, dc)]
+                    else:
+                        # If we had NOT calculated the probability, we recurse into
+                        # next possible moves, with each move being 1/8th as probable
+                        res += dp(K - 1, dr, dc, cur_prob / 8)
+
+                mmap[(K, r, c)] = res
+                return res
+        return dp(K, r, c)
+
+    # memoization but TLE
+    def knightProbability3(self, N: int, K: int, r: int, c: int) -> float:
         if r in self.dp and c in self.dp[r] and K in self.dp[r][c]:
             return self.dp[r][c][K]
 
