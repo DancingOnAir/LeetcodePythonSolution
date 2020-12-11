c9b5e07580be16e08714a156b220263ae16dbb62
DancingOnAir <taotong1984@hotmail.com>
2020-12-11 21:55:23 +0800

    feat(DynamicProgramming): tried a failed dp solution & a failed memo+recursive solution

--- DynamicProgramming/0691_stickers_to_spell_word.py
+++ DynamicProgramming/0691_stickers_to_spell_word.py
@@ -1,14 +1,67 @@
 from typing import List
-from collections import defaultdict
+from collections import Counter
 
 
 class Solution:
-    def minStickers(self, stickers: List[str], target: str) -> int:
-        target_dict = defaultdict(int)
-        for c in target:
-            target_dict[c] += 1
+    # def minStickers(self, stickers: 'List[str]', target: 'str') -> 'int':
+    #     stickers, self.map = [Counter(s) for s in stickers if set(s) & set(target)], {}
+    #     def dfs(target):
+    #         if not target: return 0
+    #         if target in self.map: return self.map[target]
+    #         cnt, res = Counter(target), float('inf')
+    #         for c in stickers: # traverse the stickers to get new target
+    #             if c[target[0]] == 0: continue # we can make sure the 1st letter will be removed to reduce the time complexity
+    #             nxt = dfs(''.join([s * t for (s, t) in (cnt - c).items()]))
+    #             if nxt != -1: res = min(res, 1 + nxt)
+    #         self.map[target] = -1 if res == float('inf') else res
+    #         return self.map[target]
+    #     return dfs(target)
 
-        print(target_dict)
+    def minStickers(self, stickers: List[str], target: str) -> int:
+        self.memo = {}
+        stickers = [Counter(s) for s in stickers if set(s) & set(target)]
+        # print(stickers)
+
+        def dfs(str):
+            if not str:
+                return 0
+
+            if str in self.memo:
+                return self.memo[str]
+
+            cnt, res = Counter(str), float('inf')
+            # print('cnt {0}'.format(cnt))
+            for c in stickers:
+                if c[target[0]] == 0:
+                    continue
+                # print('cnt - c {0}'.format(cnt - c))
+                nxt = dfs(''.join([i * j for (i, j) in (cnt - c).items()]))
+                if nxt != -1:
+                    res = min(res, 1 + nxt)
+            self.memo[str] = -1 if res == float('inf') else res
+            return self.memo[str]
+        return dfs(target)
+
+    def minStickers1(self, stickers: List[str], target: str) -> int:
+        # n = len(target)
+        # m = 1 << n
+        #
+        # dp = [0x3f3f3f3f] * m
+        # dp[0] = 0
+        # for i in range(m):
+        #     if dp[i] == -1:
+        #         continue
+        #
+        #     for sticker in stickers:
+        #         now = i
+        #         for c in sticker:
+        #             for r in range(n):
+        #                 if target[r] == c and ~((now >> r) & 1):
+        #                     now |= 1 << r
+        #                     break
+        #         dp[now] = min(dp[now], dp[i] + 1)
+        #
+        # return dp[m - 1]
         pass
 
 
