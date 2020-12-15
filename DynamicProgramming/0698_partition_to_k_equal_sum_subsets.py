113a1965e6fd33dc68947c78b66ac63261bcde92
DancingOnAir <taotong1984@hotmail.com>
2020-12-15 10:10:30 +0800

    feat(DynamicProgramming): accepted top down dp solution for 0698

--- DynamicProgramming/0698_partition_to_k_equal_sum_subsets.py
+++ DynamicProgramming/0698_partition_to_k_equal_sum_subsets.py
@@ -4,6 +4,22 @@ from typing import List
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
         target, remainder = divmod(sum(nums), k)
+        if remainder or max(nums) > target:
+            return False
+
+        memo = [None] * (1 << len(nums))
+        memo[-1] = True
+
+        def search(used, todo):
+            if memo[used] is None:
+                tar = (todo - 1) % target + 1
+                memo[used] = any(search(used | (1 << i), todo - num) for i, num in enumerate(nums) if (not ((used >> i) & 1)) and num <= tar)
+            return memo[used]
+
+        return search(0, target * k)
+
+    def canPartitionKSubsets2(self, nums: List[int], k: int) -> bool:
+        target, remainder = divmod(sum(nums), k)
         if remainder:
             return False
 
@@ -63,7 +79,11 @@ def test_can_partition_k_subsets():
 
     nums1 = [4, 3, 2, 3, 5, 2, 1]
     k1 = 4
-    assert solution.canPartitionKSubsets(nums1, k1), 'wrong result'
+    # assert solution.canPartitionKSubsets(nums1, k1), 'wrong result'
+
+    nums2 = [2, 2, 3, 2]
+    k2 = 3
+    assert not solution.canPartitionKSubsets(nums2, k2), 'wrong result'
 
 
 if __name__ == '__main__':
