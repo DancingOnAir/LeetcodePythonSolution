e8431660f217a988a3cc3c691f38b421001a339c
DancingOnAir <taotong1984@hotmail.com>
2020-12-17 16:37:25 +0800

    feat(DynamicProgramming): accepted pythonic solution for q0131

--- DynamicProgramming/0131_palindrome_partitioning.py
+++ DynamicProgramming/0131_palindrome_partitioning.py
@@ -3,6 +3,9 @@ from typing import List
 
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
+        return [[s[:i]] + rest for i in range(1, len(s)+1) if s[:i] == s[i - 1::-1] for rest in self.partition(s[i:])] or [[]]
+
+    def partition1(self, s: str) -> List[List[str]]:
         n = len(s)
 
         dp = [[] for _ in range(n+1)]
