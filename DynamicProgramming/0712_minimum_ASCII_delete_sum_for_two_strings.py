92244152389bfe69e574672db6147c6b74939952
DancingOnAir <taotong1984@hotmail.com>
2020-12-18 23:49:09 +0800

    feat(DynamicProgramming): accepted dp solution for q0712

--- DynamicProgramming/0712_minimum_ASCII_delete_sum_for_two_strings.py
+++ DynamicProgramming/0712_minimum_ASCII_delete_sum_for_two_strings.py
@@ -5,30 +5,16 @@ from collections import Counter
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
         l1, l2 = len(s1), len(s2)
+        dp = [[0] * (l2 + 1) for _ in range(l1 + 1)]
 
-        dp = [[0] * (l1 + 1) for _ in range(l2 + 1)]
-
-        common_str = ''
         for i in range(l1):
             for j in range(l2):
-                dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1])
-                if s1[i] == s2[j] and dp[i][j] + 1 >= dp[i + 1][j + 1]:
-                    dp[i + 1][j + 1] = dp[i][j] + 1
-                    if common_str and common_str[-1] < s1[i]:
-                        common_str.
-                        common_str[-1] = s1[i]
-                    else:
-                        common_str += s1[i]
-        print(common_str)
-        res = 0
-        for i, val in enumerate(s1):
-            if val not in common_str:
-                res += ord(val)
+                if s1[i] == s2[j]:
+                    dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1])
+                else:
+                    dp[i + 1][j + 1] = dp[i][j] + ord(s1[i])
 
-        for i, val in enumerate(s2):
-            if val not in common_str:
-                res += ord(val)
-
+        res = sum(map(ord, s1 + s2)) - dp[l1][l2] * 2
         return res
 
 
