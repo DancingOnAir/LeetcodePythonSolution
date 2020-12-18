7464dcf9c6b7c2f281254fc37c0cf292c0ea5a95
DancingOnAir <taotong1984@hotmail.com>
2020-12-19 00:43:38 +0800

    feat(DynamicProgramming): accepted 2d dp solution for q0712

--- DynamicProgramming/0712_minimum_ASCII_delete_sum_for_two_strings.py
+++ DynamicProgramming/0712_minimum_ASCII_delete_sum_for_two_strings.py
@@ -6,6 +6,21 @@ class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
         l1, l2 = len(s1), len(s2)
         dp = [[0] * (l2 + 1) for _ in range(l1 + 1)]
+        for i in range(l2):
+            dp[0][i + 1] = dp[0][i] + ord(s2[i])
+
+        for i in range(l1):
+            dp[i + 1][0] = dp[i][0] + ord(s1[i])
+            for j in range(l2):
+                if s1[i] == s2[j]:
+                    dp[i + 1][j + 1] = dp[i][j]
+                else:
+                    dp[i + 1][j + 1] = min(dp[i + 1][j] + ord(s2[j]), dp[i][j + 1] + ord(s1[i]))
+        return dp[l1][l2]
+
+    def minimumDeleteSum1(self, s1: str, s2: str) -> int:
+        l1, l2 = len(s1), len(s2)
+        dp = [[0] * (l2 + 1) for _ in range(l1 + 1)]
 
         for i in range(l1):
             for j in range(l2):
