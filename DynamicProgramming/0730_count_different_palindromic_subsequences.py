b6ffbadff9e6b5cd11372c3b7a983b7b2a6a468f
DancingOnAir <taotong1984@hotmail.com>
2020-12-21 22:06:33 +0800

    feat(DynamicProgramming): accepted dp solution for q0730

--- DynamicProgramming/0730_count_different_palindromic_subsequences.py
+++ DynamicProgramming/0730_count_different_palindromic_subsequences.py
@@ -1,24 +1,34 @@
 class Solution:
+    # https://leetcode.com/problems/count-different-palindromic-subsequences/discuss/109507/Java-96ms-DP-Solution-with-Detailed-Explanation
     def countPalindromicSubsequences(self, S: str) -> int:
         n = len(S)
         if n < 2:
             return n
 
-        def getPalindromicSubsequences(s: str):
-            pass
+        dp = [[1 if i == j else 0 for i in range(n)] for j in range(n)]
 
-        dp = [0] * (n + 1)
-        memo = list()
-        for i in range(n):
-            for j in range(i+1)[::-1]:
-                if S[j] == S[i] and getPalindromicSubsequences(S[j:i+1]) not in memo:
-                    dp[i + 1] += dp[j] + 1
-                    memo.append()
+        for distance in range(1, n):
+            for i in range(n - distance):
+                j = i + distance
+
+                if S[i] != S[j]:
+                    dp[i][j] = dp[i + 1][j] + dp[i][j - 1] - dp[i + 1][j - 1]
                 else:
-                    dp[i + 1]
-            #     for k in range():
-            #         dp[i+1] =
-        return
+                    left, right = i + 1, j - 1
+                    while left <= right and S[left] != S[i]:
+                        left += 1
+                    while left <= right and S[right] != S[j]:
+                        right -= 1
+
+                    dp[i][j] = 2 * dp[i + 1][j - 1]
+                    if left == right:
+                        dp[i][j] += 1
+                    elif left > right:
+                        dp[i][j] += 2
+                    else:
+                        dp[i][j] -= dp[left + 1][right - 1]
+
+        return dp[0][n - 1] % (10 ** 9 + 7)
 
 
 def test_count_palindromic_subsequences():
