2db49078e8e2d90332e705d6c27ef09453a36fd1
DancingOnAir <taotong1984@hotmail.com>
2020-12-22 11:53:56 +0800

    feat(DynamicProgramming): accepted 3d dp solution for q0730

--- DynamicProgramming/0730_count_different_palindromic_subsequences.py
+++ DynamicProgramming/0730_count_different_palindromic_subsequences.py
@@ -1,6 +1,39 @@
 class Solution:
-    # https://leetcode.com/problems/count-different-palindromic-subsequences/discuss/109507/Java-96ms-DP-Solution-with-Detailed-Explanation
     def countPalindromicSubsequences(self, S: str) -> int:
+        n = len(S)
+        MOD = 10 ** 9 + 7
+        dp = [[[0] * n for _ in range(n)] for _ in range(4)]
+
+        for i in range(n-1, -1, -1):
+            for j in range(i, n):
+                for k in range(4):
+                    c = chr(ord('a') + k)
+                    if i == j:
+                        if S[i] == c:
+                            dp[k][i][j] = 1
+                        else:
+                            dp[k][i][j] = 0
+                    else:
+                        if S[i] != c:
+                            dp[k][i][j] = dp[k][i+1][j]
+                        elif S[j] != c:
+                            dp[k][i][j] = dp[k][i][j-1]
+                        else:
+                            if j == i + 1:
+                                dp[k][i][j] = 2
+                            else:
+                                dp[k][i][j] = 2
+                                for m in range(4):
+                                    dp[k][i][j] += dp[m][i+1][j-1]
+                                    dp[k][i][j] %= MOD
+        res = 0
+        for k in range(4):
+            res += dp[k][0][n-1]
+            res %= MOD
+        return res
+
+    # https://leetcode.com/problems/count-different-palindromic-subsequences/discuss/109507/Java-96ms-DP-Solution-with-Detailed-Explanation
+    def countPalindromicSubsequences1(self, S: str) -> int:
         n = len(S)
         if n < 2:
             return n
