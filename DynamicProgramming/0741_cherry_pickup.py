a3cbe44b49844be52389161f43916a777be49b10
DancingOnAir <taotong1984@hotmail.com>
2020-12-29 00:24:02 +0800

    feat(DynamicProgramming): accepted bottom-top 2d dp solution

--- DynamicProgramming/0741_cherry_pickup.py
+++ DynamicProgramming/0741_cherry_pickup.py
@@ -4,6 +4,38 @@ from typing import List
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
         n = len(grid)
+        if n < 1:
+            return 0
+
+        m = (n << 1) - 1
+        dp = [[0] * n for _ in range(n)]
+        dp[0][0] = grid[0][0]
+
+        for k in range(1, m):
+            for i in range(n - 1, -1, -1):
+                for p in range(n - 1, -1, -1):
+                    j, q = k - i, k - p
+
+                    if j < 0 or j >= n or q < 0 or q >= n or grid[i][j] == -1 or grid[p][q] == -1:
+                        dp[i][p] = -1
+                        continue
+
+                    if i > 0:
+                        dp[i][p] = max(dp[i][p], dp[i - 1][p])
+                    if p > 0:
+                        dp[i][p] = max(dp[i][p], dp[i][p - 1])
+                    if i > 0 and p > 0:
+                        dp[i][p] = max(dp[i][p], dp[i - 1][p - 1])
+                    if dp[i][p] >= 0:
+                        dp[i][p] += grid[i][j]
+                        if p != i:
+                            dp[i][p] += grid[p][q]
+
+        return max(0, dp[-1][-1])
+
+    # bottom-up dp
+    def cherryPickup2(self, grid: List[List[int]]) -> int:
+        n = len(grid)
         dp = [[[-1] * n for _ in range(n)] for _ in range(n)]
         dp[0][0][0] = grid[0][0]
 
