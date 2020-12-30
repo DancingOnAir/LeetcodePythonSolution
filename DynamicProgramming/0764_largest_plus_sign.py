7c9323dcee6af689b5d57d17664d6b6e2cb6cad3
DancingOnAir <taotong1984@hotmail.com>
2020-12-30 22:39:11 +0800

    feat(DynamicProgramming): acceted dp solution for q0764

--- DynamicProgramming/0764_largest_plus_sign.py
+++ DynamicProgramming/0764_largest_plus_sign.py
@@ -2,12 +2,40 @@ from typing import List
 
 
 class Solution:
+    def orderOfLargestPlusSign(self, N: int, mines: List[List[int]]) -> int:
+        dp = [[N] * N for _ in range(N)]
+
+        for i, j in mines:
+            dp[i][j] = 0
+
+        for i in range(N):
+            l, r, u, d = 0, 0, 0, 0
+            for j, k in zip(range(N), reversed(range(N))):
+                l = l + 1 if dp[i][j] else 0
+                dp[i][j] = min(dp[i][j], l)
+
+                r = r + 1 if dp[i][k] else 0
+                dp[i][k] = min(dp[i][k], r)
+
+                u = u + 1 if dp[j][i] else 0
+                dp[j][i] = min(dp[j][i], u)
+
+                d = d + 1 if dp[k][i] else 0
+                dp[k][i] = min(dp[k][i], d)
+
+        res = 0
+        for i in range(N):
+            for j in range(N):
+                res = max(res, dp[i][j])
+        return res
+
     def validate_one(self, N, mines, x, y):
         if x < 0 or x >= N or y < 0 or y >= N or [x, y] in mines:
             return False
         return True
 
-    def orderOfLargestPlusSign(self, N: int, mines: List[List[int]]) -> int:
+    # brute force solution but TLE
+    def orderOfLargestPlusSign1(self, N: int, mines: List[List[int]]) -> int:
         res = 0
         for i in range(N):
             for j in range(N):
