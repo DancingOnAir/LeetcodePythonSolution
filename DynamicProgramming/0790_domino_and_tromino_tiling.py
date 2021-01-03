c9dd2635f911e4eb3307b856b8364a8b9670527c
DancingOnAir <taotong1984@hotmail.com>
2021-01-03 23:55:13 +0800

    feat(DynamicProgramming): added corner cases for 2d dp solution for q0790

--- DynamicProgramming/0790_domino_and_tromino_tiling.py
+++ DynamicProgramming/0790_domino_and_tromino_tiling.py
@@ -3,6 +3,8 @@ from typing import List
 
 class Solution:
     def numTilings(self, N: int) -> int:
+        if N == 1:
+            return 1
         MOD = 10 ** 9 + 7
         # dp[i][j] means at the i-th position, the status is j
         dp = [[0] * 3 for _ in range(N)]
@@ -12,9 +14,9 @@ class Solution:
         dp[1][2] = 1
 
         for i in range(2, N):
-            dp[i][0] = dp[i - 1][0] + dp[i - 2][0] + dp[i - 1][1] + dp[i - 1][2]
-            dp[i][1] = dp[i - 2][0] + dp[i - 1][2]
-            dp[i][2] = dp[i - 2][0] + dp[i - 1][1]
+            dp[i][0] = (dp[i - 1][0] + dp[i - 2][0] + dp[i - 1][1] + dp[i - 1][2]) % MOD
+            dp[i][1] = (dp[i - 2][0] + dp[i - 1][2]) % MOD
+            dp[i][2] = (dp[i - 2][0] + dp[i - 1][1]) % MOD
         return dp[N - 1][0]
 
 
