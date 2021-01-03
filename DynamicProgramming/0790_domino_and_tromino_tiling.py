37efe56ed9f409c4cd537a154a7a746d87848128
DancingOnAir <taotong1984@hotmail.com>
2021-01-03 22:49:04 +0800

    feat(DynamicProgramming): init q0790 & accepted 2d dp solution for q0790

---
+++ DynamicProgramming/0790_domino_and_tromino_tiling.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def numTilings(self, N: int) -> int:
+        MOD = 10 ** 9 + 7
+        # dp[i][j] means at the i-th position, the status is j
+        dp = [[0] * 3 for _ in range(N)]
+        dp[0][0] = 1
+        dp[1][0] = 2
+        dp[1][1] = 1
+        dp[1][2] = 1
+
+        for i in range(2, N):
+            dp[i][0] = dp[i - 1][0] + dp[i - 2][0] + dp[i - 1][1] + dp[i - 1][2]
+            dp[i][1] = dp[i - 2][0] + dp[i - 1][2]
+            dp[i][2] = dp[i - 2][0] + dp[i - 1][1]
+        return dp[N - 1][0]
+
+
+def test_num_tilings():
+    solution = Solution()
+    assert solution.numTilings(3) == 5, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_num_tilings()
