0fc0bd836440dbe9327f43c7c8d079935d3fbfca
DancingOnAir <taotong1984@hotmail.com>
2021-01-08 07:46:35 +0800

    feat(DynamicProgramming): accepted double dp solution for q0801

--- DynamicProgramming/0801_minimum_swaps_to_make_sequences_increasing.py
+++ DynamicProgramming/0801_minimum_swaps_to_make_sequences_increasing.py
@@ -2,19 +2,45 @@ from typing import List
 
 
 class Solution:
+    # def minSwap(self, A: List[int], B: List[int]) -> int:
+    #     n = len(A)
+    #     if n < 2:
+    #         return 0
+    #
+    #     swap, not_swap = 1, 0
+    #
+    #     for i in range(1, n):
+    #         not_swap_temp, swap_temp = n, n
+    #         if A[i - 1] < A[i] and B[i - 1] < B[i]:
+    #             swap_temp = swap + 1
+    #             not_swap_temp = not_swap
+    #
+    #         if A[i - 1] < B[i] and B[i - 1] < A[i]:
+    #             swap_temp = min(swap_temp, not_swap + 1)
+    #             not_swap_temp = min(not_swap_temp, swap)
+    #
+    #         swap, not_swap = swap_temp, not_swap_temp
+    #     return min(swap, not_swap)
+
     def minSwap(self, A: List[int], B: List[int]) -> int:
         n = len(A)
         if n < 2:
             return 0
 
-        dp = [0] * n
+        swap = [1] + [0] * (n - 1)
+        not_swap = [0] * n
         for i in range(1, n):
-            if A[i] > A[i - 1] and B[i] > B[i - 1]:
-                dp[i] = dp[i - 1]
-            elif A[i - 1] >= A[i] > B[i - 1] or B[i - 1] >= B[i] > A[i - 1]:
-                dp[i] = dp[i - 1] + 1
-                A[i], B[i] = B[i], A[i]
-        return dp[n - 1]
+            if (A[i - 1] < A[i] and B[i - 1] < B[i]) and (A[i - 1] < B[i] and B[i - 1] < A[i]):
+                swap[i] = min(swap[i - 1], not_swap[i - 1]) + 1
+                not_swap[i] = min(swap[i - 1], not_swap[i - 1])
+            elif A[i - 1] < A[i] and B[i - 1] < B[i]:
+                swap[i] = swap[i - 1] + 1
+                not_swap[i] = not_swap[i - 1]
+            else:
+                swap[i] = not_swap[i - 1] + 1
+                not_swap[i] = swap[i - 1]
+
+        return min(swap[n - 1], not_swap[n - 1])
 
 
 def test_min_swap():
@@ -31,6 +57,10 @@ def test_min_swap():
     A3 = [3, 3, 8, 9, 10]
     B3 = [1, 7, 4, 6, 8]
     assert solution.minSwap(A3, B3) == 1, 'wrong result'
+
+    A4 = [0, 4, 4, 5, 9]
+    B4 = [0, 1, 6, 8, 10]
+    assert solution.minSwap(A4, B4) == 1, 'wrong result'
 
 
 if __name__ == '__main__':
