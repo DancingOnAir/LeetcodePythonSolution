e5816cca26c4679bf96292c7dfc32831b162f476
DancingOnAir <taotong1984@hotmail.com>
2021-01-10 20:25:22 +0800

    feat(DynamicProgramming): accepted 2d dp solution for q0808

--- DynamicProgramming/0808_soup_servings.py
+++ DynamicProgramming/0808_soup_servings.py
@@ -3,21 +3,25 @@ from typing import List
 
 class Solution:
     def soupServings(self, N: int) -> float:
-        dp = [0.0] * N
-        dp[0] = 0.25 * (0.5 + 0.5 + 0.5 + 0.5)
-        dp[1] = 0.25 * (1 + 0.5 + 0.5 + 0.5)
-        dp[25] = 0.25 * (1 + 0 + 0.5 + 0.5)
-        dp[50] = 0.25 * (1 + 1 + 0.5 + 0)
-        dp[75] = 0.25 * (1 + 1 + 0 + 0)
-        dp[100] = 0.25 * (1 + 0 + 0 + 0)
-        dp[101] = 0.25 * (0 + 0 + 0 + 0)
+        d, m = divmod(N, 25)
+        N = d + (m > 0)
+        # every time for A = 0.25 * (4 + 3 + 2 + 1) = 2.5; for B = 0.25 * (1 + 2 + 3) = 1.5
+        # A decreases 1 more than B each time.
+        if N >= 500:
+            return 1
 
-        for i in range(1, N):
-            if i // 25 == (i - 1) // 25:
-                dp[i] = dp[i - 1]
-            else:
+        memo = dict()
 
-        return dp[-1]
+        def dp(x, y):
+            if (x, y) not in memo:
+                if x <= 0 or y <= 0:
+                    ans = 0.5 if x <= 0 and y <= 0 else 1.0 if x <= 0 else 0.0
+                else:
+                    ans = 0.25 * (dp(x - 4, y) + dp(x - 3, y - 1) + dp(x - 2, y - 2) + dp(x - 1, y - 3))
+                memo[x, y] = ans
+
+            return memo[x, y]
+        return dp(N, N)
 
 
 def test_soup_servings():
