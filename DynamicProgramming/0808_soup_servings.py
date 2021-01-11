f5ebe31e56a75b4e6de68b27bf5056a4f15021ae
DancingOnAir <taotong1984@hotmail.com>
2021-01-11 08:20:10 +0800

    feat(DynamicProgramming): accepted pythonic 2d dp solution for q0808

--- DynamicProgramming/0808_soup_servings.py
+++ DynamicProgramming/0808_soup_servings.py
@@ -1,8 +1,28 @@
-from typing import List
+from math import ceil
 
 
 class Solution:
     def soupServings(self, N: int) -> float:
+        memo = dict()
+
+        if N > 4800:
+            return 1.0
+
+        def dp(x, y):
+            if (x, y) in memo:
+                return memo[(x, y)]
+            if x <= 0 and y <= 0:
+                return 0.5
+            if x <= 0:
+                return 1.0
+            if y <= 0:
+                return 0.0
+            memo[(x, y)] = 0.25 * (dp(x-4, y) + dp(x-3, y-1) + dp(x-2, y-2) + dp(x-1, y-3))
+            return memo[(x, y)]
+        N = ceil(N / 25.0)
+        return dp(N, N)
+
+    def soupServings1(self, N: int) -> float:
         d, m = divmod(N, 25)
         N = d + (m > 0)
         # every time for A = 0.25 * (4 + 3 + 2 + 1) = 2.5; for B = 0.25 * (1 + 2 + 3) = 1.5
