a25e709f72f2a03b04bf4c9add95d1d8d322d27d
DancingOnAir <taotong1984@hotmail.com>
2021-01-15 11:02:39 +0800

    feat(DynamicProgramming): tried a TLE solution for q0837

--- DynamicProgramming/0837_new_21_game.py
+++ DynamicProgramming/0837_new_21_game.py
@@ -20,8 +20,8 @@ class Solution:
 
 def test_new_21_game():
     solution = Solution()
-    # assert solution.new21Game(10, 1, 10) == 1.00000, 'wrong result'
-    # assert solution.new21Game(6, 1, 10) == 0.60000, 'wrong result'
+    assert solution.new21Game(10, 1, 10) == 1.00000, 'wrong result'
+    assert solution.new21Game(6, 1, 10) == 0.60000, 'wrong result'
     assert solution.new21Game(21, 17, 10) == 0.73278, 'wrong result'
 
 
