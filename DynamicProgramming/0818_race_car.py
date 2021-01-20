f28ae9c4c5a1c1afcbea00618ba535cda1b71c04
DancingOnAir <taotong1984@hotmail.com>
2021-01-20 23:01:21 +0800

    feat(DynamicProgamming): init q0818

---
+++ DynamicProgramming/0818_race_car.py
@@ -0,0 +1,14 @@
+class Solution:
+    def racecar(self, target: int) -> int:
+
+        pass
+
+
+def test_race_car():
+    solution = Solution()
+    assert solution.racecar(3) == 2, 'wrong result'
+    assert solution.racecar(6) == 5, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_race_car()
