4f8d28ff125e91fbee997439fa5994e386f2af9f
DancingOnAir <taotong1984@hotmail.com>
2021-01-22 00:41:51 +0800

    feat(DynamicProgamming): accepted dp solution for q0818

--- DynamicProgramming/0818_race_car.py
+++ DynamicProgramming/0818_race_car.py
@@ -3,7 +3,44 @@ from collections import OrderedDict
 
 
 class Solution:
+    def __init__(self):
+        self.memo = {0: 0}
+
+    # bottom-up dp
+    # https://leetcode.com/problems/race-car/discuss/227415/Figures-to-make-the-DP-solution-more-straightforward
     def racecar(self, target: int) -> int:
+        dp = [0] + [0x3f3f3f3f] * target
+        for i in range(1, target+1):
+            m, j = 1, 1
+            while j < i:
+                p, q = 0, 0
+                while p < j:
+                    dp[i] = min(dp[i], m + 1 + q + 1 + dp[i - j + p])
+                    q += 1
+                    p = (1 << q) - 1
+                m += 1
+                j = (1 << m) - 1
+
+            dp[i] = min(dp[i], m + (0 if i == j else 1 + dp[j - i]))
+        return dp[target]
+
+    # bfs
+    def racecar2(self, target: int) -> int:
+        if target in self.memo:
+            return self.memo[target]
+
+        n = target.bit_length()
+        if 2 ** n - 1 == target:
+            self.memo[target] = n
+        else:
+            self.memo[target] = self.racecar(2**n - 1 - target) + n + 1
+            for m in range(n - 1):
+                self.memo[target] = min(self.memo[target], self.racecar(target - 2**(n - 1) + 2**m) + n + m + 1)
+        return self.memo[target]
+
+    # bfs solution but failed
+    # failure testing case, if the input 5, we can reach with only 7 steps: AARARAA
+    def racecar1(self, target: int) -> int:
         if not target:
             return 0
 
@@ -35,7 +72,7 @@ class Solution:
 
 def test_race_car():
     solution = Solution()
-    # assert solution.racecar(3) == 2, 'wrong result'
+    assert solution.racecar(3) == 2, 'wrong result'
     assert solution.racecar(6) == 5, 'wrong result'
 
 
