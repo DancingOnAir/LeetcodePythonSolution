dcae310b2ad04a73ad56eedf6a1b29666a01d561
DancingOnAir <taotong1984@hotmail.com>
2021-01-21 16:40:48 +0800

    feat(DynamicProgamming): tried dps solution but failed for q0818

--- DynamicProgramming/0818_race_car.py
+++ DynamicProgramming/0818_race_car.py
@@ -1,12 +1,41 @@
+from bisect import bisect_left
+from collections import OrderedDict
+
+
 class Solution:
     def racecar(self, target: int) -> int:
+        if not target:
+            return 0
 
-        pass
+        memo = dict()
+        def min_steps(target):
+            if target in memo:
+                return memo[target]
+            arr = sorted(memo.keys())
+            pos = bisect_left(arr, target)
+
+            positive_diff = target - arr[pos - 1]
+            negative_diff = arr[pos] - target
+
+            step = min(memo[arr[pos - 1]] + min_steps(positive_diff) + 2, memo[arr[pos]] + min_steps(negative_diff) + 1)
+            memo[target] = step
+            return step
+
+        total, i, step = 0, 1, 0
+        while total < target:
+            total += i
+            i *= 2
+            step += 1
+            memo[total] = step
+
+        if target == total:
+            return step
+        return min(step + min_steps(total - target) + 1, step - 1 + min_steps(target - (total - i // 2)) + 2)
 
 
 def test_race_car():
     solution = Solution()
-    assert solution.racecar(3) == 2, 'wrong result'
+    # assert solution.racecar(3) == 2, 'wrong result'
     assert solution.racecar(6) == 5, 'wrong result'
 
 
