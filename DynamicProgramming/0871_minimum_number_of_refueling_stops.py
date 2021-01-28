2582832438b6e0a650a4bb96e3ef4f5cd2a6eca1
DancingOnAir <taotong1984@hotmail.com>
2021-01-28 23:34:23 +0800

    feat(DynamicProgramming): accepted 1d dp solution for q0871

--- DynamicProgramming/0871_minimum_number_of_refueling_stops.py
+++ DynamicProgramming/0871_minimum_number_of_refueling_stops.py
@@ -2,8 +2,19 @@ from typing import List
 
 
 class Solution:
+    # dp[i] represents the farthest distance when adding i-th gas
     def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
-        pass
+        dp = [startFuel] + [0] * len(stations)
+        for i, (location, capacity) in enumerate(stations):
+            for j in range(i, -1, -1):
+                if dp[j] >= location:
+                    dp[j + 1] = max(dp[j + 1], dp[j] + capacity)
+
+        for i, dist in enumerate(dp):
+            if dist >= target:
+                return i
+
+        return -1
 
 
 def test_min_refuel_stops():
