62650683651f7fbe197605896da349493664b150
DancingOnAir <taotong1984@hotmail.com>
2021-01-29 11:34:13 +0800

    feat(DynamicProgramming): accepted priority queue solution for q0871

--- DynamicProgramming/0871_minimum_number_of_refueling_stops.py
+++ DynamicProgramming/0871_minimum_number_of_refueling_stops.py
@@ -1,9 +1,29 @@
 from typing import List
+from queue import PriorityQueue
+import heapq
 
 
 class Solution:
-    # dp[i] represents the farthest distance when adding i-th gas
     def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
+        pq = list()
+        stations.append([target, float('inf')])
+
+        pre = 0
+        res = 0
+        for location, capacity in stations:
+            startFuel -= location - pre
+            while pq and startFuel < 0:
+                startFuel += -heapq.heappop(pq)
+                res += 1
+            if startFuel < 0:
+                return -1
+
+            heapq.heappush(pq, -capacity)
+            pre = location
+        return res
+
+    # dp[i] represents the farthest distance when adding i-th gas
+    def minRefuelStops1(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
         dp = [startFuel] + [0] * len(stations)
         for i, (location, capacity) in enumerate(stations):
             for j in range(i, -1, -1):
@@ -13,7 +33,6 @@ class Solution:
         for i, dist in enumerate(dp):
             if dist >= target:
                 return i
-
         return -1
 
 
