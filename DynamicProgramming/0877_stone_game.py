b4250893fdb702a9ffb46e74ca086fcf26a8615c
DancingOnAir <taotong1984@hotmail.com>
2021-01-31 00:35:33 +0800

    feat(DynamicProgramming): accepted 1d dp solution for q0877

--- DynamicProgramming/0877_stone_game.py
+++ DynamicProgramming/0877_stone_game.py
@@ -2,8 +2,16 @@ from typing import List
 
 
 class Solution:
-    # 2d dp
     def stoneGame(self, piles: List[int]) -> bool:
+        n = len(piles)
+        dp = piles[:]
+        for d in range(1, n):
+            for i in range(n - d):
+                dp[i] = max(piles[i] - dp[i + 1], piles[i + d] - dp[i])
+        return dp[0]
+
+    # 2d dp
+    def stoneGame2(self, piles: List[int]) -> bool:
         n = len(piles)
         dp = [[0] * n for _ in range(n)]
         for i in range(n):
