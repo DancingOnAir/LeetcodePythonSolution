34db97a5d1555485f5e4d21a351c615a3e3db3ce
DancingOnAir <taotong1984@hotmail.com>
2021-01-31 00:07:18 +0800

    feat(DynamicProgramming): accepted 2d dp solution for q0877

--- DynamicProgramming/0877_stone_game.py
+++ DynamicProgramming/0877_stone_game.py
@@ -2,11 +2,25 @@ from typing import List
 
 
 class Solution:
+    # 2d dp
+    def stoneGame(self, piles: List[int]) -> bool:
+        n = len(piles)
+        dp = [[0] * n for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = piles[i]
+
+        for d in range(1, n):
+            for i in range(n - d):
+                dp[i][i + d] = max(piles[i] - dp[i + 1][i + d], piles[i + d] - dp[i][i + d - 1])
+
+        return dp[0][-1] > 0
+
+
     # https://leetcode.com/problems/stone-game/discuss/154610/DP-or-Just-return-true
     # Alex is first to pick pile.
     # piles.length is even, and this lead to an interesting fact:
     # Alex can always pick odd piles or always pick even piles!
-    def stoneGame(self, piles: List[int]) -> bool:
+    def stoneGame1(self, piles: List[int]) -> bool:
         return True
 
 
