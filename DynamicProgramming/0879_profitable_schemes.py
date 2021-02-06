0d328b8666f963a18e26cac94a972c03eed70740
DancingOnAir <taotong1984@hotmail.com>
2021-02-06 22:25:26 +0800

    feat(DynamicProgramming): accepted 3d dp solution for q0879

--- DynamicProgramming/0879_profitable_schemes.py
+++ DynamicProgramming/0879_profitable_schemes.py
@@ -2,8 +2,42 @@ from typing import List
 
 
 class Solution:
+    # dp[i][j][k] represents the total schemes of 0-i-1 crimes, j memebers and at least k profit.
+    # dp[i][j] represents total schemes of minimum profit is i and needs j members.
     def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
-        pass
+        MOD = 10 ** 9 + 7
+        l = len(group)
+
+        dp = [[[0] * (minProfit + 1) for _ in range(n + 1)] for _ in range(l + 1)]
+        for i in range(l + 1):
+            for j in range(n + 1):
+                dp[i][j][0] = 1
+
+        for i in range(1, l + 1):
+            for j in range(1, n + 1):
+                for k in range(minProfit + 1):
+                    dp[i][j][k] = dp[i - 1][j][k]
+                    if j >= group[i - 1]:
+                        dp[i][j][k] += dp[i - 1][j - group[i - 1]][max(k - profit[i - 1], 0)]
+
+                    dp[i][j][k] %= MOD
+        return dp[l][n][minProfit]
+
+        # dp = [[0] * (n + 1) for _ in range(minProfit + 1)]
+        # dp[0][0] = 1
+        #
+        # for g, p in zip(group, profit):
+        #     cur_dp = [row[:] for row in dp]
+        #
+        #     for i in range(minProfit + 1):
+        #         cur_p = min(i + p, minProfit)
+        #         for j in range(n - g + 1):
+        #             cur_g = g + j
+        #             cur_dp[cur_p][cur_g] += dp[i][j]
+        #             cur_dp[cur_p][cur_g] %= MOD
+        #     dp = cur_dp
+        #
+        # return sum(dp[-1]) % MOD
 
 
 def test_profitable_schemes():
