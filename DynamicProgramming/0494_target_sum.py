5384dad03c9b8c663c3a6d0fca5422dee858e74c
DancingOnAir <taotong1984@hotmail.com>
2021-02-13 23:37:35 +0800

    feat(DynamicProgramming): accepted knapsack 1d dp solution for q0494

--- DynamicProgramming/0494_target_sum.py
+++ DynamicProgramming/0494_target_sum.py
@@ -2,21 +2,22 @@ from typing import List
 
 
 class Solution:
+    # dp 1-0 knapsack problem, equals find a target which is (sum(nums) - S) / 2
     def findTargetSumWays(self, nums: List[int], S: int) -> int:
         if not nums:
             return 0
 
-        total = sum(nums)
-        dp = [1] + [0] * total
+        target = sum(nums) - S
+        if target < 0 or (target & 1):
+            return 0
+
+        target >>= 1
+        dp = [1] + [0] * target
 
         for num in nums:
-            for i in range(total, 0, -1):
-                if i >= num:
-                    dp[i] += dp[i - num]
-                if i + num <= total:
-                    dp[i] += dp[i + num]
-        return dp[S]
-        pass
+            for i in range(target, num - 1, -1):
+                dp[i] += dp[i - num]
+        return dp[target]
 
 
 def test_find_target_sum_ways():
