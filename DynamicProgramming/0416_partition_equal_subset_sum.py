9b98372c449179122049775c2af769520026cebb
DancingOnAir <taotong1984@hotmail.com>
2021-02-16 11:57:12 +0800

    feat(DynamicProgramming): accepted 1d dp solution for 0-1 knapsack problem for q0416

--- DynamicProgramming/0416_partition_equal_subset_sum.py
+++ DynamicProgramming/0416_partition_equal_subset_sum.py
@@ -2,8 +2,22 @@ from typing import List
 
 
 class Solution:
-    # dp
+    # do solution for 0-1 knapsack problem
     def canPartition(self, nums: List[int]) -> bool:
+        target = sum(nums)
+        n = len(nums)
+        if target & 1:
+            return False
+
+        target >>= 1
+        dp = [True] + [False] * target
+        for x in nums:
+            for i in range(target, x - 1, -1):
+                dp[i] |= dp[i - x]
+        return dp[-1]
+
+    # dp
+    def canPartition2(self, nums: List[int]) -> bool:
         target, n = sum(nums), len(nums)
         if target & 1:
             return False
@@ -18,7 +32,7 @@ class Solution:
         return False
 
     # brute force dict
-    def canPartition(self, nums: List[int]) -> bool:
+    def canPartition1(self, nums: List[int]) -> bool:
         if sum(nums) & 1 == 0:
             mid = sum(nums) >> 1
             possible_sums = {0}
