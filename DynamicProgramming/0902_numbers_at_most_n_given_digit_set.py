06f2ad003b36411baaa80a59491f30a3ecbadede
DancingOnAir <taotong1984@hotmail.com>
2021-02-20 00:06:45 +0800

    feat(DynamicProgramming): accepted dp solution for q0902

--- DynamicProgramming/0902_numbers_at_most_n_given_digit_set.py
+++ DynamicProgramming/0902_numbers_at_most_n_given_digit_set.py
@@ -3,7 +3,26 @@ from bisect import bisect_right, bisect_left
 
 
 class Solution:
+    # https://leetcode.com/problems/numbers-at-most-n-given-digit-set/discuss/559624/Python-Digit-DP-(Pattern-For-Similar-Questions)
+    # Variable idx is the current position in N,
+    # prefix tells if the current number is the prefix of N,
+    # bigger tells if the current number will be bigger than N when we reach full length.
     def atMostNGivenDigitSet(self, digits: List[str], n: int) -> int:
+        N = list(map(int, str(n)))
+        D = list(map(int, digits))
+
+        def dp(idx, prefix, bigger):
+            if idx == len(N):
+                return not bigger
+            if not prefix and not bigger:
+                return 1 + len(D) * dp(idx + 1, False, False)
+
+            return 1 + sum(dp(idx + 1, prefix and d == N[idx], bigger or (prefix and d > N[idx])) for d in D)
+
+        return dp(0, True, False) - 1
+
+    # math solution
+    def atMostNGivenDigitSet1(self, digits: List[str], n: int) -> int:
         N = str(n)
         n = len(N)
         res = sum(len(digits) ** i for i in range(1, n))
