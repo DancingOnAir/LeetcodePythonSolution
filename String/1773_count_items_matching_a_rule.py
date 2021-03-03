f620cdcda87775b6dc4354d2e0926c286277ad18
DancingOnAir <taotong1984@hotmail.com>
2021-03-03 12:01:46 +0800

    feat(String): accepted more pythonic solution for q1773

--- String/1773_count_items_matching_a_rule.py
+++ String/1773_count_items_matching_a_rule.py
@@ -4,6 +4,9 @@ from collections import Counter
 
 class Solution:
     def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        return sum((ruleKey, ruleValue) in zip(('type', 'color', 'name'), i) for i in items)
+
+    def countMatches2(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
         return sum((ruleKey, ruleValue) in (('type', t), ('color', c), ('name', n)) for t, c, n in items)
 
     def countMatches1(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
