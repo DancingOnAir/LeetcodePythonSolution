126b953bc23b680841c10238d3391ab7b59c2cf4
DancingOnAir <taotong1984@hotmail.com>
2021-03-04 13:05:17 +0800

    feat(String):accepted pythonic solution for q1768

--- String/1768_merge_strings_alternately.py
+++ String/1768_merge_strings_alternately.py
@@ -1,5 +1,13 @@
 class Solution:
+
     def mergeAlternately(self, word1: str, word2: str) -> str:
+        res = ''
+        for i in range(min(len(word1), len(word2))):
+            res += word1[i] + word2[i]
+
+        return res + word1[i+1:] + word2[i+1:]
+
+    def mergeAlternately1(self, word1: str, word2: str) -> str:
         l1, l2 = len(word1), len(word2)
         min_l = min(l1, l2)
         res = [''] * (min_l * 2)
