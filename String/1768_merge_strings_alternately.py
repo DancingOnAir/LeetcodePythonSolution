11e3f9a2b8ffa11136c28ffa3cf57dc56756befe
DancingOnAir <taotong1984@hotmail.com>
2021-03-04 13:40:05 +0800

    feat(String):accepted regular solution for q1768

--- String/1768_merge_strings_alternately.py
+++ String/1768_merge_strings_alternately.py
@@ -3,6 +3,18 @@ from itertools import zip_longest
 
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
+        i, j = 0, 0
+        res = ''
+        while i < len(word1) or j < len(word2):
+            if i < len(word1):
+                res += word1[i]
+                i += 1
+            if j < len(word2):
+                res += word2[j]
+                j += 1
+        return res
+
+    def mergeAlternately3(self, word1: str, word2: str) -> str:
         return ''.join(a + b for a, b in zip_longest(word1, word2, fillvalue=''))
 
     def mergeAlternately2(self, word1: str, word2: str) -> str:
