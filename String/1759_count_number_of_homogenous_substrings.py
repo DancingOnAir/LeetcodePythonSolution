be5c0d027e2218c306d06abe1f7cfc555d93b413
DancingOnAir <taotong1984@hotmail.com>
2021-03-05 10:28:07 +0800

    feat(String): accepted solution for q1759

--- String/1759_count_number_of_homogenous_substrings.py
+++ String/1759_count_number_of_homogenous_substrings.py
@@ -4,11 +4,21 @@ from itertools import groupby
 class Solution:
     # https://stackoverflow.com/questions/773/how-do-i-use-itertools-groupby
     # itertools.groupby
-    def countHomogenous(self, s: str) -> int:
+    def countHomogenous2(self, s: str) -> int:
         res = 0
         for c, s in groupby(s):
             n = len(list(s))
             res += n * (n + 1) // 2
+        return res % (10 ** 9 + 7)
+
+    def countHomogenous(self, s: str) -> int:
+        res, cnt = 0, 0
+        for i, val in enumerate(s):
+            if not i or s[i - 1] == val:
+                cnt += 1
+            else:
+                cnt = 1
+            res += cnt
         return res % (10 ** 9 + 7)
 
     def countHomogenous1(self, s: str) -> int:
