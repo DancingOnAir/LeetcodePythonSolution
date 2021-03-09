a12bf4e81f0c84e2e55d17ac031e4a3b20b4f470
DancingOnAir <taotong1984@hotmail.com>
2021-03-10 00:13:51 +0800

    feat(String): accepted dp solution for q1745

--- String/1745_palindrome_partitioning_iv.py
+++ String/1745_palindrome_partitioning_iv.py
@@ -1,6 +1,28 @@
 class Solution:
-    # brute force solution
+    # dp
     def checkPartitioning(self, s: str) -> bool:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n):
+                l = j - i + 1
+                if l == 1:
+                    dp[i][j] = True
+                elif l == 2:
+                    dp[i][j] = (s[i] == s[j])
+                else:
+                    dp[i][j] = (s[i] == s[j]) and dp[i + 1][j - 1]
+
+        for i in range(n):
+            for j in range(i + 2, n):
+                left, mid, right = dp[0][i], dp[i + 1][j - 1], dp[j][n - 1]
+                if left and mid and right:
+                    return True
+        return False
+
+    # brute force solution
+    def checkPartitioning1(self, s: str) -> bool:
         n = len(s)
         # split string s into 3 parts: s[:left], s[left, right], s[:right]
         for i in range(1, n - 1):
