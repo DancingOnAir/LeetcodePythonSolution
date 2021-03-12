e23f89c400e7787106e623b28fd6d7146e84d566
DancingOnAir <taotong1984@hotmail.com>
2021-03-12 11:24:46 +0800

    feat(String): accepted pythonic solution for q1736

--- String/1736_latest_time_by_replacing_hidden_digits.py
+++ String/1736_latest_time_by_replacing_hidden_digits.py
@@ -3,6 +3,22 @@ from re import match
 
 class Solution:
     def maximumTime(self, time: str) -> str:
+        time = list(time)
+        for i, val in enumerate(time):
+            if val == '?':
+                if i == 0:
+                    time[i] = '2' if time[i + 1] in '?0123' else '1'
+                elif i == 1:
+                    time[i] = '3' if time[0] == '2' else '9'
+                elif i == 3:
+                    time[i] = '5'
+                else:
+                    time[i] = '9'
+
+        return ''.join(time)
+
+    # regex
+    def maximumTime2(self, time: str) -> str:
         for h in range(24)[::-1]:
             for m in range(60)[::-1]:
                 t = f'{h:02}:{m:02}'
