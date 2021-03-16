b70cedbfb81fa27dcfe905a083c5e3a6bff0ffd0
DancingOnAir <taotong1984@hotmail.com>
2021-03-17 00:22:22 +0800

    feat(String): accepted yield solution for q1662

--- String/1662_check_if_two_string_arrays_are_equivalent.py
+++ String/1662_check_if_two_string_arrays_are_equivalent.py
@@ -3,6 +3,20 @@ from typing import List
 
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        def gen(word: List[str]):
+            for s in word:
+                for c in s:
+                    yield c
+            # ensure False when len(word1) != len(word2)
+            yield None
+
+        for c1, c2 in zip(gen(word1), gen(word2)):
+            if c1 != c2:
+                return False
+        return True
+
+    # O(m + n)
+    def arrayStringsAreEqual1(self, word1: List[str], word2: List[str]) -> bool:
         return ''.join(word1) == ''.join(word2)
 
 
