3ac69dcd20661ed36b753ad6620ee1a291a611df
DancingOnAir <taotong1984@hotmail.com>
2021-03-18 09:34:52 +0800

    feat(String): accepted more pythonic solution for q1790

--- String/1790_check_if_one_string_swap_can_make_strings_equal.py
+++ String/1790_check_if_one_string_swap_can_make_strings_equal.py
@@ -1,5 +1,10 @@
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        diff = [[x, y] for x, y in zip(s1, s2) if x != y]
+
+        return not len(diff) or (len(diff) == 2 and diff[0] == diff[1][::-1])
+
+    def areAlmostEqual2(self, s1: str, s2: str) -> bool:
         if sorted(s1) != sorted(s2):
             return False
 
