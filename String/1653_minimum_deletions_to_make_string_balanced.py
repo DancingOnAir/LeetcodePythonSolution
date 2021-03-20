df8e37b62f0855a4c5ad7b598b705570944bdcb6
DancingOnAir <taotong1984@hotmail.com>
2021-03-20 11:36:32 +0800

    feat(String): accepted dp solution for q1653

--- String/1653_minimum_deletions_to_make_string_balanced.py
+++ String/1653_minimum_deletions_to_make_string_balanced.py
@@ -1,8 +1,18 @@
 class Solution:
-
+    # dp
+    def minimumDeletions(self, s: str) -> int:
+        cnt_b = 0
+        dp = [0]
+        for c in s:
+            if c == 'b':
+                cnt_b += 1
+                dp.append(dp[-1])
+            else:
+                dp.append(min(cnt_b, dp[-1] + 1))
+        return dp[-1]
 
     # two passes with space O(1)
-    def minimumDeletions(self, s: str) -> int:
+    def minimumDeletions2(self, s: str) -> int:
         cnt_a, cnt_b, res = s.count('a'), 0, len(s)
         for c in s:
             if c == 'b':
