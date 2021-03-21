e8d15e5e92152588f0346982a44e7d57f5eccf5a
DancingOnAir <taotong1984@hotmail.com>
2021-03-21 09:00:49 +0800

    feat(String): init q1624 & accepted solution for q1624

---
+++ String/1624_largest_substring_between_two_equal_characters.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxLengthBetweenEqualCharacters(self, s: str) -> int:
+        memo = dict()
+        res = -1
+        for i, val in enumerate(s):
+            if val not in memo:
+                memo[val] = i
+            else:
+                res = max(res, i - memo[val] - 1)
+        return res
+
+
+def test_max_length_between_equal_characters():
+    solution = Solution()
+    assert solution.maxLengthBetweenEqualCharacters('aa') == 0, 'wrong result'
+    assert solution.maxLengthBetweenEqualCharacters('abca') == 2, 'wrong result'
+    assert solution.maxLengthBetweenEqualCharacters('cbzxy') == -1, 'wrong result'
+    assert solution.maxLengthBetweenEqualCharacters('cabbac') == 4, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_max_length_between_equal_characters()
