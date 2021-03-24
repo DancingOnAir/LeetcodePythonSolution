57a8a5775deba5c1de06a080c82b58099325e6a6
DancingOnAir <taotong1984@hotmail.com>
2021-03-24 09:46:22 +0800

    feat(String): init q1796 & accepted solution for q1796

---
+++ String/1796_second_largest_digit_in_a_string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def secondHighest(self, s: str) -> int:
+        first, second = -1, -1
+        for c in s:
+            if c.isdigit():
+                if first < int(c):
+                    first, second = int(c), first
+                elif second < int(c) < first:
+                    second = int(c)
+        return second
+
+
+def test_second_highest():
+    solution = Solution()
+    assert solution.secondHighest('dfa12321afd') == 2, 'wrong result'
+    assert solution.secondHighest('abc1111') == -1, 'wrong result'
+    assert solution.secondHighest('sjhtz8344') == 4, 'wrong result'
+
+if __name__ == '__main__':
+    test_second_highest()
