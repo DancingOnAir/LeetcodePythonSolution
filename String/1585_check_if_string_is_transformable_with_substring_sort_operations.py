ffda88fc3f0d889fa7d7eea6537e0187ad57709a
DancingOnAir <taotong1984@hotmail.com>
2021-03-28 14:13:55 +0800

    feat(String): init q1585 & accepted pythonic solution for q1585

---
+++ String/1585_check_if_string_is_transformable_with_substring_sort_operations.py
@@ -0,0 +1,30 @@
+from collections import Counter, deque
+
+
+class Solution:
+    def isTransformable(self, s: str, t: str) -> bool:
+        pos = {i: deque() for i in range(10)}
+        for i, c in enumerate(s):
+            pos[int(c)].append(i)
+
+        for i, c in enumerate(t):
+            digit = int(c)
+            if not pos[digit]:
+                return False
+            if any(pos[j] and pos[j][0] < pos[digit][0] for j in range(digit)):
+                return False
+            pos[digit].popleft()
+        return True
+
+
+def test_is_transformable():
+    solution = Solution()
+    assert solution.isTransformable('84532', '34852'), 'wrong result'
+    assert solution.isTransformable('34521', '23415'), 'wrong result'
+    assert not solution.isTransformable('12345', '12435'), 'wrong result'
+    assert not solution.isTransformable('1', '2'), 'wrong result'
+    assert not solution.isTransformable('12', '21'), 'wrong result'
+
+
+if __name__ == '__main__':
+    test_is_transformable()
