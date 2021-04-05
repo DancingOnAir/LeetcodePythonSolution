f318fe011d1a5c23e02355acbfa40c2d04be450f
DancingOnAir <taotong1984@hotmail.com>
2021-04-05 09:54:52 +0800

    feat(String): accepted xor solution for q1544

--- String/1544_make_the_string_great.py
+++ String/1544_make_the_string_great.py
@@ -1,5 +1,20 @@
 class Solution:
+    # ^ is logical xor:
+    # The decimal values of lowercase and uppercase English letters differ with each other by 32.
+    # Take an example: a -> 97(1100001), A -> 65 (1000001). So we have a ^ 32 = A, A ^ 32 = a.
+    #    1 1 0 0 0 0 1 (97)
+    # ^  0 1 0 0 0 0 0 (32)
+    #    1 0 0 0 0 0 1 (65)
     def makeGood(self, s: str) -> str:
+        stk = list()
+        for c in s:
+            if stk and stk[-1] == chr(ord(c) ^ 32):
+                stk.pop()
+            else:
+                stk.append(c)
+        return ''.join(stk)
+
+    def makeGood1(self, s: str) -> str:
         if not s:
             return s
 
