b0203cbdb1a35d2a8f664a91f66b63e921529476
DancingOnAir <taotong1984@hotmail.com>
2021-04-07 08:26:13 +0800

    feat(String): init q1556 & accepted list solution for q1556

---
+++ String/1556_thousand_separator.py
@@ -0,0 +1,32 @@
+class Solution:
+    # def thousandSeparator(self, n: int) -> str:
+    #     n = str(n)
+    #     q, r = divmod(len(n), 3)
+    #
+    #     if not r:
+    #         res = n[:3]
+    #     else:
+    #         res = n[:r]
+    #     for i in range(1 if not r else 0, q):
+    #         res += '.' + n[r + 3 * i: r + 3 * i + 3]
+    #     return res
+
+    def thousandSeparator1(self, n: int) -> str:
+        res = list()
+        for i, c in enumerate(str(n)[::-1]):
+            res.append(c)
+            if (i + 1) % 3 == 0 and (i + 1) != len(str(n)):
+                res.append('.')
+        return ''.join(res[::-1])
+
+
+def test_thousand_separator():
+    solution = Solution()
+    assert solution.thousandSeparator(987) == "987", 'wrong result'
+    assert solution.thousandSeparator(1234) == "1.234", 'wrong result'
+    assert solution.thousandSeparator(123456789) == "123.456.789", 'wrong result'
+    assert solution.thousandSeparator(0) == "0", 'wrong result'
+
+
+if __name__ == '__main__':
+    test_thousand_separator()
