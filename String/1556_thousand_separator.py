ad6133d793507bfd14f7c4eda399869aeed46d49
DancingOnAir <taotong1984@hotmail.com>
2021-04-07 08:51:15 +0800

    feat(String): accepted pythonic solution for q1556

--- String/1556_thousand_separator.py
+++ String/1556_thousand_separator.py
@@ -1,5 +1,10 @@
 class Solution:
     def thousandSeparator(self, n: int) -> str:
+        s = str(n)[::-1]
+        res = '.'.join(s[i: i+3] for i in range(0, len(s), 3))
+        return res[::-1]
+
+    def thousandSeparator2(self, n: int) -> str:
         n = str(n)
         q, r = divmod(len(n), 3)
 
