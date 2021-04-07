a51292bae9fd219ceb46349278cc6237311aac12
DancingOnAir <taotong1984@hotmail.com>
2021-04-08 00:34:51 +0800

    feat(String): init q1529 & accepted solution for q1529

---
+++ String/1529_bulb_switcher_iv.py
@@ -0,0 +1,27 @@
+from itertools import groupby
+
+
+class Solution:
+    # def minFlips(self, target: str) -> int:
+    #     return len(list(groupby('0' + target))) - 1
+
+    def minFlips1(self, target: str) -> int:
+        res = flip = 0
+        for c in target:
+            if flip ^ int(c):
+                flip ^= 1
+                res += 1
+
+        return res
+
+
+def test_min_filps():
+    solution = Solution()
+    assert solution.minFlips('10111') == 3, 'wrong result'
+    assert solution.minFlips('101') == 3, 'wrong result'
+    assert solution.minFlips('00000') == 0, 'wrong result'
+    assert solution.minFlips('001011101') == 5, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_min_filps()
