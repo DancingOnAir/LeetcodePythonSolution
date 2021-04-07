23c27831a90319ef6c8372796ca0d2bfcac9add6
DancingOnAir <taotong1984@hotmail.com>
2021-04-07 08:52:21 +0800

    feat(String): accepted pythonic f-string solution for q1556

--- String/1556_thousand_separator.py
+++ String/1556_thousand_separator.py
@@ -1,5 +1,8 @@
 class Solution:
     def thousandSeparator(self, n: int) -> str:
+        return f'{n:,}'.replace(',', '.')
+
+    def thousandSeparator3(self, n: int) -> str:
         s = str(n)[::-1]
         res = '.'.join(s[i: i+3] for i in range(0, len(s), 3))
         return res[::-1]
