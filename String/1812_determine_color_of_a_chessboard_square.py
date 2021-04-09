7f84bafd03f222450ffcdc3faf47145a27eb4c29
DancingOnAir <taotong1984@hotmail.com>
2021-04-10 00:02:53 +0800

    feat(String): init q1812 & accepted bit manipulation soution for q1812

---
+++ String/1812_determine_color_of_a_chessboard_square.py
@@ -0,0 +1,14 @@
+class Solution:
+    def squareIsWhite(self, coordinates: str) -> bool:
+        return (ord(coordinates[0]) & 1) ^ (int(coordinates[1]) & 1)
+
+
+def test_square_is_white():
+    solution = Solution()
+    assert not solution.squareIsWhite('a1'), 'wrong result'
+    assert solution.squareIsWhite('h3'), 'wrong result'
+    assert not solution.squareIsWhite('c7'), 'wrong result'
+
+
+if __name__ == '__main__':
+    test_square_is_white()
