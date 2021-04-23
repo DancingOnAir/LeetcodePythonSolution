5b3a2879ef92343cfbed0931dd68bfa638586bbe
DancingOnAir <taotong1984@hotmail.com>
2021-04-23 09:10:52 +0800

    fix(Math): accepted recursive solution for q0050

--- Math/0050_Pow(x, n).py
+++ Math/0050_Pow(x, n).py
@@ -2,7 +2,19 @@ from math import isclose
 
 
 class Solution:
+    # recursive
     def myPow(self, x: float, n: int) -> float:
+        if not n:
+            return 1
+        elif n < 0:
+            return 1 / self.myPow(x, -n)
+
+        if n & 1:
+            return x * self.myPow(x, n - 1)
+        return self.myPow(x*x, n >> 1)
+
+    # iterative
+    def myPow1(self, x: float, n: int) -> float:
         if not n:
             return 1
         elif n < 0:
