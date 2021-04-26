ad3d060d722a16f4786c7c6093647f69c7b8275d
DancingOnAir <taotong1984@hotmail.com>
2021-04-26 09:15:55 +0800

    feat(Array): accepted faster solution for q1455

--- String/1455_check_if_a_word_occurs_as_a_prefix_of_any_word_in_a_sentence.py
+++ String/1455_check_if_a_word_occurs_as_a_prefix_of_any_word_in_a_sentence.py
@@ -1,6 +1,12 @@
 class Solution:
     def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
         for i, w in enumerate(sentence.split()):
+            if searchWord == w[:len(searchWord)]:
+                return i + 1
+        return -1
+
+    def isPrefixOfWord1(self, sentence: str, searchWord: str) -> int:
+        for i, w in enumerate(sentence.split()):
             if w.startswith(searchWord):
                 return i + 1
         return -1
