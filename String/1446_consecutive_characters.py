a5c455d12f29885ee36dea322da28cceceef1700
DancingOnAir <taotong1984@hotmail.com>
2021-05-08 14:51:29 +0800

    feat(String): accepted solution for q1446

--- String/1446_consecutive_characters.py
+++ String/1446_consecutive_characters.py
@@ -3,6 +3,17 @@ from itertools import groupby
 
 class Solution:
     def maxPower(self, s: str) -> int:
+        res = cur = 1
+        for i in range(1, len(s)):
+            if s[i] == s[i - 1]:
+                cur += 1
+                res = max(res, cur)
+            else:
+                cur = 1
+
+        return res
+
+    def maxPower1(self, s: str) -> int:
         return max([len(list(g)) for k, g in groupby(s)])
 
 
