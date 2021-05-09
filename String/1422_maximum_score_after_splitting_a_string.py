a1ef9a9eca0216eb55fc6e1ca45e35ad49b4894d
DancingOnAir <taotong1984@hotmail.com>
2021-05-09 09:52:01 +0800

    feat(String): accepted line scan solution for q1422

--- String/1422_maximum_score_after_splitting_a_string.py
+++ String/1422_maximum_score_after_splitting_a_string.py
@@ -1,5 +1,16 @@
 class Solution:
     def maxScore(self, s: str) -> int:
+        diff = ones = zeros = 0
+        for i in range(1, len(s) - 1):
+            if s[i] == '1':
+                ones += 1
+            else:
+                zeros += 1
+            diff = max(diff, zeros - ones)
+        return diff + ones + (s[0] == '0') + (s[-1] == '1')
+
+    # regular method
+    def maxScore1(self, s: str) -> int:
         total = sum(map(int, s))
 
         n = len(s)
