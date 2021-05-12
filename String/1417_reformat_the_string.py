b99f1602e6940844ad962c12838a2e8dbad938a0
DancingOnAir <taotong1984@hotmail.com>
2021-05-12 11:02:11 +0800

    feat(String): init q1417 & accepted solution for q1417

---
+++ String/1417_reformat_the_string.py
@@ -0,0 +1,32 @@
+from itertools import zip_longest
+
+
+class Solution:
+    def reformat(self, s: str) -> str:
+        digits = ''
+        alphabets = ''
+
+        for c in s:
+            if c.isdigit():
+                digits += c
+            else:
+                alphabets += c
+        if abs(len(alphabets) - len(digits)) > 1:
+            return ''
+
+        if len(digits) >= len(alphabets):
+            return ''.join([i[0]+i[1] for i in zip_longest(digits, alphabets, fillvalue='')])
+        return ''.join([i[0] + i[1] for i in zip_longest(alphabets, digits, fillvalue='')])
+
+
+def test_reformat():
+    solution = Solution()
+    assert solution.reformat('a0b1c2') == '0a1b2c', 'wrong result'
+    assert solution.reformat('leetcode') == '', 'wrong result'
+    assert solution.reformat('1229857369') == '', 'wrong result'
+    assert solution.reformat('covid2019') == 'c2o0v1i9d', 'wrong result'
+    assert solution.reformat('ab123') == '1a2b3', 'wrong result'
+
+
+if __name__ == '__main__':
+    test_reformat()
