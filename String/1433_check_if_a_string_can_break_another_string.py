3e466ce270538824eb4646581a5d66d990c48ee2
DancingOnAir <taotong1984@hotmail.com>
2021-05-13 09:11:39 +0800

    feat(String): accepted pythonic solution for q1433

--- String/1433_check_if_a_string_can_break_another_string.py
+++ String/1433_check_if_a_string_can_break_another_string.py
@@ -1,5 +1,8 @@
 class Solution:
     def checkIfCanBreak(self, s1: str, s2: str) -> bool:
+        return all(x <= y for x, y in zip(*sorted([sorted(s1), sorted(s2)])))
+
+    def checkIfCanBreak1(self, s1: str, s2: str) -> bool:
         n = len(s1)
         if n == 1:
             return True
