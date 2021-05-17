5b0a8365764f602d61984e3507f61d28d3d15ce1
DancingOnAir <taotong1984@hotmail.com>
2021-05-17 10:43:03 +0800

    feat(String): accepted two points solution for q1404

--- String/1404_number_of_steps_to_reduce_a_number_in_binary_representation_to_one.py
+++ String/1404_number_of_steps_to_reduce_a_number_in_binary_representation_to_one.py
@@ -1,6 +1,17 @@
 class Solution:
-    # https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/discuss/1184352/JavaPython-Clean-and-Concise-Clear-Explanation-O(N)
+    # https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/discuss/564323/Clean-Python-3-two-pointers-O(n)O(1)
     def numSteps(self, s: str) -> int:
+        middle_zeros = i = 0
+        for j in range(1, len(s)):
+            if s[j] == '1':
+                middle_zeros += j - i - 1
+                i = j
+        if not i:
+            return len(s) - 1
+        return len(s) + 1 + middle_zeros
+
+    # https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/discuss/1184352/JavaPython-Clean-and-Concise-Clear-Explanation-O(N)
+    def numSteps2(self, s: str) -> int:
         res = carry = 0
         for c in s[:0:-1]:
             if int(c) + carry == 1:
