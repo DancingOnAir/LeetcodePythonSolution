25e3b6778bda0f71f03c66660a09006dbebdca90
DancingOnAir <taotong1984@hotmail.com>
2021-05-20 00:38:31 +0800

    feat(String): accepted pythonic soluiton for q1374

--- String/1374_generate_a_string_with_characters_that_have_odd_counts.py
+++ String/1374_generate_a_string_with_characters_that_have_odd_counts.py
@@ -1,5 +1,8 @@
 class Solution:
     def generateTheString(self, n: int) -> str:
+        return 'ab'[n & 1] * (n - 1) + 'b'
+
+    def generateTheString1(self, n: int) -> str:
         if n & 1:
             return 'a' * n
         return 'a' * (n - 1) + 'b'
