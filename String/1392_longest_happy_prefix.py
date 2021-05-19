14ebefc474b72eff566e33c9d505f110f43eb688
DancingOnAir <taotong1984@hotmail.com>
2021-05-19 11:18:35 +0800

    feat(String): accepted kmp soluiton for q1392

--- String/1392_longest_happy_prefix.py
+++ String/1392_longest_happy_prefix.py
@@ -1,5 +1,33 @@
 class Solution:
+    # https://leetcode.com/problems/longest-happy-prefix/discuss/547187/Python-Deterministic-Finite-Automaton-inspired-by-KMP
+    # kmp
     def longestPrefix(self, s: str) -> str:
+        n = len(s)
+        if n < 2:
+            return ''
+        cnt = [0] * n
+        d = 0
+        for i in range(1, n):
+            while d and s[d] != s[i]:
+                d = cnt[d - 1]
+            d += (s[d] == s[i])
+            cnt[i] = d
+        return s[:cnt[n - 1]]
+
+    # incremental hash
+    def longestPrefix2(self, s: str) -> str:
+        res, l, r, mod = 0, 0, 0, 10 ** 9 + 7
+        for i in range(len(s) - 1):
+            l = (l * 128 + ord(s[i])) % mod
+            # Specifically, we get the ith letter from the end using s[~i], note ~i is -i-1
+            r = (r + pow(128, i, mod) * ord(s[~i])) % mod
+            #  for the collisions, we can easily check if prefix is equal to the suffix.
+            if l == r:
+                res = i + 1
+        return s[: res]
+
+    # brute force
+    def longestPrefix1(self, s: str) -> str:
         n = len(s)
         if n < 2:
             return ""
