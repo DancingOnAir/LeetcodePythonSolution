4aee32332d69678c2ea01175e8d28448f5100352
DancingOnAir <taotong1984@hotmail.com>
2021-05-20 00:28:18 +0800

    feat(String): init q1374 & accepted soluiton for q1374

---
+++ String/1374_generate_a_string_with_characters_that_have_odd_counts.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generateTheString(self, n: int) -> str:
+        if n & 1:
+            return 'a' * n
+        return 'a' * (n - 1) + 'b'
+
+
+def test_generate_the_string():
+    solution = Solution()
+    assert solution.generateTheString(4) == 'aaab', 'wrong result'
+    assert solution.generateTheString(2) == 'ab', 'wrong result'
+    assert solution.generateTheString(7) == 'aaaaaaa', 'wrong result'
+
+
+if __name__ == '__main__':
+    test_generate_the_string()
