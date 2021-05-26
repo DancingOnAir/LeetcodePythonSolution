417a48b0f42071ea8914ccb121494b6fdb58b4aa
DancingOnAir <taotong1984@hotmail.com>
2021-05-26 14:45:13 +0800

    feat(String): accepted trie solution for q1268

--- String/1268_search_suggestions_system.py
+++ String/1268_search_suggestions_system.py
@@ -2,9 +2,41 @@ from typing import List
 from bisect import bisect_left
 
 
+class Trie:
+    def __init__(self):
+        self.children = dict()
+        self.suggestion = list()
+
+
 class Solution:
-    # binary search
+    def _insert(self, product: str, root: Trie) -> None:
+        trie = root
+        for c in product:
+            if c not in trie.children:
+                trie.children[c] = Trie()
+            trie = trie.children[c]
+            trie.suggestion.append(product)
+            trie.suggestion.sort()
+            if len(trie.suggestion) > 3:
+                trie.suggestion.pop()
+
+    def _search(self, searchWord: str, root: Trie) -> List[str]:
+        res = list()
+        for c in searchWord:
+            if root:
+                root = root.children.get(c)
+            res.append(root.suggestion if root else [])
+        return res
+
+    # trie
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        root = Trie()
+        for p in products:
+            self._insert(p, root)
+        return self._search(searchWord, root)
+
+    # binary search
+    def suggestedProducts2(self, products: List[str], searchWord: str) -> List[List[str]]:
         products.sort()
         cur, res = '', list()
         for c in searchWord:
