4913fcae722824e03755eeeb59cf211929dfaade
DancingOnAir <taotong1984@hotmail.com>
2021-05-28 00:25:38 +0800

    feat(String): accepted pythonic solution for q1247

--- String/1247_minimum_swaps_to_make_strings_equal.py
+++ String/1247_minimum_swaps_to_make_strings_equal.py
@@ -1,6 +1,13 @@
 class Solution:
     def minimumSwap(self, s1: str, s2: str) -> int:
         xy = yx = 0
+        for c1, c2 in zip(s1, s2):
+            xy += c1 == 'x' and c2 == 'y'
+            yx += c1 == 'y' and c2 == 'x'
+        return xy // 2 + yx // 2 + xy % 2 + yx % 2 if not ((xy + yx) & 1) else -1
+
+    def minimumSwap1(self, s1: str, s2: str) -> int:
+        xy = yx = 0
         for i in range(len(s1)):
             if s1[i] != s2[i]:
                 if s1[i] == 'x':
