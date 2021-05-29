a007854a7e1803c9ff14b498f10c6a34ed142ee6
DancingOnAir <taotong1984@hotmail.com>
2021-05-30 00:02:10 +0800

    feat(String): init q1221 & accepted solution for q1221

---
+++ String/1221_split_a_string_in_balanced_strings.py
@@ -0,0 +1,22 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        res = cnt = 0
+        for c in s:
+            cnt += 1 if c == 'L' else -1
+
+            if not cnt:
+                res += 1
+        return res
+
+
+def test_balanced_string_split():
+    solution = Solution()
+
+    assert solution.balancedStringSplit('RLRRLLRLRL') == 4, 'wrong result'
+    assert solution.balancedStringSplit('RLLLLRRRLR') == 3, 'wrong result'
+    assert solution.balancedStringSplit('LLLLRRRR') == 1, 'wrong result'
+    assert solution.balancedStringSplit('RLRRRLLRLL') == 2, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_balanced_string_split()
