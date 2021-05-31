9c963aadd02864be063aee44e44c79a579b6e8fc
DancingOnAir <taotong1984@hotmail.com>
2021-05-31 08:58:59 +0800

    feat(String): init q1189 & accepted python solution for q1189

---
+++ String/1189_maximum_number_of_balloons.py
@@ -0,0 +1,23 @@
+from collections import Counter
+
+
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        n = len(text) // len('balloon')
+        cnt = Counter(text)
+        for i in range(n, -1, -1):
+            if len(Counter('balloon' * i) - cnt) == 0:
+                return i
+        return 0
+
+
+def test_max_number_of_balloons():
+    solution = Solution()
+
+    assert solution.maxNumberOfBalloons('nlaebolko') == 1, 'wrong result'
+    assert solution.maxNumberOfBalloons('loonbalxballpoon') == 2, 'wrong result'
+    assert solution.maxNumberOfBalloons('leetcode') == 0, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_max_number_of_balloons()
