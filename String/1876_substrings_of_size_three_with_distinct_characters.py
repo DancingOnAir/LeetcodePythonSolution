aaa1d7d1c8bc5aabf00459d2441e852ee8a4d3d5
DancingOnAir <taotong1984@hotmail.com>
2021-06-02 09:25:54 +0800

    feat(String): accepted sliding window solution for q1876

--- String/1876_substrings_of_size_three_with_distinct_characters.py
+++ String/1876_substrings_of_size_three_with_distinct_characters.py
@@ -1,5 +1,26 @@
 class Solution:
+    # sliding window
     def countGoodSubstrings(self, s: str) -> int:
+        repeat = res = 0
+        cnt = [0] * 26
+
+        for i, c in enumerate(s):
+            if cnt[ord(c) - 97] == 1:
+                repeat += 1
+            cnt[ord(c) - 97] += 1
+
+            if i >= 3:
+                if cnt[ord(s[i-3]) - 97] == 2:
+                    repeat -= 1
+                cnt[ord(s[i-3]) - 97] -= 1
+
+            if i >= 2 and repeat == 0:
+                res += 1
+
+        return res
+
+    # set
+    def countGoodSubstrings1(self, s: str) -> int:
         n = len(s)
         if n < 3:
             return 0
@@ -13,7 +34,7 @@ class Solution:
 
 def test_count_good_substrings():
     solution = Solution()
-    # assert solution.countGoodSubstrings('xyzzaz') == 1, 'wrong result'
+    assert solution.countGoodSubstrings('xyzzaz') == 1, 'wrong result'
     assert solution.countGoodSubstrings('aababcabc') == 4, 'wrong result'
 
 
