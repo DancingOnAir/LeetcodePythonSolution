09df301b2a034e0cdcedff07d328d075cd7773c4
DancingOnAir <taotong1984@hotmail.com>
2021-06-03 00:06:43 +0800

    feat(String): init q1880 & accepted solution for q1880

---
+++ String/1880_check_if_word_equals_summation_of_two_words.py
@@ -0,0 +1,28 @@
+class Solution:
+    # def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
+    #     # 'a' - '0' == 49
+    #     def numeric_total(s: str):
+    #         # return int(''.join([str(ord(c) - 97) for c in s]))
+    #         return int(''.join([chr(ord(c) - 49) for c in s]))
+    #     return numeric_total(firstWord) + numeric_total(secondWord) == numeric_total(targetWord)
+
+    def isSumEqual1(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
+        def get_num(s: str):
+            res = 0
+            for c in s:
+                res = res * 10 + (ord(c) - 97)
+            return res
+
+        return get_num(firstWord) + get_num(secondWord) == get_num(targetWord)
+
+
+def test_is_sum_equal():
+    solution = Solution()
+    assert solution.isSumEqual('j', 'j', 'bi'), 'wrong result'
+    assert solution.isSumEqual('acb', 'cba', 'cdb'), 'wrong result'
+    assert not solution.isSumEqual('aaa', 'a', 'aab'), 'wrong result'
+    assert solution.isSumEqual('aaa', 'a', 'aaaa'), 'wrong result'
+
+
+if __name__ == '__main__':
+    test_is_sum_equal()
