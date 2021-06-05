1a9dd64a2b7aec03443c8c90f84fc920459de71d
DancingOnAir <taotong1984@hotmail.com>
2021-06-05 23:28:02 +0800

    feat(String): acceptd pythonic solution for q1138

--- String/1138_alphabet_board_path.py
+++ String/1138_alphabet_board_path.py
@@ -1,5 +1,20 @@
 class Solution:
-    def alphabetBoardPath(self, target: str) -> str:
+    def alphabetBoardPath(self, target):
+        m = {c: [i // 5, i % 5] for i, c in enumerate("abcdefghijklmnopqrstuvwxyz")}
+        x0, y0 = 0, 0
+        res = []
+        for c in target:
+            x, y = m[c]
+            if y < y0: res.append('L' * (y0 - y))
+            if x < x0: res.append('U' * (x0 - x))
+            if x > x0: res.append('D' * (x - x0))
+            if y > y0: res.append('R' * (y - y0))
+            res.append('!')
+            x0, y0 = x, y
+        return "".join(res)
+
+    # simulate movements, consider current and previous character is 'z'
+    def alphabetBoardPath1(self, target: str) -> str:
         x = y = 0
         res = ''
         for c in target:
