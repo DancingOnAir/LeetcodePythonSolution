7f6c81769aba83e4da634bb4b573a24b3d08bd29
DancingOnAir <taotong1984@hotmail.com>
2021-06-11 00:29:14 +0800

    fix(string): init q1071 & accepted gcd python solution for q1071

---
+++ String/1071_greatest_common_divisor_of_strings.py
@@ -0,0 +1,31 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        def gcd(a: int, b: int) -> int:
+            if a < b:
+                a, b = b, a
+            while a % b != 0:
+                a, b = b, a%b
+            return b
+
+        l1 = len(str1)
+        l2 = len(str2)
+        x = gcd(l1, l2)
+
+        for i in range(x, 0, -1):
+            res = str1[:i]
+            if res * (l1 // i) == str1 and res * (l2 // i) == str2:
+                return res
+        return ''
+
+
+def test_gcd_of_strings():
+    solution = Solution()
+
+    assert solution.gcdOfStrings('ABCABC', 'ABC') == 'ABC', 'wrong result'
+    assert solution.gcdOfStrings('ABABAB', 'ABAB') == 'AB', 'wrong result'
+    assert solution.gcdOfStrings('LEET', 'CODE') == '', 'wrong result'
+    assert solution.gcdOfStrings('ABCDEF', 'ABC') == '', 'wrong result'
+
+
+if __name__ == '__main__':
+    test_gcd_of_strings()
