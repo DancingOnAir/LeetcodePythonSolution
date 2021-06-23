12118b9c2400af4af9fbaff7b6963baf6fb87852
DancingOnAir <taotong1984@hotmail.com>
2021-06-24 00:38:25 +0800

    feat(String): init q0917 & accepted stack solution for q0917

---
+++ String/0917_reverse_only_letters.py
@@ -0,0 +1,29 @@
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        s = list(s)
+        non_letters = dict()
+        letters = list()
+        for i, c in enumerate(s):
+            if not c.isalpha():
+                non_letters[i] = c
+            else:
+                letters.append(c)
+
+        res = list()
+        while len(res) < len(s):
+            if len(res) in non_letters:
+                res.append(non_letters[len(res)])
+            else:
+                res.append(letters.pop())
+        return ''.join(res)
+
+
+def test_reverse_only_letters():
+    solution = Solution()
+    assert solution.reverseOnlyLetters('ab-cd') == 'dc-ba', 'wrong result'
+    assert solution.reverseOnlyLetters('a-bC-dEf-ghIj') == 'j-Ih-gfE-dCba', 'wrong result'
+    assert solution.reverseOnlyLetters('Test1ng-Leet=code-Q!') == 'Qedo1ct-eeLg=ntse-T!', 'wrong result'
+
+
+if __name__ == '__main__':
+    test_reverse_only_letters()
