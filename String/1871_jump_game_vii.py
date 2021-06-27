2d9f8f059a25c40a689129667cdfc5f798711022
DancingOnAir <taotong1984@hotmail.com>
2021-06-27 19:40:25 +0800

    feat(String): accepted presum & dp solution for q1871

--- String/1871_jump_game_vii.py
+++ String/1871_jump_game_vii.py
@@ -1,5 +1,23 @@
 class Solution:
+    # presum + dp
+    # dp[i] = 1 means it can jump from 0 to i otherwise, 0 means can not
+    # pre represents sum(dp[i) i from 0 to i
     def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        n = len(s)
+        dp = [1] + [0] * (n - 1)
+        pre = [0] * n
+        for i in range(1, minJump):
+            pre[i] = 1
+        for i in range(minJump, n):
+            left, right = i - maxJump, i - minJump
+            if s[i] == '0':
+                total = pre[right] - (0 if left < 1 else pre[left - 1])
+                dp[i] = int(total != 0)
+            pre[i] = pre[i - 1] + dp[i]
+        return bool(dp[-1])
+
+    # recursive but TLE
+    def canReach1(self, s: str, minJump: int, maxJump: int) -> bool:
         if s[-1] != '0':
             return False
 
