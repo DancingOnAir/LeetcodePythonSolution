9b999bd4f593eaa9128da80c5c8bb07afddf1624
DancingOnAir <taotong1984@hotmail.com>
2021-07-12 08:03:54 +0800

    feat(String): accepted one line python solution for q1784

--- String/1784_check_if_binary_string_has_at_most_one_segment_of_ones.py
+++ String/1784_check_if_binary_string_has_at_most_one_segment_of_ones.py
@@ -3,6 +3,9 @@ from itertools import groupby
 
 class Solution:
     def checkOnesSegment(self, s: str) -> bool:
+        return '01' not in s
+
+    def checkOnesSegment1(self, s: str) -> bool:
         return sum(1 for k, g in groupby(s) if k == '1') <= 1
 
 
