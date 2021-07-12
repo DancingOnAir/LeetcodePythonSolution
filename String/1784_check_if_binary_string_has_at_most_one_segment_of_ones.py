b3744bd50136077e5e1f0da1742e0b33591a061b
DancingOnAir <taotong1984@hotmail.com>
2021-07-12 08:01:47 +0800

    feat(String): init q1784 & accepted groupby python solution for q1784

---
+++ String/1784_check_if_binary_string_has_at_most_one_segment_of_ones.py
@@ -0,0 +1,17 @@
+from itertools import groupby
+
+
+class Solution:
+    def checkOnesSegment(self, s: str) -> bool:
+        return sum(1 for k, g in groupby(s) if k == '1') <= 1
+
+
+def test_check_ones_segment():
+    solution = Solution()
+
+    assert not solution.checkOnesSegment('1001'), 'wrong result'
+    assert solution.checkOnesSegment('110'), 'wrong result'
+
+
+if __name__ == '__main__':
+    test_check_ones_segment()
