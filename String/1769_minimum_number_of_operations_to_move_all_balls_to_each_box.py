d10f44a56dabd0a1e089a9e2ce34b054c41ede7f
DancingOnAir <taotong1984@hotmail.com>
2021-07-13 08:14:34 +0800

    feat(String): accepeted 100 faster python solution for q1769

--- String/1769_minimum_number_of_operations_to_move_all_balls_to_each_box.py
+++ String/1769_minimum_number_of_operations_to_move_all_balls_to_each_box.py
@@ -3,6 +3,24 @@ from typing import List
 
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
+        cost = count = 0
+        n = len(boxes)
+        res = [0] * n
+        for i in range(1, n):
+            if boxes[i - 1] == '1':
+                count += 1
+            cost += count
+            res[i] += cost
+
+        cost = count = 0
+        for i in range(n - 2, -1, -1):
+            if boxes[i + 1] == '1':
+                count += 1
+            cost += count
+            res[i] += cost
+        return res
+
+    def minOperations1(self, boxes: str) -> List[int]:
         memo = [i for i, c in enumerate(boxes) if c == '1']
         res = list()
         for i, c in enumerate(boxes):
