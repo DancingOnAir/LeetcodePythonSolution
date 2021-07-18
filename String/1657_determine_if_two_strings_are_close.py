3c9cd121175be830f058f7e580024f585384d5d1
DancingOnAir <taotong1984@hotmail.com>
2021-07-19 07:33:18 +0800

    feat(String): init q1657 & accepted counter python solution for q1657

---
+++ String/1657_determine_if_two_strings_are_close.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        c1, c2 = Counter(word1), Counter(word2)
+        if sorted(list(c1.keys())) != sorted(list(c2.keys())) or sorted(list(c1.values())) != sorted(list(c2.values())):
+            return False
+        return True
+
+
+def test_close_strings():
+    solution = Solution()
+
+    assert solution.closeStrings('abc', 'bca'), 'wrong result'
+    assert not solution.closeStrings('a', 'aa'), 'wrong result'
+    assert solution.closeStrings('cabbba', 'abbccc'), 'wrong result'
+    assert not solution.closeStrings('cabbba', 'aabbss'), 'wrong result'
+
+
+if __name__ == '__main__':
+    test_close_strings()
