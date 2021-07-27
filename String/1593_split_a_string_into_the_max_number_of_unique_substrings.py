978179b61aa1ea2fce4e9d7b4d8980f76f6d4e0f
DancingOnAir <taotong1984@hotmail.com>
2021-07-27 11:28:55 +0800

    feat(String): accepted backtrack python solution for q1593

--- String/1593_split_a_string_into_the_max_number_of_unique_substrings.py
+++ String/1593_split_a_string_into_the_max_number_of_unique_substrings.py
@@ -1,5 +1,24 @@
 class Solution:
     def maxUniqueSplit(self, s: str) -> int:
+        def backtrack(idx, split):
+            if idx >= n:
+                nonlocal max_split
+                max_split = max(max_split, split)
+            else:
+                for i in range(idx, n):
+                    ss = s[idx: i+1]
+                    if ss not in seen:
+                        seen.add(ss)
+                        backtrack(i + 1, split + 1)
+                        seen.remove(ss)
+
+        n = len(s)
+        max_split = 1
+        seen = set()
+        backtrack(0, 0)
+        return max_split
+
+    def maxUniqueSplit1(self, s: str) -> int:
         def dfs(idx, tmp):
             if len(s) == idx:
                 return len(tmp) if tmp[-1] not in tmp[:-1] else 0
@@ -11,7 +30,7 @@ class Solution:
 
 def test_max_unique_split():
     solution = Solution()
-    # assert solution.maxUniqueSplit('ababccc') == 5, 'wrong result'
+    assert solution.maxUniqueSplit('ababccc') == 5, 'wrong result'
     assert solution.maxUniqueSplit('aba') == 2, 'wrong result'
     assert solution.maxUniqueSplit('aa') == 1, 'wrong result'
 
