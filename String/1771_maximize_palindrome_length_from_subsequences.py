4e2f451c65f8a0d1fe7fb2a26444c4f9d33268de
DancingOnAir <taotong1984@hotmail.com>
2021-07-28 15:07:03 +0800

    feat(String): init q1771 & accepted dp python solution for q1771

---
+++ String/1771_maximize_palindrome_length_from_subsequences.py
@@ -0,0 +1,31 @@
+class Solution:
+    def longestPalindrome(self, word1: str, word2: str) -> int:
+        n1, n2 = len(word1), len(word2)
+        w = word1 + word2
+        n = n1 + n2
+        dp = [[0] * n for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = 1
+
+        res = 0
+        for i in range(n - 1, -1, -1):
+            for j in range(i + 1, n):
+                if w[i] == w[j]:
+                    dp[i][j] = dp[i + 1][j - 1] + 2
+                    if i < n1 <= j:
+                        res = max(res, dp[i][j])
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+
+        return res
+
+
+def test_longest_palindrome():
+    solution = Solution()
+    assert solution.longestPalindrome('cacb', 'cbba') == 5, 'wrong result'
+    assert solution.longestPalindrome('ab', 'ab') == 3, 'wrong result'
+    assert solution.longestPalindrome('aa', 'bb') == 0, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_longest_palindrome()
