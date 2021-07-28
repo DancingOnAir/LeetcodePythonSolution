ce82710b7ffe63ab8d9d435e7b457ca8f1c0cda3
DancingOnAir <taotong1984@hotmail.com>
2021-07-28 16:06:04 +0800

    feat(String): accepted optimized dp python solution for q1771

--- String/1771_maximize_palindrome_length_from_subsequences.py
+++ String/1771_maximize_palindrome_length_from_subsequences.py
@@ -1,5 +1,27 @@
+import string
+from functools import lru_cache
+
+
 class Solution:
     def longestPalindrome(self, word1: str, word2: str) -> int:
+        @lru_cache(None)
+        def helper(lo, hi):
+            if lo >= hi:
+                return int(lo == hi)
+            if w[lo] == w[hi]:
+                return 2 + helper(lo+1, hi - 1)
+            return max(helper(lo + 1, hi), helper(lo, hi - 1))
+
+        res = 0
+        w = word1 + word2
+        for c in string.ascii_lowercase:
+            i = word1.find(c)
+            j = word2.rfind(c)
+            if i != - 1 and j != -1:
+                res = max(res, helper(i, j + len(word1)))
+        return res
+
+    def longestPalindrome1(self, word1: str, word2: str) -> int:
         n1, n2 = len(word1), len(word2)
         w = word1 + word2
         n = n1 + n2
