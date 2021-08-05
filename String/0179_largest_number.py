5e893ecb39dc4f0c97fd573bd524b4d09a055eec
DancingOnAir <taotong1984@hotmail.com>
2021-08-05 08:00:31 +0800

    feat(String): accepted sort pythonic solution for q0179

--- String/0179_largest_number.py
+++ String/0179_largest_number.py
@@ -3,7 +3,12 @@ from functools import cmp_to_key
 
 
 class Solution:
+    # (a>b) - (a<b) 等价于 a<b时为-1, a=b时为0, a>b时为1
+    # 这里x+y=a, y+x=b, 为了逆序排列参数 lambda y, x 而不是x, y.
     def largestNumber(self, nums: List[int]) -> str:
+        return ''.join(sorted(map(str, nums), key=cmp_to_key(lambda y, x:  ((x+y) > (y+x)) - ((x+y) < (y+x)) ))).lstrip('0') or '0'
+
+    def largestNumber1(self, nums: List[int]) -> str:
         def cmp(x, y):
             if x + y < y + x:
                 return -1
