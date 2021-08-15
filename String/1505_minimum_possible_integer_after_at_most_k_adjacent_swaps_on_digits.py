0672443bac824fe417c639dce24cbbe3d0b7c312
DancingOnAir <taotong1984@hotmail.com>
2021-08-15 22:10:19 +0800

    feat(String): init q1505 & accepted string simulate python solution for q1505

---
+++ String/1505_minimum_possible_integer_after_at_most_k_adjacent_swaps_on_digits.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minInteger(self, num: str, k: int) -> str:
+        sorted_num = ''.join(sorted(num))
+        start = 0
+        while k > 0:
+            if sorted_num == num:
+                return num
+
+            end = min(k + 1, len(num[start:])) + start
+            min_num = min(num[start: end])
+            min_pos = num.find(min_num, start, end)
+
+            num = num[: start] + num[min_pos] + num[start: min_pos] + num[min_pos + 1:]
+            k -= (min_pos - start)
+            start += 1
+        return num
+
+
+def test_min_integer():
+    solution = Solution()
+
+    assert solution.minInteger("4321", 4) == "1342", 'wrong result'
+    assert solution.minInteger("100", 1) == "010", 'wrong result'
+    assert solution.minInteger("36789", 1000) == "36789", 'wrong result'
+    assert solution.minInteger("22", 22) == "22", 'wrong result'
+    assert solution.minInteger("9438957234785635408", 23) == "0345989723478563548", 'wrong result'
+
+
+if __name__ == '__main__':
+    test_min_integer()
