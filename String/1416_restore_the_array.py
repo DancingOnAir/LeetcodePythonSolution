df03dee6b7f22fec1641b9c5223ecf0070c357e1
DancingOnAir <taotong1984@hotmail.com>
2021-08-23 11:54:39 +0800

    feat(String): accepted dp python solution for q1416

--- String/1416_restore_the_array.py
+++ String/1416_restore_the_array.py
@@ -1,43 +1,30 @@
 class Solution:
     def numberOfArrays(self, s: str, k: int) -> int:
-        nums = list()
-        i = 0
-        while i < len(s):
-            j = i + 1
-            while j < len(s) and s[j] == '0':
-                j += 1
-            nums.append(int(s[i: j]))
-            i = j
+        MOD = 10 ** 9 + 7
+        m, n = len(str(k)), len(s)
+        #dp[i] 表示前i个数字进行恢复的方案数
+        dp = [1] + [0] * n
+        for i in range(1, n + 1):
+            num, base = 0, 1
+            j = i - 1
+            while j >= 0 and i - j <= m:
+                num += (ord(s[j]) - 48) * base
+                if num > k:
+                    break
+                if s[j] != '0':
+                    dp[i] += dp[j]
+                base *= 10
+                j -= 1
 
-        n = len(nums)
-        if nums[0] > k:
-            return 0
-
-        dp = [0] * n
-        dp[0] = 1
-
-        memo = [nums[0]]
-
-        for i in range(1, n):
-            if nums[i] > k:
-                return 0
-
-            for j, m in enumerate(memo):
-                new_num = m * 10 + nums[i]
-                if new_num <= k:
-                    memo[j] = new_num
-
-            dp[i] = dp[i - 1] + len(memo)
-            memo += [nums[i]] * dp[i - 1]
-
-        return dp[n - 1]
+            dp[i] %= MOD
+        return dp[n]
 
 
 def test_number_of_arrays():
     solution = Solution()
 
-    # assert solution.numberOfArrays("1000", 1000) == 1, 'wrong result'
-    # assert solution.numberOfArrays("1000", 10) == 0, 'wrong result'
+    assert solution.numberOfArrays("1000", 1000) == 1, 'wrong result'
+    assert solution.numberOfArrays("1000", 10) == 0, 'wrong result'
     assert solution.numberOfArrays("1317", 2000) == 8, 'wrong result'
     assert solution.numberOfArrays("2020", 30) == 1, 'wrong result'
     assert solution.numberOfArrays("1234567890", 90) == 34, 'wrong result'
