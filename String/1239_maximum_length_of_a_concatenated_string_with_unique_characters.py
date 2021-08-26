48d32546ec5c020cb78c4a0fd3cf7bb550f13369
DancingOnAir <taotong1984@hotmail.com>
2021-08-27 00:31:52 +0800

    feat(String): init q1239 & acceptd set python solution for q1239

---
+++ String/1239_maximum_length_of_a_concatenated_string_with_unique_characters.py
@@ -0,0 +1,30 @@
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def maxLength(self, arr: List[str]) -> int:
+        dp = [set()]
+
+        for a in arr:
+            if len(set(a)) < len(a):
+                continue
+            a = set(a)
+            for d in dp[:]:
+                if a & d:
+                    continue
+                dp.append(a | d)
+        return max(len(d) for d in dp)
+
+
+def test_max_length():
+    solution = Solution()
+
+    assert solution.maxLength(["un", "iq", "ue"]) == 4, 'wrong result'
+    assert solution.maxLength(["cha", "r", "act", "ers"]) == 6, 'wrong result'
+    assert solution.maxLength(["abcdefghijklmnopqrstuvwxyz"]) == 26, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_max_length()
+
