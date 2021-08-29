9259f19888f5d6ffe6a5a5b9eead516404eba3d1
DancingOnAir <taotong1984@hotmail.com>
2021-08-30 07:13:59 +0800

    feat(String): init q1154 & accepted python solution for q1154

---
+++ String/1154_day_of_the_year.py
@@ -0,0 +1,22 @@
+class Solution:
+    def dayOfYear(self, date: str) -> int:
+        def is_leap_year(y):
+            return (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0)
+
+        y, m, d = map(int, date.split('-'))
+        months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+        return d + sum(months[:m - 1]) + (1 if is_leap_year(y) and m > 2 else 0)
+
+
+def test_day_of_year():
+    solution = Solution()
+
+    assert solution.dayOfYear("2019-01-09") == 9, 'wrong result'
+    assert solution.dayOfYear("2019-02-10") == 41, 'wrong result'
+    assert solution.dayOfYear("2003-03-01") == 60, 'wrong result'
+    assert solution.dayOfYear("2004-03-01") == 61, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_day_of_year()
