3e771b4842a04ba4273dc62ac1c27d4f53cef293
DancingOnAir <taotong1984@hotmail.com>
2021-09-12 16:45:29 +0800

    feat(String): accepted wormholes python solution for q1190

--- String/1190_reverse_substrings_between_each_pair_of_parentheses.py
+++ String/1190_reverse_substrings_between_each_pair_of_parentheses.py
@@ -1,5 +1,30 @@
 class Solution:
+    # wormholes
     def reverseParentheses(self, s: str) -> str:
+        opened_bracket = list()
+        pair = dict()
+        for i, c in enumerate(s):
+            if c == '(':
+                opened_bracket.append(i)
+            elif c == ')':
+                j = opened_bracket.pop()
+                pair[i] = j
+                pair[j] = i
+
+        res = list()
+        i = 0
+        d = 1
+        while i < len(s):
+            if s[i] in '()':
+                i = pair[i]
+                d = -d
+            else:
+                res.append(s[i])
+            i += d
+        return ''.join(res)
+
+    # stack
+    def reverseParentheses1(self, s: str) -> str:
         stk = list()
         for c in s:
             if c == ')':
