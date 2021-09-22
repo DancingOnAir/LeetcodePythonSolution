11fe8de9493c4ca6e66fde9ed6f19057f2938028
DancingOnAir <taotong1984@hotmail.com>
2021-09-22 23:55:52 +0800

    feat(String): implemented manachers algorithm for calculating the longest palindrome for q1960

--- String/1960_maximum_product_of_the_length_of_two_palindromic_substrings.py
+++ String/1960_maximum_product_of_the_length_of_two_palindromic_substrings.py
@@ -1,5 +1,21 @@
 class Solution:
     def maxProduct(self, s: str) -> int:
+        def manachers(S):
+            A = '@#' + '#'.join(S) + '#$'
+            Z = [0] * len(A)
+            center = right = 0
+            for i in range(1, len(A) - 1):
+                if i < right:
+                    Z[i] = min(right - i, Z[2 * center - i])
+                while A[i + Z[i] + 1] == A[i - Z[i] - 1]:
+                    Z[i] += 1
+                if i + Z[i] > right:
+                    center, right = i, i + Z[i]
+            return Z[2: -2: 2]
+
+        # def helper(s):
+        #     man, n = manachers(s), len(s)
+        #     ints =
         pass
 
 
