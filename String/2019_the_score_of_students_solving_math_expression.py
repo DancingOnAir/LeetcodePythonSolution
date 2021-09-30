fea23996d044d9edfbbd27f095b90924b7c812f9
DancingOnAir <taotong1984@hotmail.com>
2021-09-30 16:48:11 +0800

    feat(String): init q2019 & tried a TLE python solution for q2019

---
+++ String/2019_the_score_of_students_solving_math_expression.py
@@ -0,0 +1,71 @@
+from typing import List
+
+
+class Solution:
+    def scoreOfStudents(self, s: str, answers: List[int]) -> int:
+        def cal(digits, ops, correct_cal_seq):
+            if len(digits) == 1:
+                if correct_cal_seq:
+                    nonlocal correct_result
+                    correct_result = digits[0]
+
+                results.add(digits[0])
+                return
+
+            for i in range(len(ops)):
+                if ops[i] == '+':
+                    res = digits[i] + digits[i + 1]
+                else:
+                    res = digits[i] * digits[i + 1]
+
+                new_pops = ops[:]
+                new_pops.pop(i)
+                new_digits = digits[:i] + [res] + digits[i + 2:]
+
+                if new_pops.count('*') == 0:
+                    if len(new_pops) == add_op or correct_cal_seq:
+                        cal(new_digits, new_pops, True)
+                    else:
+                        cal(new_digits, new_pops, False)
+                else:
+                    cal(new_digits, new_pops, False)
+
+        ops = list()
+        digits = list()
+        add_op = multiply_op = 0
+        for c in s:
+            if c.isdigit():
+                digits.append(int(c))
+            else:
+                if c == '+':
+                    add_op += 1
+                else:
+                    multiply_op += 1
+                ops.append(c)
+
+        results = set()
+        correct_result = -1
+
+        cal(digits, ops, False)
+
+        res = 0
+        for ans in answers:
+            if ans == correct_result:
+                res += 5
+            elif ans in results:
+                res += 2
+        return res
+
+
+def test_score_of_students():
+    solution = Solution()
+
+    assert solution.scoreOfStudents("7+3*1*2", [20, 13, 42]) == 7, 'wrong result'
+    assert solution.scoreOfStudents("3+5*2", [13, 0, 10, 13, 13, 16, 16]) == 19, 'wrong result'
+    assert solution.scoreOfStudents("6+0*1", [12, 9, 6, 4, 8, 6]) == 10, 'wrong result'
+    assert solution.scoreOfStudents("1+2*3+4", [13, 21, 11, 15]) == 11, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_score_of_students()
+
