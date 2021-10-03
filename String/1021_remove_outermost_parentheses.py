10c3f08e9d0b841cb7c0163173637ccd7caf46a2
DancingOnAir <taotong1984@hotmail.com>
2021-10-03 22:27:14 +0800

    feat(String): init q1021 & accepted stack python solution for q1021

---
+++ String/1021_remove_outermost_parentheses.py
@@ -0,0 +1,32 @@
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        stk = list()
+        depth = 0
+        res = ''
+        i = 0
+        while i < len(s):
+            while not stk or depth > 0:
+                if s[i] == '(':
+                    depth += 1
+                else:
+                    depth -= 1
+
+                stk.append(s[i])
+                i += 1
+
+            res += ''.join(stk)[1:-1]
+            stk = list()
+
+        return res
+
+
+def test_remove_outer_parentheses():
+    solution = Solution()
+
+    assert solution.removeOuterParentheses("(()())(())") == "()()()", 'wrong result'
+    assert solution.removeOuterParentheses("(()())(())(()(()))") == "()()()()(())", 'wrong result'
+    assert solution.removeOuterParentheses("()()") == "", 'wrong result'
+
+
+if __name__ == '__main__':
+    test_remove_outer_parentheses()
