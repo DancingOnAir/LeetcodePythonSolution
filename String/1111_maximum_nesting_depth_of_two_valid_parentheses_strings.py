951beb4bba3cc9f6f2d2e4f84ad3e089bc305e28
DancingOnAir <taotong1984@hotmail.com>
2021-10-04 09:55:07 +0800

    feat(String): init q1111 & accepted stack python solution for q1111

---
+++ String/1111_maximum_nesting_depth_of_two_valid_parentheses_strings.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def maxDepthAfterSplit(self, seq: str) -> List[int]:
+        res = list()
+        stk = list()
+        cnt = 0
+
+        for c in seq:
+            if c == '(':
+                cnt += 1
+                res += [0] if cnt & 1 else [1]
+            else:
+                res += [0] if cnt & 1 else [1]
+                cnt -= 1
+        return res
+
+
+def test_max_depth_after_split():
+    solution = Solution()
+
+    assert solution.maxDepthAfterSplit("(()())") == [0, 1, 1, 1, 1, 0]
+    assert solution.maxDepthAfterSplit("()(())()") == [0, 0, 0, 1, 1, 0, 1, 1]
+
+
+if __name__ == '__main__':
+    test_max_depth_after_split()
