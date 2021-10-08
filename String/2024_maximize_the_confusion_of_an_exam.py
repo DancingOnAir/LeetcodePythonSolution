f3e7e49157fa04ebf441d6996677d94cb584abfb
DancingOnAir <taotong1984@hotmail.com>
2021-10-09 07:36:07 +0800

    feat(String): init q2024 & accepted sliding window python solution for q2024

---
+++ String/2024_maximize_the_confusion_of_an_exam.py
@@ -0,0 +1,28 @@
+from collections import Counter
+
+
+class Solution:
+    # standard sliding window
+    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+        max_freq = i = 0
+        c = Counter()
+        for j, ans in enumerate(answerKey):
+            c[ans] += 1
+            max_freq = max(max_freq, c[ans])
+            if j - i + 1 > max_freq + k:
+                c[answerKey[i]] -= 1
+                i += 1
+
+        return len(answerKey) - i
+
+
+def test_max_consecutive_answers():
+    solution = Solution()
+
+    assert solution.maxConsecutiveAnswers("TTFF", 2) == 4, 'wrong result'
+    assert solution.maxConsecutiveAnswers("TFFT", 1) == 3, 'wrong result'
+    assert solution.maxConsecutiveAnswers("TTFTTFTT", 1) == 5, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_max_consecutive_answers()
