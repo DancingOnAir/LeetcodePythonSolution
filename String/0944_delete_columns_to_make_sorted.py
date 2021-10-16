3e36a95ab3fdf2e29f65893c39341ac320744dc2
DancingOnAir <taotong1984@hotmail.com>
2021-10-17 00:28:53 +0800

    feat(String): init q0944 & accepted zip python solution for q0944

---
+++ String/0944_delete_columns_to_make_sorted.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        res = 0
+        for col in zip(*strs):
+            for i in range(1, len(col)):
+                if col[i - 1] > col[i]:
+                    res += 1
+                    break
+        return res
+
+
+def test_min_deletion_size():
+    solution = Solution()
+
+    assert solution.minDeletionSize(["cba", "daf", "ghi"]) == 1, 'wrong result'
+    assert solution.minDeletionSize(["a", "b"]) == 0, 'wrong result'
+    assert solution.minDeletionSize(["zyx", "wvu", "tsr"]) == 3, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_min_deletion_size()
