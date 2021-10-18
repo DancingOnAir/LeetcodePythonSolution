9cf322b603f88c45961ad8432184f42ee1765da2
DancingOnAir <taotong1984@hotmail.com>
2021-10-18 17:04:36 +0800

    feat(String): accepted pythonic solution for q1041

--- String/1041_RobotBoundedInCircle.py
+++ String/1041_RobotBoundedInCircle.py
@@ -1,5 +1,16 @@
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
+        x, y, dx, dy = 0, 0, 0, 1
+        for i in instructions:
+            if i == 'R':
+                dx, dy = dy, -dx
+            elif i == 'L':
+                dx, dy = -dy, dx
+            else:
+                x, y = x + dx, y + dy
+        return (x, y) == (0, 0) or (dx, dy) != (0, 1)
+
+    def isRobotBounded1(self, instructions: str) -> bool:
         pos = [0, 0]
         direction = [[0, 1], [1, 0], [0, -1], [-1, 0]]
         direction_idx = 0
