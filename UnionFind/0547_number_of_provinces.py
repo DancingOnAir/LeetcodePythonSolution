91285ec5cf84f3071436a04f7d8507d539f2586f
DancingOnAir <taotong1984@hotmail.com>
2021-10-22 21:19:28 +0800

    feat(UnionFind): init q0547 & accepted union find python solution for q0547

---
+++ UnionFind/0547_number_of_provinces.py
@@ -0,0 +1,45 @@
+from typing import List
+
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        if 1 == n:
+            return 1
+
+        parents = list(range(200))
+
+        def find(idx):
+            while idx != parents[idx]:
+                idx = parents[idx]
+            return idx
+
+        def union(idx1, idx2):
+            p1 = find(idx1)
+            p2 = find(idx2)
+            if p1 == p2:
+                return
+            parents[p1] = p2
+
+        for i in range(n):
+            for j in range(i, n):
+                if isConnected[i][j] == 1:
+                    union(i, j)
+
+        res = set()
+        for i in range(n):
+            if find(i) not in res:
+                res.add(find(i))
+        return len(res)
+
+
+def test_find_circle_num():
+    solution = Solution()
+
+    assert solution.findCircleNum([[1, 0, 0, 1], [0, 1, 1, 0], [0, 1, 1, 1], [1, 0, 1, 1]]) == 1, 'wrong result'
+    assert solution.findCircleNum([[1, 1, 0], [1, 1, 0], [0, 0, 1]]) == 2, 'wrong result'
+    assert solution.findCircleNum([[1, 0, 0], [0, 1, 0], [0, 0, 1]]) == 3, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_find_circle_num()
