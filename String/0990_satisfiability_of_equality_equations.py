5071ae65ea55fdd1d8fe9368e5354aab08fd959b
DancingOnAir <taotong1984@hotmail.com>
2021-10-22 20:08:40 +0800

    feat(String): accepted union find pythonic solution for q0990

--- String/0990_satisfiability_of_equality_equations.py
+++ String/0990_satisfiability_of_equality_equations.py
@@ -2,6 +2,37 @@ from typing import List
 
 
 class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        parents = list(range(26))
+
+        def find(idx):
+            while idx != parents[idx]:
+                idx = parents[idx]
+            return idx
+
+        def union(idx1, idx2):
+            p1 = find(idx1)
+            p2 = find(idx2)
+
+            if p1 == p2:
+                return
+            parents[p1] = p2
+
+        for e in equations:
+            if e[1] == "=":
+                idx1 = ord(e[0]) - ord('a')
+                idx2 = ord(e[3]) - ord('a')
+                union(idx1, idx2)
+
+        for e in equations:
+            if e[1] == '!':
+                idx1 = ord(e[0]) - ord('a')
+                idx2 = ord(e[3]) - ord('a')
+                if find(idx1) == find(idx2):
+                    return False
+
+        return True
+
     class UnionFind(object):
         def __init__(self):
             self.parent = list(range(26))
@@ -16,7 +47,7 @@ class Solution:
         def union(self, index1, index2):
             self.parent[self.find(index1)] = self.find(index2)
 
-    def equationsPossible(self, equations: List[str]) -> bool:
+    def equationsPossible1(self, equations: List[str]) -> bool:
         uf = Solution.UnionFind()
         for e in equations:
             if e[1] == "=":
