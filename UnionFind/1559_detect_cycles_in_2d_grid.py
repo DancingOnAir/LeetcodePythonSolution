9da85e638c751c5e26e74553a4e7d61ddb08bd25
DancingOnAir <taotong1984@hotmail.com>
2021-10-28 23:52:30 +0800

    feat(UnionFind): init q1559 & accepted union find python solution for q1559

--- UnionFind/1559_detect_cycles_in_2d_grid.py
+++ UnionFind/1559_detect_cycles_in_2d_grid.py
@@ -28,7 +28,52 @@ class UnionFind(object):
 
 
 class Solution:
+    class UF(object):
+        def __init__(self, n):
+            self.parent = list(range(n))
+            self.weight = [1] * n
+
+        def find(self, p):
+            if p != self.parent[p]:
+                p = self.find(self.parent[p])
+            return p
+
+        def unite(self, p, q):
+            r1 = self.find(p)
+            r2 = self.find(q)
+
+            if r1 != r2:
+                if self.weight[r1] < self.weight[r2]:
+                    r1, r2 = r2, r1
+                self.parent[r2] = r1
+                self.weight[r1] += self.weight[r2]
+
     def containsCycle(self, grid: List[List[str]]) -> bool:
+        row = len(grid)
+        col = len(grid[0])
+        uf = Solution.UF(row * col)
+
+        for i in range(row):
+            for j in range(col):
+                roots = set()
+                for di, dj in [[0, -1], [-1, 0]]:
+                    ni = i + di
+                    nj = j + dj
+
+                    if ni < 0 or nj < 0 or grid[i][j] != grid[ni][nj]:
+                        continue
+
+                    root = uf.find(ni * col + nj)
+                    if root in roots:
+                        return True
+                    roots.add(root)
+
+                for r in roots:
+                    uf.unite(r, i * col + j)
+
+        return False
+
+    def containsCycle1(self, grid: List[List[str]]) -> bool:
         row = len(grid)
         col = len(grid[0])
         uf = UnionFind(row * col)
