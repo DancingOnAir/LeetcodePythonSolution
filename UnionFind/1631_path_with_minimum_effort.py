93f036ba8203d19f8988cd665d87762509008acd
DancingOnAir <taotong1984@hotmail.com>
2021-10-31 23:11:51 +0800

    feat(UnionFind): accepted path compression union find python solution for q1631

--- UnionFind/1631_path_with_minimum_effort.py
+++ UnionFind/1631_path_with_minimum_effort.py
@@ -2,8 +2,61 @@ from typing import List
 
 
 class Solution:
+    class UF(object):
+        def __init__(self, n):
+            self.parent = list(range(n))
+            self.sz = [1] * n
+            self.cnt = n
+
+        def find(self, p):
+            while p != self.parent[p]:
+                self.parent[p] = self.parent[self.parent[p]]
+                p = self.parent[p]
+            return p
+
+        def unite(self, p, q):
+            rp = self.find(p)
+            rq = self.find(q)
+
+            if rp == rq:
+                return
+
+            if self.sz[rp] < self.sz[rq]:
+                self.parent[rp] = rq
+                self.sz[rq] += self.sz[rp]
+            else:
+                self.parent[rq] = rp
+                self.sz[rp] += self.sz[rq]
+            self.cnt -= 1
+
+        def connnected(self, p, q):
+            rp = self.find(p)
+            rq = self.find(q)
+            return rp == rq
+
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        pass
+        row = len(heights)
+        col = len(heights[0])
+
+        edges = list()
+        for i in range(row):
+            for j in range(col):
+                idx = i * col + j
+                if i > 0:
+                    edges.append((idx - col, idx, abs(heights[i][j] - heights[i - 1][j])))
+                if j > 0:
+                    edges.append((idx - 1, idx, abs(heights[i][j] - heights[i][j - 1])))
+
+        edges.sort(key=lambda x: x[2])
+
+        uf = Solution.UF(row * col)
+        res = 0
+        for i, j, v in edges:
+            uf.unite(i, j)
+            if uf.connnected(0, row * col - 1):
+                res = v
+                break
+        return res
 
 
 def test_minimum_effort_path():
