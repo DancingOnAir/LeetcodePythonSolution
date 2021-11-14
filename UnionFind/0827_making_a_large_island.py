511c7479a20934a2c821086ab7acdf871aa4303a
DancingOnAir <taotong1984@hotmail.com>
2021-11-15 00:00:23 +0800

    feat(union find): accepted improved union find python solution for q0827

--- UnionFind/0827_making_a_large_island.py
+++ UnionFind/0827_making_a_large_island.py
@@ -20,7 +20,7 @@ class Solution:
             rq = self.find(q)
 
             if rp == rq:
-                return
+                return False
 
             if self.sz[rp] < self.sz[rq]:
                 self.parent[rp] = rq
@@ -29,6 +29,14 @@ class Solution:
                 self.parent[rq] = rp
                 self.sz[rp] += self.sz[rq]
             self.cnt -= 1
+
+            return True
+
+        def isConnnected(self, p, q):
+            return self.find(p) == self.find(q)
+
+        def getSize(self, p):
+            return self.sz[self.find(p)]
 
     def largestIsland(self, grid: List[List[int]]) -> int:
         n = len(grid)
@@ -40,33 +48,49 @@ class Solution:
                         if 0 <= x < n and 0 <= y < n and grid[x][y] == 1:
                             uf.unite(i * n + j, x * n + y)
 
-        idx_to_area = defaultdict(int)
-        idx_to_neighbor = defaultdict(list)
+        res = max(uf.sz)
         for i in range(n):
             for j in range(n):
-                if grid[i][j] == 1:
-                    p = uf.find(i * n + j)
-
-                    idx_to_area[p] += 1
+                if grid[i][j] == 0:
+                    cur = 1
+                    rs = set()
                     for x, y in (i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j):
-                        if 0 <= x < n and 0 <= y < n and grid[x][y] == 0:
-                            # if idx_to_neighbor[p][-1]:
-                            idx_to_neighbor[p].append(set())
-
-                            for a, b in (x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y):
-                                if 0 <= a < n and 0 <= b < n and (a, b) != (i, j) and grid[a][b] == 1:
-                                    q = uf.find(a * n + b)
-                                    if p != q:
-                                        idx_to_neighbor[p][-1].add(q)
-
-        if not idx_to_neighbor:
-            return min((max(idx_to_area.values()) if idx_to_area else 0) + 1, n * n)
-
-        res = 0
-        for p, qs in idx_to_neighbor.items():
-            for q in qs:
-                res = max(res, idx_to_area[p] + sum(idx_to_area[i] for i in q) + 1)
+                        if 0 <= x < n and 0 <= y < n and grid[x][y] == 1:
+                            p = uf.find(x * n + y)
+                            if p not in rs:
+                                cur += uf.sz[p]
+                                rs.add(p)
+                    res = max(res, cur)
         return res
+
+        # original solution
+        # idx_to_area = defaultdict(int)
+        # idx_to_neighbor = defaultdict(list)
+        # for i in range(n):
+        #     for j in range(n):
+        #         if grid[i][j] == 1:
+        #             p = uf.find(i * n + j)
+        #
+        #             idx_to_area[p] += 1
+        #             for x, y in (i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j):
+        #                 if 0 <= x < n and 0 <= y < n and grid[x][y] == 0:
+        #                     # if idx_to_neighbor[p][-1]:
+        #                     idx_to_neighbor[p].append(set())
+        #
+        #                     for a, b in (x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y):
+        #                         if 0 <= a < n and 0 <= b < n and (a, b) != (i, j) and grid[a][b] == 1:
+        #                             q = uf.find(a * n + b)
+        #                             if p != q:
+        #                                 idx_to_neighbor[p][-1].add(q)
+        #
+        # if not idx_to_neighbor:
+        #     return min((max(idx_to_area.values()) if idx_to_area else 0) + 1, n * n)
+        #
+        # res = 0
+        # for p, qs in idx_to_neighbor.items():
+        #     for q in qs:
+        #         res = max(res, idx_to_area[p] + sum(idx_to_area[i] for i in q) + 1)
+        # return res
 
 
 def test_largest_island():
