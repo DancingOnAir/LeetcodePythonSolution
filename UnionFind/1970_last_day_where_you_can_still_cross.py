146c6b62db517b477cfe12476c48f8285d1720bd
DancingOnAir <taotong1984@hotmail.com>
2021-11-16 12:03:01 +0800

    feat(union find): accepted another union find python solution for q1970

--- UnionFind/1970_last_day_where_you_can_still_cross.py
+++ UnionFind/1970_last_day_where_you_can_still_cross.py
@@ -42,7 +42,9 @@ class Solution:
         def isConnected(self, p, q):
             return self.find(p) == self.find(q)
 
-    def latestDayToCross(self, row: int, col: int, cells: List[List[int]]) -> int:
+    # join the water not land
+    # https://leetcode.com/problems/last-day-where-you-can-still-cross/discuss/1403988/Python-3-Union-Find-join-the-water-not-the-land-explanation-with-pictures.
+    def latestDayToCross1(self, row: int, col: int, cells: List[List[int]]) -> int:
         def neighbors(r, c):
             for dr in (-1, 0, 1):
                 for dc in (-1, 0, 1):
@@ -65,6 +67,63 @@ class Solution:
 
             seen.add((x, y))
         return row * col
+
+    # initial all the elements of graph to 1(blocked), reverse loop the cells, change 1 to 0, join the lands until it can go through.
+    # https://leetcode.com/problems/last-day-where-you-can-still-cross/discuss/1403930/Python-Union-Find-solution-explained
+    class UF2(object):
+        def __init__(self, n):
+            self.parent = list(range(n))
+            self.sz = [1] * n
+
+        def find(self, p):
+            while p != self.parent[p]:
+                self.parent[p] = self.parent[self.parent[p]]
+                p = self.parent[p]
+
+            return p
+
+        def unite(self, p, q):
+            rp = self.find(p)
+            rq = self.find(q)
+
+            if rp == rq:
+                return False
+
+            if self.sz[rp] < self.sz[rq]:
+                rp, rq = rq, rp
+
+            self.parent[rq] = rp
+            self.sz[rp] += self.sz[rq]
+
+            return True
+
+    def latestDayToCross(self, row: int, col: int, cells: List[List[int]]) -> int:
+        def neighbors(r, c):
+            for dr, dc in (-1, 0), (1, 0), (0, -1), (0, 1):
+                if row > r + dr >= 0 <= c + dc < col:
+                    yield r + dr, c + dc
+
+        def index(r, c):
+            return r * col + c + 1
+
+        uf = Solution.UF2(row * col + 2)
+        grid = [[1] * col for _ in range(row)]
+        # cells = [x - 1for x, y in cells]
+        for i in range(len(cells) - 1, -1, -1):
+            x, y = cells[i][0] - 1, cells[i][1] - 1
+            grid[x][y] = 0
+
+            for nx, ny in neighbors(x, y):
+                if grid[nx][ny] == 0:
+                    uf.unite(index(x, y), index(nx, ny))
+
+            if 0 == x:
+                uf.unite(0, index(x, y))
+            if row - 1 == x:
+                uf.unite(row * col + 1, index(x, y))
+
+            if uf.find(0) == uf.find(row * col + 1):
+                return i
 
 
 def test_latest_day_to_cross():
