4e1f5d7b6fab4e77e8c36b5bb3abf78aaf5acf1f
DancingOnAir <taotong1984@hotmail.com>
2021-11-18 10:09:56 +0800

    feat(HashTable): accepted brute force python solution for q2068

--- HashTable/2068_check_whether_two_strings_are_almost_equivalent.py
+++ HashTable/2068_check_whether_two_strings_are_almost_equivalent.py
@@ -3,6 +3,13 @@ from collections import Counter
 
 class Solution:
     def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
+        freq = [0] * 26
+        for i in range(len(word1)):
+            freq[ord(word1[i]) - ord('a')] += 1
+            freq[ord(word2[i]) - ord('a')] -= 1
+        return all(abs(x) < 4 for x in freq)
+
+    def checkAlmostEquivalent1(self, word1: str, word2: str) -> bool:
         c = (Counter(word1) - Counter(word2)) | (Counter(word2) - Counter(word1))
         return all(v < 4 for v in c.values()) if len(c) > 0 else True
 
