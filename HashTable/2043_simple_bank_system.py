4950f610f3e840fe7bc261028af7d299729f6f4f
DancingOnAir <taotong1984@hotmail.com>
2021-11-19 07:53:21 +0800

    feat(HashTable): accepted list python solution for q2043

--- HashTable/2043_simple_bank_system.py
+++ HashTable/2043_simple_bank_system.py
@@ -3,6 +3,31 @@ from typing import List
 
 class Bank:
     def __init__(self, balance: List[int]):
+        self.balance = balance
+        self.n = len(balance)
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if self.withdraw(account1, money):
+            if self.deposit(account2, money):
+                return True
+            self.deposit(account1, money)
+        return False
+
+    def deposit(self, account: int, money: int) -> bool:
+        if 1 <= account <= self.n:
+            self.balance[account - 1] += money
+            return True
+        return False
+
+    def withdraw(self, account: int, money: int) -> bool:
+        if 1 <= account <= self.n and self.balance[account - 1] >= money:
+            self.balance[account - 1] -= money
+            return True
+        return False
+
+
+class Bank1:
+    def __init__(self, balance: List[int]):
         self.balance = {i + 1: v for i, v in enumerate(balance)}
 
     def transfer(self, account1: int, account2: int, money: int) -> bool:
