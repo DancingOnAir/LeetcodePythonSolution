6e2410fa13bd292ab9bba18d85857f9b3683bdb4
DancingOnAir <taotong1984@hotmail.com>
2021-11-19 07:37:21 +0800

    feat(HashTable): init q2043 & accepted dict python solution for q2043

---
+++ HashTable/2043_simple_bank_system.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+class Bank:
+    def __init__(self, balance: List[int]):
+        self.balance = {i + 1: v for i, v in enumerate(balance)}
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if account1 not in self.balance or account2 not in self.balance or self.balance[account1] < money:
+            return False
+
+        self.balance[account1] -= money
+        self.balance[account2] += money
+        return True
+
+    def deposit(self, account: int, money: int) -> bool:
+        if account not in self.balance:
+            return False
+
+        self.balance[account] += money
+        return True
+
+    def withdraw(self, account: int, money: int) -> bool:
+        if account not in self.balance or self.balance[account] < money:
+            return False
+
+        self.balance[account] -= money
+        return True
+
+
+def test_bank():
+    bank = Bank([10, 100, 20, 50, 30])
+    assert bank.withdraw(3, 10), 'wrong result'
+    assert bank.transfer(5, 1, 20), 'wrong result'
+    assert bank.deposit(5, 20), 'wrong result'
+    assert not bank.transfer(3, 4, 15), 'wrong result'
+    assert not bank.withdraw(10, 50), 'wrong result'
+
+
+if __name__ == '__main__':
+    test_bank()
