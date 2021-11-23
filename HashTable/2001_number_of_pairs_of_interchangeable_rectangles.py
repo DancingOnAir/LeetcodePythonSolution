93952a3ef280d25478a92e3a2b57df40e2963d9a
DancingOnAir <taotong1984@hotmail.com>
2021-11-24 07:44:45 +0800

    feat(HashTable): accepted gcd + Counter python solution for q2001

--- HashTable/2001_number_of_pairs_of_interchangeable_rectangles.py
+++ HashTable/2001_number_of_pairs_of_interchangeable_rectangles.py
@@ -4,6 +4,22 @@ from collections import Counter
 
 class Solution:
     def interchangeableRectangles(self, rectangles: List[List[int]]) -> int:
+        def gcd(a, b):
+            return a if b == 0 else gcd(b, a%b)
+
+        ratio = Counter()
+        res = 0
+        for rect in rectangles:
+            s = gcd(rect[0], rect[1])
+            w = rect[0] // s
+            h = rect[1] // s
+
+            if (w, h) in ratio:
+                res += ratio[(w, h)]
+            ratio[(w, h)] += 1
+        return res
+
+    def interchangeableRectangles1(self, rectangles: List[List[int]]) -> int:
         c = Counter()
         for rec in rectangles:
             c[rec[1] / rec[0]] += 1
