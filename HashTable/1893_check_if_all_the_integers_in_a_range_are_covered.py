66109ee9f5766e4ed28039fc5929bc6517fe95e2
DancingOnAir <taotong1984@hotmail.com>
2021-11-26 00:03:35 +0800

    feat(HashTable): init q1893 & accepted set python solution for q1893

---
+++ HashTable/1893_check_if_all_the_integers_in_a_range_are_covered.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        x = set(range(left, right+1))
+        for r in ranges:
+            x -= set(range(r[0], r[1]+1))
+        return len(x) == 0
+
+
+def test_is_covered():
+    solution = Solution()
+    assert solution.isCovered([[1, 2], [3, 4], [5, 6]], 2, 5), 'wrong result'
+    assert not solution.isCovered([[1, 10], [10, 20]], 21, 21), 'wrong result'
+
+
+if __name__ == '__main__':
+    test_is_covered()
