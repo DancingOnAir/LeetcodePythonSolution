77b38a17d5605fb583e93c75719ae0c687884238
DancingOnAir <taotong1984@hotmail.com>
2021-11-26 00:06:46 +0800

    feat(HashTable): accepted straight forword python solution for q1893

--- HashTable/1893_check_if_all_the_integers_in_a_range_are_covered.py
+++ HashTable/1893_check_if_all_the_integers_in_a_range_are_covered.py
@@ -3,6 +3,15 @@ from typing import List
 
 class Solution:
     def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        cnt = 0
+        for x in range(left, right + 1):
+            for l, r in ranges:
+                if l <= x <= r:
+                    cnt += 1
+                    break
+        return cnt == right - left + 1
+
+    def isCovered1(self, ranges: List[List[int]], left: int, right: int) -> bool:
         x = set(range(left, right+1))
         for r in ranges:
             x -= set(range(r[0], r[1]+1))
