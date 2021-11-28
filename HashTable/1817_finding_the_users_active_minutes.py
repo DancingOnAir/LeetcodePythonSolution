b83c416bfb74e8c51516ada442de8b0d6e2b484b
DancingOnAir <taotong1984@hotmail.com>
2021-11-29 00:13:30 +0800

    feat(HashTable): init q1817 & accepted deafultdict python solution for q1817

---
+++ HashTable/1817_finding_the_users_active_minutes.py
@@ -0,0 +1,28 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def findingUsersActiveMinutes(self, logs: List[List[int]], k: int) -> List[int]:
+        uam = defaultdict(set)
+        for log in logs:
+            uam[log[0]].add(log[1])
+
+        m = defaultdict(int)
+        for idx, times in uam.items():
+            m[len(times)] += 1
+
+        res = list()
+        for i in range(1, k + 1):
+            res.append(m[i])
+        return res
+
+
+def test_finding_users_active_minutes():
+    solution = Solution()
+    assert solution.findingUsersActiveMinutes([[0, 5], [1, 2], [0, 2], [0, 5], [1, 3]], 5) == [0, 2, 0, 0, 0], 'wrong result'
+    assert solution.findingUsersActiveMinutes([[1, 1], [2, 2], [2, 3]], 4) == [1, 1, 0, 0], 'wrong result'
+
+
+if __name__ == '__main__':
+    test_finding_users_active_minutes()
