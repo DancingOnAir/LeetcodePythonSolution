76f68608ffe51cf61c6142f9428321769a3a4f9e
DancingOnAir <taotong1984@hotmail.com>
2021-12-07 09:35:25 +0800

    feat(HashTable): accepted python solution for q1711

--- HashTable/1711_count_good_meals.py
+++ HashTable/1711_count_good_meals.py
@@ -3,7 +3,7 @@ from collections import Counter
 
 
 class Solution:
-    def countPairs(self, deliciousness: List[int]) -> int:
+    def countPairs1(self, deliciousness: List[int]) -> int:
         freq = Counter(deliciousness)
 
         res = 0
@@ -17,9 +17,26 @@ class Solution:
 
         return res % (10 ** 9 + 7)
 
+    def countPairs(self, deliciousness: List[int]) -> int:
+        freq = Counter(deliciousness)
+        max_two_sum = sum(sorted(deliciousness)[-2:])
+
+        res = 0
+        for x in freq:
+            k = 1
+            while k <= max_two_sum:
+                if k - x <= x and k - x in freq:
+                    if x == k // 2:
+                        res += freq[x] * (freq[x] - 1) // 2
+                    else:
+                        res += freq[x] * freq[k - x]
+                k <<= 1
+        return res % (10 ** 9 + 7)
+
 
 def test_count_pairs():
     solution = Solution()
+    assert solution.countPairs([64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64]) == 528, 'wrong result'
     assert solution.countPairs([1, 3, 5, 7, 9]) == 4, 'wrong result'
     assert solution.countPairs([1, 1, 1, 3, 3, 3, 7]) == 15, 'wrong result'
 
