555fb7cf433523c338c64af7552540cf6cb870b2
DancingOnAir <taotong1984@hotmail.com>
2021-12-10 10:34:27 +0800

    feat(HashTable): init q1742 & accepted math python solution for q1742

--- HashTable/1742_maximum_number_of_balls_in_a_box.py
+++ HashTable/1742_maximum_number_of_balls_in_a_box.py
@@ -2,6 +2,22 @@ from collections import defaultdict
 
 
 class Solution:
+    # math
+    def countBalls(self, lowLimit: int, highLimit: int) -> int:
+        # high limit <= 10 ** 5, the max digit sum is 9 * 5 = 45
+        box = [0] * 46
+        idx = sum(map(int, str(lowLimit)))
+
+        for x in range(lowLimit, highLimit+1):
+            while x % 10 == 0:
+                idx -= 9
+                x //= 10
+
+            box[idx] += 1
+            idx += 1
+        return max(box)
+
+    # brute force
     def countBalls(self, lowLimit: int, highLimit: int) -> int:
         freq = defaultdict(int)
         for x in range(lowLimit, highLimit+1):
