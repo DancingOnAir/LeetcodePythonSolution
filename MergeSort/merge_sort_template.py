fd2148fdb6562defdbea264bf0f629dde679cb74
DancingOnAir <taotong1984@hotmail.com>
2022-01-11 22:28:19 +0800

    feat(merge_sort_template): implemented merge sort top-down algorithm

--- MergeSort/merge_sort_template.py
+++ MergeSort/merge_sort_template.py
@@ -1,25 +1,27 @@
 class MergeSort:
     def __init__(self, arr):
         self.arr = arr
-        self.aux = ['' for _ in range(len(arr))]
+        # self.aux = ['' for _ in range(len(arr))]
+        self.merge_sort(0, len(self.arr) - 1)
 
     def merge(self, lo, mid, hi):
         i, j = lo, mid + 1
+        aux = self.arr[lo: hi+1]
+        # for k in range(lo, hi + 1):
+        #     self.aux[k] = self.arr[k]
 
         for k in range(lo, hi + 1):
-            self.aux[k] = self.arr[k]
-        for k in range(lo, hi + 1):
             if i > mid:
-                self.arr[k] = self.aux[j]
+                self.arr[k] = aux[j - lo]
                 j += 1
             elif j > hi:
-                self.arr[k] = self.aux[i]
+                self.arr[k] = aux[i - lo]
                 i += 1
-            elif self.arr[i] > self.arr[j]:
-                self.arr[k] = self.aux[j]
+            elif aux[i - lo] > aux[j - lo]:
+                self.arr[k] = aux[j - lo]
                 j += 1
             else:
-                self.arr[k] = self.aux[i]
+                self.arr[k] = aux[i - lo]
                 i += 1
 
     def merge_sort(self, lo, hi):
@@ -31,14 +33,11 @@ class MergeSort:
         self.merge_sort(mid + 1, hi)
         self.merge(lo, mid, hi)
 
-    def sort(self):
-        self.merge_sort(0, len(self.arr) - 1)
-
 
 def test_merge_sort():
     a = ['M', 'E', 'R', 'G', 'E', 'S', 'O', 'R', 'T', 'E', 'X', 'A', 'M', 'P', 'L', 'E']
+    print(a)
     merge_sort = MergeSort(a)
-    merge_sort.sort()
     print(merge_sort.arr)
 
     pass
