11490f1774c466985827f95915918096ac2accac
DancingOnAir <taotong1984@hotmail.com>
2022-01-22 14:04:01 +0800

    feat(HashTable): accepted bytearry python solution for q2133

--- HashTable/2133_check_if_every_row_and_column_contains_all_numbe.py
+++ HashTable/2133_check_if_every_row_and_column_contains_all_numbe.py
@@ -3,10 +3,28 @@ from collections import Counter
 
 
 class Solution:
+    # bytearray
     def checkValid(self, matrix: List[List[int]]) -> bool:
+        n = len(matrix)
+
+        for r in range(n):
+            row = bytearray(n + 1)
+            col = bytearray(n + 1)
+            for c in range(n):
+                ro, co = matrix[r][c], matrix[c][r]
+                row[ro] += 1
+                col[co] += 1
+
+                if row[ro] > 1 or col[co] > 1:
+                    return False
+        return True
+
+    # one line by measuring length of row & col
+    def checkValid2(self, matrix: List[List[int]]) -> bool:
         return all(len(set(m)) == len(matrix) for m in matrix + list(zip(*matrix)))
 
-    def checkValid(self, matrix: List[List[int]]) -> bool:
+    # Counter
+    def checkValid1(self, matrix: List[List[int]]) -> bool:
         c = Counter(range(1, len(matrix) + 1))
 
         for row in matrix:
