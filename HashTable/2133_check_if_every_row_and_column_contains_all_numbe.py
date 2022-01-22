e782f9ee1618133da3be9af5916210db86102d04
DancingOnAir <taotong1984@hotmail.com>
2022-01-22 11:42:41 +0800

    feat(HashTable): init q2133 & accepted python solution for q2133:

---
+++ HashTable/2133_check_if_every_row_and_column_contains_all_numbe.py
@@ -0,0 +1,26 @@
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def checkValid(self, matrix: List[List[int]]) -> bool:
+        c = Counter(range(1, len(matrix) + 1))
+
+        for row in matrix:
+            if len(c - Counter(row)) > 0:
+                return False
+        for col in zip(*matrix):
+            if len(c - Counter(col)) > 0:
+                return False
+        return True
+
+
+def test_check_valid():
+    solution = Solution()
+
+    assert solution.checkValid([[1, 2, 3], [3, 1, 2], [2, 3, 1]]), 'wrong result'
+    assert not solution.checkValid([[1, 1, 1], [1, 2, 3], [1, 2, 3]]), 'wrong result'
+
+
+if __name__ == '__main__':
+    test_check_valid()
