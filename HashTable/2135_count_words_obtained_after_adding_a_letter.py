05b9a0c19408db2f1533d8cde9d2f3ff6db7cd64
DancingOnAir <taotong1984@hotmail.com>
2022-01-23 10:46:38 +0800

    feat(HashTable): init q2135 & accepted bitmask python solution for q2135

---
+++ HashTable/2135_count_words_obtained_after_adding_a_letter.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class Solution:
+    def wordCount(self, startWords: List[str], targetWords: List[str]) -> int:
+        seen = set()
+        for w in startWords:
+            cur = 0
+            for c in w:
+                cur ^= 1 << (ord(c) - ord('a'))
+            seen.add(cur)
+
+        res = 0
+        for w in targetWords:
+            cur = 0
+            for c in w:
+                cur ^= 1 << (ord(c) - ord('a'))
+            for c in w:
+                if cur ^ (1 << (ord(c) - ord('a'))) in seen:
+                    res += 1
+                    break
+        return res
+
+
+
+def test_word_count():
+    solution = Solution()
+
+    assert solution.wordCount(["ant", "act", "tack"], ["tack", "act", "acti"]) == 2, 'wrong result'
+    assert solution.wordCount(["ab", "a"], ["abc", "abcd"]) == 1, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_word_count()
+
