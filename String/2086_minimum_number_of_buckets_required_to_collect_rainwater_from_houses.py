8474a53772281ef95c53b1eeb7b418fd05e144ab
DancingOnAir <taotong1984@hotmail.com>
2022-01-29 21:50:28 +0800

    feat(String): accepted dp python solution for q2086

--- String/2086_minimum_number_of_buckets_required_to_collect_rainwater_from_houses.py
+++ String/2086_minimum_number_of_buckets_required_to_collect_rainwater_from_houses.py
@@ -1,8 +1,27 @@
 class Solution:
-    def minimumBuckets(self, street: str) -> int:
+    # greedy
+    def minimumBuckets1(self, street: str) -> int:
         if street == 'H' or street.startswith('HH') or street.endswith('HH') or 'HHH' in street:
             return -1
         return street.count('H') - street.count('H.H')
+
+    # dp
+    def minimumBuckets(self, street: str) -> int:
+        street = list(street)
+        res = 0
+        for i, val in enumerate(street):
+            if val == 'H':
+                if i > 0 and street[i - 1] == 'B':
+                    continue
+                elif i < len(street) - 1 and street[i + 1] == '.':
+                    res += 1
+                    street[i + 1] = 'B'
+                elif i > 0 and street[i - 1] == '.':
+                    res += 1
+                    street[i - 1] = 'B'
+                else:
+                    return -1
+        return res
 
 
 def test_minimum_buckets():
