88cca9e53bcd800f6944e06d70d90ce30a1b1e6b
DancingOnAir <taotong1984@hotmail.com>
2022-02-07 14:33:02 +0800

    feat(String): accepted forward python solution for q2109

--- String/2109_adding_spaces_to_a_string.py
+++ String/2109_adding_spaces_to_a_string.py
@@ -2,16 +2,26 @@ from typing import List
 
 
 class Solution:
+
     def addSpaces(self, s: str, spaces: List[int]) -> str:
-        pass
+        res = list()
+        j = 0
+        for i, ch in enumerate(s):
+            if j < len(spaces) and i == spaces[j]:
+                res.append(' ')
+                j += 1
+
+            res.append(ch)
+
+        return ''.join(res)
 
 
-def test_add_spaces():
+def AddSpaces():
     solution = Solution()
-    assert solution.addSpaces("LeetcodeHelpsMeLearn", [8,13,15]) == "Leetcode Helps Me Learn", 'wrong result'
-    assert solution.addSpaces("icodeinpython", [1,5,7,9]) == "i code in py thon", 'wrong result'
-    assert solution.addSpaces("spacing", [0,1,2,3,4,5,6]) == " s p a c i n g", 'wrong result'
+    assert solution.addSpaces("LeetcodeHelpsMeLearn", [8, 13, 15]) == "Leetcode Helps Me Learn", 'wrong result'
+    assert solution.addSpaces("icodeinpython", [1, 5, 7, 9]) == "i code in py thon", 'wrong result'
+    assert solution.addSpaces("spacing", [0, 1, 2, 3, 4, 5, 6]) == " s p a c i n g", 'wrong result'
 
 
 if __name__ == '__main__':
-    test_add_spaces()
+    AddSpaces()
