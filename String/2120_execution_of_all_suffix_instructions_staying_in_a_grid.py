2c991e525689fd7bb2e5a1a67215fcc6065cac05
DancingOnAir <taotong1984@hotmail.com>
2022-02-12 00:03:12 +0800

    feat(String): accepted python solution for q2120

--- String/2120_execution_of_all_suffix_instructions_staying_in_a_grid.py
+++ String/2120_execution_of_all_suffix_instructions_staying_in_a_grid.py
@@ -3,7 +3,29 @@ from typing import List
 
 class Solution:
     def executeInstructions(self, n: int, startPos: List[int], s: str) -> List[int]:
-        return [1, 5, 4, 3, 1, 0]
+        def get_num_instruction(s):
+            # x, y
+            moves = [0, 0]
+            for i, ch in enumerate(s):
+                if ch == 'R':
+                    moves[0] += 1
+                elif ch == 'L':
+                    moves[0] -= 1
+                elif ch == 'U':
+                    moves[1] += 1
+                else:
+                    moves[1] -= 1
+
+                if (moves[0] > 0 and moves[0] > limits[0]) or (moves[0] < 0 and abs(moves[0]) > limits[2]) or (moves[1] > 0 and moves[1] > limits[3]) or (moves[1] < 0 and abs(moves[1]) > limits[1]):
+                    return i
+
+            return len(s)
+        # RDLU
+        limits = [n - 1 - startPos[1], n - 1 - startPos[0], startPos[1], startPos[0]]
+        res = list()
+        for i in range(len(s)):
+            res.append(get_num_instruction(s[i:]))
+        return res
 
 
 def test_execute_instructions():
