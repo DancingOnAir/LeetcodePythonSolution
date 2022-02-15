cbbb8f7d12f4a9f01ef2341f18be746fc41b1bc3
DancingOnAir <taotong1984@hotmail.com>
2022-02-15 15:36:03 +0800

    feat(String): init q2147 & accepted math python solution for q2147

--- String/2147_number_of_ways_to_divide_a_long_corridor.py
+++ String/2147_number_of_ways_to_divide_a_long_corridor.py
@@ -1,10 +1,24 @@
 class Solution:
     def numberOfWays(self, corridor: str) -> int:
-        pass
+        m = list()
+        for i, ch in enumerate(corridor):
+            if ch == 'S':
+                m.append(i)
+
+        n = len(m)
+        if n == 0 or (n & 1):
+            return 0
+
+        res = 1
+        for i in range(1, n - 2, 2):
+            res *= (m[i + 1] - m[i])
+
+        return res % (10 ** 9 + 7)
 
 
 def test_number_of_ways():
     solution = Solution()
+    assert solution.numberOfWays('SSPSSPSSSPPSPSPPS') == 8, 'wrong reuslt'
     assert solution.numberOfWays('SSPPSPS') == 3, 'wrong reuslt'
     assert solution.numberOfWays('PPSPSP') == 1, 'wrong reuslt'
     assert solution.numberOfWays('S') == 0, 'wrong reuslt'
