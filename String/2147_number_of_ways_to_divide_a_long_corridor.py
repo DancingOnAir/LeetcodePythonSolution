6c77f544e389ff4b9ddf561b094361ed23c526a4
DancingOnAir <taotong1984@hotmail.com>
2022-02-15 17:55:11 +0800

    feat(Strign): accepted dp python solution for q2147

--- String/2147_number_of_ways_to_divide_a_long_corridor.py
+++ String/2147_number_of_ways_to_divide_a_long_corridor.py
@@ -1,9 +1,17 @@
 class Solution:
+    # dp
     def numberOfWays(self, corridor: str) -> int:
-        m = list()
-        for i, ch in enumerate(corridor):
+        zero_seat_num, one_seat_num, two_seat_num = 1, 0, 0
+        for ch in corridor:
             if ch == 'S':
-                m.append(i)
+                zero_seat_num, one_seat_num, two_seat_num = 0, zero_seat_num + two_seat_num, one_seat_num
+            else:
+                zero_seat_num, one_seat_num, two_seat_num = zero_seat_num + two_seat_num, one_seat_num, two_seat_num
+        return two_seat_num % (10 ** 9 + 7)
+
+    # match combination
+    def numberOfWays1(self, corridor: str) -> int:
+        m = list(i for i, ch in enumerate(corridor) if ch == 'S')
 
         n = len(m)
         if n == 0 or (n & 1):
