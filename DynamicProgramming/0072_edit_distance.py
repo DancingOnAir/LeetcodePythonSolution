715c6d6504def40049d21fcd6635230bff87cf31
DancingOnAir <taotong1984@hotmail.com>
2022-02-17 11:06:45 +0800

    feat(String) improved dp bottom-up python solution for q0072

--- DynamicProgramming/0072_edit_distance.py
+++ DynamicProgramming/0072_edit_distance.py
@@ -1,4 +1,5 @@
 class Solution:
+    # bottom-up
     def minDistance(self, word1: str, word2: str) -> int:
         l1, l2 = len(word1), len(word2)
         if not l1:
@@ -8,14 +9,13 @@ class Solution:
             return l1
 
         dp = [[0] * (l2+1) for _ in range(l1+1)]
-        for i in range(1, l1+1):
-            dp[i][0] = i
-        for j in range(1, l2+1):
-            dp[0][j] = j
-
-        for i in range(1, l1+1):
-            for j in range(1, l2+1):
-                if word1[i - 1] == word2[j - 1]:
+        for i in range(l1+1):
+            for j in range(l2+1):
+                if i == 0:
+                    dp[i][j] = j
+                elif j == 0:
+                    dp[i][j] = i
+                elif word1[i - 1] == word2[j - 1]:
                     dp[i][j] = dp[i - 1][j - 1]
                 else:
                     dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
