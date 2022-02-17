91b4b92b329b4d9d5bd9189f7f47e67675134d12
DancingOnAir <taotong1984@hotmail.com>
2022-02-17 11:21:31 +0800

    feat(String) accepted dp bottom-up space optimized python solution for q0072

--- DynamicProgramming/0072_edit_distance.py
+++ DynamicProgramming/0072_edit_distance.py
@@ -1,6 +1,28 @@
+from functools import lru_cache
+
+
 class Solution:
-    # top-down
+    # bottom-up, space optimized
     def minDistance(self, word1: str, word2: str) -> int:
+        l1, l2 = len(word1), len(word2)
+        cur, pre = [0] * (l2+1), [0] * (l2+1)
+
+        for i in range(l1 + 1):
+            for j in range(l2 + 1):
+                if i == 0:
+                    cur[j] = j
+                elif j == 0:
+                    cur[j] = i
+                elif word1[i - 1] == word2[j - 1]:
+                    cur[j] = pre[j - 1]
+                else:
+                    cur[j] = min(pre[j - 1], pre[j], cur[j - 1]) + 1
+            cur, pre = pre, cur
+        return pre[l2]
+
+    # top-down
+    def minDistance2(self, word1: str, word2: str) -> int:
+        @lru_cache(None)
         def dp(i, j):
             if i == 0:
                 return j
@@ -13,7 +35,7 @@ class Solution:
         return dp(len(word1), len(word2))
 
     # bottom-up
-    def minDistance(self, word1: str, word2: str) -> int:
+    def minDistance1(self, word1: str, word2: str) -> int:
         l1, l2 = len(word1), len(word2)
         if not l1:
             return l2
