26a0f3aa9af31e7d8415188e32507418aad1e8c9
DancingOnAir <taotong1984@hotmail.com>
2022-02-20 00:26:59 +0800

    feat(String): accepted dp python solution q2167

--- String/2167_minimum_time_to_remove_all_cars_containing_illegal_goods.py
+++ String/2167_minimum_time_to_remove_all_cars_containing_illegal_goods.py
@@ -1,6 +1,18 @@
 class Solution:
+    # |..left..|..middle..|..right..|
+    # For left and right we paid just their lengths.
+    # For middle we pay twice number of ones se have inside,
+    # so we have: len(left) + 2* count(middle, 1) + len(right)
+    # = len(left) + len(middle) + len(right) + 2*count(middle, 1) - len(middle)
+    # = n + count(middle, 1) - count(middle, 0).
     def minimumTime(self, s: str) -> int:
-        pass
+        def min_sum(nums):
+            dp = dp_min = float('inf')
+            for num in nums:
+                dp = min(num, dp + num)
+                dp_min = min(dp, dp_min)
+            return min(0, dp_min)
+        return len(s) + min_sum([1 if x == '1' else -1 for x in s])
 
 
 def test_minimum_time():
