bdc1c5689b40b90d53590f14a0c4b8703980f57a
DancingOnAir <taotong1984@hotmail.com>
2022-02-21 17:20:55 +0800

    feat(String): accepted top-down dp python solution for q1048

--- String/1048_longest_string_chain.py
+++ String/1048_longest_string_chain.py
@@ -1,8 +1,20 @@
 from typing import List
+from functools import lru_cache
 
 
 class Solution:
+    # top-down dp
     def longestStrChain(self, words: List[str]) -> int:
+        @lru_cache(None)
+        def dp(w):
+            if w not in set_words:
+                return 0
+            return max(dp(w[:i] + w[i+1:]) for i in range(len(w))) + 1
+        set_words = set(words)
+        return max(dp(w) for w in words)
+
+    # bottom-up dp
+    def longestStrChain1(self, words: List[str]) -> int:
         dp = dict()
         for w in sorted(words, key=lambda x: len(x)):
             dp[w] = max(dp.get(w[: i] + w[i+1:], 0) + 1 for i in range(len(w)))
