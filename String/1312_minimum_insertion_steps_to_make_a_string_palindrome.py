aa719982a826725ae984620a1e01442bf408e022
DancingOnAir <taotong1984@hotmail.com>
2022-02-23 16:29:29 +0800

    feat(String): accepted lcs dp python solution for q1312

--- String/1312_minimum_insertion_steps_to_make_a_string_palindrome.py
+++ String/1312_minimum_insertion_steps_to_make_a_string_palindrome.py
@@ -1,6 +1,29 @@
 class Solution:
-    # equivalent to get max length of the palindrome subsequence
+    # Step1.
+    # Initialize dp[n+1][n+1],
+    # where dp[i][j] means the length of the longest common sequence between
+    # i first letters in s1 and j first letters in s2.
+    #
+    # Step2.
+    # Find the longest common sequence between s1 and s2,
+    # where s1 = s and s2 = reversed(s)
+    #
+    # Step3.
+    # return n - dp[n][n]
     def minInsertions(self, s: str) -> int:
+        n = len(s)
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+        for i in range(n):
+            for j in range(n):
+                # s[n - j - 1] = s[-j - 1] = s[~j]
+                dp[i + 1][j + 1] = dp[i][j] + 1 if s[i] == s[~j] else max(dp[i][j + 1], dp[i + 1][j])
+        return n - dp[n][n]
+
+
+    # initialize dp[n+1][n+1],
+    # where dp[i][j] means the length of the longest palindrome sequence between
+    # i start letter in s and j length of substring.
+    def minInsertions1(self, s: str) -> int:
         n = len(s)
         dp = [[0] * (n + 1) for _ in range(n)]
 
