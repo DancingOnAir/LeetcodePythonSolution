1eed3746f781333cfc3c6a1c30e4a75744f7ccb4
DancingOnAir <taotong1984@hotmail.com>
2022-02-28 12:16:16 +0800

    feat(String): accepted pythonic python solution for q0984

--- String/0984_string_without_AAA_or_BBB.py
+++ String/0984_string_without_AAA_or_BBB.py
@@ -1,5 +1,22 @@
 class Solution:
     def strWithout3a3b(self, a: int, b: int) -> str:
+        res = list()
+        while a or b:
+            if len(res) >= 2 and res[-1] == res[-2]:
+                write_a = (res[-1] == 'b')
+            else:
+                write_a = (a >= b)
+
+            if write_a:
+                res.append('a')
+                a -= 1
+            else:
+                res.append('b')
+                b -= 1
+
+        return ''.join(res)
+
+    def strWithout3a3b1(self, a: int, b: int) -> str:
         if not a:
             return 'b' * b
         if not b:
