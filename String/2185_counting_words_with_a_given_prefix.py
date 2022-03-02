a84c7aa40b9dd182b63c7627bff5f40a32ac73c1
DancingOnAir <taotong1984@hotmail.com>
2022-03-03 01:37:36 +0800

    feat(String): init q2185 & accepted python solution for q2185

---
+++ String/2185_counting_words_with_a_given_prefix.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        return sum(w.startswith(pref) for w in words)
+
+
+def test_prefix_count():
+    solution = Solution()
+    assert solution.prefixCount(["pay", "attention", "practice", "attend"], 'at') == 2, 'wrong result'
+    assert solution.prefixCount(["leetcode", "win", "loops", "success"], 'code') == 0, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_prefix_count()
