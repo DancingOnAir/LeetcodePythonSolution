b84c924b6a43e434313023975fcf804e15e563cc
DancingOnAir <taotong1984@hotmail.com>
2022-03-04 03:00:38 +0800

    feat(String): init q2186 & accpeted counter python solution for q2186

---
+++ String/2186_minimum_number_of_steps_to_make_two_strings_anagram_II.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        return sum((Counter(s) - Counter(t)).values()) + sum((Counter(t) - Counter(s)).values())
+
+
+def test_min_steps():
+    solution = Solution()
+    assert solution.minSteps('leetcode', 'coats') == 7, 'wrong result'
+    assert solution.minSteps('night', 'thing') == 0, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_min_steps()
