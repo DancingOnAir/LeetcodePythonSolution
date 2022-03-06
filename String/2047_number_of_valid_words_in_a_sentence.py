fb1d11adb77222f7667cd001561dc84546caee27
DancingOnAir <taotong1984@hotmail.com>
2022-03-06 21:49:20 +0800

    feat(String): accepted pythonic solution for q2047

--- String/2047_number_of_valid_words_in_a_sentence.py
+++ String/2047_number_of_valid_words_in_a_sentence.py
@@ -1,5 +1,25 @@
 class Solution:
     def countValidWords(self, sentence: str) -> int:
+        def valid(word):
+            num_hyphen = 0
+
+            for i, ch in enumerate(word):
+                if ch.isdigit():
+                    return False
+                if ch.isalpha():
+                    continue
+                if ch == '-':
+                    num_hyphen += 1
+                    if num_hyphen > 1:
+                        return False
+                    if i == 0 or i == len(word) - 1 or not word[i - 1].isalpha() or not word[i + 1].isalpha():
+                        return False
+                elif i < len(word) - 1:
+                    return False
+            return True
+        return sum(valid(w) for w in sentence.split())
+
+    def countValidWords1(self, sentence: str) -> int:
         res = 0
         for w in sentence.split():
             flag = 1
