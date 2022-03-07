3561ef4123c6da858d7d6547d70d81879dd21b48
DancingOnAir <taotong1984@hotmail.com>
2022-03-07 11:08:53 +0800

    feat(String): accepted binary search python solution ofr q2055

--- String/2055_plates_between_candles.py
+++ String/2055_plates_between_candles.py
@@ -1,9 +1,27 @@
 from typing import List
-from bisect import bisect_left
+from bisect import bisect_left, bisect_right
 
 
 class Solution:
+    # Find out indices of candies and add them to a list A
+    # For each query [a,b],
+    # find out the candies after a and the candies before b.
+    #
+    # A[i] - A[j] + 1 is the length between two candies,
+    # i - j + 1is the number of candies.
+    # (A[j] - A[i]) - (j - i) is the number of plates between two candies.
     def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
+        A = [i for i, ch in enumerate(s) if ch == '|']
+
+        res = list()
+        for a, b in queries:
+            i = bisect_left(A, a)
+            j = bisect_right(A, b) - 1
+            res.append(A[j] - A[i] - (j - i) if j > i else 0)
+        return res
+
+    # find the nearest candle index on the left and on the right
+    def platesBetweenCandles1(self, s: str, queries: List[List[int]]) -> List[int]:
         num_plate = 0
         memo = dict()
         for i, ch in enumerate(s):
@@ -16,7 +34,7 @@ class Solution:
         res = list()
         for q in queries:
             p1 = bisect_left(candle, q[0])
-            p2 = bisect_left(candle, q[1])
+            p2 = bisect(candle, q[1])
             if p1 == p2:
                 res.append(0)
                 continue
