9cba973130e45603615feda9df473b9554af7abc
DancingOnAir <taotong1984@hotmail.com>
2022-03-07 10:45:17 +0800

    feat(String): accepted bisect_left python solution ofr q2055

--- String/2055_plates_between_candles.py
+++ String/2055_plates_between_candles.py
@@ -1,17 +1,30 @@
 from typing import List
+from bisect import bisect_left
 
 
 class Solution:
     def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
-        num_asterisk = 0
+        num_plate = 0
         memo = dict()
         for i, ch in enumerate(s):
             if ch == '*':
-                num_asterisk += 1
+                num_plate += 1
             else:
-                memo[i] = num_asterisk
+                memo[i] = num_plate
 
-        pass
+        candle = list(memo.keys())
+        res = list()
+        for q in queries:
+            p1 = bisect_left(candle, q[0])
+            p2 = bisect_left(candle, q[1])
+            if p1 == p2:
+                res.append(0)
+                continue
+
+            if p2 == len(candle) or (candle[p2] > q[1] and p2 > p1):
+                p2 -= 1
+            res.append(memo[candle[p2]] - memo[candle[p1]])
+        return res
 
 
 def test_plates_between_candles():
