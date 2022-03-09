9fa9dca6ad52b0b4d07f8d790cee8af1e40dceb0
DancingOnAir <taotong1984@hotmail.com>
2022-03-09 22:17:44 +0800

    feat(String): accepted straight forward python solution for q2056

--- String/2056_number_of_valid_move_combinations_on_chessboard.py
+++ String/2056_number_of_valid_move_combinations_on_chessboard.py
@@ -1,17 +1,31 @@
 from typing import List
-from itertools import product
+from itertools import product, chain
+from collections import Counter
 
 
 class Solution:
     def countCombinations(self, pieces: List[str], positions: List[List[int]]) -> int:
         def dfs(pos, dirs, stopped_mask):
-            print(dirs)
             if stopped_mask == 0:
                 return
             self.res.add(tuple(pos))
             for active in range(1 << len(dirs)):
+                if stopped_mask & active != active:
+                    continue
+                new_pos = list(pos)
+                new_mask = stopped_mask ^ active
 
-            pass
+                for i in range(len(new_pos)):
+                    new_pos[i] = (new_pos[i][0] + dirs[i][0] * ((new_mask >> i) & 1), new_pos[i][1] + dirs[i][1] * ((new_mask >> i) & 1))
+
+                if len(Counter(new_pos)) < len(dirs):
+                    continue
+
+                all_c = list(chain(*new_pos))
+                # check whether new pos is out of board
+                if min(all_c) <= 0 or max(all_c) > 8:
+                    continue
+                dfs(new_pos, dirs, new_mask)
 
         moves = dict()
         moves['rook'] = ((1, 0), (-1, 0), (0, 1), (0, -1))
