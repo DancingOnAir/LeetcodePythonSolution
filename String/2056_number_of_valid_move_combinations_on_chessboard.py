5d8e7a9ade1120ecd25cd9ccdeceb470b88c614d
DancingOnAir <taotong1984@hotmail.com>
2022-03-09 17:35:19 +0800

    feat(String): implmented python solution for q2056

--- String/2056_number_of_valid_move_combinations_on_chessboard.py
+++ String/2056_number_of_valid_move_combinations_on_chessboard.py
@@ -1,9 +1,28 @@
 from typing import List
+from itertools import product
 
 
 class Solution:
     def countCombinations(self, pieces: List[str], positions: List[List[int]]) -> int:
-        pass
+        def dfs(pos, dirs, stopped_mask):
+            print(dirs)
+            if stopped_mask == 0:
+                return
+            self.res.add(tuple(pos))
+            for active in range(1 << len(dirs)):
+
+            pass
+
+        moves = dict()
+        moves['rook'] = ((1, 0), (-1, 0), (0, 1), (0, -1))
+        moves['queen'] = ((1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1))
+        moves['bishop'] = ((1, 1), (1, -1), (-1, 1), (-1, -1))
+
+        positions = [tuple(p) for p in positions]
+        self.res = set()
+        for dirs in product(*(moves[p] for p in pieces)):
+            dfs(positions, dirs, (1 << len(pieces)) - 1)
+        return len(self.res)
 
 
 def test_count_combinations():
