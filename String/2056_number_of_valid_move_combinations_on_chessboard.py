879d03bd64c696873568c797502882196d6a39d3
DancingOnAir <taotong1984@hotmail.com>
2022-03-11 17:39:57 +0800

    feat(String): implemented dfs python solution for q2056

--- String/2056_number_of_valid_move_combinations_on_chessboard.py
+++ String/2056_number_of_valid_move_combinations_on_chessboard.py
@@ -5,6 +5,83 @@ from collections import Counter
 
 class Solution:
     def countCombinations(self, pieces: List[str], positions: List[List[int]]) -> int:
+        def valid(x, y):
+            return 0 <= x < 8 and 0 <= y < 8
+
+        def check(pos1, new_pos1, pos2, new_pos2):
+            t1 = new_pos1[-1]
+            t2 = new_pos2[-1]
+            for i in range(1, max(t1, t2) + 1):
+                if i <= t1:
+                    pos1[0] += new_pos1[0]
+                    pos1[1] += new_pos1[1]
+
+                if i <= t2:
+                    pos2[0] += new_pos2[0]
+                    pos2[1] += new_pos2[1]
+
+                if pos1 == pos2:
+                    return False
+            return True
+
+        def fill(i, x, y, dirs):
+            for k in range(4):
+                dir = dirs[k]
+                nx, ny = x + dir[0], y + dir[1]
+                t = 1
+
+                while valid(nx, ny):
+                    self.moves[i].append([dir[0], dir[1], 1])
+                    nx += dir[0]
+                    ny += dir[1]
+                    t += 1
+
+        def dfs(cur, new_pos, pos):
+            if cur >= len(self.moves):
+                self.res += 1
+                return
+
+            for m in self.moves:
+                ok = True
+                for i in range(len(new_pos)):
+                    if not check(pos[i], new_pos[i], pos[cur], m):
+                        ok = False
+                        break
+
+                if not ok:
+                    continue
+
+                new_pos.append(m)
+                dfs(cur + 1, new_pos, pos)
+                new_pos.pop()
+
+        direction_line = ((0, 1), (1, 0), (0, -1), (-1, 0))
+        direction_diagonal = ((1, 1), (-1, 1), (-1, -1), (-1, 1))
+        n = len(pieces)
+        self.moves = [[] for _ in range(n)]
+
+        for p in positions:
+            p[0] -= 1
+            p[1] -= 1
+
+        for i in range(n):
+            x, y = positions[i][0], positions[i][1]
+            self.moves[i].append([0, 0, 0])
+
+            if pieces[i] == 'rook':
+                fill(i, x, y, direction_line)
+            elif pieces[i] == 'bishop':
+                fill(i, x, y, direction_diagonal)
+            else:
+                fill(i, x, y, direction_line)
+                fill(i, x, y, direction_diagonal)
+
+        new_pos = list(list())
+        self.res = 0
+        dfs(0, new_pos, positions)
+        return self.res
+
+    def countCombinations1(self, pieces: List[str], positions: List[List[int]]) -> int:
         def dfs(pos, dirs, stopped_mask):
             if stopped_mask == 0:
                 return
