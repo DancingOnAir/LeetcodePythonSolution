6bfdebc56e65bdaa90a299ec8207340942d85a4b
DancingOnAir <taotong1984@hotmail.com>
2022-03-13 11:12:38 +0800

    feat(String): init q0943

---
+++ String/0943_find_the_shortest_superstring.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def shortestSuperstring(self, words: List[str]) -> str:
+        pass
+
+
+def test_shortest_superstring():
+    solution = Solution()
+
+    assert solution.shortestSuperstring(["alex", "loves", "leetcode"]) == 'alexlovesleetcode'
+    assert solution.shortestSuperstring(["catg", "ctaagt", "gcta", "ttca", "atgcatc"]) == 'gctaagttcatgcatc'
+
+
+if __name__ == '__main__':
+    test_shortest_superstring()
