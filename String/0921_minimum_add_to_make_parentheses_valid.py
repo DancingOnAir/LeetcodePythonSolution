d00dcc13cd797e917cfc6335428eebaefe37e2e1
DancingOnAir <taotong1984@hotmail.com>
2022-03-14 16:02:15 +0800

    feat(String): accepted time O(n), space O(1) python solution for q0921

--- String/0921_minimum_add_to_make_parentheses_valid.py
+++ String/0921_minimum_add_to_make_parentheses_valid.py
@@ -1,5 +1,16 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
+        left = right = 0
+        for c in s:
+            if c == '(':
+                right += 1
+            elif right > 0:
+                right -= 1
+            else:
+                left += 1
+        return left + right
+
+    def minAddToMakeValid1(self, s: str) -> int:
         stk = list()
         closing_bracket = 0
         for c in s:
