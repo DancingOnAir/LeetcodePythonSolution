8a7d5125bf5e5e078a61d79135a7c3ebff7dab95
DancingOnAir <taotong1984@hotmail.com>
2022-03-18 11:41:36 +0800

    feat(String): accepted trie python solution for q0677

--- String/0677_map_sum_pairs.py
+++ String/0677_map_sum_pairs.py
@@ -1,7 +1,37 @@
 from collections import defaultdict
 
 
+class TrieNode:
+    def __init__(self):
+        self.children = dict()
+        self.freq = 0
+
+
 class MapSum:
+    def __init__(self):
+        self.root = TrieNode()
+        self.m = dict()
+
+    def insert(self, key: str, val: int) -> None:
+        delta = val - self.m.get(key, 0)
+        self.m[key] = val
+
+        node = self.root
+        node.freq += delta
+        for ch in key:
+            node = node.children.setdefault(ch, TrieNode())
+            node.freq += delta
+
+    def sum(self, prefix: str) -> int:
+        node = self.root
+        for ch in prefix:
+            if ch not in node.children:
+                return 0
+            node = node.children[ch]
+        return node.freq
+
+
+class MapSum1:
     def __init__(self):
         self.m = defaultdict(int)
 
