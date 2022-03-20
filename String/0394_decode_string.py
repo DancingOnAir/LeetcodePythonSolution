0e8bd70f658458b0b621f25391560aaa9b8a3cc1
DancingOnAir <taotong1984@hotmail.com>
2022-03-20 12:39:50 +0800

    feat(String): accepted python solution for q0394

--- String/0394_decode_string.py
+++ String/0394_decode_string.py
@@ -1,11 +1,30 @@
 class Solution:
     def decodeString(self, s: str) -> str:
+        stk = list()
 
-        pass
+        for ch in s:
+            if ch == ']':
+                cur_str = list()
+                while stk[-1] != '[':
+                    cur_str.append(stk[-1])
+                    stk.pop()
+                # remove '['
+                stk.pop()
+                cur_num = list()
+                while stk and stk[-1].isdigit():
+                    cur_num.append(stk[-1])
+                    stk.pop()
+
+                stk.append(''.join(reversed(cur_str)) * int(''.join(reversed(cur_num))))
+            else:
+                stk.append(ch)
+
+        return ''.join(stk)
 
 
 def test_decode_string():
     solution = Solution()
+    assert solution.decodeString("10[a]") == "a" * 10, 'wrong result'
     assert solution.decodeString("3[a]2[bc]") == "aaabcbc", 'wrong result'
     assert solution.decodeString("3[a2[c]]") == "accaccacc", 'wrong result'
     assert solution.decodeString("2[abc]3[cd]ef") == "abcabccdcdcdef", 'wrong result'
