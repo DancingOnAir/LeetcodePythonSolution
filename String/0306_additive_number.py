02e80e10e87feb4cf673dc2a68dddd3094e83c82
DancingOnAir <taotong1984@hotmail.com>
2022-03-21 16:36:36 +0800

    feat(String): accepted straight forward python solution for q0306

--- String/0306_additive_number.py
+++ String/0306_additive_number.py
@@ -1,10 +1,36 @@
 class Solution:
     def isAdditiveNumber(self, num: str) -> bool:
-        pass
+        def get_next_pos(start, first_len, second_len):
+            if start + first_len + second_len == len(num):
+                return True
+
+            first = int(num[start: start + first_len])
+            second = int(num[start + first_len: start + first_len + second_len])
+            for l3 in range(max(first_len, second_len), len(num) + 1 - first_len - second_len):
+                if l3 > 1 and num[start + first_len + second_len] == '0':
+                    break
+
+                third = int(num[start + first_len + second_len: start + first_len + second_len + l3])
+                if first + second < third:
+                    break
+                elif first + second == third:
+                    start += first_len
+                    return get_next_pos(start, second_len, l3)
+            return False
+
+        for l1 in range(1, len(num) - 1):
+            for l2 in range(1, len(num) - l1):
+                if l2 > 1 and num[l1] == '0':
+                    break
+
+                if get_next_pos(0, l1, l2):
+                    return True
+        return False
 
 
 def test_is_additive_number():
     solution = Solution()
+    assert solution.isAdditiveNumber('101'), 'wrong result'
     assert solution.isAdditiveNumber('112358'), 'wrong result'
     assert solution.isAdditiveNumber('199100199'), 'wrong result'
 
