7d6324e16808933ee739f9cee765cd0cc6049eda
DancingOnAir <taotong1984@hotmail.com>
2022-03-23 23:00:57 +0800

    feat(String): init q0301 & accepted python solution for q0301

--- String/0301_remove_invalid_parentheses.py
+++ String/0301_remove_invalid_parentheses.py
@@ -3,23 +3,25 @@ from typing import List
 
 class Solution:
     def removeInvalidParentheses(self, s: str) -> List[str]:
-        def count_invalid_closing_bracket(ss):
-
-        stk = list()
-        closing_bracket = 0
-        for ch in s:
-            if ch == ')':
-                if stk:
-                   stk.pop()
-                else:
-                    closing_bracket += 1
-            elif ch == '(':
-                stk.append(ch)
-        bracket = len(stk)
+        def is_valid(s: str):
+            count = 0
+            for c in s:
+                count += (c == '(') - (c == ')')
+                if count < 0: return False
+            return not count
+        level = {s}
+        while True:
+            valid = set(filter(is_valid, level))
+            if valid:
+                return valid
+            level = {sub[:i] + sub[i+1:]
+                     for sub in level
+                     for i in range(len(sub)) if sub[i] in '()'}
 
 
 def test_remove_invalid_parentheses():
     solution = Solution()
+
     assert solution.removeInvalidParentheses("()())()") == ["(())()", "()()()"], 'wrong result'
     assert solution.removeInvalidParentheses("(a)())()") == ["(a())()", "(a)()()"], 'wrong result'
     assert solution.removeInvalidParentheses(")(") == [""], 'wrong result'
