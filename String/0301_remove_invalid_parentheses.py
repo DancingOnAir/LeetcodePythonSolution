5cc1c5bb08ee4391c047bbc9e42468c0c970825c
DancingOnAir <taotong1984@hotmail.com>
2022-03-24 13:58:15 +0800

    feat(String): updated the breadth search python solution easy understanding for q0301

--- String/0301_remove_invalid_parentheses.py
+++ String/0301_remove_invalid_parentheses.py
@@ -3,20 +3,33 @@ from typing import List
 
 class Solution:
     def removeInvalidParentheses(self, s: str) -> List[str]:
-        def is_valid(s: str):
-            count = 0
+        def isValid(s: str):
+            cnt = 0
             for c in s:
-                count += (c == '(') - (c == ')')
-                if count < 0: return False
-            return not count
-        level = {s}
+                if c == '(':
+                    cnt += 1
+                elif c == ')':
+                    cnt -= 1
+                    if cnt < 0:
+                        return False
+            return not cnt
+
+        currentLevel = {s}
         while True:
-            valid = set(filter(is_valid, level))
-            if valid:
-                return valid
-            level = {sub[:i] + sub[i+1:]
-                     for sub in level
-                     for i in range(len(sub)) if sub[i] in '()'}
+            res = list(filter(isValid, currentLevel))
+            if res:
+                return res
+
+            nextLevel = set()
+            for ss in currentLevel:
+                for i, ch in enumerate(ss):
+                    if i > 0 and ch == ss[i - 1]:
+                        continue
+                    if ch in '()':
+                        # if the substring is valid parentheses that means it remove the minimum number of invalid
+                        # parentheses, so do not need to check next level substrings.
+                        nextLevel.add(ss[:i] + ss[i+1:])
+            currentLevel = nextLevel
 
 
 def test_remove_invalid_parentheses():
