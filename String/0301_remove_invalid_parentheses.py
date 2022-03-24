715cee42e3a936b8abe9a0c1e6be079160053210
DancingOnAir <taotong1984@hotmail.com>
2022-03-24 16:08:01 +0800

    feat(String): implemented the backtracking & pruning python solution for q0301

--- String/0301_remove_invalid_parentheses.py
+++ String/0301_remove_invalid_parentheses.py
@@ -2,7 +2,56 @@ from typing import List
 
 
 class Solution:
+    # backtracking + pruning
+    # time complex O(n x 2^n)
     def removeInvalidParentheses(self, s: str) -> List[str]:
+        def isValid(s):
+            cnt = 0
+            for c in s:
+                if c == '(':
+                    cnt += 1
+                elif c == ')':
+                    cnt -= 1
+                    if cnt < 0:
+                        return False
+            return not cnt
+
+        res = list()
+        def helper(s, start, left, right):
+            if left == 0 and right == 0:
+                if isValid(s):
+                    res.append(s)
+                return
+
+            for i in range(start, len(s)):
+                if i > start and s[i] == s[i - 1]:
+                    continue
+                # 如果剩余的字符无法满足去掉的数量要求
+                if left + right > len(s) - i:
+                    break
+                # 尝试去掉一个左括号
+                if left > 0 and s[i] == '(':
+                    helper(s[:i] + s[i+1:], i, left - 1, right)
+                # 尝试去掉一个右括号
+                if right > 0 and s[i] == ')':
+                    helper(s[:i] + s[i+1:], i, left, right - 1)
+
+        left = right = 0
+        for c in s:
+            if c == '(':
+                left += 1
+            elif c == ')':
+                if left == 0:
+                    right += 1
+                else:
+                    left -= 1
+
+        helper(s, 0, left, right)
+        return res
+
+    # breadth first search
+    # time complex O(n x 2^n)
+    def removeInvalidParentheses1(self, s: str) -> List[str]:
         def isValid(s: str):
             cnt = 0
             for c in s:
