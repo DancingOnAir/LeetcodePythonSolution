8998bb4eb10a8f60b20f33157ad401bfa15b366c
DancingOnAir <taotong1984@hotmail.com>
2022-03-25 23:31:46 +0800

    feat(String): accepted pythonic solution for q2207

--- String/2207_maximize_number_of_subsequences_in_a_string.py
+++ String/2207_maximize_number_of_subsequences_in_a_string.py
@@ -1,5 +1,19 @@
 class Solution:
+    # Prefix Sum and Suffix Sum
+    # If we add p[0], the best option is to prepend it at the beginning.
+    # If we add p[1], the best option is to append it at the end.
     def maximumSubsequenceCount(self, text: str, pattern: str) -> int:
+        a = b = res = 0
+        for c in text:
+            if c == pattern[1]:
+                res += a
+            a += c == pattern[0]
+            b += c == pattern[1]
+        return res + max(a, b)
+
+    # Use two if in solution,
+    # to handle the corner case where pattern[0] == pattern[1].
+    def maximumSubsequenceCount1(self, text: str, pattern: str) -> int:
         first, second = pattern
         first_num = second_num = 0
         res = 0
@@ -7,8 +21,7 @@ class Solution:
             if text[i] == first:
                 res += second_num
                 first_num += 1
-            elif text[i] == second:
-                res += first_num
+            if text[i] == second:
                 second_num += 1
 
         return res + max(first_num, second_num)
