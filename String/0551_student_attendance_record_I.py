53d475ad8f461cdfba74f0529443fb04a0c415d4
DancingOnAir <taotong1984@hotmail.com>
2022-04-02 22:45:45 +0800

    feat(String): accepted straight forward python solution for q0551

--- String/0551_student_attendance_record_I.py
+++ String/0551_student_attendance_record_I.py
@@ -1,5 +1,21 @@
 class Solution:
     def checkRecord(self, s: str) -> bool:
+        cnt_A = cnt_L = 0
+        for c in s:
+            if c == 'A':
+                cnt_A += 1
+                if cnt_A > 1:
+                    return False
+                cnt_L = 0
+            elif c == 'L':
+                cnt_L += 1
+                if cnt_L > 2:
+                    return False
+            else:
+                cnt_L = 0
+        return True
+
+    def checkRecord1(self, s: str) -> bool:
         return s.count('A') < 2 and 'LLL' not in s
 
 
