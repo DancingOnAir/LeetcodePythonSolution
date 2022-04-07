1ff70b80bd078328eeddd136fd4036fa576c7769
DancingOnAir <taotong1984@hotmail.com>
2022-04-07 22:48:29 +0800

    feat(SegmentTree): accepted segment tree python solution for q0729

--- SegmentTree/0729_my_calendar_i.py
+++ SegmentTree/0729_my_calendar_i.py
@@ -1,8 +1,41 @@
 from collections import defaultdict
 
 
-# brute force
+# segment tree
+# build a segment tree, if new event range can put into the same side of the tree node, it's true, otherwise, false.
+class Node:
+    def __init__(self, l, r):
+        self.left, self.right = None, None
+        self.l = l
+        self.r = r
+
+
 class MyCalendar:
+    def __init__(self):
+        self.root = Node(0, 0)
+
+    def book(self, start: int, end: int) -> bool:
+        return self.update(start, end)
+
+    def update(self, start, end):
+        node = self.root
+        while True:
+            if end <= node.l:
+                if node.left is None:
+                    node.left = Node(start, end)
+                    return True
+                node = node.left
+            elif start >= node.r:
+                if node.right is None:
+                    node.right = Node(start, end)
+                    return True
+                node = node.right
+            else:
+                return False
+
+
+# brute force
+class MyCalendar1:
     def __init__(self):
         self.calendar = list()
 
