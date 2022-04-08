46a3938d7bff78c4201ed8d99dc38a32e3bc8b67
DancingOnAir <taotong1984@hotmail.com>
2022-04-08 16:27:30 +0800

    feat(SegmentTree): accepted default dict python solution for q0731

--- SegmentTree/0731_my_calendar_ii.py
+++ SegmentTree/0731_my_calendar_ii.py
@@ -1,10 +1,55 @@
-class MyCalendarTwo:
+from collections import defaultdict
 
+
+# class Node:
+#     def __init__(self, start, end):
+#         self.left, self.right = None, None
+#         self.start = start
+#         self.end = end
+#         self.overlap = 0
+#
+#
+# class MyCalendarTwo:
+#     def __init__(self):
+#         self.root = Node(0, 0)
+#
+#     def book(self, start: int, end: int) -> bool:
+#         return self.update(start, end)
+#
+#     def update(self, start, end):
+#         node = self.root
+#         while True:
+#             if start >= node.end:
+#                 node = node.right
+#                 if node is None:
+#                     node.right = Node(start, end)
+#                     return True
+#             elif end <= node.start:
+#                 node = node.left
+#                 if node is None:
+#                     node.left = Node(start, end)
+#                     return True
+#             else:
+#                 if node.overlap == 1:
+#                     return False
+
+class MyCalendarTwo:
     def __init__(self):
-        pass
+        self.calendar = defaultdict(int)
 
     def book(self, start: int, end: int) -> bool:
-        pass
+        self.calendar[start] += 1
+        self.calendar[end] -= 1
+
+        res = 0
+        for k in sorted(self.calendar.keys()):
+            res += self.calendar[k]
+            if res == 3:
+                self.calendar[start] -= 1
+                self.calendar[end] += 1
+
+                return False
+        return True
 
 
 def test_my_calendar_two():
