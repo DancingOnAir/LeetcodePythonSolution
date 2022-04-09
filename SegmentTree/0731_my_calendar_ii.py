6567078306be3f95019376932db7a778ffb98574
DancingOnAir <taotong1984@hotmail.com>
2022-04-09 22:10:48 +0800

    feat(SegmentTree): accepted segment tree python solution for q0731

--- SegmentTree/0731_my_calendar_ii.py
+++ SegmentTree/0731_my_calendar_ii.py
@@ -1,39 +1,91 @@
 from collections import defaultdict
 
 
-# class Node:
-#     def __init__(self, start, end):
-#         self.left, self.right = None, None
-#         self.start = start
-#         self.end = end
-#         self.overlap = 0
-#
-#
-# class MyCalendarTwo:
-#     def __init__(self):
-#         self.root = Node(0, 0)
-#
-#     def book(self, start: int, end: int) -> bool:
-#         return self.update(start, end)
-#
-#     def update(self, start, end):
-#         node = self.root
-#         while True:
-#             if start >= node.end:
-#                 node = node.right
-#                 if node is None:
-#                     node.right = Node(start, end)
-#                     return True
-#             elif end <= node.start:
-#                 node = node.left
-#                 if node is None:
-#                     node.left = Node(start, end)
-#                     return True
-#             else:
-#                 if node.overlap == 1:
-#                     return False
+# https://leetcode-cn.com/problems/my-calendar-ii/solution/731-xian-duan-shu-by-sthemmm-4n43/
+class Node:
+    def __init__(self, l, r):
+        self.left_child, self.right_child = None, None
+        self.left_bound = l
+        self.right_bound = r
+        self.val = 0
+        self.lazy = 0
+
+
+class SegmentTree:
+    def __init__(self, l, r):
+        self.root = Node(l, r)
+
+    def update(self, l, r):
+        self._update(self.root, l, r)
+
+    def _update(self, node, l, r):
+        if node is None:
+            return
+
+        if l > node.right_bound or r < node.left_bound:
+            return
+        elif l <= node.left_bound and r >= node.right_bound:
+            node.val += 1
+            node.lazy += 1
+        else:
+            self.pushdown(node)
+            self._update(node.left_child, l, r)
+            self._update(node.right_child, l, r)
+            self.pushup(node)
+
+    def pushdown(self, node):
+        if node is None:
+            return
+        mid = (node.left_bound + node.right_bound) >> 1
+
+        if node.left_child is None:
+            node.left_child = Node(node.left_bound, mid)
+        if node.right_child is None:
+            node.right_child = Node(mid + 1, node.right_bound)
+
+        node.left_child.lazy += node.lazy
+        node.left_child.val += node.lazy
+
+        node.right_child.lazy += node.lazy
+        node.right_child.val += node.lazy
+
+        node.lazy = 0
+
+    def pushup(self, node):
+        node.val = max(node.left_child.val, node.right_child.val)
+
+    def query(self, l, r):
+        return self._query(self.root, l, r)
+
+    def _query(self, node, l, r):
+        if node is None:
+            return 0
+
+        if l > node.right_bound or r < node.left_bound:
+            return 0
+        elif l <= node.left_bound and r >= node.right_bound:
+            return node.val
+        else:
+            self.pushdown(node)
+            left_res = self._query(node.left_child, l, r)
+            right_res = self._query(node.right_child, l, r)
+            return max(left_res, right_res)
+
 
 class MyCalendarTwo:
+    def __init__(self):
+        self.st = SegmentTree(0, 10 ** 9 + 2)
+
+    def book(self, start: int, end: int) -> bool:
+        res = self.st.query(start, end - 1)
+        if res >= 2:
+            return False
+        else:
+            self.st.update(start, end - 1)
+        return True
+
+
+class MyCalendarTwo1:
     def __init__(self):
         self.calendar = defaultdict(int)
 
