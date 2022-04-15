02ce2c6da10f845649e583c9e8188def4a467b7f
DancingOnAir <taotong1984@hotmail.com>
2022-04-15 16:09:09 +0800

    feat(SegmentTree): init q0406 & tried a TLE python solution for q0406

---
+++ SegmentTree/0406_queue_reconstruction_by_height.py
@@ -0,0 +1,29 @@
+from typing import List
+from bisect import bisect_right
+
+
+class Solution:
+    # TLE
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        people = sorted(people, key=lambda x: (x[1], x[0]))
+
+        n = len(people)
+        res = list()
+        while len(res) < n:
+            cur = list()
+            for i, p in enumerate(people):
+                if p not in res and p[1] == bisect_right(res, p):
+                    cur.append(p)
+
+            res += list(sorted(cur))
+        return res
+
+
+def test_reconstruct_queue():
+    solution = Solution()
+    assert solution.reconstructQueue([[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]) == [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]], 'wrong result'
+    assert solution.reconstructQueue([[6,0],[5,0],[4,0],[3,2],[2,2],[1,4]]) == [[4,0],[5,0],[2,2],[3,2],[1,4],[6,0]], 'wrong result'
+
+
+if __name__ == '__main__':
+    test_reconstruct_queue()
