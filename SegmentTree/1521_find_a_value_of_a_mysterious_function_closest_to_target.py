e2704cb4c80adcac58815947c9c3682bf2568435
DancingOnAir <taotong1984@hotmail.com>
2022-04-18 22:20:16 +0800

    feat(SegmentTree): accepted set operation python solution for q1521

--- SegmentTree/1521_find_a_value_of_a_mysterious_function_closest_to_target.py
+++ SegmentTree/1521_find_a_value_of_a_mysterious_function_closest_to_target.py
@@ -72,6 +72,16 @@ class SegmentTree:
 
 class Solution:
     def closestToTarget(self, arr: List[int], target: int) -> int:
+        s, res = set(), float('inf')
+
+        for a in arr:
+            s = {a & b for b in s} | {a}
+            for c in s:
+                res = min(res, abs(c - target))
+        return res
+
+    # segment tree
+    def closestToTarget1(self, arr: List[int], target: int) -> int:
         n = len(arr)
         st = SegmentTree(1, n, target)
         for i, val in enumerate(arr):
