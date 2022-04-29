449ed7729f59a71b02b4d4ce4537ad26a6a44e10
DancingOnAir <taotong1984@hotmail.com>
2022-04-29 22:30:16 +0800

    feat(Tree): init q0703 & accepted min heap python soluiton for q0703

---
+++ Tree/0703_kth_largest_element_in_a_stream.py
@@ -0,0 +1,28 @@
+from typing import List
+from heapq import heapify, heappush, heappop
+
+
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.nums = nums
+        heapify(self.nums)
+
+    def add(self, val: int) -> int:
+        heappush(self.nums, val)
+        while len(self.nums) != self.k:
+            heappop(self.nums)
+        return self.nums[0]
+
+
+def test_kth_largest():
+    obj = KthLargest(3, [4, 5, 8, 2])
+    assert obj.add(3) == 4, 'wrong result'
+    assert obj.add(5) == 5, 'wrong result'
+    assert obj.add(10) == 5, 'wrong result'
+    assert obj.add(9) == 8, 'wrong result'
+    assert obj.add(4) == 8, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_kth_largest()
