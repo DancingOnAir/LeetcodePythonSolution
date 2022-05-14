6fe16c84b2fe09bf03fccf19128ecbb39eff7acc
DancingOnAir <taotong1984@hotmail.com>
2022-05-14 19:29:35 +0800

    feat(Tree): accepted improved preorder traverse + recursive python solution for q0669

--- Tree/0669_trim_a_binary_search_tree.py
+++ Tree/0669_trim_a_binary_search_tree.py
@@ -11,6 +11,20 @@ class TreeNode:
 
 class Solution:
     def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        if root.val < low:
+            return self.trimBST(root.right, low, high)
+        elif root.val > high:
+            return self.trimBST(root.left, low, high)
+        else:
+            root.left = self.trimBST(root.left, low, high)
+            root.right = self.trimBST(root.right, low, high)
+        return root
+
+    # preorder traverse
+    def trimBST1(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
         def preorder(node: Optional[TreeNode], parent: Optional[TreeNode]):
             if node:
                 if parent is None:
