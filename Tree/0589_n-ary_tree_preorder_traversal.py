ae3232e04965b50675db4c9d3a66de2d89786e35
DancingOnAir <taotong1984@hotmail.com>
2022-05-21 00:03:11 +0800

    feat(Tree): niit q0589 & accepted stack python solution for q0589

---
+++ Tree/0589_n-ary_tree_preorder_traversal.py
@@ -0,0 +1,34 @@
+from typing import List, Optional
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+
+        stk = [root]
+        res = list()
+        while stk:
+            x = stk.pop()
+            res.append(x.val)
+            stk += reversed(x.children)
+        return res
+
+
+def test_preorder():
+    solution = Solution()
+    node3 = Node(3, [Node(5), Node(6)])
+    root = Node(1, [node3, Node(2), Node(4)])
+    assert solution.preorder(root) == [1, 3, 5, 6, 2, 4], 'wrong result'
+
+
+if __name__ == '__main__':
+    test_preorder()
+
