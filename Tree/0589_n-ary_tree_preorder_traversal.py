405bd8da1b3714e50cf55b3f7392de2e87a1a51e
DancingOnAir <taotong1984@hotmail.com>
2022-05-21 09:17:33 +0800

    feat(Tree): accepted dfs python solution for q0589

--- Tree/0589_n-ary_tree_preorder_traversal.py
+++ Tree/0589_n-ary_tree_preorder_traversal.py
@@ -10,6 +10,19 @@ class Node:
 
 class Solution:
     def preorder(self, root: 'Node') -> List[int]:
+        self.res = list()
+
+        def dfs(root):
+            if not root:
+                return
+            self.res.append(root.val)
+            for child in root.children:
+                dfs(child)
+
+        dfs(root)
+        return self.res
+
+    def preorder1(self, root: 'Node') -> List[int]:
         if not root:
             return []
 
