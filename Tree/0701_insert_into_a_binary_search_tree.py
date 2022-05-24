6a49209296e481b5a67d91f32859ed1d96b39cc6
DancingOnAir <taotong1984@hotmail.com>
2022-05-24 11:24:43 +0800

    feat(Tree): init q0701 & accepted recursive python solution for q0701

---
+++ Tree/0701_insert_into_a_binary_search_tree.py
@@ -0,0 +1,45 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        def helper(root: Optional[TreeNode], parent: Optional[TreeNode]):
+            if not root:
+                if not parent:
+                    return
+
+                if parent.val < val:
+                    parent.right = TreeNode(val)
+                else:
+                    parent.left = TreeNode(val)
+                return
+            if root.val < val:
+                helper(root.right, root)
+            else:
+                helper(root.left, root)
+
+        helper(root, None)
+        return root if root else TreeNode(val)
+
+
+def test_insert_into_bst():
+    solution = Solution()
+
+    node2 = TreeNode(2, TreeNode(1), TreeNode(3))
+    root = TreeNode(4, node2, TreeNode(7))
+    assert solution.insertIntoBST(root, 5).val == 4, 'wrong result'
+
+    root = None
+    assert solution.insertIntoBST(root, 5).val == 5, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_insert_into_bst()
