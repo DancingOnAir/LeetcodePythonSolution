21e329d55354468ad93342fd3c8706216a4b018e
DancingOnAir <taotong1984@hotmail.com>
2022-06-01 12:17:28 +0800

    feat(Tree): accepted bfs & LCA python solution for q0865

--- Tree/0865_smallest_subtree_with_all_the_deepest_nodes.py
+++ Tree/0865_smallest_subtree_with_all_the_deepest_nodes.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Set
 from collections import deque
 
 
@@ -11,6 +11,32 @@ class TreeNode:
 
 
 class Solution:
+    # find nodes of the deepest level and LCA
+    def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def LCA(root: Optional[TreeNode], nodes: Set[Optional[TreeNode]]):
+            if not root or root in nodes:
+                return root
+
+            left = LCA(root.left, nodes)
+            right = LCA(root.right, nodes)
+
+            if left and right:
+                return root
+            return left if left else right
+
+        level = [root]
+        while level:
+            cur = list()
+            for node in level:
+                if node.left:
+                    cur.append(node.left)
+                if node.right:
+                    cur.append(node.right)
+            if not cur:
+                break
+            level = cur
+        return LCA(root, set(level))
+
     #  Intuitively, we should be traversing from the children to the parent and calculate the
     #  height from bottom. So the null nodes would have height 0. The leaf nodes would have the
     #  height 1 and the root would have the max height.
@@ -22,7 +48,7 @@ class Solution:
     #  at right child.
     #
     #  Which traversal allows us to traverse from bottom-up? Postorder! So we use it in the code.
-    def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+    def subtreeWithAllDeepest1(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         def deep(node: Optional[TreeNode]):
             if not node:
                 return 0, None
@@ -46,7 +72,7 @@ def test_subtree_with_deepest():
     node5 = TreeNode(5, TreeNode(6), node2)
     node1 = TreeNode(1, TreeNode(0), TreeNode(8))
     root = TreeNode(3, node5, node1)
-    assert solution.subtreeWithAllDeepest(root) == node2, 'wrong result'
+    assert solution.subtreeWithAllDeepest(root).val == 2, 'wrong result'
 
 
 if __name__ == '__main__':
