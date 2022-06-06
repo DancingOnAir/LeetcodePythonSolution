1d2240697d4ea0addd576cbd3dc7656b5054be75
DancingOnAir <taotong1984@hotmail.com>
2022-06-06 11:34:39 +0800

    feat(Tree): init q0919 & accepeted deque python solution for q0919

---
+++ Tree/0919_complete_binary_tree_inserter.py
@@ -0,0 +1,60 @@
+from typing import Optional
+from collections import deque
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class CBTInserter:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.root = root
+        self.nxt = None
+        self.dq = deque([root])
+        while self.dq:
+            cur = self.dq[0]
+            if cur.left and cur.right:
+                self.dq.append(cur.left)
+                self.dq.append(cur.right)
+                self.dq.popleft()
+            else:
+                if cur.left:
+                    self.dq.append(cur.left)
+                self.nxt = cur
+                break
+
+    def insert(self, val: int) -> int:
+        res = self.nxt.val
+        if not self.nxt.left:
+            self.nxt.left = TreeNode(val)
+            self.dq.append(self.nxt.left)
+        else:
+            self.nxt.right = TreeNode(val)
+            self.dq.append(self.nxt.right)
+            self.dq.popleft()
+            self.nxt = self.dq[0]
+
+        return res
+
+    def get_root(self) -> Optional[TreeNode]:
+        return self.root
+
+
+def test_cbt_intserter():
+    root = TreeNode(1, TreeNode(2))
+    obj = CBTInserter(root)
+    assert obj.insert(3) == 1, 'wrong result'
+    assert obj.insert(4) == 2, 'wrong result'
+    assert obj.get_root().val == 1, 'wrong result'
+    assert obj.get_root().left.val == 2, 'wrong result'
+    assert obj.get_root().right.val == 3, 'wrong result'
+    assert obj.get_root().left.left.val == 4, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_cbt_intserter()
