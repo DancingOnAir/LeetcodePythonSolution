9361939be96cdd7bc018c53adfa85a5270aea5f7
DancingOnAir <taotong1984@hotmail.com>
2022-06-07 09:27:18 +0800

    feat(Tree): accepeted laconic python solution for q0938

--- Tree/0938_range_sum_of_bst.py
+++ Tree/0938_range_sum_of_bst.py
@@ -14,6 +14,12 @@ class Solution:
         if not root:
             return 0
 
+        return self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high) + (root.val if low <= root.val <= high else 0)
+
+    def rangeSumBST1(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        if not root:
+            return 0
+
         left = right = 0
         if root.val > high:
             left = self.rangeSumBST(root.left, low, high)
