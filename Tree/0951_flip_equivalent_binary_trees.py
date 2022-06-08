edd78857751c10fc3f1c283af00e9a08f9cd8afe
DancingOnAir <taotong1984@hotmail.com>
2022-06-08 10:50:12 +0800

    feat(Tree): accepted straightforward recursive python solution for q0951

--- Tree/0951_flip_equivalent_binary_trees.py
+++ Tree/0951_flip_equivalent_binary_trees.py
@@ -11,6 +11,11 @@ class TreeNode:
 
 class Solution:
     def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        if not root1 and not root2:
+            return True
+        elif not (root1 and root2):
+            return False
+
         if root1.val == root2.val:
             if root1.left and root2.left and root1.right and root2.right:
                 if root1.left.val == root2.left.val and root1.right.val == root2.right.val:
@@ -39,21 +44,26 @@ class Solution:
                     return self.flipEquiv(root1.right, root2.right)
                 else:
                     return False
-            return True
+            elif not (root1.left or root1.right or root2.left or root2.right):
+                return True
+
+            return False
 
 
 def test_flip_equiv():
     solution = Solution()
-    node51 = TreeNode(5, TreeNode(7), TreeNode(8))
-    node21 = TreeNode(2, TreeNode(4), node51)
-    node31 = TreeNode(3, TreeNode(6))
-    root1 = TreeNode(1, node21, node31)
+    # node51 = TreeNode(5, TreeNode(7), TreeNode(8))
+    # node21 = TreeNode(2, TreeNode(4), node51)
+    # node31 = TreeNode(3, TreeNode(6))
+    # root1 = TreeNode(1, node21, node31)
+    #
+    # node52 = TreeNode(5, TreeNode(8), TreeNode(7))
+    # node32 = TreeNode(3, None, TreeNode(6))
+    # node22 = TreeNode(2, TreeNode(4), node52)
+    # root2 = TreeNode(1, node32, node22)
+    # assert solution.flipEquiv(root1, root2), 'wrong result'
 
-    node52 = TreeNode(5, TreeNode(8), TreeNode(7))
-    node32 = TreeNode(3, None, TreeNode(6))
-    node22 = TreeNode(2, TreeNode(4), node52)
-    root2 = TreeNode(1, node32, node22)
-    assert solution.flipEquiv(root1, root2), 'wrong result'
+    assert not solution.flipEquiv(TreeNode(1, TreeNode(2), TreeNode(3)), TreeNode(1, TreeNode(2, TreeNode(3)))), 'wrong result'
 
 
 if __name__ == '__main__':
