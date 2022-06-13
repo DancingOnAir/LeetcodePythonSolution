e25273d40fc98023afd5c8695c6d3e7ef83e8e44
DancingOnAir <taotong1984@hotmail.com>
2022-06-13 15:14:29 +0800

    feat(Tree): accepted recursive python solution for q0979

--- Tree/0979_distribute_coins_in_binary_tree.py
+++ Tree/0979_distribute_coins_in_binary_tree.py
@@ -10,7 +10,17 @@ class TreeNode:
 
 
 class Solution:
-    def distributeCoins(self, root: Optional[TreeNode]) -> int:
+    # recursive
+    def distributeCoins(self, root: Optional[TreeNode], pre: Optional[TreeNode] = None) -> int:
+        if not root:
+            return 0
+        res = self.distributeCoins(root.left, root) + self.distributeCoins(root.right, root)
+        if pre:
+            pre.val += root.val - 1
+        return res + abs(root.val - 1)
+
+    # dfs
+    def distributeCoins1(self, root: Optional[TreeNode]) -> int:
         def postorder(root):
             if not root:
                 return 0
@@ -18,10 +28,9 @@ class Solution:
             left = postorder(root.left)
             right = postorder(root.right)
 
-            root.val += left + right
-            self.res += abs(root.val - 1)
+            self.res += abs(left) + abs(right)
 
-            return root.val - 1
+            return root.val - 1 + left + right
 
         self.res = 0
         postorder(root)
