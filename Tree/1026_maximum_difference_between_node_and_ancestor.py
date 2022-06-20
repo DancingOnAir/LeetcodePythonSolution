d35c321f39a703a8c4199d3e6ec5108757d32493
DancingOnAir <taotong1984@hotmail.com>
2022-06-20 16:56:38 +0800

    feat(Tree): accepted improved dfs python solution for q1026

--- Tree/1026_maximum_difference_between_node_and_ancestor.py
+++ Tree/1026_maximum_difference_between_node_and_ancestor.py
@@ -11,14 +11,27 @@ class TreeNode:
 
 class Solution:
     def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        def helper(node, lo, hi):
+            if not node:
+                return hi - lo
+            hi = max(hi, node.val)
+            lo = min(lo, node.val)
+            # 这里不需要比较hi - lo的值，因为可以把一条路径走到底，直接比较这条路径上的最小值和最大值即可。
+            return max(helper(node.left, lo, hi), helper(node.right, lo, hi))
+
+        if not root:
+            return 0
+        return helper(root, root.val, root.val)
+
+    def maxAncestorDiff1(self, root: Optional[TreeNode]) -> int:
         def dfs(node, arr):
             if not node:
                 return
 
             arr.append(node.val)
             mx = max(arr)
-            mi = min(arr)
-            self.res = max(self.res, mx - mi)
+            mn = min(arr)
+            self.res = max(self.res, mx - mn)
             dfs(node.left, arr)
             dfs(node.right, arr)
             arr.pop()
