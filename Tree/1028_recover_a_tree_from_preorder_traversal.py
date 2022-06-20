58aabc1e328703db284baa3f80128429ad6ffb80
DancingOnAir <taotong1984@hotmail.com>
2022-06-20 23:40:30 +0800

    feat(Tree): init q1028

---
+++ Tree/1028_recover_a_tree_from_preorder_traversal.py
@@ -0,0 +1,37 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def recoverFromPreorder(self, traversal: str) -> Optional[TreeNode]:
+        pass
+
+
+def test_recover_from_preorder():
+    solution = Solution()
+    root1 = solution.recoverFromPreorder("1-2--3--4-5--6--7")
+    assert root1.val == 1, 'wrong result'
+    assert root1.left.val == 2, 'wrong result'
+    assert root1.right.val == 5, 'wrong result'
+    assert root1.left.left.val == 3, 'wrong result'
+    assert root1.left.right.val == 4, 'wrong result'
+
+    root2 = solution.recoverFromPreorder("1-2--3---4-5--6---7")
+    assert root2.val == 1, 'wrong result'
+    assert root2.left.val == 2, 'wrong result'
+    assert root2.right.val == 5, 'wrong result'
+    assert root2.left.left.val == 3, 'wrong result'
+    assert root2.right.left.val == 6, 'wrong result'
+    assert root2.left.left.leftval == 4, 'wrong result'
+    assert root2.right.left.left.val == 7, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_recover_from_preorder()
