e03cf995b2ed765317776678c60bd689143fe260
DancingOnAir <taotong1984@hotmail.com>
2022-06-22 10:51:42 +0800

    feat(Tree): init q1038 & accepted inorder python solution for q1038

---
+++ Tree/1038_binary_search_tree_to_greater_sum_tree.py
@@ -0,0 +1,56 @@
+from itertools import accumulate
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        vals = list()
+        node = root
+        stk = list()
+
+        while stk or node:
+            while node:
+                stk.append(node)
+                node = node.left
+            node = stk.pop()
+            vals.append(node.val)
+            node = node.right
+
+        vals = list(accumulate(vals[::-1]))
+
+        stk = list()
+        node = root
+        while stk or node:
+            while node:
+                stk.append(node)
+                node = node.left
+            node = stk.pop()
+            node.val = vals.pop()
+            node = node.right
+        return root
+
+
+def test_bst_to_gst():
+    solution = Solution()
+
+    root1 = TreeNode(4, TreeNode(1, TreeNode(0), TreeNode(2, None, TreeNode(3))), TreeNode(6, TreeNode(5), TreeNode(7, None, TreeNode(8))))
+    root2 = solution.bstToGst(root1)
+    assert root2.val == 30, 'wrong result'
+    assert root2.left.val == 36, 'wrong result'
+    assert root2.right.val == 21, 'wrong result'
+
+    root3 = TreeNode(0, None, TreeNode(1))
+    root4 = solution.bstToGst(root3)
+    assert root4.val == 1, 'wrong result'
+    assert root4.right.val == 1, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_bst_to_gst()
