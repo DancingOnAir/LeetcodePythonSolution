7dad0edef301714b0602440e2f88f5d89bca158d
DancingOnAir <taotong1984@hotmail.com>
2022-06-25 22:03:26 +0800

    feat(Tree): init q1104 & accepted straightforward python solution for q1104

--- Tree/1104_path_in_zigzag_labelled_binary_tree.py
+++ Tree/1104_path_in_zigzag_labelled_binary_tree.py
@@ -3,7 +3,29 @@ from typing import List
 
 class Solution:
     def pathInZigZagTree(self, label: int) -> List[int]:
-        pass
+        level = 0
+        label_idx = -1
+        arr = list()
+        while 2 ** level <= label:
+            cur = list()
+            for num in range(2 ** level, 2 ** (level + 1)):
+                cur.append(num)
+                if num == label:
+                    label_idx = len(cur) - 1
+            level += 1
+            if level & 1:
+                label_idx += len(arr)
+                arr.extend(cur)
+            else:
+                label_idx = len(arr) + len(cur) - label_idx - 1
+                arr.extend(cur[::-1])
+
+        res = list()
+        while label_idx > 0:
+            res.append(arr[label_idx])
+            label_idx = (label_idx - 1) // 2
+        res.append(arr[0])
+        return res[::-1]
 
 
 def test_path_in_zigzag_tree():
