8d41b3793ba5d2aa46c33a1d607ceea5c509562b
DancingOnAir <taotong1984@hotmail.com>
2022-07-10 11:00:01 +0800

    feat(Tree): init q1373 & accepted dfs python solution for q1373

---
+++ Tree/1373_maximum_sum_bst_in_binary_tree.py
@@ -0,0 +1,59 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxSumBST(self, root: Optional[TreeNode]) -> int:
+        def dfs(node):
+            if not node:
+                return True, None, None, None
+            left_is_bst, left_node, left_min_val, left_max_val = dfs(node.left)
+            right_is_bst, right_node, right_min_val, right_max_val = dfs(node.right)
+
+            if left_is_bst and right_is_bst:
+                node_val = node.val
+                if not left_node and not right_node:
+                    self.res = max(self.res, node.val)
+                    return True, node, node_val, node_val
+                elif not right_node and left_max_val < node.val:
+                    node.val += left_node.val
+                    self.res = max(self.res, node.val)
+                    return True, node, left_min_val, node_val
+                elif not left_node and right_min_val > node.val:
+                    node.val += right_node.val
+                    self.res = max(self.res, node.val)
+                    return True, node, node_val, right_max_val
+                elif left_node and right_node and left_max_val < node.val < right_min_val:
+                    node.val += left_node.val + right_node.val
+                    self.res = max(self.res, node.val)
+                    return True, node, left_min_val, right_max_val
+
+            return False, node, node.val, node.val
+
+        self.res = 0
+        dfs(root)
+        return self.res
+
+
+def test_max_sum_bst():
+    solution = Solution()
+
+    root1 = TreeNode(1, TreeNode(4, TreeNode(2), TreeNode(4)), TreeNode(3, TreeNode(2), TreeNode(5, TreeNode(4), TreeNode(6))))
+    assert solution.maxSumBST(root1) == 20, 'wrong result'
+
+    root2 = TreeNode(4, TreeNode(3, TreeNode(1), TreeNode(2)))
+    assert solution.maxSumBST(root2) == 2, 'wrong result'
+
+    root3 = TreeNode(-4, TreeNode(-2), TreeNode(-5))
+    assert solution.maxSumBST(root3) == 0, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_max_sum_bst()
