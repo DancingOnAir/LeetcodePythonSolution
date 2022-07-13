06c6b1ad8af93748544f953b4dd24db1f7acdb4d
DancingOnAir <taotong1984@hotmail.com>
2022-07-13 22:59:33 +0800

    feat(Tree): init q1379 & accepted dfs python solution for q1379

---
+++ Tree/1379_find_a_corresponding_node_of_a_binary_tree_in_a_clone_of_that_tree.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x, left=None, right=None):
+        self.val = x
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def getTargetCopy(self, original: Optional[TreeNode], cloned: Optional[TreeNode], target: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not original:
+            return None
+        if original == target:
+            return cloned
+        return self.getTargetCopy(original.left, cloned.left, target) or self.getTargetCopy(original.right, cloned.right, target)
+
+
+def test_get_target_copy():
+    solution = Solution()
+    original1 = TreeNode(7, TreeNode(4), TreeNode(3, TreeNode(6), TreeNode(19)))
+    cloned1 = TreeNode(7, TreeNode(4), TreeNode(3, TreeNode(6), TreeNode(19)))
+    target1 = original1.right
+    res1 = cloned1.right
+    assert solution.getTargetCopy(original1, cloned1, target1) == res1, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_get_target_copy()
