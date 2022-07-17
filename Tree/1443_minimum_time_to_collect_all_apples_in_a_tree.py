7933b02e25ca57eb78f4e42bde9e518654ef3736
DancingOnAir <taotong1984@hotmail.com>
2022-07-17 23:59:00 +0800

    feat(Tree): init q1443 & accepted dfs python solution for q1443

---
+++ Tree/1443_minimum_time_to_collect_all_apples_in_a_tree.py
@@ -0,0 +1,40 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+        graph = defaultdict(list)
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+
+        seen = [False for _ in range(n)]
+
+        def dfs(node):
+            if seen[node]:
+                return 0
+            seen[node] = True
+
+            time = 0
+            for child in graph[node]:
+                time += dfs(child)
+            if time > 0:
+                return time + 2
+            return 2 if hasApple[node] else 0
+        return max(dfs(0) - 2, 0)
+
+
+def test_min_time():
+    solution = Solution()
+
+    assert solution.minTime(7, [[0, 1], [0, 2], [1, 4], [1, 5], [2, 3], [2, 6]],
+                            [False, False, True, False, True, True, False]) == 8, 'wrong result'
+    assert solution.minTime(7, [[0, 1], [0, 2], [1, 4], [1, 5], [2, 3], [2, 6]],
+                            [False, False, True, False, False, True, False]) == 6, 'wrong result'
+    assert solution.minTime(7, [[0, 1], [0, 2], [1, 4], [1, 5], [2, 3], [2, 6]],
+                            [False, False, False, False, False, False, False]) == 0, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_min_time()
