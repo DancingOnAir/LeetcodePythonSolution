a23d280c933c1781de2db654d27cb806c9966317
DancingOnAir <taotong1984@hotmail.com>
2022-07-19 10:18:24 +0800

    feat(Tree): tried a TLE python solution for q1457

--- Tree/1457_pseudo-palindromic_paths_in_a_binary_tree.py
+++ Tree/1457_pseudo-palindromic_paths_in_a_binary_tree.py
@@ -11,17 +11,18 @@ class TreeNode:
 
 
 class Solution:
+    # TLE
     def pseudoPalindromicPaths(self, root: Optional[TreeNode]) -> int:
         def preorder(node, path):
-            if not node:
-                if sum(1 for x in Counter(path).values() if x & 1) == 1:
-                    self.res += 1
-                return
+            if node:
+                path.append(node.val)
+                if not node.left and not node.right:
+                    if sum(1 for x in Counter(path).values() if x & 1) <= 1:
+                        self.res += 1
 
-            path.append(node.val)
-            preorder(node.left, path)
-            preorder(node.right, path)
-            path.pop()
+                preorder(node.left, path)
+                preorder(node.right, path)
+                path.pop()
 
         self.res = 0
         preorder(root, [])
@@ -30,6 +31,7 @@ class Solution:
 
 def test_pseudo_palindromic_paths():
     solution = Solution()
+    assert solution.pseudoPalindromicPaths(TreeNode(3, TreeNode(8), TreeNode(5, TreeNode(5, None, TreeNode(8, TreeNode(8, TreeNode(2, None, TreeNode(1))))), TreeNode(6, None, TreeNode(5))))) == 0, 'wrong result'
     assert solution.pseudoPalindromicPaths(TreeNode(2, TreeNode(3, TreeNode(3), TreeNode(1)), TreeNode(1, None, TreeNode(1)))) == 2, 'wrong result'
     assert solution.pseudoPalindromicPaths(TreeNode(2, TreeNode(1, TreeNode(1), TreeNode(3, None, TreeNode(1))), TreeNode(1))) == 1, 'wrong result'
 
