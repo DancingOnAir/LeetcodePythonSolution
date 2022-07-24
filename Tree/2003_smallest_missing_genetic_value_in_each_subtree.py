4f5d959618996a16665f105bf89cd536b4a34b42
DancingOnAir <taotong1984@hotmail.com>
2022-07-25 00:12:46 +0800

    init q2003

---
+++ Tree/2003_smallest_missing_genetic_value_in_each_subtree.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def smallestMissingValueSubtree(self, parents: List[int], nums: List[int]) -> List[int]:
+        pass
+
+
+def test_smallest_missing_value_subtree():
+    solution = Solution()
+    assert solution.smallestMissingValueSubtree([-1, 0, 0, 2], [1, 2, 3, 4]) == [5, 1, 1, 1], 'wrong result'
+    assert solution.smallestMissingValueSubtree([-1, 0, 1, 0, 3, 3], [5, 4, 6, 2, 1, 3]) == [7, 1, 1, 4, 2, 1], 'wrong result'
+    assert solution.smallestMissingValueSubtree([-1, 2, 3, 0, 2, 4, 1], [2, 3, 4, 5, 6, 7, 8]) == [1, 1, 1, 1, 1, 1, 1], 'wrong result'
+
+
+if __name__ == '__main__':
+    test_smallest_missing_value_subtree()
