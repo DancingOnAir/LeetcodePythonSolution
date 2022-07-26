ff75d642d264556bcdbf8cdc475396655ab86131
DancingOnAir <taotong1984@hotmail.com>
2022-07-27 07:51:27 +0800

    feat(Tree): tried a iterative python solution for q2331 but wrong

--- Tree/2331_evaluate_boolean_binary_tree.py
+++ Tree/2331_evaluate_boolean_binary_tree.py
@@ -11,6 +11,29 @@ class TreeNode:
 
 class Solution:
     def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        stk1 = [root]
+        stk2 = list()
+        while stk1:
+            node = stk1.pop()
+            if node.left:
+                stk1.append(node.left)
+            if node.right:
+                stk1.append(node.right)
+            stk2.append(node.val)
+
+        if len(stk2) == 1:
+            return stk2.pop()
+        res = stk2.pop()
+        while stk2:
+            cur = stk2.pop()
+            op = stk2.pop()
+            if op == 2:
+                res = res or cur
+            else:
+                res = res and cur
+        return res
+
+    def evaluateTree1(self, root: Optional[TreeNode]) -> bool:
         if root.left == root.right:
             return root.val == 1
 
