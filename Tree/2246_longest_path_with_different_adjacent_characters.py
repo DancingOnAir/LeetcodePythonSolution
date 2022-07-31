9ca56f128a635f946d05d968c9d69fde122543a6
DancingOnAir <taotong1984@hotmail.com>
2022-07-31 11:07:09 +0800

    feat(Tree): accepted improved dfs python solution for q2246

--- Tree/2246_longest_path_with_different_adjacent_characters.py
+++ Tree/2246_longest_path_with_different_adjacent_characters.py
@@ -1,8 +1,34 @@
 from typing import List
-
+from heapq import nlargest
 
 class Solution:
+    # improved dfs
     def longestPath(self, parent: List[int], s: str) -> int:
+        n = len(parent)
+        tree = [[] for _ in range(n)]
+        for i, p in enumerate(parent):
+            if p != -1:
+                tree[p].append(i)
+
+        def dfs(i):
+            nonlocal res
+            candidates = [0]
+            for child in tree[i]:
+                # 必须先计算子节点，再判断父节点和子节点字符是否一样，因为子树里也可能有最长的chain
+                cur = dfs(child)
+                if s[i] != s[child]:
+                    candidates.append(cur)
+
+            candidates = nlargest(2, candidates)
+            res = max(res, sum(candidates) + 1)
+            return max(candidates) + 1
+
+        res = 0
+        dfs(0)
+        return res
+
+    # dfs
+    def longestPath1(self, parent: List[int], s: str) -> int:
         n = len(parent)
         tree = [[] for _ in range(n)]
         for i in range(1, n):
@@ -12,6 +38,7 @@ class Solution:
             nonlocal res
             max_len = 0
             for j in tree[i]:
+                # 获取子树的最长边，此外还要加上i到j的边
                 cur_len = dfs(j) + 1
                 if s[i] != s[j]:
                     res = max(res, max_len + cur_len)
@@ -20,6 +47,7 @@ class Solution:
 
         res = 0
         dfs(0)
+        # 计算的是节点，dfs计算的是边
         return res + 1
 
 
