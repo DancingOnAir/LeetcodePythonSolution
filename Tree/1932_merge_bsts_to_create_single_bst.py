0adc9cc46a7a094c89756600fe64abb3462d69ac
DancingOnAir <taotong1984@hotmail.com>
2022-08-04 14:52:47 +0800

    feat(Tree): accepted inorder + in degress python solution for q1932

--- Tree/1932_merge_bsts_to_create_single_bst.py
+++ Tree/1932_merge_bsts_to_create_single_bst.py
@@ -1,4 +1,5 @@
-from typing import Optional, List
+from typing import Optional, List, Set
+from collections import defaultdict
 
 
 # Definition for a binary tree node.
@@ -11,7 +12,55 @@ class TreeNode:
 
 class Solution:
     def canMerge(self, trees: List[TreeNode]) -> Optional[TreeNode]:
-        return trees[0]
+        def inorder(val):
+            # check cycle
+            if val in seen:
+                self.invalid = True
+                return
+
+            seen.add(val)
+            node = nodes[val]
+            if node.left:
+                node.left = inorder(node.left.val)
+            # check inorder increasing
+            if val <= self.cur:
+                self.invalid = True
+                return
+
+            self.cur = val
+            if node.right:
+                node.right = inorder(node.right.val)
+            return node
+
+        in_degree = defaultdict(int)
+        nodes = dict()
+        for root in trees:
+            if root.val not in in_degree:
+                in_degree[root.val] = 0
+
+            if root.left:
+                in_degree[root.left.val] += 1
+                if root.left.val not in nodes:
+                    nodes[root.left.val] = root.left
+
+            if root.right:
+                in_degree[root.right.val] += 1
+                if root.right.val not in nodes:
+                    nodes[root.right.val] = root.right
+            # if leaf nodes of previous trees have the same value with this root, just replace it.
+            nodes[root.val] = root
+
+        sources = [k for k, v in in_degree.items() if v == 0]
+        if len(sources) != 1:
+            return None
+
+        self.cur = float('-inf')
+        self.invalid = False
+        seen = set()
+        res = inorder(sources[0])
+        if len(seen) != len(nodes) or self.invalid:
+            return None
+        return res
 
 
 def test_can_merge():
