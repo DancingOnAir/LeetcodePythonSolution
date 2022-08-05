c2cbefc891830df87576a5b90a5e398ec599e7e0
DancingOnAir <taotong1984@hotmail.com>
2022-08-05 14:54:22 +0800

    feat(Tree): accepted concise python solution for q1932

--- Tree/1932_merge_bsts_to_create_single_bst.py
+++ Tree/1932_merge_bsts_to_create_single_bst.py
@@ -12,6 +12,50 @@ class TreeNode:
 
 class Solution:
     def canMerge(self, trees: List[TreeNode]) -> Optional[TreeNode]:
+        def valid_bst(node, left=float('-inf'), right=float('inf')):
+            if not node:
+                return True
+            res = left < node.val < right
+            return res and valid_bst(node.left, left, node.val) and valid_bst(node.right, node.val, right)
+
+        def merge(node):
+            if node.val in roots:
+                res = roots[node.val]
+                roots.pop(node.val)
+            else:
+                res = node
+
+            if res.left:
+                res.left = merge(res.left)
+            if res.right:
+                res.right = merge(res.right)
+            return res
+
+        roots = dict()
+        st = set()
+        for t in trees:
+            roots[t.val] = t
+            if t.left:
+                st.add(t.left.val)
+            if t.right:
+                st.add(t.right.val)
+
+        root = None
+        for t in trees:
+            if t.val not in st:
+                root = t
+                break
+
+        if not root:
+            return None
+
+        merge(root)
+        return root if valid_bst(root) and len(roots) == 0 else None
+
+    # 如果有解，那么一定是唯一的
+    # 有1个bst树的根节点trees[i]作为root的话，其他的bst的节点一定不包含trees[i].val的值，不然最后合成的bst有重复值，无效的bst
+    # 存在1个以上的bst树的叶子结点含有某个bst树根节点的值吗？不可能，不然最后的合成的bst也会有重复值，无效的bst
+    def canMerge1(self, trees: List[TreeNode]) -> Optional[TreeNode]:
         def inorder(val):
             # check cycle
             if val in seen:
@@ -71,10 +115,10 @@ def test_can_merge():
     assert res1.left.val == 2, 'wrong result'
     assert res1.right.val == 5, 'wrong result'
 
-    res2 = solution.canMerge([TreeNode(5, TreeNode(3), TreeNode(8)), TreeNode(3, TreeNode(2), TreeNode(6))])
-    assert res2.val == 5, 'wrong result'
-    assert res2.left.val == 3, 'wrong result'
-    assert res2.right.val == 8, 'wrong result'
+    # res2 = solution.canMerge([TreeNode(5, TreeNode(3), TreeNode(8)), TreeNode(3, TreeNode(2), TreeNode(6))])
+    # assert res2.val == 5, 'wrong result'
+    # assert res2.left.val == 3, 'wrong result'
+    # assert res2.right.val == 8, 'wrong result'
 
     res3 = solution.canMerge([TreeNode(5, TreeNode(4)), TreeNode(3)])
     assert not res3, 'wrong result'
