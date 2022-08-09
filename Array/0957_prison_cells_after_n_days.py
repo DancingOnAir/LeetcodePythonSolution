5b39940dc101a981945ad68c8159f181e6063575
DancingOnAir <taotong1984@hotmail.com>
2022-08-09 09:47:15 +0800

    feat(Tree): accepted concise python solution for q0957

--- Array/0957_prison_cells_after_n_days.py
+++ Array/0957_prison_cells_after_n_days.py
@@ -3,6 +3,17 @@ from typing import List
 
 class Solution:
     def prisonAfterNDays(self, cells: List[int], n: int) -> List[int]:
+        seen = {str(cells): n}
+        while n:
+            seen.setdefault(str(cells), n)
+            n -= 1
+            cells = [0] + [cells[i - 1] ^ cells[i + 1] ^ 1 for i in range(1, 7)] + [0]
+
+            if str(cells) in seen:
+                n %= seen[str(cells)] - n
+        return cells
+
+    def prisonAfterNDays1(self, cells: List[int], n: int) -> List[int]:
         def changes(cells):
             res = [0] * len(cells)
             for i in range(1, len(cells) - 1):
