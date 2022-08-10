12e615b28aa2bfefe1108026f75de0664e391755
DancingOnAir <taotong1984@hotmail.com>
2022-08-11 00:37:51 +0800

    feat(Array): accepted o(n) python solution for q0135

--- Array/0135_candy.py
+++ Array/0135_candy.py
@@ -3,6 +3,28 @@ from typing import List
 
 class Solution:
     def candy(self, ratings: List[int]) -> int:
+        if not ratings:
+            return 0
+
+        increase = decrease = peak = 0
+        res = 1
+        for i in range(1, len(ratings)):
+            if ratings[i - 1] < ratings[i]:
+                increase += 1
+                peak = increase
+                res += increase + 1
+                decrease = 0
+            elif ratings[i - 1] > ratings[i]:
+                decrease += 1
+                res += 1 + decrease + (0 if decrease > peak else -1)
+                increase = 0
+            else:
+                increase = decrease = peak = 0
+                res += 1
+        return res
+
+    # two loops: from left to right and from right to left
+    def candy1(self, ratings: List[int]) -> int:
         n = len(ratings)
         res = [1] * n
         for i in range(n - 1):
