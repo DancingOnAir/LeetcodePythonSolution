d294e2d4d59688a040895709248e88f8af261c73
DancingOnAir <taotong1984@hotmail.com>
2022-08-13 22:01:45 +0800

    feat(Array): accepted quick sort python solution for q0215

--- Array/0215_kth_largest_element_in_an_array.py
+++ Array/0215_kth_largest_element_in_an_array.py
@@ -3,9 +3,28 @@ from random import choice
 
 
 class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        def partition(arr, lo, hi):
+            pivot = arr[hi]
+            ptr = lo
+            for i in range(lo, hi):
+                if arr[i] <= pivot:
+                    arr[i], arr[ptr] = arr[ptr], arr[i]
+                    ptr += 1
+            nums[ptr], nums[hi] = nums[hi], nums[ptr]
+            return ptr
+
+        def quick_sort(arr, lo, hi):
+            if lo < hi:
+                pi = partition(arr, lo, hi)
+                quick_sort(arr, lo, pi - 1)
+                quick_sort(arr, pi + 1, hi)
+        quick_sort(nums, 0, len(nums) - 1)
+        return nums[-k]
+
     # quick select
     # https://leetcode.com/problems/kth-largest-element-in-an-array/discuss/1019513/Python-QuickSelect-average-O(n)-explained
-    def findKthLargest(self, nums: List[int], k: int) -> int:
+    def findKthLargest2(self, nums: List[int], k: int) -> int:
         pivot = choice(nums)
         left = [x for x in nums if x > pivot]
         mid = [x for x in nums if x == pivot]
