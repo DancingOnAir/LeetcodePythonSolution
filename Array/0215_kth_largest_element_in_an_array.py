388aec2b80ef5121b4552c2fffa27cef9e72f59f
DancingOnAir <taotong1984@hotmail.com>
2022-08-13 12:03:51 +0800

    feat(Array): accepted quick select python solution for q0215

--- Array/0215_kth_largest_element_in_an_array.py
+++ Array/0215_kth_largest_element_in_an_array.py
@@ -1,8 +1,24 @@
 from typing import List
+from random import choice
 
 
 class Solution:
+    # quick select
+    # https://leetcode.com/problems/kth-largest-element-in-an-array/discuss/1019513/Python-QuickSelect-average-O(n)-explained
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        pivot = choice(nums)
+        left = [x for x in nums if x > pivot]
+        mid = [x for x in nums if x == pivot]
+        right = [x for x in nums if x < pivot]
+
+        if k <= len(left):
+            return self.findKthLargest(left, k)
+        elif k > len(left) + len(mid):
+            return self.findKthLargest(right, k - len(left) - len(mid))
+        else:
+            return mid[0]
+
+    def findKthLargest1(self, nums: List[int], k: int) -> int:
         return sorted(nums)[-k]
 
 
