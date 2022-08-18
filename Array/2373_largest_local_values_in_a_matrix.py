3779e3c7a320e856ca81ee7c2863d5a622ddba80
DancingOnAir <taotong1984@hotmail.com>
2022-08-18 13:55:11 +0800

    feat(Array): accepted simulation python solution for q2373

--- Array/2373_largest_local_values_in_a_matrix.py
+++ Array/2373_largest_local_values_in_a_matrix.py
@@ -3,6 +3,14 @@ from typing import List
 
 class Solution:
     def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+        n = len(grid)
+        res = [[0] * (n - 2) for _ in range(n - 2)]
+        for i in range(n - 2):
+            for j in range(n - 2):
+                res[i][j] = max(grid[ii][jj] for ii in range(i, i + 3) for jj in range(j, j + 3))
+        return res
+
+    def largestLocal1(self, grid: List[List[int]]) -> List[List[int]]:
         tmp = list()
         for row in grid:
             tmp.append([max(row[i:i + 3]) for i in range(len(row) - 2)])
