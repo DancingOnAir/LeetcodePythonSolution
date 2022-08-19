ba173800fa7d05f5c610725a06a0e4aee823d255
DancingOnAir <taotong1984@hotmail.com>
2022-08-20 00:17:41 +0800

    feat(Array): init q2363 & accepted Counter python solution for q2363

---
+++ Array/2363_merge_similar_items.py
@@ -0,0 +1,20 @@
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
+        c = Counter()
+        for v, w in items1 + items2:
+            c[v] += w
+        return [[k, v] for k, v in sorted(c.items())]
+
+
+def test_merge_similar_items():
+    solution = Solution()
+    assert solution.mergeSimilarItems([[1, 1], [4, 5], [3, 8]], [[3, 1], [1, 5]]) == [[1, 6], [3, 9], [4, 5]], 'wrong result'
+    assert solution.mergeSimilarItems([[1, 1], [3, 2], [2, 3]], [[2, 1], [3, 2], [1, 3]]) == [[1, 4], [2, 4], [3, 4]], 'wrong result'
+
+
+if __name__ == '__main__':
+    test_merge_similar_items()
