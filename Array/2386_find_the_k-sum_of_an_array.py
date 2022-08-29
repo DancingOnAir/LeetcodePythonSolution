fa971cef1b214b37ef71d8287eafbacebf45c253
DancingOnAir <taotong1984@hotmail.com>
2022-08-30 00:53:13 +0800

    feat(Array): accepted heap python solution for q2386

--- Array/2386_find_the_k-sum_of_an_array.py
+++ Array/2386_find_the_k-sum_of_an_array.py
@@ -1,9 +1,25 @@
 from typing import List
 from itertools import combinations
+from heapq import heappush, heappop
 
 
 class Solution:
+    # heap
     def kSum(self, nums: List[int], k: int) -> int:
+        m = sum(x for x in nums if x > 0)
+        pq = [(-m, 0)]
+        vals = sorted(abs(x) for x in nums)
+
+        for _ in range(k):
+            x, i = heappop(pq)
+            if i < len(vals):
+                heappush(pq, (x + vals[i], i + 1))
+                if i:
+                    heappush(pq, (x - vals[i - 1] + vals[i], i + 1))
+        return -x
+
+    # TLE
+    def kSum1(self, nums: List[int], k: int) -> int:
         n = len(nums)
         res = list()
         for i in range(n + 1):
