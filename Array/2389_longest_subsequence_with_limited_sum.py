63e6429428539090eec33911ade8d9c7f9eb394c
DancingOnAir <taotong1984@hotmail.com>
2022-09-01 22:04:40 +0800

    feat(Array): init q2389 & accepted python solution for q2389

---
+++ Array/2389_longest_subsequence_with_limited_sum.py
@@ -0,0 +1,22 @@
+from typing import List
+from itertools import accumulate
+from bisect import bisect_right
+
+
+class Solution:
+    def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        res = list()
+        pre_sum = list(accumulate(sorted(nums)))
+        for x in queries:
+            res.append(bisect_right(pre_sum, x))
+        return res
+
+
+def test_answer_queries():
+    solution = Solution()
+    assert solution.answerQueries([4, 5, 2, 1], [3, 10, 21]) == [2, 3, 4], 'wrong result'
+    assert solution.answerQueries([2, 3, 4, 5], [1]) == [0], 'wrong result'
+
+
+if __name__ == '__main__':
+    test_answer_queries()
