ac504e7175f56beb7856de6ddc1dd7dcce43f041
DancingOnAir <taotong1984@hotmail.com>
2022-09-03 23:34:48 +0800

    feat(Array): accepted super math python solution for q2358

--- Array/2358_maximum_number_of_groups_entering_a_competition.py
+++ Array/2358_maximum_number_of_groups_entering_a_competition.py
@@ -2,9 +2,18 @@ from typing import List
 
 
 class Solution:
+    # https://leetcode.com/problems/maximum-number-of-groups-entering-a-competition/discuss/2357789/JavaC%2B%2BPython-One-line-O(1)
+    # 1 + 2 + ... + k <= n
+    # k(k + 1) / 2 <= n
+    # (k + 0.5)(k + 0.5) <= n * 2 + 0.25
+    # k + 0.5 <= sqrt(n * 2 + 0.25)
+    # k <= sqrt(n * 2 + 0.25) - 0.5
+    def maximumGroups(self, grades: List[int]) -> int:
+        return int((len(grades) * 2 + 0.25) ** 0.5 - 0.5)
+
     # 不需要考虑总和，只要正整数，一定能找到一个组合满足金字塔且下层和大于上层和，那么关键是下层的数量要大于上层
     # find out the maximum result k that 1 + 2 + ... + k <= n
-    def maximumGroups(self, grades: List[int]) -> int:
+    def maximumGroups1(self, grades: List[int]) -> int:
         n = len(grades)
         k = 0
         while n >= k + 1:
