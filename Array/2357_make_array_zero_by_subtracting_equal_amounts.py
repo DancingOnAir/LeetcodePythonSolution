8583792036ecbeba6a0218d6c6d9371131092cad
DancingOnAir <taotong1984@hotmail.com>
2022-09-04 08:48:14 +0800

    feat(Array): init q2357 & accepted python solution for q2357

---
+++ Array/2357_make_array_zero_by_subtracting_equal_amounts.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        return len(set(nums)) - (1 if 0 in set(nums) else 0)
+
+
+def test_minimum_operations():
+    solution = Solution()
+    assert solution.minimumOperations([1, 5, 0, 3, 5]) == 3, 'wrong result'
+    assert solution.minimumOperations([0]) == 0, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_minimum_operations()
