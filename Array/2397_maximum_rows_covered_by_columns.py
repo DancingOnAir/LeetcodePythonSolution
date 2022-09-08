952f8e4f441376d3954d822d0badfb90eb3d5be0
DancingOnAir <taotong1984@hotmail.com>
2022-09-08 14:58:42 +0800

    feat(Array): accepted state compression python solution for q2397

--- Array/2397_maximum_rows_covered_by_columns.py
+++ Array/2397_maximum_rows_covered_by_columns.py
@@ -3,7 +3,31 @@ from itertools import combinations
 
 
 class Solution:
+    # bit manipulation & state compression
     def maximumRows(self, matrix: List[List[int]], numSelect: int) -> int:
+        m = len(matrix)
+        n = len(matrix[0])
+        # 保存每行压缩的状态
+        mask = [0] * m
+        for r in range(m):
+            for c in range(n):
+                mask[r] |= (matrix[r][c] << c)
+        res = 0
+        # 遍历从0到2^n - 1,列举选择各列的情况
+        for i in range(1 << n):
+            # 选取的列数等于numSelect
+            if sum(map(int, bin(i)[2:])) == numSelect:
+                cur = 0
+                for r in range(m):
+                    # 与操作，如果选取的列i包含了该行mask[r]所有的1
+                    if mask[r] & i == mask[r]:
+                        cur += 1
+                res = max(res, cur)
+        return res
+
+
+    # brute force
+    def maximumRows1(self, matrix: List[List[int]], numSelect: int) -> int:
         m = len(matrix)
         n = len(matrix[0])
         if numSelect == n:
