c4adb3ef883d8bd94ee008d1cebb47bd1e7c5c42
DancingOnAir <taotong1984@hotmail.com>
2022-09-09 09:16:27 +0800

    feat(Array): accepted set python solution for q2395

--- Array/2395_find_subarrays_with_equal_sum.py
+++ Array/2395_find_subarrays_with_equal_sum.py
@@ -2,7 +2,17 @@ from typing import List
 
 
 class Solution:
+    # 遍历nums，计算2个元素之和并添加到set里，如果set没有重复值，那么它的长度应该等于len(nums) - 1
     def findSubarrays(self, nums: List[int]) -> bool:
+        m = set()
+        for i in range(1, len(nums)):
+            m.add(nums[i - 1] + nums[i])
+        if len(m) == len(nums) - 1:
+            return False
+        return True
+
+    # 遍历nums，计算2个元素之和并添加到set里，判断set里有没有重复的
+    def findSubarrays1(self, nums: List[int]) -> bool:
         m = set()
         for i, val in enumerate(nums):
             if i > 0:
