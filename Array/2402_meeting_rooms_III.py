865123976afece3756202cb070f00b20ff2de6b0
DancingOnAir <taotong1984@hotmail.com>
2022-09-13 18:46:19 +0800

    feat(Array): init q2402 & accepted heapq python solution for q2402

--- Array/2402_meeting_rooms_III.py
+++ Array/2402_meeting_rooms_III.py
@@ -1,10 +1,27 @@
 from typing import List
+from heapq import heapify, heappush, heappop
 
 
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
-
-        pass
+        # 可以使用的房间号
+        ready = [x for x in range(n)]
+        # 正在使用的房间，元素[end_time, room_index]
+        rooms = list()
+        heapify(ready)
+        res = [0] * n
+        for start, end in sorted(meetings):
+            while rooms and rooms[0][0] <= start:
+                end_time, room_index = heappop(rooms)
+                heappush(ready, room_index)
+            if ready:
+                room_index = heappop(ready)
+                heappush(rooms, [end, room_index])
+            else:
+                end_time, room_index = heappop(rooms)
+                heappush(rooms, [end_time + end - start, room_index])
+            res[room_index] += 1
+        return res.index(max(res))
 
 
 def test_most_booked():
