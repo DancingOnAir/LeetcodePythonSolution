c233c9748d886c0d30761f8f19c8daedffb2ce58
DancingOnAir <taotong1984@hotmail.com>
2022-09-15 00:19:53 +0800

    feat(Array): init q2352 & accepted dict + zip python solution for q2352

---
+++ Array/2352_equal_row_and_column_pairs.py
@@ -0,0 +1,28 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def equalPairs(self, grid: List[List[int]]) -> int:
+        m = defaultdict(int)
+        for r in grid:
+            k = ','.join(map(str, r))
+            m[k] += 1
+
+        res = 0
+        for c in zip(*grid):
+            k = ','.join(map(str, c))
+            if k in m:
+                res += m[k]
+        return res
+
+
+def test_equal_pairs():
+    solution = Solution()
+    assert solution.equalPairs([[11, 1], [1, 11]]) == 2, 'wrong result'
+    assert solution.equalPairs([[3, 2, 1], [1, 7, 6], [2, 7, 7]]) == 1, 'wrong result'
+    assert solution.equalPairs([[3, 1, 2, 2], [1, 4, 4, 5], [2, 4, 2, 2], [2, 4, 2, 2]]) == 3, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_equal_pairs()
