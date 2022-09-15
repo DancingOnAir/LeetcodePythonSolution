b8b1b115b2febade5ec1e6a5e9d6b91d2398b639
DancingOnAir <taotong1984@hotmail.com>
2022-09-15 22:27:30 +0800

    feat(Array): init q2343 & accepted dict python solution for q2343

---
+++ Array/2343_query_kth_smallest_trimmed_number.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def smallestTrimmedNumbers(self, nums: List[str], queries: List[List[int]]) -> List[int]:
+        res = list()
+        m = dict()
+        for k, t in queries:
+            if t not in m:
+                m[t] = sorted([s[-t:], i] for i, s in enumerate(nums))
+            res.append(m[t][k - 1][1])
+        return res
+
+
+def test_smallest_trimmed_numbers():
+    solution = Solution()
+    assert solution.smallestTrimmedNumbers(["87", "70"], [[2, 1], [1, 2], [1, 2], [1, 1]]) == [2, 2, 1, 0]
+    assert solution.smallestTrimmedNumbers(["102", "473", "251", "814"], [[1, 1], [2, 3], [4, 2], [1, 2]]) == [2, 2, 1,
+                                                                                                               0]
+    assert solution.smallestTrimmedNumbers(["24", "37", "96", "04"], [[2, 1], [2, 2]]) == [3, 0]
+
+
+if __name__ == '__main__':
+    test_smallest_trimmed_numbers()
