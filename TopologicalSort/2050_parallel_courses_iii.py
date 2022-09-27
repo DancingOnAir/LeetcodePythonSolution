71f905c713d87cff0368e1583cbb1544fe555df1
DancingOnAir <taotong1984@hotmail.com>
2022-09-27 23:32:40 +0800

    feat(TopologicalSort): init q2050 & accepted topological sort python solution for q2050

---
+++ TopologicalSort/2050_parallel_courses_iii.py
@@ -0,0 +1,39 @@
+from typing import List
+from collections import deque, defaultdict
+
+
+class Solution:
+    def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
+        graph = [[] for _ in range(n)]
+        in_degree = [0] * n
+        for u, v in relations:
+            graph[u - 1].append(v - 1)
+            in_degree[v - 1] += 1
+
+        distance = [0] * n
+        dq = deque([])
+        for i, val in enumerate(in_degree):
+            if val == 0:
+                dq.append(i)
+                distance[i] = time[i]
+
+        while dq:
+            u = dq.popleft()
+            for v in graph[u]:
+                distance[v] = max(distance[v], distance[u] + time[v])
+                in_degree[v] -= 1
+                if in_degree[v] == 0:
+                    dq.append(v)
+
+        return max(distance)
+
+
+def test_minimum_time():
+    solution = Solution()
+    assert solution.minimumTime(3, [[1, 3], [2, 3]], [3, 2, 5]) == 8, 'wrong result'
+    assert solution.minimumTime(5, [[1, 5], [2, 5], [3, 5], [3, 4], [4, 5]], [1, 2, 3, 4, 5]) == 12, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_minimum_time()
+
