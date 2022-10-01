c7fbf7c92286e8db81e15ea69ba29d84dc72cfa8
DancingOnAir <taotong1984@hotmail.com>
2022-10-01 10:40:58 +0800

    feat(Array): init q2418 & accepted one line python solution for q2418

---
+++ Array/2418_sort_the_people.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        return [names[i] for _, i in sorted({val: i for i, val in enumerate(heights)}.items(), key=lambda x: -x[0])]
+
+
+def test_sort_people():
+    solution = Solution()
+    assert solution.sortPeople(["Mary", "John", "Emma"], [180, 165, 170]) == ["Mary", "Emma", "John"], 'wrong result'
+    assert solution.sortPeople(["Alice", "Bob", "Bob"], [155, 185, 150]) == ["Bob", "Alice", "Bob"], 'wrong result'
+
+
+if __name__ == '__main__':
+    test_sort_people()
+
