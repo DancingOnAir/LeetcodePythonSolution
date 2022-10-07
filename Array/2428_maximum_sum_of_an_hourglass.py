53798fcc0b8264037cc032e20ac0cb4b89fabd03
DancingOnAir <taotong1984@hotmail.com>
2022-10-07 11:36:20 +0800

    feat(Array): accepted brute force python solution for q2428

--- Array/2428_maximum_sum_of_an_hourglass.py
+++ Array/2428_maximum_sum_of_an_hourglass.py
@@ -4,6 +4,13 @@ from itertools import accumulate
 
 class Solution:
     def maxSum(self, grid: List[List[int]]) -> int:
+        res = 0
+        for i in range(1, len(grid) - 1):
+            for j in range(1, len(grid[0]) - 1):
+                res = max(res, grid[i - 1][j - 1] + grid[i - 1][j] + grid[i - 1][j + 1] + grid[i][j] + grid[i + 1][j - 1] + grid[i + 1][j] + grid[i + 1][j + 1])
+        return res
+
+    def maxSum1(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
         pre_sum = [[] for _ in range(m)]
         for i in range(m):
