fb1300911735362ac8c2fe4afe4d8c16d93b0917
DancingOnAir <taotong1984@hotmail.com>
2022-10-14 11:10:40 +0800

    feat(SweepLine): init q2251 & accepted sweep line + bisect python solution for q2251

---
+++ SweepLine/2251_number_of_flowers_in_full_bloom.py
@@ -0,0 +1,31 @@
+from typing import List
+from bisect import bisect_right
+from collections import defaultdict
+
+
+class Solution:
+    # TLE
+    def fullBloomFlowers(self, flowers: List[List[int]], persons: List[int]) -> List[int]:
+        endpoints = defaultdict(int)
+        for i, j in flowers:
+            endpoints[i] += 1
+            endpoints[j + 1] += -1
+
+        bloom = list()
+        cur = 0
+        for i in sorted(endpoints):
+            cur += endpoints[i]
+            bloom.append(cur)
+
+        return [bloom[bisect_right(sorted(endpoints), p) - 1] for p in persons]
+
+
+def test_full_bloom_flowers():
+    solution = Solution()
+    assert solution.fullBloomFlowers([[19, 37], [19, 38], [19, 35]], [6, 7, 21, 1, 13, 37, 5, 37, 46, 43]) == [0, 0, 3, 0, 0, 2, 0, 2, 0, 0], 'wrong result'
+    assert solution.fullBloomFlowers([[1, 6], [3, 7], [9, 12], [4, 13]], [2, 3, 7, 11]) == [1, 2, 2, 2], 'wrong result'
+    assert solution.fullBloomFlowers([[1, 10], [3, 3]], [3, 3, 2]) == [2, 2, 1], 'wrong result'
+
+
+if __name__ == '__main__':
+    test_full_bloom_flowers()
