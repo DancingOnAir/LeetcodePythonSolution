7dd0f213fa0de5db51e0262f9386bb620a2d40d3
DancingOnAir <taotong1984@hotmail.com>
2022-10-19 07:43:33 +0800

    feat(Array): accepted loop once python solution for q2442

--- Array/2442_count_number_of_distinct_integers_after_reverse_operations.py
+++ Array/2442_count_number_of_distinct_integers_after_reverse_operations.py
@@ -3,6 +3,9 @@ from typing import List
 
 class Solution:
     def countDistinctIntegers(self, nums: List[int]) -> int:
+        return len({b for a in nums for b in (a, int(str(a)[::-1]))})
+
+    def countDistinctIntegers1(self, nums: List[int]) -> int:
         return len({int(str(x)[::-1]) for x in nums} | set(nums))
 
 
