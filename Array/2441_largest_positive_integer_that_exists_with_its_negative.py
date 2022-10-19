f44e74e91a16b41b0f5bd3acb64198722b516b4f
DancingOnAir <taotong1984@hotmail.com>
2022-10-19 21:33:09 +0800

    feat(Array): init q2441 & accepted set python solution for q2441

---
+++ Array/2441_largest_positive_integer_that_exists_with_its_negative.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def findMaxK(self, nums: List[int]) -> int:
+        return max([x for x in nums if -x in set(nums)], default=-1)
+
+
+def test_find_max_k():
+    solution = Solution()
+    assert solution.findMaxK([-1, 2, -3, 3]) == 3, 'wrong result'
+    assert solution.findMaxK([-1, 10, 6, 7, -7, 1]) == 7, 'wrong result'
+    assert solution.findMaxK([-10, 8, 6, 7, -2, -3]) == -1, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_find_max_k()
