4c3d0a7d9990dc5148f54589f98d9e835e6b02a1
DancingOnAir <taotong1984@hotmail.com>
2022-10-20 23:10:57 +0800

    feat(Array): init q2443 & accepted one line python solution for q2443

---
+++ Array/2443_sum_of_number_and_its_reverse.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sumOfNumberAndReverse(self, num: int) -> bool:
+        return any(x + int(str(x)[::-1]) == num for x in range(num + 1))
+
+
+def test_sum_of_number_and_reverse():
+    solution = Solution()
+    assert solution.sumOfNumberAndReverse(443), 'wrong result'
+    assert not solution.sumOfNumberAndReverse(63), 'wrong result'
+    assert solution.sumOfNumberAndReverse(181), 'wrong result'
+
+
+if __name__ == '__main__':
+    test_sum_of_number_and_reverse()
