a59c0efe3c78417db7f389d25d94a74251515d18
DancingOnAir <taotong1984@hotmail.com>
2022-10-24 09:50:24 +0800

    feat(Array): accepted python solution for q2341

--- Array/2341_maximum_number_of_pairs_in_array.py
+++ Array/2341_maximum_number_of_pairs_in_array.py
@@ -4,6 +4,15 @@ from collections import Counter
 
 class Solution:
     def numberOfPairs(self, nums: List[int]) -> List[int]:
+        pairs = 0
+        cnt = [0] * 101
+        for x in nums:
+            cnt[x] += 1
+            pairs += (1 if cnt[x] % 2 == 0 else 0)
+
+        return [pairs, len(nums) - pairs * 2]
+
+    def numberOfPairs1(self, nums: List[int]) -> List[int]:
         a = b = 0
         for v in Counter(nums).values():
             a += v // 2
