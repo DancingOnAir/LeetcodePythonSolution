240a0e55b1b5b0146e54c2c472ce54e94313ee86
DancingOnAir <taotong1984@hotmail.com>
2022-10-27 10:08:29 +0800

    feat(Array): accepted heap & smart python solution for q2335

--- Array/2335_minimum_amount_of_time_to_fill_cups.py
+++ Array/2335_minimum_amount_of_time_to_fill_cups.py
@@ -1,8 +1,28 @@
 from typing import List
+from heapq import heapify, heappop, heappush
 
 
 class Solution:
     def fillCups(self, amount: List[int]) -> int:
+        return max(max(amount), (sum(amount) + 1) // 2)
+
+    # heap
+    def fillCups2(self, amount: List[int]) -> int:
+        heap = [-x for x in amount]
+        heapify(heap)
+        res = 0
+        while heap[0] != 0:
+            mx1 = heappop(heap)
+            mx2 = heappop(heap)
+            res += 1
+
+            heappush(heap, mx1 + 1)
+            heappush(heap, mx2 + 1)
+
+        return res
+
+    # greedy
+    def fillCups1(self, amount: List[int]) -> int:
         amount.sort()
         res = 0
         while amount[2] != 0:
