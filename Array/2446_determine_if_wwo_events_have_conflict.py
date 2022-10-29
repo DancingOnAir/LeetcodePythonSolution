a30596eef7bb2dbd005a4e50b6ae7677500ea2ac
DancingOnAir <taotong1984@hotmail.com>
2022-10-29 12:19:24 +0800

    feat(Array): accepted python solution for q2446

--- Array/2446_determine_if_wwo_events_have_conflict.py
+++ Array/2446_determine_if_wwo_events_have_conflict.py
@@ -3,6 +3,9 @@ from typing import List
 
 class Solution:
     def haveConflict(self, event1: List[str], event2: List[str]) -> bool:
+        return max(event1[0], event2[0]) <= min(event1[1], event2[1])
+
+    def haveConflict1(self, event1: List[str], event2: List[str]) -> bool:
         return not (event1[1] < event2[0] or event1[0] > event2[1])
 
 
