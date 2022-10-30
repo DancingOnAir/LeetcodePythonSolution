d1e477e3666701bc0b93a4dc14f79a9a531631e3
DancingOnAir <taotong1984@hotmail.com>
2022-10-30 23:25:56 +0800

    feat(Array): accepted concise python solution for q2326

--- Array/2326_spiral_matrix_iv.py
+++ Array/2326_spiral_matrix_iv.py
@@ -10,13 +10,37 @@ class ListNode:
 
 class Solution:
     def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
-        pass
+        res = [[-1] * n for _ in range(m)]
+        r = c = 0
+        dr, dc = 0, 1
+
+        while head:
+            res[r][c] = head.val
+            if r + dr < 0 or r + dr >= m or c + dc < 0 or c + dc >= n or res[r + dr][c + dc] != -1:
+                dr, dc = dc, -dr
+            r += dr
+            c += dc
+            head = head.next
+
+        return res
 
 
 def test_spiral_matrix():
     solution = Solution()
-    assert solution.spiralMatrix(3, 5, [3,0,2,6,8,1,7,9,4,2,5,5,0]) == [[3,0,2,6,8],[5,0,-1,-1,1],[5,2,4,9,7]], 'wrong result'
-    assert solution.spiralMatrix(1, 4, [0,1,2]) == [[0,1,2,-1]], 'wrong result'
+
+    nums = [3, 0, 2, 6, 8, 1, 7, 9, 4, 2, 5, 5, 0]
+    head = cur = ListNode(nums[0])
+    for x in nums[1:]:
+        cur.next = ListNode(x)
+        cur = ListNode(x)
+    assert solution.spiralMatrix(3, 5, head) == [[3, 0, 2, 6, 8], [5, 0, -1, -1, 1], [5, 2, 4, 9, 7]], 'wrong result'
+
+    nums = [0, 1, 2]
+    head = cur = ListNode(nums[0])
+    for x in nums[1:]:
+        cur.next = ListNode(x)
+        cur = ListNode(x)
+    assert solution.spiralMatrix(1, 4, head) == [[0, 1, 2, -1]], 'wrong result'
 
 
 if __name__ == '__main__':
