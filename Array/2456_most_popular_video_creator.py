698e3b9ec28fa01120572f1e5feda11a1ac5caba
DancingOnAir <taotong1984@hotmail.com>
2022-11-02 22:51:40 +0800

    feat(Array): init q2456 & accepted python solution for q2456

---
+++ Array/2456_most_popular_video_creator.py
@@ -0,0 +1,32 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def mostPopularCreator(self, creators: List[str], ids: List[str], views: List[int]) -> List[List[str]]:
+        m = dict()
+        for i in range(len(creators)):
+            if creators[i] not in m:
+                m[creators[i]] = [i, views[i]]
+            else:
+                if views[i] > views[m[creators[i]][0]] or (views[i] == views[m[creators[i]][0]] and ids[i] < ids[m[creators[i]][0]]):
+                    m[creators[i]][0] = i
+                m[creators[i]][1] += views[i]
+
+        res = list()
+        mx = max(m.values(), key=lambda x: x[1])
+        for k, v in m.items():
+            if v[1] == mx[1]:
+                res.append([k, ids[v[0]]])
+        return res
+
+
+def test_most_popular_creator():
+    solution = Solution()
+    assert solution.mostPopularCreator(["alice", "bob", "alice", "chris"], ["one", "two", "three", "four"], [5, 10, 5, 4]) == [["alice", "one"], ["bob", "two"]], 'wrong result'
+    assert solution.mostPopularCreator(["alice", "alice", "alice"], ["a", "b", "c"], [1, 2, 2]) == [["alice", "b"]], 'wrong result'
+
+
+if __name__ == '__main__':
+    test_most_popular_creator()
+
