aa150420e002c5f57f0b0ba460d1a9f80f223a6e
DancingOnAir <taotong1984@hotmail.com>
2022-11-12 19:37:05 +0800

    feat(Array): accepted one heap python solution for q2462

--- Array/2462_total_cost_to_hire_k_workers.py
+++ Array/2462_total_cost_to_hire_k_workers.py
@@ -4,6 +4,24 @@ from heapq import heapify, heappop, heappush
 
 class Solution:
     def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
+        n, res = len(costs), 0
+        pairs = [(v, i) for i, v in enumerate(costs)]
+        l, r = min(candidates, n // 2), max(n - candidates, n // 2)
+        pq = pairs[:l] + pairs[r:]
+
+        heapify(pq)
+        for _ in range(k):
+            cost, i = heappop(pq)
+            if i < l:
+                i, l = l, l + 1
+            if i >= r:
+                i, r = r - 1, r - 1
+            if l <= r:
+                heappush(pq, pairs[i])
+            res += cost
+        return res
+
+    def totalCost1(self, costs: List[int], k: int, candidates: int) -> int:
         res = 0
         n = len(costs)
         i, j = 0, n - 1
