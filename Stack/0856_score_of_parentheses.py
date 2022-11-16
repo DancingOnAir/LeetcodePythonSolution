c25524715840a6b937562e98b663084880775148
DancingOnAir <taotong1984@hotmail.com>
2022-11-16 11:10:49 +0800

    feat(Stack): accepted stack python solution for q0856

--- Stack/0856_score_of_parentheses.py
+++ Stack/0856_score_of_parentheses.py
@@ -1,17 +1,20 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
         stk = list()
-        res = 0
+        cur = 0
 
-        for ch in s:
-            if ch == ')':
-
-            stk.append(ch)
-        pass
+        for i, ch in enumerate(s):
+            if ch == '(':
+                stk.append(cur)
+                cur = 0
+            else:
+                cur = stk.pop() + max(1, cur * 2)
+        return cur
 
 
 def test_score_of_parentheses():
     solution = Solution()
+    assert solution.scoreOfParentheses("(()(()))") == 6, 'wrong result'
     assert solution.scoreOfParentheses("()") == 1, 'wrong result'
     assert solution.scoreOfParentheses("(())") == 2, 'wrong result'
     assert solution.scoreOfParentheses("()()") == 2, 'wrong result'
