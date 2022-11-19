28155ea36cbb6feeb038f826bcbc584035723fc5
DancingOnAir <taotong1984@hotmail.com>
2022-11-19 21:21:06 +0800

    feat(Stack): init q1130 & accepted intuition python solution for q1130

---
+++ Stack/1130_minimum_cost_tree_from_leaf_values.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def mctFromLeafValues(self, arr: List[int]) -> int:
+        res = 0
+        while len(arr) > 1:
+            idx = arr.index(min(arr))
+            if 0 < idx < len(arr) - 1:
+                res += min(arr[idx - 1], arr[idx + 1]) * arr[idx]
+            else:
+                res += (arr[1] if idx == 0 else arr[-2]) * arr[idx]
+            arr.pop(idx)
+        return res
+
+
+def test_mct_from_leaf_values():
+    solution = Solution()
+    assert solution.mctFromLeafValues([6, 2, 4]) == 32, 'wrong result'
+    assert solution.mctFromLeafValues([4, 11]) == 44, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_mct_from_leaf_values()
+
