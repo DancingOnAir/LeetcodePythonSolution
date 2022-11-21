bb302d9bd1ed86c841d9e5a655700b46fcf1c7c5
DancingOnAir <taotong1984@hotmail.com>
2022-11-21 11:20:07 +0800

    feat(Stack): accepted heap python solution for q1425

--- Stack/1425_constrained_subsequence_sum.py
+++ Stack/1425_constrained_subsequence_sum.py
@@ -1,9 +1,24 @@
 from typing import List
 from collections import deque
+from heapq import heappop, heappush
 
 
 class Solution:
     def constrainedSubsetSum(self, nums: List[int], k: int) -> int:
+        h = [(-nums[0], 0)]
+        res = nums[0]
+        for i in range(1, len(nums)):
+            remove = i - k - 1
+            while remove >= h[0][1]:
+                heappop(h)
+
+            cur = max(-h[0][0], 0) + nums[i]
+            res = max(res, cur)
+            heappush(h, (-cur, i))
+
+        return res
+
+    def constrainedSubsetSum1(self, nums: List[int], k: int) -> int:
         dq = deque()
 
         for i in range(len(nums)):
@@ -23,6 +38,8 @@ class Solution:
 
 def test_constrained_subset_sum():
     solution = Solution()
+    assert solution.constrainedSubsetSum([-8269, 3217, -4023, -4138, -683, 6455, -3621, 9242, 4015, -3790],
+                                         1) == 16091, 'wrong result'
     assert solution.constrainedSubsetSum([-5266, 4019, 7336, -3681, -5767], 2) == 11355, 'wrong result'
     assert solution.constrainedSubsetSum([10, 2, -10, 5, 20], 2) == 37, 'wrong result'
     assert solution.constrainedSubsetSum([-1, -2, -3], 1) == -1, 'wrong result'
