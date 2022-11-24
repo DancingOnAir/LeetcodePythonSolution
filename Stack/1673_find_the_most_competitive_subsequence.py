5121c7f8a818404bdfd6e568806072e502bebc1c
DancingOnAir <taotong1984@hotmail.com>
2022-11-24 09:38:43 +0800

    feat(Stack): accepted concise monotone stack python solution for q1673

--- Stack/1673_find_the_most_competitive_subsequence.py
+++ Stack/1673_find_the_most_competitive_subsequence.py
@@ -3,11 +3,21 @@ from typing import List
 
 class Solution:
     def mostCompetitive(self, nums: List[int], k: int) -> List[int]:
-        n = len(nums)
+        attempts = len(nums) - k
+        stk = list()
+        for x in nums:
+            while stk and stk[-1] > x and attempts > 0:
+                stk.pop()
+                attempts -= 1
+            stk.append(x)
+
+        return stk[:k]
+
+    def mostCompetitive1(self, nums: List[int], k: int) -> List[int]:
         stk = list()
         for i, x in enumerate(nums):
             while stk and stk[-1] > x:
-                if len(stk) + n - i == k:
+                if len(stk) + len(nums) - i == k:
                     return stk + nums[i:]
                 stk.pop()
             stk.append(x)
@@ -17,7 +27,7 @@ class Solution:
 
 def test_most_competitive():
     solution = Solution()
-    # assert solution.mostCompetitive([3, 5, 2, 6], 2) == [2, 6], 'wrong result'
+    assert solution.mostCompetitive([3, 5, 2, 6], 2) == [2, 6], 'wrong result'
     assert solution.mostCompetitive([2, 4, 3, 3, 5, 4, 9, 6], 4) == [2, 3, 3, 4], 'wrong result'
 
 
