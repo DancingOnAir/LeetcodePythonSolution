6011a683de0394b04f601760222b43fb8c3eb5c1
DancingOnAir <taotong1984@hotmail.com>
2022-11-25 11:08:30 +0800

    feat(Stack): accepted monotone stack python solution for q1776

--- Stack/1776_car_fleet_ii.py
+++ Stack/1776_car_fleet_ii.py
@@ -2,7 +2,27 @@ from typing import List
 
 
 class Solution:
+    # monotone stack
     def getCollisionTimes(self, cars: List[List[int]]) -> List[float]:
+        n = len(cars)
+        stk = list()
+        res = [-1] * n
+        for i in range(n - 1, -1, -1):
+            p, s = cars[i]
+            # Imagine a,b,c on the road
+            # if the a catches b later than b catched c,
+            # then a won't catch b but b+c.
+            while stk and (cars[stk[-1]][1] >= s or (cars[stk[-1]][0] - p) / (s - cars[stk[-1]][1]) >= res[stk[-1]] > 0):
+                stk.pop()
+
+            if stk:
+                res[i] = (cars[stk[-1]][0] - p) / (s - cars[stk[-1]][1])
+            stk.append(i)
+
+        return res
+
+    # brute force, TLE
+    def getCollisionTimes1(self, cars: List[List[int]]) -> List[float]:
         n = len(cars)
         res = [-1] * n
         for i in range(n - 1, -1, -1):
