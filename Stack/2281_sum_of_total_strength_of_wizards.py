eecfc21496c9dde3b783e4ce3f857a971e068951
DancingOnAir <taotong1984@hotmail.com>
2022-11-26 15:41:05 +0800

    feat(Stack): accepted left & right monotone stack python solution for q2281

--- Stack/2281_sum_of_total_strength_of_wizards.py
+++ Stack/2281_sum_of_total_strength_of_wizards.py
@@ -1,9 +1,47 @@
 from typing import List
+from itertools import  accumulate
 
 
 class Solution:
-    # TLE
+    # one pass
     def totalStrength(self, strength: List[int]) -> int:
+        pass
+
+    # https://leetcode.com/problems/sum-of-total-strength-of-wizards/solutions/2061985/java-c-python-one-pass-solution/
+    # for each strength[i] as minimum, calculate sum
+    def totalStrength2(self, strength: List[int]) -> int:
+        mod = 10 ** 9 + 7
+        n = len(strength)
+
+        # next small on the right
+        right = [n] * n
+        stk = list()
+        for i in range(n):
+            while stk and strength[stk[-1]] > strength[i]:
+                right[stk.pop()] = i
+            stk.append(i)
+
+        # next small on the left
+        left = [-1] * n
+        stk = list()
+        for i in range(n - 1, -1, -1):
+            while stk and strength[stk[-1]] >= strength[i]:
+                left[stk.pop()] = i
+            stk.append(i)
+        # 注意这里
+        pre_sum = list(accumulate(accumulate(strength), initial=0))
+        res = 0
+        for i in range(n):
+            l, r = left[i], right[i]
+            l_pre_sum = pre_sum[i] - pre_sum[max(l, 0)]
+            r_pre_sum = pre_sum[r] - pre_sum[i]
+            ln, rn = i - l, r - i
+            res += strength[i] * (r_pre_sum * ln - l_pre_sum * rn)
+            res %= mod
+        return res
+
+    # TLE
+    def totalStrength1(self, strength: List[int]) -> int:
         n = len(strength)
         pre_sum = [0]
         for s in strength:
