1bc56f875d98790cf997277f743846c2d58f5915
DancingOnAir <taotong1984@hotmail.com>
2022-12-11 10:22:29 +0800

    feat(dfs): init q2101 & accepted dfs python solution for q2101

---
+++ DFS/2101_detonate_the_maximum_bombs.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+class Solution:
+    def maximumDetonation(self, bombs: List[List[int]]) -> int:
+        n = len(bombs)
+        g = [[] for _ in range(n)]
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                dis = (bombs[i][0] - bombs[j][0]) ** 2 + (bombs[i][1] - bombs[j][1]) ** 2
+                if dis <= bombs[i][2] ** 2:
+                    g[i].append(j)
+                if dis <= bombs[j][2] ** 2:
+                    g[j].append(i)
+
+        res = 0
+        for i in range(n):
+            seen = {i}
+            cur = 1
+            q = [i]
+            while q:
+                u = q.pop()
+                for v in g[u]:
+                    if v not in seen:
+                        seen.add(v)
+                        cur += 1
+                        q.append(v)
+            res = max(res, cur)
+
+        return res
+
+
+def test_maximum_detonation():
+    solution = Solution()
+    assert solution.maximumDetonation([[2, 1, 3], [6, 1, 4]]) == 2, 'wrong result'
+    assert solution.maximumDetonation([[1, 1, 5], [10, 10, 5]]) == 1, 'wrong result'
+    assert solution.maximumDetonation([[1, 2, 3], [2, 3, 1], [3, 4, 2], [4, 5, 3], [5, 6, 4]]) == 5, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_maximum_detonation()
