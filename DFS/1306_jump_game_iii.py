babc1b0dbb5de5d37a41428de1b5bb653dc1a8e3
DancingOnAir <taotong1984@hotmail.com>
2022-12-15 12:02:08 +0800

    feat(DFS): accepted concise dfs python solution for q1306

--- DFS/1306_jump_game_iii.py
+++ DFS/1306_jump_game_iii.py
@@ -3,6 +3,12 @@ from typing import List
 
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
+        if 0 <= start < len(arr) and arr[start] >= 0:
+            arr[start] = - arr[start]
+            return arr[start] == 0 or self.canReach(arr, start + arr[start]) or self.canReach(arr, start - arr[start])
+        return False
+
+    def canReach1(self, arr: List[int], start: int) -> bool:
         n = len(arr)
 
         def dfs(i, seen):
