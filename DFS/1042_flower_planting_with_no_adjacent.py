689aece2de706d9d4c2367978ef74b02a52a1b56
DancingOnAir <taotong1984@hotmail.com>
2022-12-23 14:48:14 +0800

    feat(dfs): accepted concise python solution for q1042

--- DFS/1042_flower_planting_with_no_adjacent.py
+++ DFS/1042_flower_planting_with_no_adjacent.py
@@ -3,6 +3,19 @@ from typing import List
 
 class Solution:
     def gardenNoAdj(self, n: int, paths: List[List[int]]) -> List[int]:
+        res = [-1] * n
+        g = [[] for _ in range(n)]
+        for u, v in paths:
+            g[u - 1].append(v - 1)
+            g[v - 1].append(u - 1)
+
+        for i in range(n):
+            res[i] = ({1, 2, 3, 4} - {res[j] for j in g[i]}).pop()
+
+        return res
+
+    # dfs
+    def gardenNoAdj1(self, n: int, paths: List[List[int]]) -> List[int]:
         g = [[] for _ in range(n)]
         for u, v in paths:
             g[u - 1].append(v - 1)
@@ -24,7 +37,6 @@ class Solution:
 
 def test_garden_no_adj():
     solution = Solution()
-    assert solution.gardenNoAdj(6, [[6, 4], [6, 1], [3, 1], [4, 5], [2, 1], [5, 6], [5, 2]]) == [1, 2, 3], 'wrong result'
     assert solution.gardenNoAdj(3, [[1, 2], [2, 3], [3, 1]]) == [1, 2, 3], 'wrong result'
     assert solution.gardenNoAdj(4, [[1, 2], [3, 4]]) == [1, 2, 1, 2], 'wrong result'
     assert solution.gardenNoAdj(4, [[1, 2], [2, 3], [3, 4], [4, 1], [1, 3], [2, 4]]) == [1, 2, 3, 4], 'wrong result'
