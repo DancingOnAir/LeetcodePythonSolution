b19e7bc3741adb128bc6da63fd7f906eedf4d110
DancingOnAir <taotong1984@hotmail.com>
2022-12-25 12:03:57 +0800

    feat(dfs): accepted dfs python solution for q2328

--- DFS/2328_number_of_increasing_paths_in_a_grid.py
+++ DFS/2328_number_of_increasing_paths_in_a_grid.py
@@ -1,14 +1,28 @@
 from typing import List
+from functools import lru_cache
 
 
 class Solution:
+    def countPaths(self, grid: List[List[int]]) -> int:
+        m, n, mod = len(grid), len(grid[0]), 10 ** 9 + 7
+
+        @lru_cache(None)
+        def dfs(i, j):
+            res = 1
+            for x, y in ([i, j - 1], [i, j + 1], [i - 1, j], [i + 1, j]):
+                if 0 <= x < m and 0 <= y < n and grid[x][y] < grid[i][j]:
+                   res += dfs(x, y) % mod
+            return res
+
+        return sum(dfs(i, j) for i in range(m) for j in range(n)) % mod
+
     # dp[i][j] means the number of paths ending at A[i][j].
     # We initial all dp[i][j] = 1.
     # Because we want strictly increasing paths, there is no cycle of path.
     # We iterative all A[i][j] in increasing order,
     # d[i][j] += dp[x][y] from smaller neighbours.
     # Finally return the sum of dp.
-    def countPaths(self, grid: List[List[int]]) -> int:
+    def countPaths1(self, grid: List[List[int]]) -> int:
         m, n, mod = len(grid), len(grid[0]), 10 ** 9 + 7
         dp = [[1] * n for _ in range(m)]
         for v, i, j in sorted([grid[i][j], i, j] for i in range(m) for j in range(n)):
