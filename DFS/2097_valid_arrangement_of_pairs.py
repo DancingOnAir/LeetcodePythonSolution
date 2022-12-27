e049887c3518a8fe2adc357c55eb8415da31f516
DancingOnAir <taotong1984@hotmail.com>
2022-12-27 08:12:14 +0800

    feat(dfs): accepted dfs iterative python solution for q2097

--- DFS/2097_valid_arrangement_of_pairs.py
+++ DFS/2097_valid_arrangement_of_pairs.py
@@ -3,7 +3,32 @@ from collections import defaultdict
 
 
 class Solution:
+    # iterative
     def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
+        g = defaultdict(list)
+        degree = defaultdict(int)
+        for u, v in pairs:
+            g[u].append(v)
+            degree[u] += 1
+            degree[v] -= 1
+
+        start = pairs[0][0]
+        for k, v in degree.items():
+            if v == 1:
+                start = k
+                break
+
+        res = list()
+        stk = [start]
+        while stk:
+            while g[stk[-1]]:
+                stk.append(g[stk[-1]].pop())
+            res.append(stk.pop())
+        res.reverse()
+        return [[res[i], res[i + 1]] for i in range(len(res) - 1)]
+
+    # recursive
+    def validArrangement1(self, pairs: List[List[int]]) -> List[List[int]]:
         g = defaultdict(list)
         # net out degree
         degree = defaultdict(int)
@@ -11,12 +36,6 @@ class Solution:
             g[u].append(v)
             degree[u] += 1
             degree[v] -= 1
-
-        for k, v in degree.items():
-            # check whether Eulerian Circuit
-            if v == 1:
-                u = k
-                break
 
         res = list()
 
@@ -27,14 +46,20 @@ class Solution:
                 dfs(v)
                 res.append([u, v])
 
-        dfs(u)
+        for k, v in degree.items():
+            # check whether Eulerian Circuit
+            if v == 1:
+                dfs(k)
+                break
+        if not res:
+            dfs(pairs[0][0])
         return res[::-1]
 
 
 def test_valid_arrangement():
     solution = Solution()
-    # assert solution.validArrangement([[17, 18], [18, 10], [10, 18]]) == [[17, 18], [18, 10], [10, 18]], 'wrong result'
-    # assert solution.validArrangement([[5, 1], [4, 5], [11, 9], [9, 4]]) == [[11, 9], [9, 4], [4, 5], [5, 1]], 'wrong result'
+    assert solution.validArrangement([[17, 18], [18, 10], [10, 18]]) == [[17, 18], [18, 10], [10, 18]], 'wrong result'
+    assert solution.validArrangement([[5, 1], [4, 5], [11, 9], [9, 4]]) == [[11, 9], [9, 4], [4, 5], [5, 1]], 'wrong result'
     assert solution.validArrangement([[1, 3], [3, 2], [2, 1]]) == [[1, 3], [3, 2], [2, 1]], 'wrong result'
     assert solution.validArrangement([[1, 2], [1, 3], [2, 1]]) == [[1, 2], [2, 1], [1, 3]], 'wrong result'
 
