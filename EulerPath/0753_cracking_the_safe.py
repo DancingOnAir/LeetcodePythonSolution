2f4668a046997506b2afc56f14a159e252f5972d
DancingOnAir <taotong1984@hotmail.com>
2022-12-30 11:20:01 +0800

    feat(EulerPath): accepted euler path python solution for q0753

--- EulerPath/0753_cracking_the_safe.py
+++ EulerPath/0753_cracking_the_safe.py
@@ -3,11 +3,25 @@ from collections import defaultdict
 
 class Solution:
     def crackSafe(self, n: int, k: int) -> str:
-        pass
+        highest = 10 ** (n - 1)
+        seen = set()
+        res = list()
+
+        def dfs(u):
+            for i in range(k):
+                v = u * 10 + i
+                if v not in seen:
+                    seen.add(v)
+                    dfs(v % highest)
+                    res.append(str(i))
+
+        dfs(0)
+        return ''.join(res) + '0' * (n - 1)
 
 
 def test_crack_safe():
     solution = Solution()
+    assert solution.crackSafe(2, 3) == "10", 'wrong result'
     assert solution.crackSafe(1, 2) == "10", 'wrong result'
     assert solution.crackSafe(2, 2) == "01100", 'wrong result'
 
