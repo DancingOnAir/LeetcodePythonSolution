3b8e7e260b2f85f9b77d3ae94c917445c09c1895
DancingOnAir <taotong1984@hotmail.com>
2023-01-15 13:29:21 +0800

    feat(BitManipulation): init q2275 & accepted python solution for q2275

---
+++ BitManipulation/2275_largest_combination_with_bitwise_and_greater_than_zero.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def largestCombination(self, candidates: List[int]) -> int:
+        res = 0
+        for i in range(24):
+            res = max(res, sum(1 for x in candidates if x & (1 << i)))
+        return res
+
+
+def test_largest_combination():
+    solution = Solution()
+    # 0001 0000, 0001 0001, 0100 0111, 0011 1110, 0000 0110, 0001 1000, 0000 1110
+    assert solution.largestCombination([16, 17, 71, 62, 12, 24, 14]) == 4, 'wrong result'
+    assert solution.largestCombination([8, 8]) == 2, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_largest_combination()
