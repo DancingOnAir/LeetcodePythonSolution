59ba9208729baf0133e72fa37d60ba1478110cb5
DancingOnAir <taotong1984@hotmail.com>
2023-01-26 00:10:46 +0800

    feat(BitManipulation): init q1829 & accepted python solution for q1829

---
+++ BitManipulation/1829_maximum_xor_for_each_query.py
@@ -0,0 +1,26 @@
+from typing import List
+from itertools import accumulate
+
+
+class Solution:
+    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
+        res = list()
+        max_num = 2 ** maximumBit - 1
+        acc = 0
+        for x in nums:
+            acc ^= x
+            # 下面2种方法均可
+            # res.append(max_num - acc)
+            res.append(max_num ^ acc)
+        return res[::-1]
+
+
+def test_get_maximum_xor():
+    solution = Solution()
+    assert solution.getMaximumXor([0, 1, 1, 3], 2) == [0, 3, 2, 3], 'wrong result'
+    assert solution.getMaximumXor([2, 3, 4, 7], 3) == [5, 2, 6, 5], 'wrong result'
+    assert solution.getMaximumXor([0, 1, 2, 2, 5, 7], 3) == [4, 3, 6, 4, 6, 7], 'wrong result'
+
+
+if __name__ == '__main__':
+    test_get_maximum_xor()
