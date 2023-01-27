57150d9749bec4ef6e7a638bdd1c322dc47e5ea6
DancingOnAir <taotong1984@hotmail.com>
2023-01-27 08:46:25 +0800

    feat(BitManipulation):init q2546 & accepted python solution for q2546

---
+++ BitManipulation/2546_apply_bitwise_operations_to_make_strings_equal.py
@@ -0,0 +1,13 @@
+class Solution:
+    def makeStringsEqual(self, s: str, target: str) -> bool:
+        return (s == target) or (s.count('1') > 0 and target.count('1') > 0)
+
+
+def test_make_string_equal():
+    solution = Solution()
+    assert solution.makeStringsEqual("1010", "0110"), 'wrong result'
+    assert not solution.makeStringsEqual("11", "00"), 'wrong result'
+
+
+if __name__ == '__main__':
+    test_make_string_equal()
