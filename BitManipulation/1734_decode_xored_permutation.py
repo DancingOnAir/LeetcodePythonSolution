e61ac4f481223fee2687f1ffa79767a2c0a73a24
DancingOnAir <taotong1984@hotmail.com>
2023-01-29 01:13:33 +0800

    feat(BitManipulation): accepted xor pythonic solution for q1734

--- BitManipulation/1734_decode_xored_permutation.py
+++ BitManipulation/1734_decode_xored_permutation.py
@@ -1,11 +1,18 @@
 from typing import List
 from functools import reduce
+from itertools import accumulate
 
 
 class Solution:
+    # 和下面的方法类似，不过首先计算的是第一个数
+    # https://leetcode.com/problems/decode-xored-permutation/solutions/1031107/java-c-python-straight-forward-solution/
+    def decode(self, encoded: List[int]) -> List[int]:
+        first = reduce(lambda a, b: a ^ b, encoded[::-2] + list(range(len(encoded) + 2)))
+        return list(accumulate([first] + encoded, lambda a, b: a ^ b))
+
     # 先计算全部的xor值, 再计算encoded里的偶数项的xor值, 2者的xor就是原数组最后的元素值。
     # e.g. 原数组[a, b, c, d, e], 那么encoded为[a^b, b^c, c^d, d^e],可以观察到encoded的偶然项的xor为a^b^c^d
-    def decode(self, encoded: List[int]) -> List[int]:
+    def decode1(self, encoded: List[int]) -> List[int]:
         n = len(encoded)
         total_xor = reduce(lambda a, b: a ^ b, [i for i in range(1, n + 2)])
         res = [0] * (n + 1)
