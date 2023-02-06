68419aefd0e789209c219a320296752c6056b86a
DancingOnAir <taotong1984@hotmail.com>
2023-02-06 13:01:18 +0800

    feat(BitManipulation): accepted bit manipulation python solution for q1318

--- BitManipulation/1318_minimum_flips_to_make_a_or_b_equal_to_c.py
+++ BitManipulation/1318_minimum_flips_to_make_a_or_b_equal_to_c.py
@@ -1,5 +1,14 @@
 class Solution:
     def minFlips(self, a: int, b: int, c: int) -> int:
+        ab, s, res = a | b, (a | b) ^ c, 0
+        for i in range(31):
+            mask = 1 << i
+            if s & mask > 0:
+                res += 2 if (a & mask) == (b & mask) and (c & mask) == 0 else 1
+
+        return res
+
+    def minFlips(self, a: int, b: int, c: int) -> int:
         s = bin(((a | b) ^ c))[2:][::-1]
         res = 0
         for i, v in enumerate(s):
