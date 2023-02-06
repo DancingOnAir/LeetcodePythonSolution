4edd6042153abbe466aff39cfa0ea947df2e7979
DancingOnAir <taotong1984@hotmail.com>
2023-02-06 23:38:21 +0800

    feat(BitManipulation): init q1342 & accepted python solution for q1342

---
+++ BitManipulation/1342_number_of_steps_to_reduce_a_number_to_zero.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        res = 0
+        while num:
+            if num & 1:
+                num -= 1
+            else:
+                num >>= 1
+            res += 1
+        return res
+
+
+def test_number_of_steps():
+    solution = Solution()
+    # 1110
+    assert solution.numberOfSteps(14) == 6, 'wrong result'
+    # 1000
+    assert solution.numberOfSteps(8) == 4, 'wrong result'
+    # 0111 1011
+    assert solution.numberOfSteps(123) == 12, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_number_of_steps()
