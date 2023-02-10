0e4978b0ffc927432ee78ce70e829fed56d3e57f
DancingOnAir <taotong1984@hotmail.com>
2023-02-11 00:33:57 +0800

    feat(BinarySearch): accepted one line set intersection python solution for q2540

--- BinarySearch/2540_minimum_common_value.py
+++ BinarySearch/2540_minimum_common_value.py
@@ -3,6 +3,9 @@ from typing import List
 
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        return min(set(nums1) & set(nums2), default=-1)
+
+    def getCommon1(self, nums1: List[int], nums2: List[int]) -> int:
         i = j = 0
         while i < len(nums1) and j < len(nums2):
             if nums1[i] < nums2[j]:
