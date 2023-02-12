4d32ea21e9050d5e1eb85f839b71a01e432f9770
DancingOnAir <taotong1984@hotmail.com>
2023-02-12 17:16:55 +0800

    feat(BinarySearch): init q2554 & accepted binary search python solution for q2554

--- BinarySearch/2554_maximum_number_of_integers_to_choose_from_a_range_i.py
+++ BinarySearch/2554_maximum_number_of_integers_to_choose_from_a_range_i.py
@@ -1,8 +1,33 @@
 from typing import List
+from bisect import bisect_left, bisect_right
 
 
 class Solution:
     def maxCount(self, banned: List[int], n: int, maxSum: int) -> int:
+        pre_sum = [0]
+        banned = sorted(set(banned))
+        for x in banned:
+            pre_sum.append(x + pre_sum[-1])
+
+        left, right, mx = 1, n, 0
+        res = 0
+        while left <= right:
+            mid = left + (right - left) // 2
+            pos = bisect_right(banned, mid)
+            total = (1 + mid) * mid // 2 - pre_sum[pos]
+            if total < maxSum:
+                if total > mx:
+                    mx = total
+                    res = mid - pos
+
+                left = mid + 1
+            elif total > maxSum:
+                right = mid - 1
+            else:
+                return mid - pos
+        return res
+
+    def maxCount1(self, banned: List[int], n: int, maxSum: int) -> int:
         i = j = total = res = 0
         banned.sort()
         while i < n:
@@ -24,6 +49,7 @@ class Solution:
 
 def test_max_count():
     solution = Solution()
+    # assert solution.maxCount([11], 7, 50) == 7, 'wrong result'
     # assert solution.maxCount([1, 6, 5], 5, 6) == 2, 'wrong result'
     assert solution.maxCount([1, 2, 3, 4, 5, 6, 7], 8, 1) == 0, 'wrong result'
 
