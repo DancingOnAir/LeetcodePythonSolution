f036121317ac880b17331306363782b94394ae1e
DancingOnAir <taotong1984@hotmail.com>
2023-02-22 09:32:47 +0800

    feat(BinarySearch): accepted union find object python solution for q2424

--- UnionFind/2424_longest_uploaded_prefix.py
+++ UnionFind/2424_longest_uploaded_prefix.py
@@ -1,4 +1,44 @@
+class UF:
+    def __init__(self, n):
+        self.parents = list(range(n))
+        self.sz = [0] * n
+
+    def find(self, p):
+        while p != self.parents[p]:
+            self.parents[p] = self.parents[self.parents[p]]
+            p = self.parents[p]
+        return p
+
+    def unite(self, p, q):
+        rp = self.find(p)
+        rq = self.find(q)
+
+        if rp != rq:
+            self.parents[rp] = rq
+            self.sz[rq] += self.sz[rp]
+
+    def get_size(self, p):
+        return self.sz[self.find(p)]
+
+
 class LUPrefix:
+    def __init__(self, n: int):
+        self.uf = UF(n)
+        self.n = n
+
+    def upload(self, video: int) -> None:
+        i = video - 1
+        self.uf.sz[i] += 1
+        if i > 0 and self.uf.get_size(i - 1) > 0:
+            self.uf.unite(i - 1, i)
+        if i + 1 < self.n and self.uf.get_size(i + 1) > 0:
+            self.uf.unite(i, i + 1)
+
+    def longest(self) -> int:
+        return self.uf.get_size(0)
+
+
+class LUPrefix1:
     def __init__(self, n: int):
         self.n = n
         self.parent = [0] * (n + 1)
