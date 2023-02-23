cccc7b3a79bc7cbea182f997a6a698819a3a1bc4
DancingOnAir <taotong1984@hotmail.com>
2023-02-23 09:27:03 +0800

    feat(BinarySearch): accepted presum + binary search python solution for q2302

--- BinarySearch/2302_count_subarrays_with_score_less_than_k.py
+++ BinarySearch/2302_count_subarrays_with_score_less_than_k.py
@@ -2,7 +2,30 @@ from typing import List
 
 
 class Solution:
+    # pre-sum + binary search
     def countSubarrays(self, nums: List[int], k: int) -> int:
+        pre_sum = [0]
+        for x in nums:
+            pre_sum.append(pre_sum[-1] + x)
+
+        res = 0
+        for i in range(1, len(nums) + 1):
+            l, r = i, len(nums)
+            cur = -1
+            while l <= r:
+                mid = l + (r - l) // 2
+                total = (pre_sum[mid] - pre_sum[i - 1]) * (mid - i + 1)
+                if total >= k:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+                    cur = mid
+            if cur != -1:
+                res += cur - i + 1
+        return res
+
+    # sliding window
+    def countSubarrays1(self, nums: List[int], k: int) -> int:
         res = i = total = 0
         for j, v in enumerate(nums):
             total += v
