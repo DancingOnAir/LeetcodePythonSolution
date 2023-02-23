5490b9823f2dd79a87a3dd3091b621d67eec2bf1
DancingOnAir <taotong1984@hotmail.com>
2023-02-23 22:52:26 +0800

    feat(BinarySearch): init q2070 & accepted pre max + binary search cpp solution for q2070

---
+++ BinarySearch/2070_most_beautiful_item_for_each_query.py
@@ -0,0 +1,42 @@
+from typing import List
+
+
+class Solution:
+    def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
+        items.sort()
+        mx = 0
+        for i, v in enumerate(items):
+            mx = max(mx, v[1])
+            items[i][1] = mx
+
+        res = list()
+        for i, v in enumerate(queries):
+            l, r = 0, len(items) - 1
+            while l <= r:
+                mid = l + (r - l) // 2
+                if items[mid][0] <= v:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+
+            res.append(items[l - 1][1] if l > 0 else 0)
+        return res
+
+
+def test_maximum_beauty():
+    solution = Solution()
+    assert solution.maximumBeauty(
+        [[193, 732], [781, 962], [864, 954], [749, 627], [136, 746], [478, 548], [640, 908], [210, 799], [567, 715],
+         [914, 388], [487, 853], [533, 554], [247, 919], [958, 150], [193, 523], [176, 656], [395, 469], [763, 821],
+         [542, 946], [701, 676]],
+        [885, 1445, 1580, 1309, 205, 1788, 1214, 1404, 572, 1170, 989, 265, 153, 151, 1479, 1180, 875, 276, 1584]) == [
+               962, 962, 962, 962, 746, 962, 962, 962, 946, 962, 962, 919, 746, 746, 962, 962, 962, 919,
+               962], 'wrong result'
+    assert solution.maximumBeauty([[1, 2], [3, 2], [2, 4], [5, 6], [3, 5]], [1, 2, 3, 4, 5, 6]) == [2, 4, 5, 5, 6,
+                                                                                                    6], 'wrong result'
+    assert solution.maximumBeauty([[1, 2], [1, 2], [1, 3], [1, 4]], [1]) == [4], 'wrong result'
+    assert solution.maximumBeauty([[10, 1000]], [5]) == [0], 'wrong result'
+
+
+if __name__ == '__main__':
+    test_maximum_beauty()
