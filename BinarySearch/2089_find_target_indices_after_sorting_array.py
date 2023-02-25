1489926d2a3da5adaebb890f5007d26435827a77
DancingOnAir <taotong1984@hotmail.com>
2023-02-25 09:49:14 +0800

    feat(BinarySearch): init q2089 & accepted binary search python solution for q2089

---
+++ BinarySearch/2089_find_target_indices_after_sorting_array.py
@@ -0,0 +1,24 @@
+from typing import List
+from bisect import bisect_left, bisect_right
+
+
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        l = bisect_left(nums, target)
+        r = bisect_right(nums, target)
+        if l >= len(nums) or nums[l] != target:
+            return []
+        return list(range(l, r))
+
+
+def test_target_indices():
+    solution = Solution()
+    assert solution.targetIndices([1], 2) == [], 'wrong result'
+    assert solution.targetIndices([1, 2, 5, 2, 3], 2) == [1, 2], 'wrong result'
+    assert solution.targetIndices([1, 2, 5, 2, 3], 3) == [3], 'wrong result'
+    assert solution.targetIndices([1, 2, 5, 2, 3], 5) == [4], 'wrong result'
+
+
+if __name__ == '__main__':
+    test_target_indices()
