d528d902c011c6c7ee057945fafa04cb2d5cfcdf
DancingOnAir <taotong1984@hotmail.com>
2023-02-26 01:11:21 +0800

    feat(BinarySearch): accepted binary search python solution for q2498

--- BinarySearch/2498_frog_jump_ii.py
+++ BinarySearch/2498_frog_jump_ii.py
@@ -3,6 +3,41 @@ from typing import List
 
 class Solution:
     def maxJump(self, stones: List[int]) -> int:
+        def helper(k):
+            seen = set()
+            cur = pre = 0
+            while cur < len(stones):
+                if stones[cur] - stones[pre] > k:
+                    if cur - 1 == pre:
+                        return False
+                    pre = cur - 1
+                    seen.add(pre)
+                cur += 1
+
+            cur = pre = len(stones) - 1
+            seen.add(pre)
+            while cur >= 0:
+                if cur in seen:
+                    cur -= 1
+                    continue
+
+                if stones[pre] - stones[cur] <= k:
+                    pre = cur
+                else:
+                    return False
+                cur -= 1
+            return True
+
+        l, r = 0, 10 ** 9
+        while l <= r:
+            mid = l + (r - l) // 2
+            if helper(mid):
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l
+
+    def maxJump1(self, stones: List[int]) -> int:
         if len(stones) <= 2:
             return stones[1] - stones[0]
         res = 0
