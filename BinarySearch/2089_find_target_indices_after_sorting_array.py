d6fc8f0a9b9fa7e50eb7f78df320469988536412
DancingOnAir <taotong1984@hotmail.com>
2023-02-25 09:53:29 +0800

    feat(BinarySearch):accepted binary search python solution for q2089

--- BinarySearch/2089_find_target_indices_after_sorting_array.py
+++ BinarySearch/2089_find_target_indices_after_sorting_array.py
@@ -5,6 +5,15 @@ from bisect import bisect_left, bisect_right
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
         nums.sort()
+        left = bisect_left(nums, target)
+        right = left
+        for i in range(left, len(nums)):
+            if nums[i] == target:
+                right += 1
+        return list(range(left, right))
+
+    def targetIndices1(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
         l = bisect_left(nums, target)
         r = bisect_right(nums, target)
         if l >= len(nums) or nums[l] != target:
