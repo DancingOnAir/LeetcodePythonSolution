190a8684bf9766c823a18f01686032bfede2f1c8
DancingOnAir <taotong1984@hotmail.com>
2023-02-26 23:30:53 +0800

    feat(BinarySearch): accepted concise binary search python solution for q2187

--- BinarySearch/2187_minimum_time_to_complete_trips.py
+++ BinarySearch/2187_minimum_time_to_complete_trips.py
@@ -3,6 +3,19 @@ from typing import List
 
 class Solution:
     def minimumTime(self, time: List[int], totalTrips: int) -> int:
+        def possible(x):
+            return sum(x // t for t in time) >= totalTrips
+
+        l, r = 1, totalTrips * min(time)
+        while l <= r:
+            mid = l + (r - l) // 2
+            if possible(mid):
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l
+
+    def minimumTime1(self, time: List[int], totalTrips: int) -> int:
         def possible(k):
             total = 0
             for t in time:
