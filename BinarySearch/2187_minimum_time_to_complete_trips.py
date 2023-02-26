3a31b04b6ac3002886ff26269557cd59868774f1
DancingOnAir <taotong1984@hotmail.com>
2023-02-26 23:27:02 +0800

    feat(BinarySearch): init q2187 & accepted binary search python solution for q2187

---
+++ BinarySearch/2187_minimum_time_to_complete_trips.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Solution:
+    def minimumTime(self, time: List[int], totalTrips: int) -> int:
+        def possible(k):
+            total = 0
+            for t in time:
+                if t > k:
+                    break
+                total += k // t
+            return total >= totalTrips
+
+        n = len(time)
+        time.sort()
+        left, right = 1, totalTrips * time[0]
+        while left <= right:
+            mid = left + (right - left) // 2
+            if possible(mid):
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
+
+
+def test_minimum_time():
+    solution = Solution()
+    assert solution.minimumTime([9, 3, 10, 5], 2) == 5, 'wrong result'
+    assert solution.minimumTime([1, 2, 3], 5) == 3, 'wrong result'
+    assert solution.minimumTime([2], 1) == 2, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_minimum_time()
