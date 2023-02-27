e43245f7a3971c2815b9d44a5c220bace7a1b3df
DancingOnAir <taotong1984@hotmail.com>
2023-02-27 23:06:14 +0800

    feat(BinarySearch): init q2501 & accepted bianry search python solution for q2501

---
+++ BinarySearch/2501_longest_square_streak_in_an_array.py
@@ -0,0 +1,42 @@
+from typing import List
+
+
+class Solution:
+    def longestSquareStreak(self, nums: List[int]) -> int:
+        def binary_search(i):
+            l, r = i + 1, len(nums) - 1
+            k = nums[i] ** 2
+            while l <= r:
+                mid = l + (r - l) // 2
+                if nums[mid] < k:
+                    l = mid + 1
+                elif nums[mid] > k:
+                    r = mid - 1
+                else:
+                    return mid
+            return -1
+
+        nums.sort()
+        seen = set()
+        res = -1
+        for i, v in enumerate(nums):
+            if v not in seen:
+                temp = {v}
+                cur = binary_search(i)
+                while cur != -1:
+                    temp.add(nums[cur])
+                    cur = binary_search(cur)
+                if len(temp) > 1:
+                    res = max(res, len(temp))
+                seen.union(temp)
+        return res
+
+
+def test_longest_square_streak():
+    solution = Solution()
+    assert solution.longestSquareStreak([4, 3, 6, 16, 8, 2]) == 3, 'wrong result'
+    assert solution.longestSquareStreak([2, 3, 5, 6, 7]) == -1, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_longest_square_streak()
