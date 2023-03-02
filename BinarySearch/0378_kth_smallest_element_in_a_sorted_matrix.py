aab361ec0d32740410e7f3db8ac5bca199074d41
DancingOnAir <taotong1984@hotmail.com>
2023-03-02 23:25:45 +0800

    feat(BinarySearch): accepted binary search python solution for q0378

--- BinarySearch/0378_kth_smallest_element_in_a_sorted_matrix.py
+++ BinarySearch/0378_kth_smallest_element_in_a_sorted_matrix.py
@@ -3,8 +3,33 @@ from bisect import bisect_left
 
 
 class Solution:
-    # brute force
+    # binary search
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        # leetcode 240: search a 2d matrix II,找到不大于x的元素有多少个
+        def count_less_or_equal(x):
+            cnt = 0
+            c = n - 1
+            for r in range(n):
+                while c >= 0 and matrix[r][c] > x:
+                    c -= 1
+                cnt += c + 1
+            return cnt
+
+        n = len(matrix)
+        l, r = matrix[0][0], matrix[-1][-1]
+        res = -1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if count_less_or_equal(mid) >= k:
+                res = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return res
+
+    # brute force
+    def kthSmallest1(self, matrix: List[List[int]], k: int) -> int:
         arr = list()
         for r in matrix:
             arr += r
