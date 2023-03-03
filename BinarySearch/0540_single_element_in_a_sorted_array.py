b0cf1e07d46e2cbc957cc708473e91ea8bf74bee
DancingOnAir <taotong1984@hotmail.com>
2023-03-03 22:41:06 +0800

    feat(BinarySearch): init q0540 & accepted concise xor python solution for q0540

---
+++ BinarySearch/0540_single_element_in_a_sorted_array.py
@@ -0,0 +1,18 @@
+from typing import List
+from bisect import bisect_left, bisect_right
+from functools import reduce
+
+
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        return reduce(lambda a, b: a ^ b, nums)
+
+
+def test_single_non_duplicate():
+    solution = Solution()
+    assert solution.singleNonDuplicate([1, 1, 2, 3, 3, 4, 4, 8, 8]) == 2, 'wrong result'
+    assert solution.singleNonDuplicate([3, 3, 7, 7, 10, 11, 11]) == 10, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_single_non_duplicate()
