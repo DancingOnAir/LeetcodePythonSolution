23952f985117d4bfc15da71934f20b4a049adef9
DancingOnAir <taotong1984@hotmail.com>
2023-03-13 00:15:05 +0800

    feat(BinarySearch): accepted concise binary search python solution for q0162

--- BinarySearch/0162_find_peak_element.py
+++ BinarySearch/0162_find_peak_element.py
@@ -2,7 +2,18 @@ from typing import List
 
 
 class Solution:
+    # https://leetcode.com/problems/find-peak-element/solutions/1334653/python-binary-search-solution-explained/?orderBy=most_votes
     def findPeakElement(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 2
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] < nums[mid + 1]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
+
+    def findPeakElement1(self, nums: List[int]) -> int:
         nums = [-1] + nums + [-1]
         left, right = 1, len(nums) - 2
         while left <= right:
