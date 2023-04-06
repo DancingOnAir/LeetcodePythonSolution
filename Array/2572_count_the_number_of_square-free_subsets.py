e6604b554b3d88032740086f267eb3c7d263b1f1
DancingOnAir <taotong1984@hotmail.com>
2023-04-06 10:52:15 +0500

    feat(Array): init q2572 & accepted knapsack dp python solution for q2572

--- Array/2572_count_the_number_of_square-free_subsets.py
+++ Array/2572_count_the_number_of_square-free_subsets.py
@@ -4,20 +4,35 @@ from collections import Counter
 
 class Solution:
     def squareFreeSubsets(self, nums: List[int]) -> int:
-        def get_factor(x):
-            factors = Counter()
-            i = 2
-            while x > 1:
-                if x % i == 0:
-                    factors[i] += 1
-                    x //= i
-                else:
-                    i += 1
-            return factors
+        # 30以内的质数
+        primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+        # sf意思是square-free number, 这里sf_mask[i]表示数字i的质因数集合，使用二进制表示。
+        sf_mask = [0] * 31
+        for i in range(2, 31):
+            for j, p in enumerate(primes):
+                if i % p == 0:
+                    if i % (p * p) == 0:
+                        sf_mask[i] = -1
+                        break
+                    # 把质因数p的下标加入到集合中
+                    sf_mask[i] |= 1 << j
 
-        
-
-        pass
+        MOD = 10 ** 9 + 7
+        M = 1 << len(primes)
+        # f[i]表示恰好组成质因数集合i的方案数
+        f = [0] * M
+        # 质因数集合为空集的方案数为1
+        f[0] = 1
+        for x in nums:
+            mask = sf_mask[x]
+            if mask >= 0:
+                for j in range(M - 1, mask - 1, -1):
+                    # j是mask的子集
+                    if (j | mask) == j:
+                        # 不选mask和选mask
+                        f[j] = (f[j] + f[j ^ mask]) % MOD
+        # -1 是为了去掉nums的空集
+        return (sum(f) - 1) % MOD
 
 
 def test_square_free_subsets():
