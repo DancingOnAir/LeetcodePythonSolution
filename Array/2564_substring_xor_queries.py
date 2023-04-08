c7a352c4ed60c52246d15054e2e1cfeed008827f
DancingOnAir <taotong1984@hotmail.com>
2023-04-09 03:51:40 +0800

    feat(Array): accepted hash map python solution for q2564

--- Array/2564_substring_xor_queries.py
+++ Array/2564_substring_xor_queries.py
@@ -1,8 +1,26 @@
 from typing import List
+from collections import defaultdict
 
 
 class Solution:
     def substringXorQueries(self, s: str, queries: List[List[int]]) -> List[List[int]]:
+        n = len(s)
+        seen = defaultdict(lambda: [-1, -1])
+        for i in range(n - 1, -1, -1):
+            if s[i] == '0':
+                seen[0] = [i, i]
+                continue
+
+            v = 0
+            for j in range(i, n):
+                v = v * 2 + int(s[j])
+                if v > 2 ** 32:
+                    break
+                seen[v] = [i, j]
+        return [seen[a ^ b] for a, b in queries]
+
+    # brute force
+    def substringXorQueries1(self, s: str, queries: List[List[int]]) -> List[List[int]]:
         res = list()
         for a, b in queries:
             cur = bin(a ^ b)[2:]
