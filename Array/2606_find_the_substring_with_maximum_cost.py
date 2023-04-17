7c34da77ccd695c8cde391ac0baf010c4a008bdf
DancingOnAir <taotong1984@hotmail.com>
2023-04-17 10:57:02 +0800

    feat(Array): accepted concise Kadane python solution for q2606

--- Array/2606_find_the_substring_with_maximum_cost.py
+++ Array/2606_find_the_substring_with_maximum_cost.py
@@ -3,6 +3,14 @@ from typing import List
 
 class Solution:
     def maximumCostSubstring(self, s: str, chars: str, vals: List[int]) -> int:
+        m = dict(zip(chars, vals))
+        res = cur = 0
+        for ch in s:
+            cur = max(cur + m.get(ch, ord(ch) - 96), 0)
+            res = max(res, cur)
+        return res
+
+    def maximumCostSubstring1(self, s: str, chars: str, vals: List[int]) -> int:
         m = {chars[i]: vals[i] for i in range(len(chars))}
         res = cur = 0
         for i, ch in enumerate(s):
