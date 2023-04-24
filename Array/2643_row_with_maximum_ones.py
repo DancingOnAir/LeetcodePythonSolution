3a9b654b7d10339d4008ed5606986609b8c98956
DancingOnAir <taotong1984@hotmail.com>
2023-04-24 22:36:46 +0800

    feat(Array): init q2643 & accepted python solution for q2643

---
+++ Array/2643_row_with_maximum_ones.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
+        res = [-1, -1]
+        for i, r in enumerate(mat):
+            total = sum(r)
+            if total > res[1]:
+                res = [i, total]
+        return res
+
+
+def test_row_and_maximum_ones():
+    solution = Solution()
+    assert solution.rowAndMaximumOnes([[0, 1], [1, 0]]) == [0, 1], 'wrong result'
+    assert solution.rowAndMaximumOnes([[0, 0, 0], [0, 1, 1]]) == [1, 2], 'wrong result'
+    assert solution.rowAndMaximumOnes([[0, 0], [1, 1], [0, 0]]) == [1, 2], 'wrong result'
+
+
+if __name__ == '__main__':
+    test_row_and_maximum_ones()
