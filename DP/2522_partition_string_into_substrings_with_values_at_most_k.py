ebc3659433e25d2593c7a5c683bf05549cb6d10d
DancingOnAir <taotong1984@hotmail.com>
2023-05-04 09:02:26 +0800

    feat(DP): accepted dp + greedy python solution for q2522

--- DP/2522_partition_string_into_substrings_with_values_at_most_k.py
+++ DP/2522_partition_string_into_substrings_with_values_at_most_k.py
@@ -1,6 +1,22 @@
 class Solution:
-    # pythonic greedy
+    # dp
     def minimumPartition(self, s: str, k: int) -> int:
+        n = len(s)
+        dp = [0] + [10 ** 6] * n
+        for i in range(1, n + 1):
+            tot, p = 0, 1
+            for j in range(i - 1, -1, -1):
+                tot += int(s[j]) * p
+                p *= 10
+
+                if tot > k:
+                    break
+                dp[i] = min(dp[i], dp[j] + 1)
+
+        return -1 if dp[n] == 10 ** 6 else dp[n]
+
+    # pythonic greedy
+    def minimumPartition2(self, s: str, k: int) -> int:
         # 这里res初始化为1，是因为最后一个满足要求的字符串总是没有被统计，比如s='62', k=7
         res, cur = 1, 0
         for v in map(int, s):
