cf8d9a5b4eef3831e4fcd43470fe55048f570ac7
DancingOnAir <taotong1984@hotmail.com>
2023-05-06 13:00:37 +0800

    feat(Kanpsack): init q2585 & accepted 0-1 knapsack python solution for q2585

--- DP/Knapsack/2585_number_of_ways_to_earn_points.py
+++ DP/Knapsack/2585_number_of_ways_to_earn_points.py
@@ -2,9 +2,16 @@ from typing import List
 
 
 class Solution:
+    # 0-1背包问题
+    # dp[i] 表示target为i时有多少种组合
     def waysToReachTarget(self, target: int, types: List[List[int]]) -> int:
-
-        pass
+        mod = 10 ** 9 + 7
+        dp = [1] + [0] * target
+        for cnt, mark in types:
+            for i in range(target, -1, -1):
+                for j in range(1, min(cnt, i // mark) + 1):
+                    dp[i] = (dp[i] + dp[i - j * mark]) % mod
+        return dp[-1]
 
 
 def test_ways_to_reach_target():
