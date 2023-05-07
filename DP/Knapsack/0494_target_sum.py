88918e384b445e355e173f4618a8cf61a7971b69
DancingOnAir <taotong1984@hotmail.com>
2023-05-07 18:40:45 +0800

    feat(Knapsack): accepted recursive dp python solution for q0494

--- DP/Knapsack/0494_target_sum.py
+++ DP/Knapsack/0494_target_sum.py
@@ -3,9 +3,28 @@ from functools import lru_cache
 
 
 class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        target += sum(nums)
+        if target < 0 or target % 2:
+            return 0
+
+        target //= 2
+        n = len(nums)
+
+        dp = [[0] * (target + 1) for _ in range(n + 1)]
+        dp[0][0] = 1
+
+        for i, x in enumerate(nums):
+            for c in range(target + 1):
+                if c < x:
+                    dp[i + 1][c] = dp[i][c]
+                else:
+                    dp[i + 1][c] = dp[i][c] + dp[i][c - x]
+        return dp[-1][-1]
+
     # nums 原本所有的数之和为tot，需要添加正号的整数之和为p，那么 p - (tot - p) = target
     # 即p = (tot + target) // 2
-    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+    def findTargetSumWays1(self, nums: List[int], target: int) -> int:
         n = len(nums)
         target += sum(nums)
         if target < 0 or target % 2:
