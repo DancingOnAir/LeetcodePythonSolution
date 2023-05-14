8d96a8c9be5c8a56d6aa6060d3ed5bb2a51614e6
DancingOnAir <taotong1984@hotmail.com>
2023-05-14 10:46:26 +0800

    feat(DP/Stock): accepted 2-d dp python solution for q0309

--- DP/Stock/0309_best_time_to_buy_and_sell_stock_with_cooldown.py
+++ DP/Stock/0309_best_time_to_buy_and_sell_stock_with_cooldown.py
@@ -1,11 +1,24 @@
 from typing import List
 from math import inf
+from functools import lru_cache
 
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
+        # dp[i][0] 表示卖出股票，dp[i][1] 表示持有股票，dp[i][2] 表示在冷却时间
+        dp = [[0] * 2 for _ in range(n + 2)]
+        dp[0][1] = -inf
+        dp[1][1] = -inf
+        for i, p in enumerate(prices):
+            dp[i + 2][0] = max(dp[i + 1][0], dp[i + 1][1] + p)
+            dp[i + 2][1] = max(dp[i + 1][1], dp[i][0] - p)
+        return dp[n + 1][0]
 
+    def maxProfit1(self, prices: List[int]) -> int:
+        n = len(prices)
+
+        @lru_cache(None)
         def dfs(i, hold):
             if i < 0:
                 return -inf if hold else 0
