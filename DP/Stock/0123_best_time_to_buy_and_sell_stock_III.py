1dd8163b46eb6eb0ddd5b9933ded0b9ea6127aad
DancingOnAir <taotong1984@hotmail.com>
2023-05-17 01:30:18 +0800

    feat(DP/Stock): acceptd 3-d dp python solution for q0123

--- DP/Stock/0123_best_time_to_buy_and_sell_stock_III.py
+++ DP/Stock/0123_best_time_to_buy_and_sell_stock_III.py
@@ -1,10 +1,24 @@
 from typing import List
 from functools import lru_cache
+from math import inf
 
 
 class Solution:
-    # dfs
     def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        dp = [[[-inf] * 2 for _ in range(4)] for _ in range(n + 1)]
+        for j in range(1, 4):
+            dp[0][j][0] = 0
+
+        for i, p in enumerate(prices):
+            for j in range(3):
+                dp[i + 1][j + 1][0] = max(dp[i][j + 1][0], dp[i][j][1] + p)
+                dp[i + 1][j + 1][1] = max(dp[i][j + 1][1], dp[i][j + 1][0] - p)
+
+        return dp[n][3][0]
+
+    # dfs
+    def maxProfit1(self, prices: List[int]) -> int:
         n = len(prices)
 
         @lru_cache(None)
