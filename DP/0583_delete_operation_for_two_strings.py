4f5be159d3ab597f5654e8ab613210444fe9b1ed
DancingOnAir <taotong1984@hotmail.com>
2023-05-25 08:59:43 +0800

    feat(DP/Robber): accepeted dfs & dp python solution for q0583

--- DP/0583_delete_operation_for_two_strings.py
+++ DP/0583_delete_operation_for_two_strings.py
@@ -1,7 +1,34 @@
+from functools import lru_cache
 
 
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
+        l1, l2 = len(word1), len(word2)
+        dp = [[0] * (l2 + 1) for _ in range(l1 + 1)]
+
+        for i, x in enumerate(word1):
+            for j, y in enumerate(word2):
+                if x == y:
+                    dp[i + 1][j + 1] = dp[i][j] + 1
+                else:
+                    dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1])
+
+        return l1 + l2 - 2 * dp[l1][l2]
+
+    # dfs
+    def minDistance2(self, word1: str, word2: str) -> int:
+        l1, l2 = len(word1), len(word2)
+
+        @lru_cache(None)
+        def dfs(i, j):
+            if i < 0 or j < 0:
+                return 0
+            if word1[i] == word2[j]:
+                return dfs(i - 1, j - 1) + 1
+            return max(dfs(i - 1, j), dfs(i, j - 1))
+        return l1 + l2 - 2 * dfs(l1 - 1, l2 - 1)
+
+    def minDistance1(self, word1: str, word2: str) -> int:
         l1, l2 = len(word1), len(word2)
         dp = [[0] * (l2 + 1) for _ in range(l1 + 1)]
         for i in range(l1):
