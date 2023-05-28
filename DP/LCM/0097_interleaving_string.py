7beed7e91e3f611359a7ca2e3fa6e9dbda4e7711
DancingOnAir <taotong1984@hotmail.com>
2023-05-28 09:39:16 +0800

    feat(DP/LCM): accepted dp python solution for q0097

--- DP/LCM/0097_interleaving_string.py
+++ DP/LCM/0097_interleaving_string.py
@@ -2,7 +2,37 @@ from functools import lru_cache
 
 
 class Solution:
+    # 3-d dp
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        l1, l2, l3 = len(s1), len(s2), len(s3)
+        if l1 + l2 != l3:
+            return False
+
+        dp = [[False] * (l2 + 1) for _ in range(l1 + 1)]
+        dp[0][0] = True
+        for i, x in enumerate(s1):
+            if x == s3[i]:
+                dp[i + 1][0] = True
+            else:
+                break
+
+        for j, y in enumerate(s2):
+            if y == s3[j]:
+                dp[0][j + 1] = True
+            else:
+                break
+
+        for i, x in enumerate(s1):
+            for j, y in enumerate(s2):
+                k = i + j + 1
+                if x == s3[k]:
+                    dp[i + 1][j + 1] |= dp[i][j + 1]
+                if y == s3[k]:
+                    dp[i + 1][j + 1] |= dp[i + 1][j]
+        return dp[l1][l2]
+
+    # dfs + cache
+    def isInterleave1(self, s1: str, s2: str, s3: str) -> bool:
         l1, l2, l3 = len(s1), len(s2), len(s3)
 
         @lru_cache(None)
