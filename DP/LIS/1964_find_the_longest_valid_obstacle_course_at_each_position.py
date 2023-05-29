6de90a7860009a3c76b7c6bcdb9ea6164e17aa93
DancingOnAir <taotong1984@hotmail.com>
2023-05-30 00:40:50 +0800

    feat(DP/LIS): init q1964 & tried dp and dfs python solution for q1964 but TLE

---
+++ DP/LIS/1964_find_the_longest_valid_obstacle_course_at_each_position.py
@@ -0,0 +1,42 @@
+from typing import List
+from functools import lru_cache
+
+
+class Solution:
+    # TLE dp
+    def longestObstacleCourseAtEachPosition(self, obstacles: List[int]) -> List[int]:
+        n = len(obstacles)
+
+        dp = [0] * n
+        for i in range(n):
+            for j in range(i):
+                if obstacles[j] <= obstacles[i]:
+                    dp[i] = max(dp[i], dp[j])
+            dp[i] += 1
+        return dp
+
+    # TLE dfs
+    def longestObstacleCourseAtEachPosition1(self, obstacles: List[int]) -> List[int]:
+        n = len(obstacles)
+
+        @lru_cache(None)
+        def dfs(i):
+            sz = 0
+
+            for j in range(i):
+                if obstacles[j] <= obstacles[i]:
+                    sz = max(sz, dfs(j))
+            return sz + 1
+
+        return [dfs(i) for i in range(n)]
+
+
+def test_longest_obstacle_course_at_each_position():
+    solution = Solution()
+    assert solution.longestObstacleCourseAtEachPosition([1, 2, 3, 2]) == [1, 2, 3, 3], 'wrong result'
+    assert solution.longestObstacleCourseAtEachPosition([2, 2, 1]) == [1, 2, 1], 'wrong result'
+    assert solution.longestObstacleCourseAtEachPosition([3, 1, 5, 6, 4, 2]) == [1, 1, 2, 3, 2, 2], 'wrong result'
+
+
+if __name__ == '__main__':
+    test_longest_obstacle_course_at_each_position()
