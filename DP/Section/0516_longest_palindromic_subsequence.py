cd97da7afd84270c45dd8bb4e1452d5e53520b65
DancingOnAir <taotong1984@hotmail.com>
2023-06-03 01:24:52 +0800

    feat(DP/Section): init q0516 & accepted dp & dfs python solution for q0516

--- DP/Section/0516_longest_palindromic_subsequence.py
+++ DP/Section/0516_longest_palindromic_subsequence.py
@@ -1,9 +1,38 @@
-from typing import List
+from functools import lru_cache
 
 
 class Solution:
-    # 1d dp
+    # dp
     def longestPalindromeSubseq(self, s: str) -> int:
+        n = len(s)
+        dp = [[0] * n for _ in range(n)]
+        for i in range(n - 1, -1, -1):
+            dp[i][i] = 1
+            for j in range(i + 1, n):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i + 1][j - 1] + 2
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+        return dp[0][n - 1]
+
+    # https://www.bilibili.com/video/BV1Gs4y1E7EU/?spm_id_from=333.788&vd_source=e6f3bca3cb4f75b9e8b036e0e78f1541
+    # dfs
+    def longestPalindromeSubseq3(self, s: str) -> int:
+        n = len(s)
+
+        @lru_cache(None)
+        def dfs(i, j):
+            if i > j:
+                return 0
+            elif i == j:
+                return 1
+            if s[i] == s[j]:
+                return dfs(i + 1, j - 1) + 2
+            return max(dfs(i + 1, j), dfs(i, j - 1))
+        return dfs(0, n - 1)
+
+    # 1d dp
+    def longestPalindromeSubseq2(self, s: str) -> int:
         n = len(s)
         if n < 2:
             return n
