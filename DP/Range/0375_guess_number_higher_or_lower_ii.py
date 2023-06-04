75f064a66bf0065bc6e9ff7ac9cf84944cda4699
DancingOnAir <taotong1984@hotmail.com>
2023-06-05 07:13:52 +0800

    feat(DP/Range): accepted dp python solution for q0375

--- DP/Range/0375_guess_number_higher_or_lower_ii.py
+++ DP/Range/0375_guess_number_higher_or_lower_ii.py
@@ -3,8 +3,18 @@ from functools import lru_cache
 
 
 class Solution:
-    # dfs + cache
+    # dp
     def getMoneyAmount(self, n: int) -> int:
+        dp = [[inf] * (n + 1) for _ in range(n + 1)]
+        for i in range(n, 0, -1):
+            dp[i][i] = 0
+            dp[i - 1][i] = i - 1
+            for j in range(i + 2, n + 1):
+                dp[i][j] = min(max(dp[i][k - 1], dp[k + 1][j]) + k for k in range(i + 1, j))
+        return dp[1][n]
+
+    # dfs + cache
+    def getMoneyAmount1(self, n: int) -> int:
         @lru_cache(None)
         def dfs(i, j):
             if i == j:
