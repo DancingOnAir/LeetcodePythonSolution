916b912f5ecf77df1212304bc41214e8b0e5af62
DancingOnAir <taotong1984@hotmail.com>
2023-06-22 00:51:41 +0800

    feat(DP/Tree): init q0713 & accepted dfs python solution for q0713

---
+++ TwoPointers/SlidingWindow/0713_subarray_product_less_than_k.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        left = res = 0
+        total = 1
+        for right, x in enumerate(nums):
+            total *= x
+            while left <= right and total >= k:
+                total //= nums[left]
+                left += 1
+            res += right - left + 1
+        return res
+
+
+def test_num_subarray_product_less_than_k():
+    solution = Solution()
+    assert solution.numSubarrayProductLessThanK([10, 5, 2, 6], 100) == 8, 'wrong result'
+    assert solution.numSubarrayProductLessThanK([1, 2, 3], 0) == 0, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_num_subarray_product_less_than_k()
