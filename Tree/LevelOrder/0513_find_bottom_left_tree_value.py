ce689395c8a9348b51f6f599c277368b121ade50
DancingOnAir <taotong1984@hotmail.com>
2023-08-18 07:55:15 +0800

    feat(Tree/LevelOrder): init q0513 & accepted python solution for q0513

---
+++ Tree/LevelOrder/0513_find_bottom_left_tree_value.py
@@ -0,0 +1,42 @@
+from typing import Optional
+from collections import deque
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return -1
+
+        res = list()
+        dq = deque([root])
+        while dq:
+            vals = list()
+            for _ in range(len(dq)):
+                node = dq.popleft()
+                vals.append(node.val)
+
+                if node.left:
+                    dq.append(node.left)
+                if node.right:
+                    dq.append(node.right)
+            res.append(vals)
+
+        return res[-1][0]
+
+
+def test_find_bottom_left_value():
+    solution = Solution()
+    assert solution.findBottomLeftValue(TreeNode(2, TreeNode(1), TreeNode(3))) == 1, 'wrong result'
+    assert solution.findBottomLeftValue(TreeNode(1, TreeNode(2, TreeNode(4)), TreeNode(3, TreeNode(5, TreeNode(7)), TreeNode(6)))) == 7, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_find_bottom_left_value()
