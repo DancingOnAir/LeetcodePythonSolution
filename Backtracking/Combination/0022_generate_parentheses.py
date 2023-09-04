039697dfb5df4fde5f098fdde1ad83f3eed7d1b1
DancingOnAir <taotong1984@hotmail.com>
2023-09-05 07:26:04 +0800

    feat(Backtracking/Combination): accepted backtracking mode2 python solution for q0022

--- Backtracking/Combination/0022_generate_parentheses.py
+++ Backtracking/Combination/0022_generate_parentheses.py
@@ -2,7 +2,8 @@ from typing import List
 
 
 class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
+    # mode1 choose or node choose
+    def generateParenthesis1(self, n: int) -> List[str]:
         res = list()
         path = list()
         m = n * 2
@@ -22,6 +23,28 @@ class Solution:
                 dfs(i + 1, open)
                 path.pop()
         dfs(0, 0)
+        return res
+
+    # mode 2
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        path = []
+
+        def dfs(i, left, right):
+            if len(path) == n:
+                s = [')'] * n * 2
+                for j in path:
+                    s[j] = '('
+                res.append(''.join(s))
+                return
+
+            # 可以填充0到left - right个右括号
+            for j in range(left - right + 1):
+                # 填充1个左括号
+                path.append(i + j)
+                dfs(i + j + 1, left + 1, right + j)
+                path.pop()
+        dfs(0, 0, 0)
         return res
 
 
