19968a040095808447e33352d7a1251e91eb081e
DancingOnAir <taotong1984@hotmail.com>
2023-09-10 00:41:33 +0800

    feat(Backtracking/permutation): accepted concise & improved backtracking python solution for q0051

--- Backtracking/Permutation/0051_n-queens.py
+++ Backtracking/Permutation/0051_n-queens.py
@@ -5,6 +5,46 @@ class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         res = []
         cols = [0] * n
+        m = 2 * n - 1
+        on_path = [False] * n
+        diag1 = [False] * m
+        diag2 = [False] * m
+
+        def dfs(r):
+            if r == n:
+                res.append(["." * c + "Q" + "." * (n - 1 - c) for c in cols])
+                return
+            for c in range(n):
+                if not on_path[c] and not diag1[r + c] and not diag2[r - c]:
+                    cols[r] = c
+                    on_path[c] = diag1[r + c] = diag2[r - c] = True
+                    dfs(r + 1)
+                    on_path[c] = diag1[r + c] = diag2[r - c] = False
+        dfs(0)
+        return res
+
+    # pythonic
+    def solveNQueens2(self, n: int) -> List[List[str]]:
+        res = []
+        cols = [0] * n
+
+        def dfs(r, s):
+            if r == n:
+                res.append(["." * c + "Q" + "." * (n - 1 - c) for c in cols])
+                return
+
+            for c in s:
+                if all(r + c != R + cols[R] and r - c != R - cols[R] for R in range(r)):
+                    cols[r] = c
+                    dfs(r + 1, s - {c})
+
+        dfs(0, set(range(n)))
+        return res
+
+    # normal
+    def solveNQueens1(self, n: int) -> List[List[str]]:
+        res = []
+        cols = [0] * n
 
         def valid(r, c):
             for R in range(r):
