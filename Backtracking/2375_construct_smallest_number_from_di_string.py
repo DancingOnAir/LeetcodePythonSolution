a585ede6f63acb29c7e9fb382e14201e3bce1b38
DancingOnAir <taotong1984@hotmail.com>
2023-09-21 00:45:37 +0800

    feat(Backtracking): init q2375 & accepted string manipulaiton python solution for q2375

---
+++ Backtracking/2375_construct_smallest_number_from_di_string.py
@@ -0,0 +1,29 @@
+class Solution:
+    def smallestNumber(self, pattern: str) -> str:
+        n = len(pattern)
+        res = [str(i) for i in range(1, n + 2)]
+
+        i, j = 0, 0
+        while i < n:
+            if pattern[i] == 'I':
+                i += 1
+                j += 1
+            else:
+                cnt = 0
+                while i < n and pattern[i] == 'D':
+                    i += 1
+                    cnt += 1
+                res[j: j + cnt + 1] = res[j: j + cnt + 1][::-1]
+                j = i
+
+        return ''.join(res)
+
+
+def test_smallest_number():
+    solution = Solution()
+    assert solution.smallestNumber("IIIDIDDD") == "123549876", 'wrong result'
+    assert solution.smallestNumber("DDD") == "4321", "wrong result"
+
+
+if __name__ == '__main__':
+    test_smallest_number()
