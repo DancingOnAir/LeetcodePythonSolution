b14e5b15f8dc4bc6a02f0ae3029d80b12dbdc84a
DancingOnAir <taotong1984@hotmail.com>
2023-09-30 10:24:09 +0800

    feat(String): accepted improved python solution for q2839

--- String/2839_check_if_strings_can_be_made_equal_with_operations_i.py
+++ String/2839_check_if_strings_can_be_made_equal_with_operations_i.py
@@ -1,6 +1,9 @@
 class Solution:
     def canBeEqual(self, s1: str, s2: str) -> bool:
-        return all((s1[i] == s2[i] and s1[i + 2] == s2[i + 2]) or (s1[i] == s2[i + 2] and s1[i + 2] == s2[i]) for i in range(2))
+        return {s1[0], s1[2]} == {s2[0], s2[2]} and {s1[1], s1[3]} == {s2[1], s2[3]}
+
+    def canBeEqual1(self, s1: str, s2: str) -> bool:
+        return all({s1[i], s1[i + 2]} == {s2[i], s2[i + 2]} for i in range(2))
 
 
 def test_can_be_equal():
