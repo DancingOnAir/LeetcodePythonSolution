1c6d4d9350ff1c0127e1ba8873d611552daec48e
DancingOnAir <taotong1984@hotmail.com>
2023-10-01 00:04:47 +0800

    feat(String): init q2840 & accepted python solution for q2840

---
+++ String/2840_check_if_strings_can_be_made_equal_with_operations_ii.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+
+class Solution:
+    def checkStrings(self, s1: str, s2: str) -> bool:
+        def helper(start):
+            c1, c2 = Counter(), Counter()
+            for i in range(start, len(s1), 2):
+                c1[s1[i]] += 1
+                c2[s2[i]] += 1
+            return c1 == c2
+        return helper(0) and helper(1)
+
+
+def test_check_strings():
+    solution = Solution()
+    assert solution.checkStrings("abcdba", "cabdab"), 'wrong result'
+    assert not solution.checkStrings("abe", "bea"), 'wrong result'
+
+
+if __name__ == '__main__':
+    test_check_strings()
