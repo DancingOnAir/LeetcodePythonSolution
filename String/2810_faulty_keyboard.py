c10bac09792973bd2deb5822f89e920620038826
DancingOnAir <taotong1984@hotmail.com>
2023-10-04 00:46:05 +0800

    feat(String): accepted improved simulation python solution for q2810

--- String/2810_faulty_keyboard.py
+++ String/2810_faulty_keyboard.py
@@ -1,5 +1,21 @@
+from collections import deque
+
+
 class Solution:
     def finalString(self, s: str) -> str:
+        dq = deque()
+        cnt = s.count('i') % 2
+        for c in s:
+            if c == 'i':
+                cnt ^= 1
+            else:
+                if cnt:
+                    dq.appendleft(c)
+                else:
+                    dq.append(c)
+        return ''.join(dq)
+
+    def finalString1(self, s: str) -> str:
         res = []
         for i, c in enumerate(s):
             if c == 'i':
