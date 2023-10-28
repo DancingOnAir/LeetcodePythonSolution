11eb06f480cc5c1e2803f4e8b2e5deb678e3c14f
DancingOnAir <taotong1984@hotmail.com>
2023-10-29 01:43:16 +0800

    feat(String): accepted python solution for q2514

--- String/2514_count_anagrams.py
+++ String/2514_count_anagrams.py
@@ -1,9 +1,27 @@
 from collections import Counter
 from itertools import permutations
+from math import factorial
 
 
 class Solution:
     def countAnagrams(self, s: str) -> int:
+        def helper(w):
+            fact = factorial(len(w))
+            c = Counter(w)
+            inv_fact = 1
+            for v in c.values():
+                inv_fact *= factorial(v)
+            return fact // inv_fact
+
+        res = 1
+        mod = 10 ** 9 + 7
+        for w in s.split():
+            res *= helper(w)
+
+        return res % mod
+
+    # TLE
+    def countAnagrams1(self, s: str) -> int:
         def helper(w):
             return len(set(permutations(w)))
 
