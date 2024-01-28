82205f2057162426daf15b45f95a08af22975ea9
DancingOnAir <taotong1984@hotmail.com>
2024-01-28 11:10:40 +0800

    feat(Trie): accepted trie python solution for q0745

--- Trie/0745_prefix_and_suffix_search.py
+++ Trie/0745_prefix_and_suffix_search.py
@@ -2,6 +2,46 @@ from typing import List
 from collections import defaultdict
 
 
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.idx = 0
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word, idx):
+        root = self.root
+        root.idx = idx
+        for ch in word:
+            root = root.children.setdefault(ch, TrieNode())
+            root.idx = idx
+
+    def start_with(self, word):
+        root = self.root
+        for ch in word:
+            if ch not in root.children:
+                return -1
+            root = root.children[ch]
+        return root.idx
+
+
+# https://leetcode.com/problems/prefix-and-suffix-search/solutions/1185171/python-two-solutions-trie-and-bruteforce-explained/
+class WordFilter:
+    def __init__(self, words: List[str]):
+        self.trie = Trie()
+        for i, w in enumerate(words):
+            long = w + '#' + w
+            for j in range(len(w)):
+                self.trie.insert(long[j:], i)
+
+    def f(self, pref: str, suff: str) -> int:
+        res = self.trie.start_with(suff + '#' + pref)
+        return res
+
+
 class WordFilter1:
     def __init__(self, words: List[str]):
         self.pres = defaultdict(set)
