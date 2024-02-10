55309d0dc65f8de2663271a68593540040ba3a2d
DancingOnAir <taotong1984@hotmail.com>
2024-02-10 13:32:51 +0800

    feat(String): accepted python solution for q0657

--- String/0657_robot_return_to_origin.py
+++ String/0657_robot_return_to_origin.py
@@ -1,5 +1,8 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
+        return moves.count('U') == moves.count('D') and moves.count('L') == moves.count('R')
+
+    def judgeCircle1(self, moves: str) -> bool:
         res = [0, 0]
         for move in moves:
             if move == 'U':
