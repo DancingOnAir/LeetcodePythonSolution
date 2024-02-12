5e386a5907d38ff05a6383e8478ae02c5519de58
DancingOnAir <taotong1984@hotmail.com>
2024-02-12 08:24:42 +0800

    feat(String): accepted priority queue python solution for q0649

--- String/0649_dota2_senate.py
+++ String/0649_dota2_senate.py
@@ -3,7 +3,23 @@ from collections import deque
 
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
-        pass
+        n = len(senate)
+        rq, dq = deque(), deque()
+        for i, s in enumerate(senate):
+            if s == 'R':
+                rq.append(i)
+            else:
+                dq.append(i)
+
+        while len(rq) > 0 and len(dq) > 0:
+            r = rq.popleft()
+            d = dq.popleft()
+
+            if r < d:
+                rq.append(r + n)
+            else:
+                dq.append(d + n)
+        return "Radiant" if len(rq) > 0 else "Dire"
 
 
 def test_predict_party_victory():
