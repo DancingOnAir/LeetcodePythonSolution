868540430d5e177d1fce21dc6511a429a842b528
DancingOnAir <taotong1984@hotmail.com>
2024-03-15 08:17:07 +0800

    feat(BinaryIndexTree): init q2940 & accepted binary index tree python solution for q2940

--- SegmentTree/BinaryIndexTree/2940_find_building_where_alice_and_bob_can_meet.py
+++ SegmentTree/BinaryIndexTree/2940_find_building_where_alice_and_bob_can_meet.py
@@ -1,30 +1,52 @@
 from typing import List
 from heapq import heappop, heappush
 
-# class Fenwick:
-#     def __init__(self, n):
-#         self.tree = [0] * n
-#
-#     def add(self, i, v):
-#         while i < len(self.tree):
-#             self.tree[i] += v
-#             i += i & -i
-#
-#     def query(self, i):
-#         res = 0
-#         while i > 0:
-#             res += self.tree[i]
-#             i -= i & -i
-#         return res
 
-class SegmentTree:
+class Fenwick:
     def __init__(self, n):
-        self.tree = [0] * (4 * n)
+        self.tree = [float('inf')] * n
+
+    def add(self, i, v):
+        while i < len(self.tree):
+            self.tree[i] = min(self.tree[i], v)
+            i += i & -i
+
+    def query(self, i):
+        res = float('inf')
+        while i > 0:
+            res = min(res, self.tree[i])
+            i -= i & -i
+        return res
 
 
 class Solution:
-    # heap
     def leftmostBuildingQueries(self, heights: List[int], queries: List[List[int]]) -> List[int]:
+        res = [-1] * len(queries)
+        left = []
+        for i, (a, b) in enumerate(queries):
+            if a > b:
+                a, b = b, a
+            if a == b or heights[a] < heights[b]:
+                res[i] = b
+            else:
+                left.append((-heights[a] - 1, i, b))
+        for i, v in enumerate(heights):
+            left.append((-v, -1, i))
+        left.sort()
+
+        n = len(heights)
+        t = Fenwick(n + 1)
+        for v, i, j in left:
+            if i == -1:
+                t.add(n - j, j)
+            else:
+                res[i] = t.query(n - j)
+                if res[i] == float('inf'):
+                    res[i] = -1
+        return res
+
+    # discretization + heap
+    def leftmostBuildingQueries1(self, heights: List[int], queries: List[List[int]]) -> List[int]:
         res = [-1] * len(queries)
         left = [[] for _ in heights]
         for i, (a, b) in enumerate(queries):
