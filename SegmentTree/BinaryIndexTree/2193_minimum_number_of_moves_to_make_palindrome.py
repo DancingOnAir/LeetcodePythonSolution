f36e842b48d854c6500261d1046be446c03c92d7
DancingOnAir <taotong1984@hotmail.com>
2024-03-16 12:13:02 +0800

    feat(BinaryIndexedTree): init q2193 & accepted greedy python solution for q2193

---
+++ SegmentTree/BinaryIndexTree/2193_minimum_number_of_moves_to_make_palindrome.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minMovesToMakePalindrome(self, s: str) -> int:
+        s = list(s)
+        res = 0
+        while s:
+            i = s.index(s[-1])
+            if i == len(s) - 1:
+                res += i // 2
+            else:
+                res += i
+                s.pop(i)
+            s.pop()
+
+        return res
+
+
+def test_min_moves_to_make_palindrome():
+    solution = Solution()
+    assert solution.minMovesToMakePalindrome("aabb") == 2, 'wrong result'
+    assert solution.minMovesToMakePalindrome("letelt") == 2, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_min_moves_to_make_palindrome()
