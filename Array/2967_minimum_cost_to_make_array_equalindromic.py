ef6e938f8cc8de83fb3f9a83964b22b4feeb6093
DancingOnAir <taotong1984@hotmail.com>
2024-05-30 07:14:53 +0800

    feat(Array): init q2967 & accepted concise python solution for q2967

--- Array/2967_minimum_cost_to_make_array_equalindromic.py
+++ Array/2967_minimum_cost_to_make_array_equalindromic.py
@@ -1,8 +1,47 @@
 from typing import List
+from bisect import bisect_left
 
 
 class Solution:
+    # https://leetcode.cn/problems/minimum-cost-to-make-array-equalindromic/solutions/2569308/yu-chu-li-hui-wen-shu-zhong-wei-shu-tan-7j0zy/
     def minimumCost(self, nums: List[int]) -> int:
+        def generate_palindromic_num():
+            pals = []
+            base = 1
+
+            while base <= 10000:
+                # 奇数长度回文字
+                for i in range(base, base * 10):
+                    x = i
+                    t = i // 10
+                    while t:
+                        x = x * 10 + t % 10
+                        t //= 10
+                    pals.append(x)
+                # 偶数长度回文字
+                if base <= 1000:
+                    for i in range(base, base * 10):
+                        x = t = i
+                        while t:
+                            x = x * 10 + t % 10
+                            t //= 10
+                        pals.append(x)
+                base *= 10
+            pals.append(1_000_000_001)
+            return pals
+
+        def cost(i):
+            return sum(abs(x - pals[i]) for x in nums)
+
+        pals = generate_palindromic_num()
+        nums.sort()
+        mid = nums[len(nums) // 2]
+        i = bisect_left(pals, mid)
+        if pals[i] <= mid:
+            return cost(i)
+        return min(cost(i - 1), cost(i))
+
+    def minimumCost1(self, nums: List[int]) -> int:
         def cost(x):
             return sum(abs(a - x) for a in nums)
 
