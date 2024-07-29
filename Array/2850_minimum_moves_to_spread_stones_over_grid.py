7358bbbfec0e1afc1971bf9118c5abab949f12a2
DancingOnAir <taotong1984@hotmail.com>
2024-07-30 07:19:34 +0800

    feat(Array): accepted backtracking python solution for q2850

--- Array/2850_minimum_moves_to_spread_stones_over_grid.py
+++ Array/2850_minimum_moves_to_spread_stones_over_grid.py
@@ -4,6 +4,31 @@ from itertools import product, permutations
 
 class Solution:
     def minimumMoves(self, grid: List[List[int]]) -> int:
+        def backtracking(consumers, suppliers, idx):
+            if idx == len(zeros):
+                return 0
+            res = float('inf')
+            a, b = consumers[idx]
+            for x, y in suppliers:
+                if grid[x][y] == 1:
+                    continue
+
+                grid[x][y] -= 1
+                res = min(res, abs(x - a) + abs(y - b) + backtracking(consumers, suppliers, idx + 1))
+                grid[x][y] += 1
+            return res
+
+        zeros, spares = [], []
+        for i, j in product(range(3), range(3)):
+            stones = grid[i][j]
+            if stones == 0:
+                zeros.append((i, j))
+            elif stones > 1:
+                spares.append((i, j))
+        return backtracking(zeros, spares, 0)
+
+    # brute force
+    def minimumMoves1(self, grid: List[List[int]]) -> int:
         dist = lambda x, y: abs(x[0] - y[0]) + abs(x[1] - y[1])
         zeros, spares = [], []
         for i, j in product(range(3), range(3)):
