ae9e09ce5db8f812e4eacfecba6e33ce868509d3
DancingOnAir <taotong1984@hotmail.com>
2024-10-15 07:13:39 +0800

    feat(String): init q3083 & accepted python solution for q3083

---
+++ String/3083_existence_of_a_substring_in_a_string_and_its_reverse.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isSubstringPresent(self, s: str) -> bool:
+        m = set()
+        for i in range(len(s) - 1):
+            if s[i] == s[i + 1]:
+                return True
+
+            if s[i] + s[i + 1] in m:
+                return True
+            m.add(s[i + 1] + s[i])
+        return False
+
+
+def test_is_substring_present():
+    solution = Solution()
+    assert solution.isSubstringPresent("leetcode"), 'wrong result'
+    assert solution.isSubstringPresent("abcba"), 'wrong result'
+    assert not solution.isSubstringPresent("abcd"), 'wrong result'
+
+
+if __name__ == '__main__':
+    test_is_substring_present()
