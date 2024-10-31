f64b51d265ec516ac8995edbdd79a3c76e28c68d
DancingOnAir <taotong1984@hotmail.com>
2024-11-01 07:12:49 +0800

    feat(SlidingWindow): init q3013 & accepted python solution for q3013

--- TwoPointers/SlidingWindow/3013_divide_an_array_into_subarrays_with_minimum_cost_ii.py
+++ TwoPointers/SlidingWindow/3013_divide_an_array_into_subarrays_with_minimum_cost_ii.py
@@ -5,6 +5,57 @@ from sortedcontainers import SortedList
 class Solution:
     def minimumCost(self, nums: List[int], k: int, dist: int) -> int:
         n = len(nums)
+        k -= 1
+        L, R = SortedList(nums[1: dist + 2]), SortedList()
+        tot = sum(nums[:dist + 2])
+
+        def add(val):
+            if L and val < L[-1]:
+                nonlocal tot
+                tot += val
+                L.add(val)
+            else:
+                R.add(val)
+
+        def remove(val):
+            if val in L:
+                nonlocal tot
+                tot -= val
+                L.remove(val)
+            else:
+                R.remove(val)
+
+        def l2r():
+            nonlocal tot
+            p = L.pop()
+            tot -= p
+            R.add(p)
+
+        def r2l():
+            nonlocal tot
+            p = R.pop(0)
+            tot += p
+            L.add(p)
+
+        while len(L) > k:
+            l2r()
+
+        res = tot
+        for i in range(dist + 2, len(nums)):
+            out = nums[i - dist - 1]
+            remove(out)
+            add(nums[i])
+
+            if len(L) == k - 1:
+                r2l()
+            elif len(L) == k + 1:
+                l2r()
+            res = min(res, tot)
+        return res
+
+    # bisect & sorted list
+    def minimumCost1(self, nums: List[int], k: int, dist: int) -> int:
+        n = len(nums)
         sl = SortedList(nums[1: dist + 1])
         cur, res = sum(sl[:k-2]), float('inf')
         for i in range(dist + 1, n):
