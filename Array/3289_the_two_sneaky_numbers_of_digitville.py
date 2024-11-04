b9b4eb159c7217fc4ed5054ef6dd23759b497b29
DancingOnAir <taotong1984@hotmail.com>
2024-11-05 07:14:32 +0800

    feat(BinarySearch): init q3289 & accepted bit python solution for q3289

--- Array/3289_the_two_sneaky_numbers_of_digitville.py
+++ Array/3289_the_two_sneaky_numbers_of_digitville.py
@@ -1,8 +1,25 @@
 from typing import List
+from functools import reduce
+from operator import xor
 
 
 class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        xor_n2 = reduce(xor, nums)
+        xor_n = reduce(xor, range(len(nums) - 2))
+
+        xor_ab = xor_n2 ^ xor_n
+        bit_diff = xor_ab & (-xor_ab)
+
+        xor_n2_bit = reduce(xor, (x for x in nums if (x & bit_diff)))
+        xor_n2_no_bit = reduce(xor, (x for x in nums if not (x & bit_diff)))
+
+        xor_n_bit = reduce(xor, (x for x in range(len(nums) - 2) if (x & bit_diff)))
+        xor_n_no_bit = reduce(xor, (x for x in range(len(nums) - 2) if not (x & bit_diff)))
+
+        return [xor_n_no_bit ^ xor_n2_no_bit, xor_n_bit ^ xor_n2_bit]
+
+    def getSneakyNumbers1(self, nums: List[int]) -> List[int]:
         m = [0] * (len(nums) - 2)
         res = []
         for x in nums:
