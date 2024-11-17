ef61ed8b7bc42b59a26568ac759d7b4f060b7845
DancingOnAir <taotong1984@hotmail.com>
2024-11-18 07:08:58 +0800

    feat(DiffArray): init q3347 & accepted diff array python solution for q3347

--- Array/diff_array/3346_maximum_frequency_of_an_element_after_performing_operations_i.py
+++ Array/diff_array/3346_maximum_frequency_of_an_element_after_performing_operations_i.py
@@ -3,9 +3,33 @@ from collections import defaultdict
 
 
 class Solution:
+    # https://leetcode.com/problems/maximum-frequency-of-an-element-after-performing-operations-i/solutions/6027282/java-c-python-two-cases/
+    def maxFrequency(self, nums: List[int], k: int, numOperations: int) -> int:
+        n = len(nums)
+        nums.sort()
+
+        cnt = defaultdict(int)
+        res = i = j = 0
+        for x in nums:
+            while j < n and x + k >= nums[j]:
+                cnt[nums[i]] += 1
+                j += 1
+            while i < n and x - k > nums[i]:
+                cnt[nums[i]] -= 1
+                i += 1
+            cur = min(j - i, cnt[x] + numOperations)
+            res = max(res, cur)
+
+        i = 0
+        for j in range(n):
+            while nums[i] + k + k < nums[j]:
+                i += 1
+            res = max(res, min(j - i + 1, numOperations))
+        return res
+
     # 数组A的差分数组为D，其中D[i]=A[i]-A[i-1](i>0)且D[0]=A[0]
     # A[i]的差分数组是D[i]， 而D[i]的前缀和是A[i]
-    def maxFrequency(self, nums: List[int], k: int, numOperations: int) -> int:
+    def maxFrequency1(self, nums: List[int], k: int, numOperations: int) -> int:
         diff = defaultdict(int)
         cnt = defaultdict(int)
         for x in nums:
