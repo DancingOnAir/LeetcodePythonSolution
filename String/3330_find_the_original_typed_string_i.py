6f371eec5ac51ab190d175b801f73a2f4d792eee
DancingOnAir <taotong1984@hotmail.com>
2024-11-24 09:16:08 +0800

    feat(String): accepted concise python solution for q3330

--- String/3330_find_the_original_typed_string_i.py
+++ String/3330_find_the_original_typed_string_i.py
@@ -1,5 +1,12 @@
 class Solution:
     def possibleStringCount(self, word: str) -> int:
+        res = 1
+        for i in range(len(word) - 1):
+            if word[i] == word[i + 1]:
+                res += 1
+        return res
+
+    def possibleStringCount1(self, word: str) -> int:
         res, cnt = 1, 0
         for i in range(1, len(word)):
             if word[i] == word[i - 1]:
