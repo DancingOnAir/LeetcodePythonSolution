be5709dabdf81fd420b7218df5f9a37a2953885a
DancingOnAir <taotong1984@hotmail.com>
2024-12-08 10:21:49 +0800

    feat(presum): init q3364 & accepted python solution for q3364

---
+++ Array/presum/3364_minimum_positive_sum_subarray.py
@@ -0,0 +1,27 @@
+from itertools import accumulate
+from typing import List
+
+
+class Solution:
+    def minimumSumSubarray(self, nums: List[int], l: int, r: int) -> int:
+        res = float('inf')
+        n = len(nums)
+        ps = list(accumulate(nums, initial=0))
+        for left in range(n):
+            for right in range(left+1, n+1):
+                cur = ps[right] - ps[left]
+                sz = right - left
+                if l <= sz <= r and 0 < cur < res:
+                    res = cur
+        return res if res < float('inf') else -1
+
+
+def test_minimum_sum_subarray():
+    solution = Solution()
+    assert solution.minimumSumSubarray([3, -2, 1, 4], l=2, r=3) == 1, 'wrong result'
+    assert solution.minimumSumSubarray([-2, 2, -3, 1], l=2, r=3) == -1, 'wrong result'
+    assert solution.minimumSumSubarray([1, 2, 3, 4], l=2, r=4) == 3, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_minimum_sum_subarray()
