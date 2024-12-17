a28d87b880655c15b16082776901655d093ef3ca
DancingOnAir <taotong1984@hotmail.com>
2024-12-18 00:33:58 +0800

    feat(HashTable): init q2475 & accepted python solution for q2475

---
+++ HashTable/2475_number_of_unequal_triplets_in_array.py
@@ -0,0 +1,25 @@
+from typing import List
+from collections import Counter
+from itertools import combinations
+
+
+class Solution:
+    def unequalTriplets(self, nums: List[int]) -> int:
+        res = 0
+        cnt = Counter(nums)
+        if len(cnt) < 3:
+            return res
+
+        for i, j, k in combinations(cnt, 3):
+            res += cnt[i] * cnt[j] * cnt[k]
+        return res
+
+
+def test_unequal_triplets():
+    solution = Solution()
+    assert solution.unequalTriplets([4, 4, 2, 4, 3]) == 3, 'wrong result'
+    assert solution.unequalTriplets([1, 1, 1, 1, 1]) == 0, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_unequal_triplets()
