5bf5652795927f3723cd4cc24b40166b1636eeab
DancingOnAir <taotong1984@hotmail.com>
2025-01-28 11:06:46 +0800

    feat(HashTable): init q3381 & accepted improved  python solution for q3381

--- HashTable/3381_maximum_subarray_sum_with_length_divisible_by_k.py
+++ HashTable/3381_maximum_subarray_sum_with_length_divisible_by_k.py
@@ -12,7 +12,7 @@ class Solution:
             s += x
             i = j % k
             res = max(res, s - min_s[i])
-            min_s[i] = min(min_s[i], x)
+            min_s[i] = min(min_s[i], s)
         return res
 
     def maxSubarraySum1(self, nums: List[int], k: int) -> int:
@@ -28,7 +28,8 @@ class Solution:
 
 def test_max_subarray_sum():
     solution = Solution()
-    # assert solution.maxSubarraySum([1, 2], 1) == 3, 'wrong result'
+    assert solution.maxSubarraySum([3,-11,8], 1) == 8, 'wrong result'
+    assert solution.maxSubarraySum([1, 2], 1) == 3, 'wrong result'
     assert solution.maxSubarraySum([-1, -2, -3, -4, -5], 4) == -10, 'wrong result'
     assert solution.maxSubarraySum([-5, 1, 2, -3, 4], 2) == 4, 'wrong result'
 
