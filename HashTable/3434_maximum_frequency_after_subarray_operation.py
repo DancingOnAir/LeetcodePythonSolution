9cc29411aaffeb2edf4a44c7fe4bc33a674b06d4
DancingOnAir <taotong1984@hotmail.com>
2025-02-01 03:12:41 +0800

    feat(HashTable): init q3434 & accepted python solution for q3434

--- HashTable/3434_maximum_frequency_after_subarray_operation.py
+++ HashTable/3434_maximum_frequency_after_subarray_operation.py
@@ -1,10 +1,25 @@
 from typing import List
+from collections import defaultdict
 
 
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
-        
-        pass
+        n = len(nums)
+        pre_sum = [0]
+        for x in nums:
+            pre_sum.append(pre_sum[-1] + (x == k))
+
+        pos = defaultdict(list)
+        for i, x in enumerate(nums):
+            pos[k - x].append(i + 1)
+
+        res = 0
+        for p in pos.values():
+            mx = float('-inf')
+            for i, idx in enumerate(p):
+                mx = max(mx, pre_sum[idx - 1] - i + 1)
+                res = max(res, pre_sum[n] - pre_sum[idx] + i + mx)
+        return res
 
 
 def test_max_frequency():
