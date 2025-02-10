17afdedec305ed3aef1e3aa69a231ccbe5899d79
DancingOnAir <taotong1984@hotmail.com>
2025-02-11 07:04:58 +0800

    feat(HashTable): init q3396 & accepted python solution for q3396

---
+++ Array/3396_minimum_number_of_operations_to_make_elements_in_array_distinct.py
@@ -0,0 +1,23 @@
+from typing import List
+from math import ceil
+
+
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        m = set()
+        for i, x in enumerate(nums[::-1]):
+            if x in m:
+                return ceil((len(nums) - i) / 3)
+            m.add(x)
+        return 0
+
+
+def test_minimum_operations():
+    solution = Solution()
+    # assert solution.minimumOperations([1, 2, 3, 4, 2, 3, 3, 5, 7]) == 2, 'wrong result'
+    assert solution.minimumOperations([4, 5, 6, 4, 4]) == 2, 'wrong result'
+    assert solution.minimumOperations([6, 7, 8, 9]) == 0, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_minimum_operations()
