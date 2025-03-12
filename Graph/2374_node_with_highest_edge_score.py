89d17ceecab05a7e3ea8afa11a17602a3ef73856
DancingOnAir <taotong1984@hotmail.com>
2025-03-13 06:44:25 +0800

    feat(HashTable): init q2374 & accepted python solution for q2374

---
+++ Graph/2374_node_with_highest_edge_score.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def edgeScore(self, edges: List[int]) -> int:
+        g = [0] * len(edges)
+        for i, x in enumerate(edges):
+            g[x] += i
+        return sorted((-x, i) for i, x in enumerate(g))[0][1]
+
+
+def test_edge_score():
+    solution = Solution()
+    assert solution.edgeScore([1, 0, 0, 0, 0, 7, 7, 5]) == 7, 'wrong result'
+    assert solution.edgeScore([2, 0, 0, 2]) == 0, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_edge_score()
