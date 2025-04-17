9c93bbd3d10fd47000f3f8594c9ac322ed70f661
DancingOnAir <taotong1984@hotmail.com>
2025-04-18 07:14:30 +0800

    feat(Math): init q2400 and accepted python solution for q2400

---
+++ Math/2400_number_of_ways_to_reach_a_position_after_exactly_k_steps.py
@@ -0,0 +1,27 @@
+from functools import lru_cache
+
+
+class Solution:
+    def numberOfWays(self, startPos: int, endPos: int, k: int) -> int:
+        mod = 10 ** 9 + 7
+
+        # 表示当前在x，还剩left步时，走到终点的方案数
+        @lru_cache(None)
+        def dfs(x, left):
+            if abs(x - endPos) > left:
+                return 0
+            if left == 0:
+                return 1
+            return (dfs(x - 1, left - 1) + dfs(x + 1, left - 1)) % mod
+
+        return dfs(startPos, k)
+
+
+def test_number_of_ways():
+    solution = Solution()
+    assert solution.numberOfWays(1, 2, 3) == 3, 'wrong result'
+    assert solution.numberOfWays(2, 5, 10) == 0, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_number_of_ways()
