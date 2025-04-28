d8c7cde8d238e8e35cdd762d36eaccb3ee66d247
DancingOnAir <taotong1984@hotmail.com>
2025-04-29 07:18:41 +0800

    feat(Math): init q0781 & accepted python solution for q0781

---
+++ Math/0781_rabbits_in_forest.py
@@ -0,0 +1,25 @@
+from typing import List
+from collections import Counter
+from math import ceil
+
+
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        cnt = Counter(answers)
+        res = 0
+        for k, v in cnt.items():
+            q, r = divmod(v, k + 1)
+            res += q * (k + 1)
+            if r > 0:
+                res += k + 1
+        return res
+
+
+def test_num_rabbits():
+    solution = Solution()
+    # assert solution.numRabbits([1, 1, 2]) == 5, 'wrong result'
+    assert solution.numRabbits([10, 10, 10]) == 11, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_num_rabbits()
