93ddadd70048cac6c499e35a8277d53b342ac586
DancingOnAir <taotong1984@hotmail.com>
2026-01-24 18:29:58 +0800

    feat(hash table): init q3805 & accepted python solution for q3805

---
+++ hash_table/3805_count_caesar_cipher_pairs.py
@@ -0,0 +1,28 @@
+from typing import List
+from collections import defaultdict, Counter
+
+
+class Solution:
+    def countPairs(self, words: List[str]) -> int:
+        m = defaultdict(int)
+        for w in words:
+            diff = ""
+            for i in range(len(w) - 1):
+                diff += str(ord(w[i]) - ord(w[i + 1]) - (26 if w[i] > w[i + 1] else 0))
+            m[diff] += 1
+
+        res = 0
+        for v in m.values():
+            if v > 1:
+                res += v * (v - 1) / 2
+        return res
+
+
+def test_count_pairs():
+    solution = Solution()
+    assert solution.countPairs(["fusion", "layout"]) == 1, 'wrong result'
+    assert solution.countPairs(["ab", "aa", "za", "aa"]) == 2, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_count_pairs()
