754b8802a298c156ffe4f1add2e1b49587a9e47c
DancingOnAir <taotong1984@hotmail.com>
2026-01-28 07:07:23 +0800

    feat(hash table): init q3767 & accepted python solution for q3767

---
+++ hash_table/3767_maximize_points_after_choosing_k_tasks.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Solution:
+    def maxPoints(self, technique1: List[int], technique2: List[int], k: int) -> int:
+        n = len(technique1)
+        diffs = []
+        for i in range(n):
+            diffs.append((technique1[i] - technique2[i], technique2[i]))
+        diffs.sort()
+
+        res = 0
+        while k > 0:
+            res += sum(diffs.pop())
+            k -= 1
+
+        while diffs:
+            cur = diffs.pop()
+            if cur[0] < 0:
+                res += cur[1]
+            else:
+                res += cur[0] + cur[1]
+        return res
+
+
+def test_max_points():
+    solution = Solution()
+    assert solution.maxPoints([5, 2, 10], [10, 3, 8], 2) == 22, 'wrong result'
+    assert solution.maxPoints([10, 20, 30], [5, 15, 25], 2) == 60, 'wrong result'
+    assert solution.maxPoints([1, 2, 3], [4, 5, 6], 0) == 15, 'wrong result'
+
+
+if __name__ == '__main__':
+    test_max_points()
