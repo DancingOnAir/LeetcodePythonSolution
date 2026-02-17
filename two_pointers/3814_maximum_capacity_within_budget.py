b6622e920ab0677c8619b2f6efac086ee8cbb253
DancingOnAir <taotong1984@hotmail.com>
2026-02-17 11:26:11 +0800

    feat(two pointers): implmented mono stack & two pointers solution for q3814

--- two_pointers/3814_maximum_capacity_within_budget.py
+++ two_pointers/3814_maximum_capacity_within_budget.py
@@ -3,7 +3,34 @@ from bisect import bisect_left
 
 
 class Solution:
+    # monostack
+    def maxCapacity(self, costs: List[int], capacity: List[int], budget: int) -> int:
+        a = [(cost, cap) for cost, cap in zip(costs, capacity) if cost < budget]
+        a.sort(key=lambda x: x[0])
 
+        stk = [(0, 0)]
+        res = 0
+        for cost, cap in a:
+            while cost + stk[-1][0] >= budget:
+                stk.pop()
+            res = max(res, stk[-1][1] + cap)
+            if cap > stk[-1][1]:
+                stk.append((cost, cap))
+        return res
+
+    # two points
+    def maxCapacity2(self, costs: List[int], capacity: List[int], budget: int) -> int:
+        a = [(cost, cap) for cost, cap in zip(costs, capacity) if cost < budget]
+        a.sort(key=lambda x: x[0])
+
+        pre_max = [0] * (len(a) + 1)
+        l = res = 0
+        for r in range(len(a) - 1, -1, -1):
+            while l < r and a[l][0] + a[r][0] < budget:
+                pre_max[l + 1] = max(pre_max[l], a[l][1])
+                l += 1
+            res = max(res, pre_max[min(l, r)] + a[r][1])
+        return res
 
     # binary search
     def maxCapacity1(self, costs: List[int], capacity: List[int], budget: int) -> int:
