436e1debcdb1c3dfbabfc6984bb85ba0e6349855
Tong Tao <ttong@soundhound.com>
2026-03-17 13:46:18 +0800

    feat(string): init q3838 & accepted python solution for q3838

--- String/3838_weighted_word_mapping.py
+++ String/3838_weighted_word_mapping.py
@@ -11,9 +11,15 @@ class Solution:
 
 def test_map_word_weights():
     solution = Solution()
-    assert solution.mapWordWeights(["abcd","def","xyz"], [5,3,12,14,1,2,3,2,10,6,6,9,7,8,7,10,8,9,6,9,9,8,3,7,7,2]) == "rij", 'wrong result'
-    assert solution.mapWordWeights(["a","b","c"], [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]) == "yyy", 'wrong result'
-    assert solution.mapWordWeights(["abcd"], [7,5,3,4,3,5,4,9,4,2,2,7,10,2,5,10,6,1,2,2,4,1,3,4,4,5]) == "g", 'wrong result'
+    assert solution.mapWordWeights(["abcd", "def", "xyz"],
+                                   [5, 3, 12, 14, 1, 2, 3, 2, 10, 6, 6, 9, 7, 8, 7, 10, 8, 9, 6, 9, 9, 8, 3, 7, 7,
+                                    2]) == "rij", 'wrong result'
+    assert solution.mapWordWeights(["a", "b", "c"],
+                                   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                    1]) == "yyy", 'wrong result'
+    assert solution.mapWordWeights(["abcd"],
+                                   [7, 5, 3, 4, 3, 5, 4, 9, 4, 2, 2, 7, 10, 2, 5, 10, 6, 1, 2, 2, 4, 1, 3, 4, 4,
+                                    5]) == "g", 'wrong result'
 
 
 if __name__ == '__main__':
