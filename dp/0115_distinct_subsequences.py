ecd7668fa7fc7566498a3ccf0a908db1bf3ea08c
Tong Tao <ttong@soundhound.com>
2026-03-20 00:28:10 +0800

    feat(dp): implemented python dp solution for q0115

--- dp/0115_distinct_subsequences.py
+++ dp/0115_distinct_subsequences.py
@@ -2,7 +2,33 @@ from functools import cache
 
 
 class Solution:
+    # dp
+    def numDistinct(self, s: str, t: str) -> int:
+        n, m = len(s), len(t)
+        if n < m:
+            return 0
+        dp = [1] + [0] * m
+        for i, x in enumerate(s):
+            for j in range(min(i, m - 1), max(0, m - n + i) - 1, -1):
+                if x == t[j]:
+                    dp[j + 1] += dp[j]
+        return dp[m]
 
+    # dp
+    def numDistinct2(self, s: str, t: str) -> int:
+        n = len(s)
+        m = len(t)
+        if n < m:
+            return 0
+        # 表示s[0..i]中t[0..j]出现的个数
+        dp = [([1] + [0] * m) for _ in range(n + 1)]
+        for i, x in enumerate(s):
+            # t的长度最小值满足m - j <= n - i，最大值不可能超过i
+            for j in range(max(0, m - n + i), min(i + 1, m)):
+                dp[i + 1][j + 1] = dp[i][j + 1]
+                if x == t[j]:
+                    dp[i + 1][j + 1] += dp[i][j]
+        return dp[n][m]
 
     # dfs
     def numDistinct1(self, s: str, t: str) -> int:
