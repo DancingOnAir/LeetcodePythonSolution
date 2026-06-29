842ac7b21f1058b94b4eb5ce55ffd49439aa09fc
DancingOnAir <taotong1984@hotmail.com>
2026-06-30 07:09:42 +0800

    feat(presum): init q0525 & accepted python solution for q0525

--- array/presum/0525_contiguous_array.py
+++ array/presum/0525_contiguous_array.py
@@ -1,5 +1,16 @@
 class Solution:
     def findMaxLength(self, nums: list[int]) -> int:
+        p = {0: -1}
+        res = tot = 0
+        for i, x in enumerate(nums):
+            tot += -1 if x == 0 else 1
+            if tot in p:
+                res = max(res, i - p[tot])
+            else:
+                p[tot] = i
+        return res
+
+    def findMaxLength1(self, nums: list[int]) -> int:
         res = tot = l = 0
         for r, x in enumerate(nums):
             tot += 1 if x else -1
