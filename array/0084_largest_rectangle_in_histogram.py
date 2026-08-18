74260f9a0a49e5f5334684c5728a35475a897c24
DancingOnAir <taotong1984@hotmail.com>
2026-08-19 06:07:35 +0800

    feat(array): implemented mono stack solution for q0084

--- array/0084_largest_rectangle_in_histogram.py
+++ array/0084_largest_rectangle_in_histogram.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class Solution:
-    def largestRectangleArea(self, heights: List[int]) -> int:
+    def largestRectangleArea1(self, heights: List[int]) -> int:
         if not heights:
             return 0
 
@@ -19,15 +19,31 @@ class Solution:
 
         return res
 
+    def largestRectangleArea(self, heights: list[int]) -> int:
+        if not heights:
+            return 0
+
+        n = len(heights)
+        left = [-1] * n
+        right = [n] * n
+        stk = []
+        for i, h in enumerate(heights):
+            while stk and heights[stk[-1]] > h:
+                right[stk.pop()] = i
+
+            if stk:
+                left[i] = stk[-1]
+            stk.append(i)
+
+        res = 0
+        for h, l, r in zip(heights, left, right):
+            res = max(res, h * (r - l - 1))
+        return res
 
 def test_largest_rectangle_area():
     solution = Solution()
-
-    heights1 = [2, 1, 5, 6, 2, 3]
-    print(solution.largestRectangleArea(heights1))
-
-    heights2 = [1, 1]
-    print(solution.largestRectangleArea(heights2))
+    assert solution.largestRectangleArea([2, 1, 5, 6, 2, 3]) == 10, "wrong result"
+    assert solution.largestRectangleArea([2, 4]) == 4, "wrong result"
 
 
 if __name__ == '__main__':
