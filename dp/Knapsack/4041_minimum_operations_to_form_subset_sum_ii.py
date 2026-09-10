1371b10ac8a87d8324456c424d90cd4d6428516a
DancingOnAir <taotong1984@hotmail.com>
2026-09-11 07:39:23 +0800

    feat(dp): init q4041 & accepted python solution for q4041

--- dp/Knapsack/4041_minimum_operations_to_form_subset_sum_ii.py
+++ dp/Knapsack/4041_minimum_operations_to_form_subset_sum_ii.py
@@ -2,24 +2,25 @@ class Solution:
     def minOperations(self, nums: list[int], sum: int) -> int:
         dp = [0] + [float('inf')] * sum
         for x in nums:
-            l = x.bit_length()
             for i in range(sum, 0, -1):
                 a = 0
-                while x << a <= i:
-                    dp[i] = min(dp[i], dp[i - (x << a)] + a)
+                while x >> a:
+                    b = 0
+                    while x >> a << b <= i:
+                        dp[i] = min(dp[i], dp[i - (x >> a << b)] + a + b)
+                        b += 1
                     a += 1
-                a = l - 1
-                while a > 0 and x >> a <= i:
-                    dp[i] = min(dp[i], dp[i - (x >> a)] + a)
-                    a -= 1
+
+                    if dp[sum] == 0:
+                        return 0
         return -1 if dp[sum] == float('inf') else dp[sum]
 
 
 def test_min_operations():
     solution = Solution()
-    assert solution.minOperations([5, 6, 10], sum=4) == 3, 'wrong result'
     assert solution.minOperations([10, 2], sum=13) == 3, 'wrong result'
-    assert solution.minOperations([6, 3], sum=8) == -1, 'wrong result'
+    assert solution.minOperations([6, 3], sum=8) == 2, 'wrong result'
+    assert solution.minOperations([2, 2], sum=7) == -1, 'wrong result'
 
 
 if __name__ == '__main__':
